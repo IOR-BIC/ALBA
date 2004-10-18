3a0c49acbffab1266ab5011521f04b4e5eaeafda
ape0 <ape0>
2004-10-18 07:45:00 +0000

    defines a macro for synchronously configuring subprojects.

---
+++ modules/MAFConfigMacro.cmake
@@ -0,0 +1,29 @@
+#
+# Macro to synchronously configure a sub project
+# This simply includes a subproject Configure.cmake script
+# that should do all necessary configuration. A couple of useful
+# variables are also defined. 
+# Notice SUBPROJECT_NAME SUBPROJECT_DIR SUBPROJECT_SOURCE_DIR SUBPROJECT_BINARY_DIR are not 
+# rentrant variables
+#
+MACRO(MAF_SUBPROJECT SUBPROJECT_NAME SUBPROJECT_DIR)
+  
+  # if not previous configure errors
+  IF (NOT CONFIGURE_ERROR)  
+    IF (EXISTS ${MAF_SOURCE_DIR}/${SUBPROJECT_DIR}/Configure.cmake)
+      MESSAGE ("Configuring subproject ${SUBPROJECT_NAME}")
+      # Set some useful variables
+      SET(SUBPROJECT_BINARY_DIR ${MAF_BINARY_DIR}/${SUBPROJECT_DIR})
+      SET (${SUBPROJECT_NAME}_BINARY_DIR ${MAF_BINARY_DIR}/${SUBPROJECT_DIR})
+      SET(SUBPROJECT_SOURCE_DIR ${MAF_SOURCE_DIR}/${SUBPROJECT_DIR})
+      SET (${SUBPROJECT_NAME}_SOURCE_DIR ${MAF_SOURCE_DIR}/${SUBPROJECT_DIR})
+        
+      # set flag to advise the subfolder was found
+      SET (${SUBPROJECT_NAME}_CONFIG_FOUND 1)
+      # execute subproject configuration
+      INCLUDE (${SUBPROJECT_SOURCE_DIR}/Configure.cmake)
+    ENDIF (EXISTS ${MAF_SOURCE_DIR}/${SUBPROJECT_DIR}/Configure.cmake)
+  ELSE (NOT CONFIGURE_ERROR)
+    MESSAGE ("Not configuring ${SUBPROJECT_NAME} due to previuos errors")
+  ENDIF (NOT CONFIGURE_ERROR)
+ENDMACRO(MAF_SUBPROJECT)
