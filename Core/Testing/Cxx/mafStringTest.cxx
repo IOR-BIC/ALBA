efaa8d8109953ade1e05c1d5f038be9a4b5f4482
ape0 <ape0>
2004-10-25 18:12:01 +0000

    minor code fixing

--- Core/Testing/Cxx/mafStringTest.cxx
+++ Core/Testing/Cxx/mafStringTest.cxx
@@ -5,8 +5,8 @@ int main()
   mafString hello = "Hello World";
   mafString number(150.2);
   
-  std::cout<<hello<<std::endl;
-  std::cout<<number<<std::endl;
+  std::cout<<(const char *)hello<<std::endl;
+  std::cout<<(const char *)number<<std::endl;
   
   return 0;
 }
