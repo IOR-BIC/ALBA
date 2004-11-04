df9e42e00c777de50848efe41df59ee00309c92e
ape0 <ape0>
2004-11-04 14:53:39 +0000

    updated to use test macro from mafDefines.h

--- Core/Testing/Cxx/mafObjectTest.cpp
+++ Core/Testing/Cxx/mafObjectTest.cpp
@@ -1,19 +1,12 @@
 #include "mafObject.h"
 #include <iostream>
 
-#define ASSERT(a) if (!(a)) \
-{ \
-  std::cerr << "Test failed at line " \
-  << __LINE__ << " : " << #a << std::endl; \
-  return MAF_ERROR; \
-}
-
 class mafFooObject: public mafObject
 {
 public:
   mafTypeMacro(mafFooObject,mafObject);
   mafFooObject() {}
-  void Print(std::ostream out) {out<<"Foo";}
+  void Print(std::ostream &out) {out<<"Foo";}
 };
 
 mafCxxTypeMacro(mafFooObject);
@@ -23,7 +16,7 @@ class mafDummyObject: public mafObject
 public:
   mafTypeMacro(mafDummyObject,mafObject);
   mafDummyObject() {}
-  void Print(std::ostream out) {out<<"Dummy";}
+  void Print(std::ostream &out) {out<<"Dummy";}
 };
 
 mafCxxTypeMacro(mafDummyObject);
@@ -34,45 +27,45 @@ int main()
   mafFooObject foo;
   mafDummyObject dummy;
   
-  foo.Print(std::cout);
+  foo.Print(std::cerr);
   std::cout<<" = "<<foo.GetClassName()<<std::endl;
 
-  dummy.Print(std::cout);
+  dummy.Print(std::cerr);
   std::cout<<" = "<<dummy.GetClassName()<<std::endl;
   
-  ASSERT(foo.IsA("mafFooObject"));
-  ASSERT(foo.IsA(mafObject::GetTypeId()));
-  ASSERT(foo.IsA("mafObject"));
-  ASSERT(foo.IsA(mafObject::GetTypeId()));
-  ASSERT(!foo.IsA(dummy.GetClassId()));
-  ASSERT(!foo.IsA(dummy.GetClassName()));
-  ASSERT(!dummy.IsA(mafFooObject::GetTypeId()));
-  ASSERT(!dummy.IsA(mafFooObject::GetTypeName()));
-  ASSERT(dummy.GetTypeId()==dummy.GetClassId());
-  ASSERT(dummy.GetTypeId()==dummy.GetClassId());
-  ASSERT(mafObject::GetTypeId("mafDummyObject")==dummy.GetClassId());
+  MAF_TEST(foo.IsA("mafFooObject"));
+  MAF_TEST(foo.IsA(mafObject::GetTypeId()));
+  MAF_TEST(foo.IsA("mafObject"));
+  MAF_TEST(foo.IsA(mafObject::GetTypeId()));
+  MAF_TEST(!foo.IsA(dummy.GetClassId()));
+  MAF_TEST(!foo.IsA(dummy.GetClassName()));
+  MAF_TEST(!dummy.IsA(mafFooObject::GetTypeId()));
+  MAF_TEST(!dummy.IsA(mafFooObject::GetTypeName()));
+  MAF_TEST(dummy.GetTypeId()==dummy.GetClassId());
+  MAF_TEST(dummy.GetTypeId()==dummy.GetClassId());
+  MAF_TEST(mafObject::GetTypeId("mafDummyObject")==dummy.GetClassId());
 
   mafObject *new_dummy=dummy.NewInstance();
   mafObject *new_foo=foo.NewInstance();
 
-  ASSERT(new_dummy);
-  ASSERT(new_foo);
-  ASSERT(new_foo->IsA(foo.GetTypeId()));
-  ASSERT(new_foo->IsA(foo.GetTypeName()));
-  ASSERT(new_foo->IsA(mafFooObject::GetTypeId()));
-  ASSERT(new_foo->IsA(mafFooObject::GetTypeName()));
-  ASSERT(!new_foo->IsA(new_dummy->GetClassId()));
-  ASSERT(!new_foo->IsA(new_dummy->GetClassName()));
-  ASSERT(!new_dummy->IsA(new_foo->GetClassId()));
-  ASSERT(!new_dummy->IsA(new_foo->GetClassName()));
-  ASSERT(new_dummy->IsA(new_foo->GetTypeId())); // they are both mafObject * variables
-  ASSERT(new_dummy->IsA(new_foo->GetTypeName()));
+  MAF_TEST(new_dummy);
+  MAF_TEST(new_foo);
+  MAF_TEST(new_foo->IsA(foo.GetTypeId()));
+  MAF_TEST(new_foo->IsA(foo.GetTypeName()));
+  MAF_TEST(new_foo->IsA(mafFooObject::GetTypeId()));
+  MAF_TEST(new_foo->IsA(mafFooObject::GetTypeName()));
+  MAF_TEST(!new_foo->IsA(new_dummy->GetClassId()));
+  MAF_TEST(!new_foo->IsA(new_dummy->GetClassName()));
+  MAF_TEST(!new_dummy->IsA(new_foo->GetClassId()));
+  MAF_TEST(!new_dummy->IsA(new_foo->GetClassName()));
+  MAF_TEST(new_dummy->IsA(new_foo->GetTypeId())); // they are both mafObject * variables
+  MAF_TEST(new_dummy->IsA(new_foo->GetTypeName()));
 
-  ASSERT(mafFooObject::SafeDownCast(new_dummy)==NULL);
+  MAF_TEST(mafFooObject::SafeDownCast(new_dummy)==NULL);
 
   mafDummyObject* tmp_dummy = mafDummyObject::SafeDownCast(new_dummy);
 
-  ASSERT(tmp_dummy!=NULL);
+  MAF_TEST(tmp_dummy!=NULL);
 
   std::cout<<"Test completed successfully!"<<std::endl;
 
