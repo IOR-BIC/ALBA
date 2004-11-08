03fb85b6f2386e9ea87caf095363757efdc4bf90
ape0 <ape0>
2004-11-08 17:06:44 +0000

    removed unused classes

--- Core/Testing/Cxx/mafGObserverTest.cpp
+++ Core/Testing/Cxx/mafGObserverTest.cpp
@@ -7,34 +7,6 @@
 int main()
 {
   
-
-  MAF_TEST(hello.Length()==11);
-  
-  mafString italy = " and Italy";
-  mafString tmp = hello + italy;
-  
-  MAF_TEST(tmp=="Hello World and Italy");
-  MAF_TEST(tmp.EndsWith("Italy"));
-  MAF_TEST(tmp.StartsWith("Hello"));
-  MAF_TEST(tmp.FindFirst("and")==12);
-  
-  mafString copy=tmp;
-
-  MAF_TEST(tmp==copy);
-  
-  tmp.Erase(6,15);
-  MAF_TEST(tmp=="Hello Italy");
-
-  tmp.Erase(5);
-  MAF_TEST(tmp=="Hello");
-
-  tmp<<" everybody";
-  tmp<<italy;
-  tmp<<". Hello!";
-  
-  MAF_TEST(tmp.FindLast("Hello")==(tmp.Length()-mafString::Length("Hello!")));
-  MAF_TEST(tmp[tmp.FindLast("Hello")]=='H');
-  
   std::cout<<"Test completed successfully!"<<std::endl;
   
   return 0;
