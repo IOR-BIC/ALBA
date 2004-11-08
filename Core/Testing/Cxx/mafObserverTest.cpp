d01564e09434ae16024f001195141722ddf733b4
ape0 <ape0>
2004-11-08 17:36:43 +0000

    work in progress

--- Core/Testing/Cxx/mafObserverTest.cpp
+++ Core/Testing/Cxx/mafObserverTest.cpp
@@ -4,6 +4,22 @@
 
 #include <iostream>
 
+using namespace std;
+
+class mafSubjectTestClass:public mafObject
+{
+public:
+  mafTypeMacro(mafSubjectTestClass,mafObject);
+  mafSubjectTestClass():Listeners(this) {}
+  mafEventSource *GetListeners() {return Listeners;} 
+
+  void DoSomeThing() {Listeners->InvokeEvent(this,);
+  void DoSomeThingElse();
+  //void Print(ostream &out) {out<<"Dummy";}
+protected:
+  mafEventSource Listeners;
+};
+
 int main()
 {
   
