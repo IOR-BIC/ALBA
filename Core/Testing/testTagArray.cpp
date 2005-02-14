e3988a2eac1a8a1feeadba54b5a808c420f8dbdc
ape0 <ape0>
2005-02-14 10:23:09 +0000

    test for  the TagArray

---
+++ Core/Testing/testTagArray.cpp
@@ -0,0 +1,118 @@
+#include "mmaTagArray.h"
+#include "mafCoreDecl.h"
+#include "mafString.h"
+
+#include <iostream>
+
+//----------------------------------------------------------------------------
+//                                main
+//---------------------------------------------------------------------------
+
+int main()
+{
+  const char *strings[4]={"qui","quo","qua","paperino"};
+  const double numbers[4]={1.1,1.2,1.3,1.234};
+
+  int i=0;
+  
+  //Test TagItem
+  mmuTagItem titem("TestTAG","String Value");
+  mmuTagItem tnum("TestNUM",1235.67890123456e20);
+  mmuTagItem tmulti("MultiString",strings,4);
+  mmuTagItem tmulti_num("MultiNum",numbers,4);
+  
+  MAF_TEST(mafString("TestTAG")==titem.GetName());
+  MAF_TEST(titem.GetType()==MAF_STRING_TAG);
+  MAF_TEST(mafString(titem.GetValue())=="String Value");
+  
+  MAF_TEST(mafString("TestNUM")==tnum.GetName());
+  MAF_TEST(tnum.GetType()==MAF_NUMERIC_TAG);
+  MAF_TEST(tnum.GetValueAsDouble()==1235.67890123456e20);
+  
+  MAF_TEST(mafString("MultiString")==tmulti.GetName());
+  MAF_TEST(tmulti.GetType()==MAF_STRING_TAG);
+  for (i=0;i<4;i++)
+  {
+    MAF_TEST(mafString(tmulti.GetValue(i))==strings[i]);
+  }
+
+  MAF_TEST(mafString("MultiNum")==tmulti_num.GetName());
+  MAF_TEST(tmulti_num.GetType()==MAF_NUMERIC_TAG);
+  for (i=0;i<4;i++)
+  {
+    MAF_TEST(tmulti_num.GetValueAsDouble(i)==numbers[i]);
+  }
+
+  mafString value;
+  tmulti.GetValueAsSingleString(value);
+  mafString result="(\"qui\",\"quo\",\"qua\",\"paperino\")";
+  MAF_TEST( value==result );
+
+  tmulti_num.SetNumberOfComponents(6);
+  tmulti_num.SetComponent(3.3,2);
+  tmulti_num.SetComponent(3.5,4);
+  tmulti_num.SetComponent(3.456,5);
+
+  MAF_TEST(tmulti_num.GetNumberOfComponents()==6);
+  MAF_TEST(tmulti_num.GetComponentAsDouble(2)==3.3);
+  MAF_TEST(tmulti_num.GetComponentAsDouble(4)==3.5);
+  MAF_TEST(tmulti_num.GetComponentAsDouble(5)==3.456);
+
+  // Test1
+  mmuTagItem ti1, ti2, ti3, ti4;
+  mmaTagArray test_tag_array;
+  
+	ti1.SetName("qui");
+	ti2.SetName("quo");
+	ti3.SetName("archimede pitagorico");
+	ti4.SetName("pico de paperis");
+
+  ti1.SetValue("item1");
+	ti2.SetValue("item2");
+	ti3.SetValue(3);
+	ti4.SetValue(4);
+
+	ti1.SetType(MAF_STRING_TAG);
+	ti2.SetType(MAF_STRING_TAG);
+	ti3.SetType(MAF_NUMERIC_TAG);
+	ti4.SetType(MAF_NUMERIC_TAG);
+
+	test_tag_array.SetTag(ti1);
+	test_tag_array.SetTag(ti2);
+	test_tag_array.SetTag(ti3);
+	test_tag_array.SetTag(ti4);
+
+  std::vector<mmuTagItem *>::iterator v_iterator;
+  std::vector<mmuTagItem *> pti_vector;
+  test_tag_array.GetTagsByType(MAF_STRING_TAG, pti_vector);
+
+  std::cerr << "\n";
+  std::cerr << "\n";
+  std::cerr << "Tags of type MAF_STRING_TAG are:" << "\n";
+  std::cerr << "\n";
+
+  for (v_iterator = pti_vector.begin(); v_iterator != pti_vector.end(); v_iterator++)
+  {
+    std::cerr << (*v_iterator)->GetName() << "\n";
+    MAF_TEST((*v_iterator)->GetType()==MAF_STRING_TAG);
+  }		
+
+  pti_vector.clear();
+
+  test_tag_array.GetTagsByType(MAF_NUMERIC_TAG, pti_vector);
+
+  std::cerr << "\n";
+  std::cerr << "\n";
+  std::cerr << "Tags of type MAF_NUMERIC_TAG are:" << "\n";
+  std::cerr << "\n";
+
+  for (v_iterator = pti_vector.begin(); v_iterator != pti_vector.end(); v_iterator++)
+  {
+	  std::cerr << (*v_iterator)->GetName() << "\n";
+    MAF_TEST((*v_iterator)->GetType()==MAF_NUMERIC_TAG);
+  }
+
+  std::cout<<"Test completed successfully!"<<std::endl;
+  
+  return 0;
+}
