52baab703a6857d1bc231b27223f7545ace1a189
ape0 <ape0>
2005-02-20 23:12:37 +0000

    finalized (but still in beta)

--- Core/mafAttribute.cpp
+++ Core/mafAttribute.cpp
@@ -2,15 +2,17 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mafAttribute.cpp,v $
   Language:  C++
-  Date:      $Date: 2005-02-17 00:42:45 $
-  Version:   $Revision: 1.2 $
+  Date:      $Date: 2005-02-20 23:12:37 $
+  Version:   $Revision: 1.3 $
   Authors:   Marco Petrone
 ==========================================================================
-  Copyright (c) 2002/2004 
+  Copyright (c) 2001/2005 
   CINECA - Interuniversity Consortium (www.cineca.it)
 =========================================================================*/
 #include "mafAttribute.h"
 #include "assert.h"
+
+mafCxxAbstractTypeMacro(mafAttribute);
 
 //-------------------------------------------------------------------------
 void mafAttribute::operator=(const mafAttribute &a)
@@ -30,12 +32,37 @@ bool mafAttribute::operator==(const mafAttribute &a) const
 void mafAttribute::DeepCopy(const mafAttribute *a)
 //-------------------------------------------------------------------------
 {
+  assert(a);
   assert(a->IsA(GetTypeId()));
+  m_Name=a->GetName();
+}
+
+//-------------------------------------------------------------------------
+mafAttribute *mafAttribute::MakeCopy()
+//-------------------------------------------------------------------------
+{
+  mafAttribute *new_attr=NewInstance();
+  assert(new_attr);
+  new_attr->DeepCopy(this);
+  return new_attr;
 }
 
 //-------------------------------------------------------------------------
 bool mafAttribute::Equals(const mafAttribute *a) const
 //-------------------------------------------------------------------------
 {
-  return a->IsA(GetTypeId());
+  return a->IsA(GetTypeId()) && m_Name==a->GetName();
+}
+
+//-------------------------------------------------------------------------
+void mafAttribute::SetName(const char *name)
+//-------------------------------------------------------------------------
+{
+  m_Name=name;
+}
+//-------------------------------------------------------------------------
+const char *mafAttribute::GetName() const
+//-------------------------------------------------------------------------
+{
+  return m_Name;
 }
--- Core/mafAttribute.h
+++ Core/mafAttribute.h
@@ -2,11 +2,11 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mafAttribute.h,v $
   Language:  C++
-  Date:      $Date: 2005-02-17 00:42:46 $
-  Version:   $Revision: 1.2 $
+  Date:      $Date: 2005-02-20 23:12:37 $
+  Version:   $Revision: 1.3 $
   Authors:   Marco Petrone
 ==========================================================================
-  Copyright (c) 2002/2004 
+  Copyright (c) 2001/2005 
   CINECA - Interuniversity Consortium (www.cineca.it)
 =========================================================================*/
 #ifndef __mafAttribute_h
@@ -14,7 +14,7 @@
 
 #include "mafReferenceCounted.h" 
 #include "mafStorable.h"
-
+#include "mafString.h"
 
 /** An abstract class for objects representing an attribute for mafNodes.
   This abstract class represent the interface of an attribute for mafNodes. An attribute
@@ -27,16 +27,34 @@
 class MAF_EXPORT mafAttribute : public mafReferenceCounted, public mafStorable
 {
 public:
+  mafAbstractTypeMacro(mafAttribute,mafReferenceCounted);
+
   /** attributes must define a copy rule */
   void operator=(const mafAttribute &a);
 
   bool operator==(const mafAttribute &a) const;
 
   /** copy the content of the given attribute. Attributes must be type compatible */
-  void DeepCopy(const mafAttribute *a);
+  virtual void DeepCopy(const mafAttribute *a);
+
+  /** create a copy of this attribute */
+  mafAttribute *MakeCopy();
 
   /** return true if this attribute equals the given one */
   bool Equals(const mafAttribute *a) const;
+
+  /** 
+    Set the name of this attribute. this is typically used to identify it
+    inside the Attributes associative map stored inside a mafNode */
+  void SetName(const char *name);
+  const char *GetName() const;
+
+  /** defined to allow the definition of MakeCopy(). For mafAttribute abstract base class return NULL */
+  mafObject *NewObjectInstance() const {return NULL;}
+  /** defined to allow the definition of MakeCopy(). For mafAttribute abstract base class return NULL */
+  mafAttribute *NewInstance() const {return mafAttribute::SafeDownCast(NewObjectInstance());}
+protected:
+  mafString m_Name;
 };
 
 #endif 
