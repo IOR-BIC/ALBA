070a4872a4b4a96822e9fe402fcf8bbc181535ca
ape0 <ape0>
2005-03-02 00:33:04 +0000

    utility class

---
+++ Core/mmuTimeSet.cpp
@@ -0,0 +1,236 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: mmuTimeSet.cpp,v $
+  Language:  C++
+  Date:      $Date: 2005-03-02 00:33:04 $
+  Version:   $Revision: 1.1 $
+  Authors:   Marco Petrone
+==========================================================================
+  Copyright (c) 2001/2005 
+  CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#include "mmuTimeSet.h"
+#include <math.h>
+//------------------------------------------------------------------------------
+//mafCxxTypeMacro(mmuTimeSet)
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+mmuTimeSet::mmuTimeSet()
+//------------------------------------------------------------------------------
+{
+}
+
+//------------------------------------------------------------------------------
+mmuTimeSet::~mmuTimeSet()
+//------------------------------------------------------------------------------
+{
+}
+
+//------------------------------------------------------------------------------
+mmuTimeSet::mmuTimeSet(const mmuTimeSet& c)
+//------------------------------------------------------------------------------
+{ 
+  m_TSet=c.m_TSet;
+}
+
+//------------------------------------------------------------------------------
+mmuTimeSet &mmuTimeSet::operator=(const mmuTimeSet &o)
+//------------------------------------------------------------------------------
+{
+  m_TSet=o.m_TSet;
+  return *this;
+}
+
+//------------------------------------------------------------------------------
+bool mmuTimeSet::operator==(const mmuTimeSet &o)
+//------------------------------------------------------------------------------
+{
+  if (GetNumberOfTimeStamps()!=o.GetNumberOfTimeStamps())
+    return false;
+
+  Iterator it;
+  TSet::const_iterator it2;
+  for (it=m_TSet.begin(),it2=o.m_TSet.begin();it!=m_TSet.end();it++,it2++)
+  {
+    if (*it!=*it2)
+      return false;
+  }
+
+  return true;
+}
+//------------------------------------------------------------------------------
+void mmuTimeSet::Insert(mafTimeStamp t)
+//------------------------------------------------------------------------------
+{
+  m_TSet.insert(t);
+}
+//------------------------------------------------------------------------------
+void mmuTimeSet::Append(mafTimeStamp t)
+//------------------------------------------------------------------------------
+{
+  m_TSet.insert(m_TSet.end(),t);
+}
+//------------------------------------------------------------------------------
+void mmuTimeSet::Prepend(mafTimeStamp t)
+//------------------------------------------------------------------------------
+{
+  m_TSet.insert(m_TSet.begin(),t);
+}
+//------------------------------------------------------------------------------
+void mmuTimeSet::Remove(Iterator ts)
+//------------------------------------------------------------------------------
+{
+  m_TSet.erase(ts);
+}
+
+//------------------------------------------------------------------------------
+mafID mmuTimeSet::FindTimeStampIndex(mafTimeStamp t)
+//------------------------------------------------------------------------------
+{
+  Iterator it=FindTimeStamp(t);
+  return it!=m_TSet.end()?std::distance(m_TSet.begin(),it):-1;
+}
+//------------------------------------------------------------------------------
+mafTimeStamp mmuTimeSet::GetByIndex(mafID idx)
+//------------------------------------------------------------------------------
+{
+  Iterator it=m_TSet.begin();
+  std::advance(it,idx);
+  return (it!=m_TSet.end()?*it:-1);
+}
+//------------------------------------------------------------------------------
+mafTimeStamp mmuTimeSet::GetNearestTimeStamp(mafTimeStamp t)
+//------------------------------------------------------------------------------
+{
+  Iterator it=FindNearestTimeStamp(t);
+  return it!=m_TSet.end()?*it:-1;
+}
+//------------------------------------------------------------------------------
+mafTimeStamp mmuTimeSet::GetTimeStampBefore(mafTimeStamp t)
+//------------------------------------------------------------------------------
+{
+  Iterator it=FindTimeStampBefore(t);
+  return it!=m_TSet.end()?*it:-1;
+}
+//------------------------------------------------------------------------------
+mmuTimeSet::Iterator mmuTimeSet::FindTimeStamp(mafTimeStamp t)
+//------------------------------------------------------------------------------
+{
+  return m_TSet.find(t);
+}
+//------------------------------------------------------------------------------
+mmuTimeSet::Iterator mmuTimeSet::FindNearestTimeStamp(mafTimeStamp t)
+//------------------------------------------------------------------------------
+{
+  std::pair<Iterator,Iterator> range=m_TSet.equal_range(t);
+  if (range.first!=m_TSet.end())
+  {
+    if (range.second!=m_TSet.end())
+    {
+      return fabs(*(range.first)-t)>fabs(*(range.second)-t)?range.second:range.first;
+    }
+    else
+    {
+      return range.first;
+    }
+  }
+  else if (range.second!=m_TSet.end())
+  {
+    return range.second;
+  }
+
+  return --range.second;
+}
+//------------------------------------------------------------------------------
+mmuTimeSet::Iterator mmuTimeSet::FindTimeStampBefore(mafTimeStamp t)
+//------------------------------------------------------------------------------
+{
+  Iterator it=m_TSet.lower_bound(t); // find item < t
+ 
+  if (it==m_TSet.end()||it!=m_TSet.begin()&&*it>t)
+    --it;
+  return it;
+}
+
+//-------------------------------------------------------------------------
+void mmuTimeSet::Merge(const mmuTimeSet &v1,const mmuTimeSet &v2,mmuTimeSet &outv)
+//-------------------------------------------------------------------------
+{
+  if (&outv!=&v1 && &outv!=&v2)
+  {
+    outv.Clear();
+  }
+
+  if (&outv!=&v1)   
+  {
+    if (&outv!=&v2)
+    {
+      outv=v1;
+
+      for (TSet::const_iterator it=v2.GetConstTSet().begin();it!=v2.GetConstTSet().end();it++)
+      {
+        outv.Insert(*it);
+      }
+    }
+    else
+    {
+      for (TSet::const_iterator it=v1.GetConstTSet().begin();it!=v1.GetConstTSet().end();it++)
+      {
+        outv.Insert(*it);
+      }
+    }
+  }
+  else
+  {
+    for (TSet::const_iterator it=v2.GetConstTSet().begin();it!=v2.GetConstTSet().end();it++)
+    {
+      outv.Insert(*it);
+    }
+  }
+}
+
+//-------------------------------------------------------------------------
+void mmuTimeSet::Merge(const mmuTimeSet &v)
+//-------------------------------------------------------------------------
+{
+  Merge(*this,v,*this);
+}
+
+//-------------------------------------------------------------------------
+int mmuTimeSet::GetNumberOfTimeStamps() const
+//-------------------------------------------------------------------------
+{
+  return m_TSet.size();
+}
+
+//-------------------------------------------------------------------------
+void mmuTimeSet::Merge(const mmuTimeVector &v1,const mmuTimeVector &v2,mmuTimeVector &outv)
+//-------------------------------------------------------------------------
+{
+  std::set<mafTimeStamp> outset;
+  for (int idx=0;idx<v1.size();idx++)
+  {
+    outset.insert(v1[idx]);
+  }
+
+  for (int idx2=0;idx2<v2.size();idx2++)
+  {
+    outset.insert(v2[idx2]);
+  }
+
+  outv.clear();
+  for (std::set<mafTimeStamp>::iterator it=outset.begin();it!=outset.end();it++)
+  {
+    outv.push_back(*it);
+  }
+}
+
+//-------------------------------------------------------------------------
+void mmuTimeSet::Merge(const mmuTimeVector &v)
+//-------------------------------------------------------------------------
+{
+  for (int i=0;i<v.size();i++)
+    Insert(v[i]);
+}
---
+++ Core/mmuTimeSet.h
@@ -0,0 +1,132 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: mmuTimeSet.h,v $
+  Language:  C++
+  Date:      $Date: 2005-03-02 00:33:04 $
+  Version:   $Revision: 1.1 $
+  Authors:   Marco Petrone
+==========================================================================
+  Copyright (c) 2001/2005 
+  CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#ifndef __mmuTimeSet_h
+#define __mmuTimeSet_h
+
+#include "mmuUtility.h"
+#include "mafDefines.h"
+#include <set>
+#include <vector>
+
+//------------------------------------------------------------------------------
+// Forward declarations
+//------------------------------------------------------------------------------
+typedef std::vector<mafTimeStamp> mmuTimeVector;
+
+//------------------------------------------------------------------------------
+// mmuTimeSet
+//------------------------------------------------------------------------------
+/** class wrapping an STL "set" of mafTimeStamps providing API to query and merge.
+  mmuTimeSet 
+  @todo
+  - test Merge of variuos case of time vectors
+*/
+class MAF_EXPORT mmuTimeSet: public mmuUtility
+{
+public:
+  mmuTimeSet();
+  virtual ~mmuTimeSet();
+
+//  mafTypeMacro(mmuTimeSet,mmuUtility);
+ 
+  /** copy constructor, this makes a copy of the set */
+  mmuTimeSet(const mmuTimeSet& c);
+ 
+  /** assign operator, to copy set value from another set */
+  mmuTimeSet &operator=(const mmuTimeSet &o);
+
+  bool operator==(const mmuTimeSet &o);
+
+  typedef std::set<mafTimeStamp> TSet;
+  typedef std::set<mafTimeStamp>::iterator Iterator;
+ 
+  /** insert a timestamp in the set */
+  void Insert(mafTimeStamp t);
+
+  /** 
+    insert a timestamp forcing insertion point search to start
+    from the end of the set. In case times are inserted already 
+    ordered this ensures a constant time for insertion */
+  void Append(mafTimeStamp t);
+
+  /** 
+    insert a timestamp forcing insertion point search to start
+    from the start of the set. In case times are inserted already 
+    ordered this ensures a constant time for insertion */
+  void Prepend(mafTimeStamp t);
+
+  /** remove timestamp from the set*/
+  void Remove(Iterator ts);
+
+  /** remove all items from the internal time set */
+  void Clear() {m_TSet.clear();}
+
+  /** return reference to inner TSet object */
+  TSet &GetTSet() {return m_TSet;}
+
+  /** use this in case you have a to access the TSet of a const mmuTimeSet */ 
+  const TSet &GetConstTSet() const{return m_TSet;}
+
+  /**  Return the idx-th item of the set. return -1 if not found. */
+  mafTimeStamp GetByIndex(mafID idx);
+  
+  /** Return the timestamp with value clsest to t */
+  mafTimeStamp GetNearestTimeStamp(mafTimeStamp t);
+
+  /** Return timestamp with value <= t */
+  mafTimeStamp GetTimeStampBefore(mafTimeStamp t);
+
+  /** return index of the given timestamp. return -1 if not found. */
+  mafID FindTimeStampIndex(mafTimeStamp t);
+
+  /** 
+    Return the iterator pointing to the item with value "t". Return
+    TSet::end() if not found.*/
+  Iterator FindTimeStamp(mafTimeStamp t);
+  
+  /** 
+    Return the iterator pointing to the item with value "t". Return
+    TSet::end() if none found.*/
+  Iterator FindNearestTimeStamp(mafTimeStamp t);
+
+  /** 
+    Return the iterator pointing to the item with lower or equal to "t". Return
+    TSet::end() if none found.*/
+  Iterator FindTimeStampBefore(mafTimeStamp t);
+
+  /** Merge two different time-stamps sets*/
+  static void Merge(const mmuTimeSet &v1,const mmuTimeSet &v2,mmuTimeSet &outv);
+
+  /** Merge a time-stamps set into this set*/
+  void Merge(const mmuTimeSet &v);
+
+  /** Merge two different time-stamps vectors*/
+  void Merge(const mmuTimeVector &v1,const mmuTimeVector &v2,mmuTimeVector &outv);
+
+  /** Merge a time-stamps vector into this set*/
+  void Merge(const mmuTimeVector &v);
+
+  /** return the number of time stamps */
+  int GetNumberOfTimeStamps() const;
+
+  /** return the first time stamp */
+  Iterator Begin() {return m_TSet.begin();}
+  /** return the last+1 time stamp */
+  Iterator End() {return m_TSet.end();}
+  
+
+protected:
+  TSet m_TSet;
+};
+
+#endif /* __mmuTimeSet_h */
