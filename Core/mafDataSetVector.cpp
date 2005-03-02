80c418c1f9b49d2ad3aa0ad2271a053eb82771d1
ape0 <ape0>
2005-03-02 00:32:21 +0000

    associative sorted vector of timestamped datasets

---
+++ Core/mafDataSetVector.cpp
@@ -0,0 +1,37 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: mafDataSetVector.cpp,v $
+  Language:  C++
+  Date:      $Date: 2005-03-02 00:32:22 $
+  Version:   $Revision: 1.1 $
+  Authors:   Marco Petrone
+==========================================================================
+  Copyright (c) 2001/2005 
+  CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#ifndef __mafDataSetVector_cpp
+#define __mafDataSetVector_cpp
+
+#include "mafDataSetVector.h"
+#include "mmuTimeSet.h"
+#include "mafIndent.h"
+
+//-----------------------------------------------------------------------
+mafCxxTypeMacro(mafDataSetVector)
+//-----------------------------------------------------------------------
+
+//-----------------------------------------------------------------------
+mafDataSetVector::mafDataSetVector()
+//-----------------------------------------------------------------------
+{
+}
+
+//-----------------------------------------------------------------------
+mafDataSetVector::~mafDataSetVector()
+//-----------------------------------------------------------------------
+{
+}
+
+
+#endif
