17ef803a16e4143fc83b9c562348450f20774d12
amb0 <amb0>
2005-03-23 18:01:52 +0000

    minor: now it include wx/wx.h or wx/wxprec depending on WX_PRECOMP flag

--- Base/mafIncludeWX.h
+++ Base/mafIncludeWX.h
@@ -2,24 +2,26 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mafIncludeWX.h,v $
   Language:  C++
-  Date:      $Date: 2005-02-20 23:33:15 $
-  Version:   $Revision: 1.3 $
-  Authors:   Based on itkWindows.h (www.itk.org)
+  Date:      $Date: 2005-03-23 18:01:52 $
+  Version:   $Revision: 1.4 $
+  Authors:   Silvano Imboden
 ==========================================================================
   Copyright (c) 2001/2005 
   CINECA - Interuniversity Consortium (www.cineca.it)
 =========================================================================*/
 /** This file is used to include wx.h */
 
-#include "mafConfigure.h"
+#include "mafConfigure.h"   
 
 #ifndef __mafIncludeWX_h
 #define __mafIncludeWX_h
 
 #ifdef MAF_USE_WX
-//  #ifndef WX_PRECOMP
-    #include "wx/wx.h"
-//  #endif
+  #ifdef WX_PRECOMP
+      #include "wx/wxprec.h"  //originally was the first line of any lal cpp file 
+  #else
+      #include "wx/wx.h"      //originally was the first line of any lal header file 
+  #endif
 #endif
 
 #endif
