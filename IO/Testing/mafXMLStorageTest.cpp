5a5cf922d68b2bd708f64548f52085d81e9695e8
ape0 <ape0>
2005-04-01 10:18:25 +0000

    API improvments

--- IO/Testing/mafXMLStorageTest.cpp
+++ IO/Testing/mafXMLStorageTest.cpp
@@ -14,8 +14,8 @@ public:
   mafTypeMacro(mafDummyObject,mafObject);
   void Print(std::ostream &os,const int tabs=0) {Superclass::Print(os,tabs);os<<mafIndent(tabs)<<"Name: \""<<m_Name.GetCStr()<<"\""<<std::endl;}
   mafDummyObject() {}
-  virtual int InternalStore(mafStorageElement *element){return element->StoreText(m_Name,"Name");}
-  virtual int InternalRestore(mafStorageElement *element){return element->RestoreText(m_Name,"Name");}
+  virtual int InternalStore(mafStorageElement *element){return element->StoreText("Name",m_Name);}
+  virtual int InternalRestore(mafStorageElement *element){return element->RestoreText("Name",m_Name);}
   mafString m_Name;
 };
 
@@ -73,15 +73,15 @@ mafStorableTestObject::mafStorableTestObject()
 int mafStorableTestObject::InternalStore(mafStorageElement *element)
 //------------------------------------------------------------------------------
 {
-  element->StoreDouble(m_FValue,"FValue");
-  element->StoreInteger(m_IValue,"IValue");
-  element->StoreVectorN(m_FVector,4,"FVector");
-  element->StoreVectorN(m_IVector,4,"IVector");
-  element->StoreVectorN(m_FSTDVector,4,"FSTDVector");
-  element->StoreVectorN(m_ISTDVector,4,"ISTDVector");
-  element->StoreVectorN(m_StrVector,4,"StrVector","Items");
-  element->StoreText(m_Text,"Text");
-  element->StoreObject(m_Dummy,"Dummy");
+  element->StoreDouble("FValue",m_FValue);
+  element->StoreInteger("IValue",m_IValue);
+  element->StoreVectorN("FVector",m_FVector,4);
+  element->StoreVectorN("IVector",m_IVector,4);
+  element->StoreVectorN("FSTDVector",m_FSTDVector,4);
+  element->StoreVectorN("ISTDVector",m_ISTDVector,4);
+  element->StoreVectorN("StrVector",m_StrVector,4,"Items");
+  element->StoreText("Text",m_Text);
+  element->StoreObject("Dummy",m_Dummy);
   return MAF_OK;
 }
 
@@ -91,14 +91,14 @@ int mafStorableTestObject::InternalRestore(mafStorageElement *element)
 //------------------------------------------------------------------------------
 {
   if (
-    element->RestoreDouble(m_FValue,"FValue")||
-    element->RestoreInteger(m_IValue,"IValue")||
-    element->RestoreVectorN(m_FVector,4,"FVector")||
-    element->RestoreVectorN(m_IVector,4,"IVector")||
-    element->RestoreVectorN(m_FSTDVector,4,"FSTDVector")||
-    element->RestoreVectorN(m_ISTDVector,4,"ISTDVector")||
-    element->RestoreVectorN(m_StrVector,4,"StrVector","Items")||
-    element->RestoreText(m_Text,"Text")||
+    element->RestoreDouble("FValue",m_FValue)||
+    element->RestoreInteger("IValue",m_IValue)||
+    element->RestoreVectorN("FVector",m_FVector,4)||
+    element->RestoreVectorN("IVector",m_IVector,4)||
+    element->RestoreVectorN("FSTDVector",m_FSTDVector,4)||
+    element->RestoreVectorN("ISTDVector",m_ISTDVector,4)||
+    element->RestoreVectorN("StrVector",m_StrVector,4,"Items")||
+    element->RestoreText("Text",m_Text)||
     (m_Dummy=dynamic_cast<mafDummyObject *>(element->RestoreObject("Dummy")))==NULL)
     return MAF_ERROR;
 
@@ -106,7 +106,7 @@ int mafStorableTestObject::InternalRestore(mafStorageElement *element)
 }
 
 //------------------------------------------------------------------------------
-// object dubuf printing to output stream
+// object debug printing to output stream
 void mafStorableTestObject::Print(std::ostream &os,const int tabs)
 //------------------------------------------------------------------------------
 {
@@ -127,7 +127,10 @@ void mafStorableTestObject::Print(std::ostream &os,const int tabs)
 int main()
 //------------------------------------------------------------------------------
 {
+  // instantiate an object factory
   mafCoreFactory::Initialize();
+  // plug the nested object type in the factory (necessary
+  // to be stored/restored with StoreObject()/RestoreObject() )
   mafPlugObject<mafDummyObject>("Dummy Object");
 
   mafStorableTestObject foo;
@@ -135,48 +138,58 @@ int main()
   mafDummyObject dummy;
   dummy.m_Name = "myDummyObject";
   
+  // set the float and integer values
   foo.m_FValue=1.5;
   foo.m_IValue=15;
 
+  // set the float array
   foo.m_FVector[0]=1.0;
   foo.m_FVector[1]=1.1;
   foo.m_FVector[2]=1.2;
   foo.m_FVector[3]=1.3;
   
+  // set the integer array
   foo.m_IVector[0]=10;
   foo.m_IVector[1]=11;
   foo.m_IVector[2]=12;
   foo.m_IVector[3]=13;
 
+  // test the STL float vector
   foo.m_FSTDVector[0]=2.0;
   foo.m_FSTDVector[1]=2.1;
   foo.m_FSTDVector[2]=2.2;
   foo.m_FSTDVector[3]=2.3;
   
+  // test the STL integer vector
   foo.m_ISTDVector[0]=20;
   foo.m_ISTDVector[1]=21;
   foo.m_ISTDVector[2]=22;
   foo.m_ISTDVector[3]=23;
 
+  // test the string vector
   foo.m_StrVector[0]="qui";
   foo.m_StrVector[1]="quo";
   foo.m_StrVector[2]="qua";
   foo.m_StrVector[3]="paperino";
 
+  // test the storage of special characters in XML UTF-8
 #ifdef WIN32
   foo.m_Text="Saved String àèéìòù§°ç";
 #else
   foo.m_Text="Saved String Ã Ã¨Ã©Ã¬Ã²Ã¹";
 #endif
 
+  // set the object pointer
   foo.m_Dummy=&dummy;
 
+  // debug print
   foo.Print(std::cerr);
   
+  // create an XML storage for saving
   mafXMLStorage storage;
-  storage.SetURL("testfile.xml");
-  storage.SetFileType("TestXML");
-  storage.SetVersion("1.745");
+  storage.SetURL("testfile.xml"); // where to store
+  storage.SetFileType("TestXML"); // identification type
+  storage.SetVersion("1.745");    // file version
 
   mafStorable *storable=mafStorable::SafeCastToObject(&foo);
   MAF_TEST(storable!=NULL);
@@ -186,23 +199,27 @@ int main()
   int ret=storage.Store();
   MAF_TEST(ret==MAF_OK);
 
+  // XML storage to restore
   mafXMLStorage restore;
   restore.SetURL("testfile.xml");
   restore.SetFileType("TestXML");
   restore.SetVersion("1.745");
 
+  // create a new object to restore into
   mafStorableTestObject new_foo;
   restore.SetRoot(&new_foo);
   ret=restore.Restore();
 
   MAF_TEST(ret==MAF_OK);
 
+  // cast from mafStorable to mafObject
   mafObject *root=restore.GetRoot()->CastToObject();
-  MAF_TEST(root==&new_foo);
+  MAF_TEST(root==&new_foo); // check casting succeded
 
   std::cerr<<"*** Restored object ***\n";  
   new_foo.Print(std::cerr);
 
+  // check restored object contents
   MAF_TEST(new_foo.m_FValue==1.5);
   MAF_TEST(new_foo.m_IValue==15);
   
@@ -237,8 +254,9 @@ int main()
   MAF_TEST(new_foo.m_Text=="Saved String Ã Ã¨Ã©Ã¬Ã²Ã¹");
 #endif
 
+  // check the restored nested object has been correctly restored
   MAF_TEST(new_foo.m_Dummy!=NULL);
-  MAF_TEST(new_foo.m_Dummy->IsType(mafDummyObject))
+  MAF_TEST(new_foo.m_Dummy->IsA(mafDummyObject::GetStaticTypeId()))
   MAF_TEST(new_foo.m_Dummy->m_Name == "myDummyObject");
   
   std::cout<<"Test completed successfully!"<<std::endl;
