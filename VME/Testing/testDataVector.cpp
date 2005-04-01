99b3d8fb559281e49573525d6a6575afef844246
ape0 <ape0>
2005-04-01 10:17:05 +0000

    tests for VME classes

---
+++ VME/Testing/testDataVector.cpp
@@ -0,0 +1,10 @@
+#include "mafDataVector.h"
+#include <iostream>
+
+
+int main()
+{
+  std::cerr<<"Test completed successfully!"<<std::endl;
+
+  return MAF_OK;
+}
---
+++ VME/Testing/testMatrixVector.cpp
@@ -0,0 +1,11 @@
+#include "mafMatrixVector.h"
+#include "mafMatrix.h"
+#include <iostream>
+
+int main()
+{
+
+  std::cerr<<"Test completed successfully!"<<std::endl;
+
+  return MAF_OK;
+}
