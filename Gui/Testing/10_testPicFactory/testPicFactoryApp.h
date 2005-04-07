886d81df83e128d0c68c3fc292c7d61bbbc80063
amb0 <amb0>
2005-04-07 10:05:18 +0000

    new test

---
+++ Gui/Testing/10_testPicFactory/testPicFactoryApp.h
@@ -0,0 +1,40 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: testPicFactoryApp.h,v $
+  Language:  C++
+  Date:      $Date: 2005-04-07 10:05:19 $
+  Version:   $Revision: 1.1 $
+  Authors:   Silvano Imboden
+==========================================================================
+  Copyright (c) 2001/2005 
+  CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+#ifndef __testPicFactoryApp_H__
+#define __testPicFactoryApp_H__
+
+//----------------------------------------------------------------------------
+// Include:
+//----------------------------------------------------------------------------
+#include "mafDefines.h" //important: mafDefines should always be included as first
+#include "testPicFactoryLogic.h" 
+
+// ============================================================================
+class testPicFactoryApp : public wxApp
+// ============================================================================
+{
+public:
+    bool OnInit();
+    int  OnExit();
+protected:
+    testPicFactoryLogic *m_logic;
+
+};
+
+DECLARE_APP(testPicFactoryApp)
+
+#endif //__testPicFactoryApp_H__
+
+
+
+
+
