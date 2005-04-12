ffee07d706bef60d7a98fb09529f78863c3ccb56
ape0 <ape0>
2005-04-12 20:03:18 +0000

    fixed bugs

--- Gui/Testing/12_Bmp2Xpm/Bmp2Xpm.cpp
+++ Gui/Testing/12_Bmp2Xpm/Bmp2Xpm.cpp
@@ -2,13 +2,13 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: Bmp2Xpm.cpp,v $
   Language:  C++
-  Date:      $Date: 2005-04-11 11:25:35 $
-  Version:   $Revision: 1.3 $
+  Date:      $Date: 2005-04-12 20:03:18 $
+  Version:   $Revision: 1.4 $
   Authors:   Silvano Imboden
 ==========================================================================
   Copyright (c) 2001/2005 
   CINECA - Interuniversity Consortium (www.cineca.it)
-
+=========================================================================*/
 
 
 #include "mafDefines.h" 
