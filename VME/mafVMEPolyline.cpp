6f0c50f9c3dd2c18460a1a0bf61b842d616b0e3a
ape0 <ape0>
2005-04-19 08:37:45 +0000

    ranamed mafVMEPolyline file with the right case

---
+++ VME/mafVMEPolyline.cpp
@@ -0,0 +1,76 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: mafVMEPolyline.cpp,v $
+  Language:  C++
+  Date:      $Date: 2005-04-19 08:37:45 $
+  Version:   $Revision: 1.1 $
+  Authors:   Marco Petrone
+==========================================================================
+  Copyright (c) 2001/2005 
+  CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+
+#include "mafDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include "mafVMEPolyline.h"
+#include "vtkDataSet.h"
+#include "vtkPolyData.h"
+#include <sstream>
+//-------------------------------------------------------------------------
+mafCxxTypeMacro(mafVMEPolyline)
+//-------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------
+mafVMEPolyline::mafVMEPolyline()
+//-------------------------------------------------------------------------
+{
+}
+
+//-------------------------------------------------------------------------
+mafVMEPolyline::~mafVMEPolyline()
+//-------------------------------------------------------------------------
+{
+  // data pipe destroyed in mafVME
+  // data vector destroyed in mafVMEGeneric
+}
+
+
+//-------------------------------------------------------------------------
+mafVMEOutput *mafVMEPolyline::GetOutput()
+//-------------------------------------------------------------------------
+{
+  // allocate the right type of output on demand
+  if (m_Output==NULL)
+  {
+    SetOutput(mafVMEOutputPolyline::New()); // create the output
+  }
+  return m_Output;
+}
+
+//-------------------------------------------------------------------------
+int mafVMEPolyline::SetData(vtkPolyData *data, mafTimeStamp t, int mode)
+//-------------------------------------------------------------------------
+{
+  return Superclass::SetData(data,t,mode);
+}
+//-------------------------------------------------------------------------
+int mafVMEPolyline::SetData(vtkDataSet *data, mafTimeStamp t, int mode)
+//-------------------------------------------------------------------------
+{
+  assert(data);
+  if (data->IsA("vtkPolyData"))
+  {
+    return Superclass::SetData(data,t,mode);
+  }
+  
+  mafErrorMacro("Trying to set the wrong type of fata inside a VME Polyline :"<< (data?data->GetClassName():"NULL"));
+  return MAF_ERROR;
+}
+
---
+++ VME/mafVMEPolyline.h
@@ -0,0 +1,64 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: mafVMEPolyline.h,v $
+  Language:  C++
+  Date:      $Date: 2005-04-19 08:37:45 $
+  Version:   $Revision: 1.1 $
+  Authors:   Marco Petrone
+==========================================================================
+  Copyright (c) 2001/2005 
+  CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+#ifndef __mafVMEPolyline_h
+#define __mafVMEPolyline_h
+//----------------------------------------------------------------------------
+// Include:
+//----------------------------------------------------------------------------
+#include "mafVMEGeneric.h"
+#include "mafVMEOutputPolyline.h"
+//----------------------------------------------------------------------------
+// forward declarations :
+//----------------------------------------------------------------------------
+class vtkDataSet;
+class vtkPolyData;
+
+/** mafVMEPolyline - a VME featuring an internal array for matrices and VTK datasets.
+  mafVMEPolyline is a specialized VME inheriting the VME-Generic features to internally
+  store data, and specialized for VTK data. This specialization consists in creating
+  a specialized data pipe and to redefining some APIs for casting to concrete classes.
+  @sa mafVME mafMatrixVector mafDataVector  */
+class MAF_EXPORT mafVMEPolyline : public mafVMEGeneric
+{
+public:
+  mafTypeMacro(mafVMEPolyline,mafVMEGeneric);
+
+  /** 
+    Set data for the give timestamp. This function automatically creates a
+    a VMEItem for the data to be stored. The optional parameter specify if
+    data must be copied, referenced, or detached from original source. Default
+    is to copy data which creates a new data set copying the original one.
+    Referencing means the data is kept connected to its original source, and
+    detaching means disconnect is like referencing but also disconnect the data
+    from the source.
+    Return MAF_OK if succeeded, MAF_ERROR if they kind of data is not accepted by
+    this type of VME. */
+    virtual int SetData(vtkPolyData *data, mafTimeStamp t, int mode=MAF_VME_COPY_DATA);
+
+    /** return the right type of output */  
+    mafVMEOutputPolyline *GetPolylineOutput() {return (mafVMEOutputPolyline *)GetOutput();}
+
+    /** return the right type of output */  
+    virtual mafVMEOutput *GetOutput();
+protected:
+  mafVMEPolyline();
+  virtual ~mafVMEPolyline();
+
+private:
+  mafVMEPolyline(const mafVMEPolyline&); // Not implemented
+  void operator=(const mafVMEPolyline&); // Not implemented
+  
+  /** private to avoid calling by external classes */
+  virtual int SetData(vtkDataSet *data, mafTimeStamp t, int mode=MAF_VME_COPY_DATA);
+};
+
+#endif
