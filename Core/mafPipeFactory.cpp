acec7d0290f975da7dd4eb10c9b7313cd471d18e
ape0 <ape0>
2005-04-22 16:29:39 +0000

    added pipe factory

---
+++ Core/mafPipeFactory.cpp
@@ -0,0 +1,100 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: mafPipeFactory.cpp,v $
+  Language:  C++
+  Date:      $Date: 2005-04-22 16:29:39 $
+  Version:   $Revision: 1.1 $
+  Authors:   Marco Petrone
+==========================================================================
+  Copyright (c) 2001/2005 
+  CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+
+#include "mafDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+
+#include "mafPipeFactory.h"
+#include "mafVersion.h"
+#include "mafIndent.h"
+#include "mafPipe.h"
+#include <string>
+#include <ostream>
+
+mafPipeFactory *mafPipeFactory::m_Instance=NULL;
+
+mafCxxTypeMacro(mafPipeFactory);
+
+std::vector<std::string> mafPipeFactory::m_PipeNames;
+
+//----------------------------------------------------------------------------
+// This is used to register the factory when linking statically
+int mafPipeFactory::Initialize()
+//----------------------------------------------------------------------------
+{
+  if (m_Instance==NULL)
+  {
+    m_Instance=mafPipeFactory::New();
+
+    if (m_Instance)
+    {
+      m_Instance->RegisterFactory(m_Instance);
+      return MAF_OK;  
+    }
+    else
+    {
+      return MAF_ERROR;
+    }
+  }
+  
+  return MAF_OK;
+}
+
+//------------------------------------------------------------------------
+mafPipeFactory::mafPipeFactory()
+//------------------------------------------------------------------------------
+{
+  m_Instance = NULL;
+  
+  //
+  // Plug here Pipes in this factory
+  //
+  //mafPlugPipeMacro(mafPipeRoot,"root for generic pipes tree");
+  //mafPlugPipeMacro(mafPipeGeneric,"a generic pipe with only basic features");
+  //mafPlugObjectMacro(mafTagArray,"a basic kind of attribute used to store key-value pairs");
+}
+
+//------------------------------------------------------------------------------
+const char* mafPipeFactory::GetMAFSourceVersion() const
+//------------------------------------------------------------------------------
+{
+  return MAF_SOURCE_VERSION;
+}
+
+//------------------------------------------------------------------------------
+const char* mafPipeFactory::GetDescription() const
+//------------------------------------------------------------------------------
+{
+  return "Factory for MAF Pipes";
+}
+
+//------------------------------------------------------------------------------
+mafPipe *mafPipeFactory::CreatePipeInstance(const char *type_name)
+//------------------------------------------------------------------------------
+{
+  return mafPipe::SafeDownCast(Superclass::CreateInstance(type_name));
+}
+
+//------------------------------------------------------------------------------
+void mafPipeFactory::RegisterNewPipe(const char* pipe_name, const char* description, mafCreateObjectFunction createFunction)
+//------------------------------------------------------------------------------
+{
+  m_PipeNames.push_back(pipe_name);
+  RegisterNewObject(pipe_name,description,createFunction);
+}
---
+++ Core/mafPipeFactory.h
@@ -0,0 +1,90 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: mafPipeFactory.h,v $
+  Language:  C++
+  Date:      $Date: 2005-04-22 16:29:39 $
+  Version:   $Revision: 1.1 $
+  Authors:   Marco Petrone
+==========================================================================
+  Copyright (c) 2001/2005 
+  CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+#ifndef __mafPipeFactory_h
+#define __mafPipeFactory_h
+//----------------------------------------------------------------------------
+// includes :
+//----------------------------------------------------------------------------
+#include "mafObjectFactory.h"
+
+/** to be used internally for plugging default pipes --- calls a member function directly */
+#define mafPlugPipeMacro(pipe_type,descr) \
+  RegisterNewPipe(pipe_type::GetStaticTypeName(), descr, pipe_type::NewObject);
+  
+
+//----------------------------------------------------------------------------
+// forward declarations :
+//----------------------------------------------------------------------------  
+class mafPipe;
+
+/** Object factory for Pipes.
+  To make a new VME available in the MAF it must be plugged inside a factory, in particular
+  this factory must be of type mafPipeFactory to be able to retrieve the list of pipes plugged
+  in the factory. Also when using mafPlugPipe<pipe_type> the pipe icon is plugged inside the
+  the MAF picture factory. */
+class MAF_EXPORT mafPipeFactory : public mafObjectFactory
+{
+public: 
+  mafTypeMacro(mafPipeFactory,mafObjectFactory);
+  virtual const char* GetMAFSourceVersion() const;
+  virtual const char* GetDescription() const;
+
+  /* Initialize the factory creating and registering a new instance */
+  static int Initialize();
+  /** return the instance pointer of the factory. return NULL if not initialized yet */
+  static mafPipeFactory *GetInstance() {if (!m_Instance) Initialize(); return m_Instance;}
+
+  /** create an instance of the pipe give its type name */
+  static mafPipe *CreatePipeInstance(const char *type_name);
+   
+   /**
+    This function can be used by Application code to register new Objects's to the mflCoreFactory */
+  void RegisterNewPipe(const char* pipe_name, const char* description, mafCreateObjectFunction createFunction);
+
+  /** return list of names for pipes plugged into this factory */
+  const static std::vector<std::string> GetPipeNames() {return m_PipeNames;}
+
+protected:
+  mafPipeFactory();
+  ~mafPipeFactory() { }
+
+  static mafPipeFactory *m_Instance;
+  static std::vector<std::string> m_PipeNames; 
+  
+private:
+  mafPipeFactory(const mafPipeFactory&);  // Not implemented.
+  void operator=(const mafPipeFactory&);  // Not implemented.
+};
+
+/** Plug  a pipe in the main MAF Pipe factory.*/
+template <class T>
+class MAF_EXPORT mafPlugPipe
+{
+  public:
+  mafPlugPipe(const char *description);
+  
+};
+
+//------------------------------------------------------------------------------
+/** Plug a new Pipe class into the Pipe factory.*/
+template <class T>
+mafPlugPipe<T>::mafPlugPipe(const char *description)
+//------------------------------------------------------------------------------
+{ 
+  mafPipeFactory *factory=mafPipeFactory::GetInstance();
+  if (factory)
+  {
+    factory->RegisterNewPipe(T::GetStaticTypeName(), description, T::NewObject);
+  }
+}
+
+#endif
