ef38086b876500c9d6c11b7ad9fbd76add542f11
aqd0 <aqd0>
2005-04-27 12:50:15 +0000

    VTKImporter test.

---
+++ Operations/Testing/VTKImporter/testVTKImporterApp.cpp
@@ -0,0 +1,99 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: testVTKImporterApp.cpp,v $
+  Language:  C++
+  Date:      $Date: 2005-04-27 12:50:16 $
+  Version:   $Revision: 1.1 $
+  Authors:   Silvano Imboden
+==========================================================================
+  Copyright (c) 2001/2005 
+  CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+
+#include "mafDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+
+#include "testVTKImporterApp.h"
+#include "mafVMEFactory.h"
+#include "mafPics.h"
+#include "mmgMDIFrame.h"
+
+#include "mafNodeFactory.h"
+#include "mafNodeGeneric.h"
+#include "mafNodeRoot.h"
+#include "mafVMERoot.h"
+#include "mafVMESurface.h"
+#include "mafPipeFactory.h"
+#include "mafPipeSurface.h"
+
+#include "mmoCreateVmeSurface.h"
+#include "mafViewVTK.h"
+#include "testView.h"
+//--------------------------------------------------------------------------------
+// Create the Application
+//--------------------------------------------------------------------------------
+IMPLEMENT_APP(testVTKImporterApp)
+
+//--------------------------------------------------------------------------------
+bool testVTKImporterApp::OnInit()
+//--------------------------------------------------------------------------------
+{
+  mafPics.Initialize();	
+
+  int result = mafVMEFactory::Initialize();
+  assert(result==MAF_OK);
+  
+  // Inizializzazione e Fill della PipeFactory -- potrebbe essere un SideEffect del Plug dei Nodi
+  result = mafPipeFactory::Initialize();
+  assert(result==MAF_OK);
+  mafPlugPipe<mafPipeSurface>("mafPipeSurface");
+
+  m_logic = new mafLogicWithManagers();
+  //m_logic->PlugTimebar(false);
+  //m_logic->PlugMenu(false);
+  //m_logic->PlugToolbar(false);
+  //m_logic->PlugLogbar(false);
+  //m_logic->PlugSidebar(false);
+  //m_logic->PlugOpManager(false);
+  //m_logic->PlugViewManager(false);
+  //m_logic->PlugVMEManager(false);  // the VmeManager at the moment cause 4 leaks of 200+32+24+56 bytes  //SIL. 20-4-2005: 
+  m_logic->Configure();
+
+  m_logic->GetTopWin()->SetTitle("testView");
+  SetTopWindow(mafGetFrame());  
+
+  m_logic->Plug(new mmoCreateVmeSurface("Add Vme Surface \tCtrl+A"));
+  //m_logic->Plug(new testView("testView"));
+  testView *tv = new testView("testView");
+  tv->PlugVisualPipe("mafVMESurface", "mafPipeSurface");
+  m_logic->Plug(tv);
+
+  m_logic->Show();
+  m_logic->Init(0,NULL); // calls FileNew - which create the root
+  return TRUE;
+}
+//--------------------------------------------------------------------------------
+int testVTKImporterApp::OnExit()
+//--------------------------------------------------------------------------------
+{
+  cppDEL(m_logic);
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Operations/Testing/VTKImporter/testVTKImporterApp.h
@@ -0,0 +1,41 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: testVTKImporterApp.h,v $
+  Language:  C++
+  Date:      $Date: 2005-04-27 12:50:16 $
+  Version:   $Revision: 1.1 $
+  Authors:   Silvano Imboden
+==========================================================================
+  Copyright (c) 2001/2005 
+  CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+#ifndef __testVTKImporterApp_H__
+#define __testVTKImporterApp_H__
+//----------------------------------------------------------------------------
+// Include:
+//----------------------------------------------------------------------------
+#include "mafLogicWithManagers.h" 
+
+// ============================================================================
+class testVTKImporterApp : public wxApp
+// ============================================================================
+{
+public:
+    bool OnInit();
+    int  OnExit();
+protected:
+    mafLogicWithManagers *m_logic;
+
+};
+
+DECLARE_APP(testVTKImporterApp)
+
+#endif //__testVTKImporterApp_H__
+
+
+
+
+
+
+
+
