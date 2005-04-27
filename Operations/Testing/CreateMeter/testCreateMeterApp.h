b38283f85419835f5a8e2214bcf7ae5781729ebd
aqd0 <aqd0>
2005-04-27 16:25:19 +0000

    CreateMeter testing

---
+++ Operations/Testing/CreateMeter/testCreateMeterApp.h
@@ -0,0 +1,31 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: testCreateMeterApp.h,v $
+  Language:  C++
+  Date:      $Date: 2005-04-27 16:25:19 $
+  Version:   $Revision: 1.1 $
+  Authors:   Paolo Quadrani
+==========================================================================
+  Copyright (c) 2001/2005 
+  CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+#ifndef __testCreateMeterApp_H__
+#define __testCreateMeterApp_H__
+//----------------------------------------------------------------------------
+// Include:
+//----------------------------------------------------------------------------
+#include "mafLogicWithManagers.h" 
+
+// ============================================================================
+class testCreateMeterApp : public wxApp
+// ============================================================================
+{
+public:
+  bool OnInit();
+  int  OnExit();
+
+protected:
+  mafLogicWithManagers *m_logic;
+};
+DECLARE_APP(testCreateMeterApp)
+#endif //__testCreateMeterApp_H__
