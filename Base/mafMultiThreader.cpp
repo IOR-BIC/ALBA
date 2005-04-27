80ad41daecaae8927b5139597b4849c1c2980c2d
ape0 <ape0>
2005-04-27 14:36:30 +0000

    added class for spawnig threads

---
+++ Base/mafMultiThreader.cpp
@@ -0,0 +1,231 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: mafMultiThreader.cpp,v $
+Language:  C++
+Date:      $Date: 2005-04-27 14:36:30 $
+Version:   $Revision: 1.1 $
+Authors:   Based on mafMultiThreader (www.vtk.org), adapted by Marco Petrone
+==========================================================================
+Copyright (c) 2001/2005 
+CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+#include "mafMultiThreader.h"
+#include "mafMutexLock.h"
+#include "mafIndent.h"
+
+// These are the includes necessary for multithreaded rendering on an SGI
+// using the sproc() call
+#ifdef CMAKE_USE_SPROC_INIT
+#include <sys/resource.h>
+#include <sys/prctl.h>
+#include <wait.h>
+#include <errno.h>
+#endif
+
+// Need to define "mmuExternCThreadFunctionType" to avoid warning on some
+// platforms about passing function pointer to an argument expecting an
+// extern "C" function.  Placing the typedef of the function pointer type
+// inside an extern "C" block solves this problem.
+#if defined(CMAKE_USE_PTHREADS_INIT) || defined(VTK_HP_PTHREADS)
+#include <pthread.h>
+extern "C" { typedef void *(*mmuExternCThreadFunctionType)(void *); }
+#else
+typedef mmuThreadFunctionType mmuExternCThreadFunctionType;
+#endif
+
+#ifdef __APPLE__
+#include <Carbon/Carbon.h>
+#endif
+
+// Constructor. Default all the methods to NULL. Since the
+// ThreadInfoArray is static, the ThreadIDs can be initialized here
+// and will not change.
+mafMultiThreader::mafMultiThreader()
+{
+  int i;
+
+  for ( i = 0; i < MAF_MAX_THREADS; i++ )
+  {
+    m_ThreadInfoArray[i].m_ThreadID           = i;
+    m_ThreadInfoArray[i].m_ActiveFlag         = NULL;
+    m_ThreadInfoArray[i].m_ActiveFlagLock     = NULL;
+    m_SpawnedThreadActiveFlag[i]              = 0;
+    m_SpawnedThreadActiveFlagLock[i]          = NULL;
+    m_SpawnedThreadInfoArray[i].m_ThreadID    = i;
+  }
+}
+
+// Destructor. Nothing allocated so nothing needs to be done here.
+mafMultiThreader::~mafMultiThreader()
+{
+}
+
+int mafMultiThreader::SpawnThread( mmuThreadFunctionType f, void *UserData )
+{
+  int id;
+
+  // avoid a warning
+  mmuThreadFunctionType tf;
+  tf = f; tf= tf;
+  
+#ifdef CMAKE_USE_WIN32_THREADS_INIT
+  DWORD              threadId;
+#endif
+
+  id = 0;
+
+  while ( id < MAF_MAX_THREADS )
+    {
+    if ( m_SpawnedThreadActiveFlagLock[id] == NULL )
+    {
+      m_SpawnedThreadActiveFlagLock[id] = new mafMutexLock;
+    }
+    m_SpawnedThreadActiveFlagLock[id]->Lock();
+    if (m_SpawnedThreadActiveFlag[id] == 0)
+    {
+      // We've got a useable thread id, so grab it
+      m_SpawnedThreadActiveFlag[id] = 1;
+      m_SpawnedThreadActiveFlagLock[id]->Unlock();
+      break;
+    }
+    m_SpawnedThreadActiveFlagLock[id]->Unlock();
+      
+    id++;
+  }
+
+  if ( id >= MAF_MAX_THREADS )
+  {
+    mafErrorMessage( "You have too many active threads!" );
+    return MAF_ERROR;
+  }
+
+
+  m_SpawnedThreadInfoArray[id].m_UserData = UserData;
+  m_SpawnedThreadInfoArray[id].m_NumberOfThreads = 1;
+  m_SpawnedThreadInfoArray[id].m_ActiveFlag = &m_SpawnedThreadActiveFlag[id];
+  m_SpawnedThreadInfoArray[id].m_ActiveFlagLock = m_SpawnedThreadActiveFlagLock[id];
+
+  // We are using sproc (on SGIs), pthreads(on Suns or HPs), 
+  // CreateThread (on win32), or generating an error  
+
+#ifdef CMAKE_USE_WIN32_THREADS_INIT
+  // Using CreateThread on a PC
+  //
+  m_SpawnedThreadProcessID[id] = 
+      CreateThread(NULL, 0, f, 
+             ((void *)(&m_SpawnedThreadInfoArray[id])), 0, &threadId);
+
+  if (m_SpawnedThreadProcessID[id] == NULL)
+  {
+    mafErrorMacro("Error in thread creation !!!");
+  } 
+#endif
+
+#ifdef CMAKE_USE_SPROC_INIT
+  // Using sproc() on an SGI
+  //
+  m_SpawnedThreadProcessID[id] = sproc( f, PR_SADDR, ( (void *)(&m_SpawnedThreadInfoArray[id]) ) );
+#endif
+
+#ifdef CMAKE_USE_PTHREADS_INIT
+  // Using POSIX threads
+  //
+  pthread_attr_t attr;
+
+#ifdef VTK_HP_PTHREADS
+  pthread_attr_create( &attr );
+#else  
+  pthread_attr_init(&attr);
+#ifndef __CYGWIN__
+  pthread_attr_setscope(&attr, PTHREAD_SCOPE_PROCESS);
+#endif
+#endif
+  
+#ifdef VTK_HP_PTHREADS
+  pthread_create( &(m_SpawnedThreadProcessID[id]),
+                  attr, f,  
+                  ( (void *)(&m_SpawnedThreadInfoArray[id]) ) );
+#else
+  pthread_create( &(m_SpawnedThreadProcessID[id]),
+                  &attr,
+                  reinterpret_cast<mmuExternCThreadFunctionType>(f),  
+                  ( (void *)(&m_SpawnedThreadInfoArray[id]) ) );
+#endif
+
+#endif
+
+#ifndef CMAKE_USE_WIN32_THREADS_INIT
+#ifndef CMAKE_USE_SPROC_INIT
+#ifndef CMAKE_USE_PTHREADS_INIT
+  // There is no multi threading, so there is only one thread.
+  // This won't work - so give an error message.
+  mafErrorMacro( << "Cannot spawn thread in a single threaded environment!" );
+  m_SpawnedThreadActiveFlagLock[id]->Delete();
+  id = -1;
+#endif
+#endif
+#endif
+
+  return id;
+}
+
+void mafMultiThreader::TerminateThread( int ThreadID )
+{
+
+  if ( !m_SpawnedThreadActiveFlag[ThreadID] ) {
+    return;
+  }
+
+  m_SpawnedThreadActiveFlagLock[ThreadID]->Lock();
+  m_SpawnedThreadActiveFlag[ThreadID] = 0;
+  m_SpawnedThreadActiveFlagLock[ThreadID]->Unlock();
+
+#ifdef CMAKE_USE_WIN32_THREADS_INIT
+  WaitForSingleObject(m_SpawnedThreadProcessID[ThreadID], INFINITE);
+  CloseHandle(m_SpawnedThreadProcessID[ThreadID]);
+#endif
+
+#ifdef CMAKE_USE_SPROC_INIT
+  siginfo_t info_ptr;
+
+  waitid( P_PID, (id_t) m_SpawnedThreadProcessID[ThreadID], 
+          &info_ptr, WEXITED );
+#endif
+
+#ifdef CMAKE_USE_PTHREADS_INIT
+  pthread_join( m_SpawnedThreadProcessID[ThreadID], NULL );
+#endif
+
+#ifndef CMAKE_USE_WIN32_THREADS_INIT
+#ifndef CMAKE_USE_SPROC_INIT
+#ifndef CMAKE_USE_PTHREADS_INIT
+  // There is no multi threading, so there is only one thread.
+  // This won't work - so give an error message.
+  mafErrorMacro(<< "Cannot terminate thread in single threaded environment!");
+#endif
+#endif
+#endif
+
+  delete m_SpawnedThreadActiveFlagLock[ThreadID];
+  m_SpawnedThreadActiveFlagLock[ThreadID] = NULL;
+
+}
+
+// Print method for the multithreader
+void mafMultiThreader::Print(std::ostream& os, const int tabs)
+{
+  mafIndent indent(tabs);
+  os << "Thread system used: " << 
+#ifdef CMAKE_USE_PTHREADS_INIT  
+   "PTHREADS"
+#elif defined CMAKE_USE_SPROC_INIT
+    "SPROC"
+#elif defined CMAKE_USE_WIN32_THREADS_INIT
+    "WIN32 Threads"
+#elif defined CMAKE_HP_PTHREADS_INIT
+    "HP PThreads"
+#else
+    "NO THREADS SUPPORT"
+#endif
+    << std::endl;
+}
---
+++ Base/mafMultiThreader.h
@@ -0,0 +1,132 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: mafMultiThreader.h,v $
+Language:  C++
+Date:      $Date: 2005-04-27 14:36:31 $
+Version:   $Revision: 1.1 $
+Authors:   Based on vtkMultiThreader (www.vtk.org), adapted by Marco Petrone
+==========================================================================
+Copyright (c) 2001/2005 
+CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#ifndef __mafMultiThreader_h
+#define __mafMultiThreader_h
+
+#include "mafDefines.h"
+#include "mafBase.h"
+#include "mafIncludeWIN32.h"
+
+#define MAF_MAX_THREADS 8
+
+#ifdef CMAKE_USE_SPROC_INIT
+#include <sys/types.h>   Needed for unix implementation of sproc
+#include <unistd.h>   Needed for unix implementation of sproc
+#endif
+
+#if defined(CMAKE_USE_PTHREADS_INIT) || defined(VTK_HP_PTHREADS)
+#include <pthread.h>   Needed for PTHREAD implementation of mutex
+#include <sys/types.h>   Needed for unix implementation of pthreads
+#include <unistd.h>   Needed for unix implementation of pthreads
+#endif
+/**
+  If CMAKE_USE_SPROC_INIT is defined, then sproc() will be used to create
+  multiple threads on an SGI. If CMAKE_USE_PTHREADS_INIT is defined, then
+  pthread_create() will be used to create multiple threads (on
+  a sun, for example)
+
+  If CMAKE_USE_PTHREADS_INIT is defined, then the multithreaded
+  function is of type void *, and returns NULL.
+  Otherwise the type is void which is correct for WIN32
+  and SPROC
+*/  
+#ifdef CMAKE_USE_SPROC_INIT
+typedef int mmuThreadProcessIDType;
+#endif
+
+
+#ifdef CMAKE_USE_PTHREADS_INIT
+typedef void *(*mmuThreadFunctionType)(void *);
+typedef pthread_t mmuThreadProcessIDType;
+#endif
+
+#ifdef CMAKE_USE_WIN32_THREADS_INIT
+typedef LPTHREAD_START_ROUTINE mmuThreadFunctionType;
+typedef HANDLE mmuThreadProcessIDType;
+#endif
+
+#if !defined(CMAKE_USE_PTHREADS_INIT) && !defined(CMAKE_USE_WIN32_THREADS_INIT)
+typedef void (*mmuThreadFunctionType)(void *);
+typedef int mmuThreadProcessIDType;
+#endif
+
+
+class mafMutexLock;
+
+/** mafMultiThreader - A class for performing multithreaded execution.
+  mafMultiThreader is a class that provides support for multithreaded
+  execution using sproc() on an SGI, or pthread_create on any platform
+  supporting POSIX threads.  This class can be used to execute a single
+  method on multiple threads, or to specify a method per thread. */
+class MAF_EXPORT mafMultiThreader: public mafBase
+{
+public:
+  mafMultiThreader();
+  ~mafMultiThreader();
+
+  void Print(std::ostream& os, const int tabs);
+
+  /**
+    This is the structure that is passed to the thread that is
+    created from the SingleMethodExecute, MultipleMethodExecute or
+    the SpawnThread method. It is passed in as a void *, and it is
+    up to the method to cast correctly and extract the information.
+    The ThreadID is a number between 0 and NumberOfThreads-1 that indicates
+    the id of this thread. The NumberOfThreads is this->NumberOfThreads for
+    threads created from SingleMethodExecute or MultipleMethodExecute,
+    and it is 1 for threads created from SpawnThread.
+    The UserData is the (void *)arg passed into the SetSingleMethod,
+    SetMultipleMethod, or SpawnThread method.*/
+#define mmuThreadInfoStruct mafMultiThreader::mmuThreadInfo
+  class mmuThreadInfo
+  {
+  public:
+    int                 m_ThreadID;
+    int                 m_NumberOfThreads;
+    int*                m_ActiveFlag;
+    mafMutexLock*       m_ActiveFlagLock;
+    void*               m_UserData;
+  };
+
+  /**
+    Create a new thread for the given function. Return a thread id
+    which is a number between 0 and MAF_MAX_THREADS - 1. This id should
+    be used to kill the thread at a later time. */
+  int SpawnThread( mmuThreadFunctionType, void *data );
+
+  /** Terminate the thread that was created with a SpawnThreadExecute() */
+  void TerminateThread( int thread_id );
+
+protected:
+
+  /**
+    An array of thread info containing a thread id
+    (0, 1, 2, .. MAF_MAX_THREADS-1), the thread count, and a pointer
+    to void so that user data can be passed to each thread */
+  mmuThreadInfo               m_ThreadInfoArray[MAF_MAX_THREADS];
+
+  /**
+    Storage of MutexFunctions and ints used to control spawned 
+    threads and the spawned thread ids */
+  int                        m_SpawnedThreadActiveFlag[MAF_MAX_THREADS];
+  mafMutexLock*              m_SpawnedThreadActiveFlagLock[MAF_MAX_THREADS];
+  mmuThreadProcessIDType     m_SpawnedThreadProcessID[MAF_MAX_THREADS];
+  mmuThreadInfo              m_SpawnedThreadInfoArray[MAF_MAX_THREADS];
+};
+
+#endif
+
+
+
+
+
