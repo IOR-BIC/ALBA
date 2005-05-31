80c4030925e1485ecff761c3e35b2dfeb901aeea
aqd0 <aqd0>
2005-05-31 09:50:08 +0000

    minor changes

--- Examples/exOperations/exOperationApp.cpp
+++ Examples/exOperations/exOperationApp.cpp
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: exOperationApp.cpp,v $
   Language:  C++
-  Date:      $Date: 2005-05-31 09:53:05 $
-  Version:   $Revision: 1.4 $
+  Date:      $Date: 2005-05-31 09:50:08 $
+  Version:   $Revision: 1.3 $
   Authors:   Paolo Quadrani
 ==========================================================================
   Copyright (c) 2001/2005 
@@ -67,7 +67,7 @@ bool exOperationApp::OnInit()
   //m_logic->PlugVMEManager(false);  // the VmeManager at the moment cause 4 leaks of 200+32+24+56 bytes  //SIL. 20-4-2005: 
   m_logic->Configure();
 
-  m_logic->GetTopWin()->SetTitle("Operations example");
+  m_logic->GetTopWin()->SetTitle("ViewVTK example");
   SetTopWindow(mafGetFrame());  
 
   m_logic->Plug(new mmoVTKImporter("VTK Importer"));
