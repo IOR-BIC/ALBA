4a0262023e092c7d02682e8758fb7247da498115
aqd0 <aqd0>
2005-07-07 15:24:37 +0000

    Changed if test into Update method

--- VME/mafMatrixInterpolator.cpp
+++ VME/mafMatrixInterpolator.cpp
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mafMatrixInterpolator.cpp,v $
   Language:  C++
-  Date:      $Date: 2005-05-12 16:22:02 $
-  Version:   $Revision: 1.4 $
+  Date:      $Date: 2005-07-07 15:24:37 $
+  Version:   $Revision: 1.5 $
   Authors:   Marco Petrone
 ==========================================================================
   Copyright (c) 2001/2005 
@@ -94,7 +94,7 @@ void mafMatrixInterpolator::Update()
   unsigned long mtime=m_UpdateTime.GetMTime();
 
   // if the current time has changed or if this object has been modified...
-  if (m_OldTimeStamp!=GetTimeStamp() || m_CurrentItem&&(m_CurrentItem->GetMTime()>mtime))
+  if (m_OldTimeStamp!=GetTimeStamp() || !m_CurrentItem || (m_CurrentItem->GetMTime()>mtime))
   {
     m_OldTimeStamp=GetTimeStamp();    
     
