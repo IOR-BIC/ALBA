b42b49ba6f5c553668b733b9301481c845430a57
aqd0 <aqd0>
2005-09-06 11:23:55 +0000

    Renamed from mmoMSFImporter

---
+++ Operations/mmoMSF1xImporter.cpp
@@ -0,0 +1,97 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: mmoMSF1xImporter.cpp,v $
+  Language:  C++
+  Date:      $Date: 2005-09-06 11:23:55 $
+  Version:   $Revision: 1.1 $
+  Authors:   Paolo Quadrani
+==========================================================================
+  Copyright (c) 2001/2005 
+  CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#include "mafDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include "mmoMSF1xImporter.h"
+#include <wx/busyinfo.h>
+#include "mafEvent.h"
+
+#include "mafVME.h"
+#include "mafVMERoot.h"
+#include "mafTagArray.h"
+#include "mafMSFImporter.h"
+
+//----------------------------------------------------------------------------
+mmoMSF1xImporter::mmoMSF1xImporter(wxString label) :
+mafOp(label)
+//----------------------------------------------------------------------------
+{
+  m_OpType  = OPTYPE_IMPORTER;
+	m_Canundo = true;
+	m_File    = "";
+
+  m_FileDir = mafGetApplicationDirectory().c_str();
+}
+//----------------------------------------------------------------------------
+mmoMSF1xImporter::~mmoMSF1xImporter( ) 
+//----------------------------------------------------------------------------
+{
+}
+//----------------------------------------------------------------------------
+mafOp* mmoMSF1xImporter::Copy()   
+//----------------------------------------------------------------------------
+{
+  //non devo incrementare l'id counter --- vfc le operazioni sono gia inserite nei menu;
+  mmoMSF1xImporter *cp = new mmoMSF1xImporter(m_Label);
+  cp->m_File			= m_File;
+  return cp;
+}
+//----------------------------------------------------------------------------
+void mmoMSF1xImporter::OpRun()   
+//----------------------------------------------------------------------------
+{
+	m_File = "";
+
+	mafString wildc = "MSF file (*.msf)|*.msf";
+  mafString f = mafGetOpenFile(m_FileDir, wildc.GetCStr(), "Choose MSF file").c_str();
+
+  int result = OP_RUN_CANCEL;
+  if(!f.IsEmpty())
+	{
+	  m_File = f;
+    wxSetWorkingDirectory(wxString(m_File.GetCStr()));
+    ImportMSF();
+	  result = OP_RUN_OK;
+	}
+	mafEventMacro(mafEvent(this,result));
+}
+//----------------------------------------------------------------------------
+void mmoMSF1xImporter::ImportMSF()
+//----------------------------------------------------------------------------
+{
+  bool success = false;
+	wxBusyInfo wait("Loading file: ...");
+  
+  m_Importer = new mafMSFImporter;
+  m_Importer->SetURL(m_File);
+  m_Importer->SetRoot((mafVMERoot *)m_Input->GetRoot());
+  
+  //mafEventMacro(mafEvent(this,BIND_TO_PROGRESSBAR,preader));
+  
+  success = (m_Importer->Restore() == MAF_OK);
+
+  if(!success)
+    mafErrorMessage("I/O Error importing MSF file.");
+
+  ((mafVMERoot *)m_Input->GetRoot())->GetStorage()->SetURL(m_File.GetCStr());
+  ((mafVMERoot *)m_Input->GetRoot())->GetStorage()->ForceParserURL();
+
+  cppDEL(m_Importer);
+}
+
---
+++ Operations/mmoMSF1xImporter.h
@@ -0,0 +1,63 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: mmoMSF1xImporter.h,v $
+  Language:  C++
+  Date:      $Date: 2005-09-06 11:23:55 $
+  Version:   $Revision: 1.1 $
+  Authors:   Paolo Quadrani
+==========================================================================
+Copyright (c) 2002/2004
+CINECA - Interuniversity Consortium (www.cineca.it) 
+=========================================================================*/
+
+#ifndef __mmoMSF1xImporter_H__
+#define __mmoMSF1xImporter_H__
+
+//----------------------------------------------------------------------------
+// Include :
+//----------------------------------------------------------------------------
+#include "mafOp.h"
+#include "mafString.h"
+
+//----------------------------------------------------------------------------
+// forward references :
+//----------------------------------------------------------------------------
+class mafNode;
+class mafVMEPolyline;
+class mafVMESurface;
+class mafVMEVolumeGray;
+class mafVMEGeneric;
+class mafMSFImporter;
+
+//----------------------------------------------------------------------------
+// mmoMSF1xImporter :
+//----------------------------------------------------------------------------
+/** */
+class mmoMSF1xImporter: public mafOp
+{
+public:
+  mmoMSF1xImporter(wxString label);
+ ~mmoMSF1xImporter(); 
+  mafOp* Copy();
+
+	/** this operation does not depend on the selected node */
+  bool Accept(mafNode* node) {return true;};
+
+	/** Builds operation's interface. */
+  void OpRun();
+
+	/** Import MSF file. */
+  void ImportMSF();
+
+  /** Set the MSF filename to be imported. 
+      This is used when the operation is executed not using user interface. */
+  void SetFileName(const char *name) {m_File = name;};
+
+protected:
+  mafString m_File;
+  mafString m_FileDir;
+  
+  mafMSFImporter *m_Importer;
+};
+#endif
+
