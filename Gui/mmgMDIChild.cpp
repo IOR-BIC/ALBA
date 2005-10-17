8eefe299eb4a62f9772f50db9f02549e6df7857c
aqd0 <aqd0>
2005-10-17 14:41:44 +0000

    minor changes

--- Gui/mmgMDIChild.cpp
+++ Gui/mmgMDIChild.cpp
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mmgMDIChild.cpp,v $
   Language:  C++
-  Date:      $Date: 2005-10-17 14:24:42 $
-  Version:   $Revision: 1.8 $
+  Date:      $Date: 2005-10-17 14:41:44 $
+  Version:   $Revision: 1.9 $
   Authors:   Silvano Imboden
 ==========================================================================
   Copyright (c) 2002/2004
@@ -62,10 +62,7 @@ mmgMDIChild::mmgMDIChild(wxMDIParentFrame* parent,mafView *view)
   m_Listener = NULL;
 
   m_win = m_view->GetWindow();
-  wxBoxSizer *win_sizer = new wxBoxSizer(wxVERTICAL);
-  win_sizer->Add(m_win,1,wxEXPAND);
   m_win->Reparent(this);
-  m_win->Fit();
   m_win->Show(true);
 
   SetIcon(mafPics.GetIcon("MDICHILD_ICON"));
