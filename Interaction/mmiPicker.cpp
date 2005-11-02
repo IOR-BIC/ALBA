046d544b805cedd494eee3c746b489acda1d3180
aqd0 <aqd0>
2005-11-02 10:37:05 +0000

    Extended Picking also to Compounded views.

--- Interaction/mmiPicker.cpp
+++ Interaction/mmiPicker.cpp
@@ -2,19 +2,29 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mmiPicker.cpp,v $
   Language:  C++
-  Date:      $Date: 2005-10-22 12:45:30 $
-  Version:   $Revision: 1.4 $
+  Date:      $Date: 2005-11-02 10:37:05 $
+  Version:   $Revision: 1.5 $
   Authors:   Marco Petrone 
 ==========================================================================
   Copyright (c) 2002/2004 
   CINECA - Interuniversity Consortium (www.cineca.it)
 =========================================================================*/
+
+
+#include "mafDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+
 #include "mmiPicker.h"
-
-
 #include "mafEvent.h"
 
-#include "mafViewVTK.h"
+#include "mafView.h"
+#include "mafViewCompound.h"
 #include "mmdTracker.h"
 #include "mmdMouse.h"
 #include "mafAvatar3D.h"
@@ -62,32 +72,38 @@ void mmiPicker::OnButtonUp(mafEventInteraction *e)
 {
   if (mmdTracker *tracker=mmdTracker::SafeDownCast((mafDevice *)e->GetSender()))
   { // is it a tracker?
-    
     mafMatrix *tracker_pose = e->GetMatrix();
-
     // extract device avatar's renderer, no avatar == no picking
     mafAvatar *avatar = tracker->GetAvatar();
     if (avatar)
     {
-      double pos_picked[3];
-      int flag=avatar->Pick(*tracker_pose);
-      if (flag) 
+      mafView *v = avatar->GetView();
+      if (v)
       {
-        // find picked position 
-        vtkAbstractPropPicker *picker = avatar->GetPicker();
-        picker->GetPickPosition(pos_picked);
-		  }
-      else if (mafAvatar3D *avatar3D=mafAvatar3D::SafeDownCast(avatar))
-      {
-        // in case of 3D avatar return 3D position
-        mafTransform::GetPosition(avatar3D->GetLastPoseMatrix(),pos_picked);
+        // compute pose in the world frame
+        mafMatrix world_pose;
+        mafAvatar3D *avatar3D=mafAvatar3D::SafeDownCast(avatar);
+        if (avatar3D)
+        {
+          avatar3D->TrackerToWorld(*tracker_pose,world_pose,mafAvatar3D::CANONICAL_TO_WORLD_SCALE);
+        }
+        else
+          world_pose = *tracker_pose;
+        double pos_picked[3];
+        if (v->Pick(world_pose))
+        {
+          vtkPoints *p = vtkPoints::New();
+          p->SetNumberOfPoints(1);
+          p->SetPoint(0,pos_picked);
+          mafEventMacro(mafEvent(this,VME_PICKED,p));
+          p->Delete();
+        }
+        else if (avatar3D)
+        {
+          // in case of 3D avatar return 3D position
+          mafTransform::GetPosition(avatar3D->GetLastPoseMatrix(),pos_picked);
+        }
       }
-      
-      vtkPoints *p = vtkPoints::New();
-		  p->SetNumberOfPoints(1);
-		  p->SetPoint(0,pos_picked);
-		  mafEventMacro(mafEvent(this,VME_PICKED,p));
-		  p->Delete();
     }
   }
   else if (mmdMouse *mouse=mmdMouse::SafeDownCast((mafDevice *)e->GetSender()))
@@ -98,9 +114,14 @@ void mmiPicker::OnButtonUp(mafEventInteraction *e)
     mouse_pos[0] = (int)tmp_pose[0];
     mouse_pos[1] = (int)tmp_pose[1];
 
-    mafViewVTK *v = mafViewVTK::SafeDownCast(mouse->GetView());
+    mafView *v = mouse->GetView();
     if (v)
     {
+      mafViewCompound *vc = mafViewCompound::SafeDownCast(v);
+      if (vc)
+      {
+        v = vc->GetSubView(mouse->GetRWI());
+      }
       if(v->Pick(mouse_pos[0],mouse_pos[1]))
       {
         double pos_picked[3];
@@ -113,4 +134,3 @@ void mmiPicker::OnButtonUp(mafEventInteraction *e)
     }
   }
 }
-
