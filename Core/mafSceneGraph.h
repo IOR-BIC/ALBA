47c66f68b32509853e6f7394991d59d91d7577ce
aqd0 <aqd0>
2005-11-03 13:56:07 +0000

    minor changes

--- Core/mafSceneGraph.h
+++ Core/mafSceneGraph.h
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mafSceneGraph.h,v $
   Language:  C++
-  Date:      $Date: 2005-10-25 16:57:45 $
-  Version:   $Revision: 1.6 $
+  Date:      $Date: 2005-11-03 13:56:07 $
+  Version:   $Revision: 1.7 $
   Authors:   Silvano Imboden
 ==========================================================================
   Copyright (c) 2002/2004
@@ -38,38 +38,46 @@ public:
   virtual void	SetListener(mafObserver *Listener) {m_Listener = Listener;};
 	//virtual void	OnEvent(mafEvent& e);
 
-	/** Add a vme to the scene graph. */
+	/** 
+  Add a vme to the scene graph. */
 	virtual void VmeAdd(mafNode *vme);
 
-	/** Remove a vme from the scene graph. */
+	/** 
+  Remove a vme from the scene graph. */
 	virtual void VmeRemove(mafNode *vme);
 
-	/** Select a vme and store a reference to it into m_SelectedVme variable. */
+	/** 
+  Select a vme and store a reference to it into m_SelectedVme variable. */
 	virtual void VmeSelect(mafNode *vme, bool select);
 
-	/** Change the visibility of the vme and if necessary create the vme related pipe. */
+	/** 
+  Change the visibility of the vme and if necessary create the vme related pipe. */
 	virtual void VmeShow(mafNode *vme, bool show);
 
-	/** Show/Hide the Vme with the same type of the passed vme */
-  //SIL. 18-4-2005: - question: Show by type should consider the derived classes or not ?
+	/** 
+  Show/Hide the Vme with the same type of the passed vme */
 	virtual void VmeShowByType(mafNode *vme, bool show);
 
-	/** Show all the same vme type. */
+	/** 
+  Show all the same vme type. */
 	//@@@ virtual void					VmeShowByType			(mafNodeBaseTypes type,bool show);
 
-	/** Show all the vme subtree. */
+	/** 
+  Show all the vme subtree. */
 	virtual void VmeShowSubTree(mafNode *vme, bool show);
 
-	/** Update the vme's properties. */
+	/** 
+  Update the vme's properties. */
 	virtual void VmeUpdateProperty(mafNode *vme, bool fromTag = false);
 
-	/** Find the corresponding vme's node. */
+	/** 
+  Find the corresponding vme's node. */
 	virtual mafSceneNode *Vme2Node(mafNode *vme);
 
-  mafSceneNode	*m_List;
-  vtkRenderer		*m_RenFront;
-  vtkRenderer		*m_RenBack;
-  mafView		    *m_View;
+  mafSceneNode	*m_List;      ///< list of visualized node
+  vtkRenderer		*m_RenFront;  ///< pointer to the front renderer
+  vtkRenderer		*m_RenBack;   ///< pointer to the back renderer
+  mafView		    *m_View;      ///< pointer to the view
 
   // Set the flags for creatable vmes type.
 	//@@@ virtual void SetCreatableFlag	(mafNodeBaseTypes type,  bool flag = true);
@@ -83,17 +91,20 @@ public:
   // Return the gui with the autoshow widgets.
 	//@@@ virtual mmgGui *GetGui();
 
-  // Return the selected vme.
+  /**
+  Return the selected vme.*/
 	virtual mafNode *GetSelectedVme() {return m_SelectedVme;};
 
-  // Used by the mmgCheckTree - return the status of a SceneNode
+  /**
+  Used by the mmgCheckTree - return the status of a SceneNode*/
 	virtual int  GetNodeStatus (mafNode *node);
 
 	// Event to show/hide the LandmarkClouds
 	//@@@ virtual void	OnOpenCloseEvent( mafSceneNode *node );
 
 protected:
-  /** recursively kill all node - starting from the tail of the list. */
+  /** 
+  recursively kill all node - starting from the tail of the list. */
   virtual void DeleteNodeList(mafSceneNode *n);
 /*	
 	bool m_creatable[NUM_OF_BASETYPE];
