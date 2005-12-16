cc426c82ef1241235b1e19d7c49847fb19cf5018
aqd0 <aqd0>
2005-12-16 18:48:40 +0000

    Added slice initialization

--- Views/mafViewOrthoSlice.cpp
+++ Views/mafViewOrthoSlice.cpp
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mafViewOrthoSlice.cpp,v $
   Language:  C++
-  Date:      $Date: 2005-12-12 11:43:42 $
-  Version:   $Revision: 1.12 $
+  Date:      $Date: 2005-12-16 18:48:40 $
+  Version:   $Revision: 1.13 $
   Authors:   Paolo Quadrani
 ==========================================================================
   Copyright (c) 2002/2004
@@ -35,7 +35,7 @@ mafCxxTypeMacro(mafViewOrthoSlice);
 //----------------------------------------------------------------------------
 
 //----------------------------------------------------------------------------
-mafViewOrthoSlice::mafViewOrthoSlice(wxString label, int num_row, int num_col, bool external)
+mafViewOrthoSlice::mafViewOrthoSlice(wxString label, bool external)
 : mafViewCompound(label, 2, 2, external)
 //----------------------------------------------------------------------------
 {
@@ -56,10 +56,6 @@ mafViewOrthoSlice::mafViewOrthoSlice(wxString label, int num_row, int num_col, b
 mafViewOrthoSlice::~mafViewOrthoSlice()
 //----------------------------------------------------------------------------
 {
-  for (int v=0;v<4;v++)
-  {
-    cppDEL(m_Views[v]);
-  }
   vtkDEL(m_ColorLUT);
 }
 //----------------------------------------------------------------------------
@@ -69,7 +65,10 @@ mafView *mafViewOrthoSlice::Copy(mafObserver *Listener)
   mafViewOrthoSlice *v = new mafViewOrthoSlice(m_Label, m_ExternalFlag);
   v->m_Listener = Listener;
   v->m_Id = m_Id;
-  v->m_PluggedChildViewList = m_PluggedChildViewList;
+  for (int i=0;i<m_PluggedChildViewList.size();i++)
+  {
+    v->m_PluggedChildViewList.push_back(m_PluggedChildViewList[i]->Copy(this));
+  }
   v->m_NumOfPluggedChildren = m_NumOfPluggedChildren;
   v->Create();
   return v;
@@ -81,38 +80,44 @@ void mafViewOrthoSlice::VmeShow(mafNode *node, bool show)
   for(int i=0; i<m_NumOfChildView; i++)
     m_ChildViewList[i]->VmeShow(node, show);
 
-  if (show && node->IsMAFType(mafVMEVolume))
+  if (node->IsMAFType(mafVMEVolume))
   {
-    m_CurrentVolume = mafVMEVolume::SafeDownCast(node);
-    double sr[2];
-    vtkDataSet *data = m_CurrentVolume->GetOutput()->GetVTKData();
-    data->Update();
-    data->GetScalarRange(sr);
-    m_Luts->SetRange((long)sr[0],(long)sr[1]);
-    m_Luts->SetSubRange((long)sr[0],(long)sr[1]);
-    m_ColorLUT->SetRange(sr);
-    m_ColorLUT->Build();
-    for(int i=0; i<m_NumOfChildView; i++)
+    if (show)
     {
-      mafPipeVolumeSlice *p = (mafPipeVolumeSlice *)((mafViewSlice *)m_ChildViewList[i])->GetNodePipe(m_CurrentVolume);
-      p->SetColorLookupTable(m_ColorLUT);
+      m_CurrentVolume = mafVMEVolume::SafeDownCast(node);
+      double sr[2],center[3];
+      vtkDataSet *data = m_CurrentVolume->GetOutput()->GetVTKData();
+      data->Update();
+      data->GetCenter(center);
+      data->GetScalarRange(sr);
+      m_Luts->SetRange((long)sr[0],(long)sr[1]);
+      m_Luts->SetSubRange((long)sr[0],(long)sr[1]);
+//      m_ColorLUT->SetRange(sr);
+//      m_ColorLUT->Build();
+      for(int i=0; i<m_NumOfChildView; i++)
+      {
+        //((mafViewSlice *)m_ChildViewList[i])->SetSlice(center);
+        mafPipeVolumeSlice *p = (mafPipeVolumeSlice *)((mafViewSlice *)m_ChildViewList[i])->GetNodePipe(m_CurrentVolume);
+        p->SetColorLookupTable(m_ColorLUT);
+      }
+      if (m_SliderX)
+      {
+        double b[6];
+        m_CurrentVolume->GetOutput()->GetVMELocalBounds(b);
+        m_Origin[0] = (b[0]+b[1])*.5;
+        m_Origin[1] = (b[2]+b[3])*.5;
+        m_Origin[2] = (b[4]+b[5])*.5;
+        m_SliderX->SetRange(b[0],b[1],m_Origin[0]);
+        m_SliderY->SetRange(b[2],b[3],m_Origin[1]);
+        m_SliderZ->SetRange(b[4],b[5],m_Origin[2]);
+        m_Gui->Update();
+      }
     }
-    if (m_SliderX)
+    else
     {
-      double b[6];
-      m_CurrentVolume->GetOutput()->GetVMELocalBounds(b);
-      m_Origin[0] = (b[0]+b[1])*.5;
-      m_Origin[1] = (b[2]+b[3])*.5;
-      m_Origin[2] = (b[4]+b[5])*.5;
-      m_SliderX->SetRange(b[0],b[1],m_Origin[0]);
-      m_SliderY->SetRange(b[2],b[3],m_Origin[1]);
-      m_SliderZ->SetRange(b[4],b[5],m_Origin[2]);
-      m_Gui->Update();
+      m_CurrentVolume = NULL;
+      m_ColorLUTEnabled = 0;
     }
-  }
-  if (!show && node->IsMAFType(mafVMEVolume))
-  {
-    m_CurrentVolume = NULL;
   }
   EnableWidgets(m_CurrentVolume != NULL);
 }
@@ -194,6 +199,8 @@ mmgGui* mafViewOrthoSlice::CreateGui()
   if (m_ColorLUT == NULL)
   {
     vtkNEW(m_ColorLUT);
+    m_ColorLUT->SetRange(0.0,255.0);
+    m_ColorLUT->Build();
   }
   m_Gui->Lut(ID_LUT_CHOOSER,"lut",m_ColorLUT);
 
@@ -208,7 +215,7 @@ mmgGui* mafViewOrthoSlice::CreateGui()
 void mafViewOrthoSlice::PackageView()
 //----------------------------------------------------------------------------
 {
-  int cam_pos[] = {CAMERA_OS_X, CAMERA_OS_Y, CAMERA_OS_Z, CAMERA_OS_P};
+  int cam_pos[4] = {CAMERA_OS_X, CAMERA_OS_Y, CAMERA_OS_Z, CAMERA_OS_P};
   for(int v=0; v<4; v++)
   {
     m_Views[v] = new mafViewSlice("Slice view", cam_pos[v]);
