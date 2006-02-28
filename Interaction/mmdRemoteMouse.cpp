6fe0991aca1ec79c5f8ce44f7add22f1007a5046
aqd0 <aqd0>
2006-02-28 14:47:40 +0000

    porting to MAF2

--- Interaction/mmdRemoteMouse.cpp
+++ Interaction/mmdRemoteMouse.cpp
@@ -2,129 +2,103 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mmdRemoteMouse.cpp,v $
   Language:  C++
-  Date:      $Date: 2005-05-18 17:29:05 $
-  Version:   $Revision: 1.2 $
+  Date:      $Date: 2006-02-28 14:47:40 $
+  Version:   $Revision: 1.3 $
   Authors:   Marco Petrone
 ==========================================================================
   Copyright (c) 2002/2004 
   CINECA - Interuniversity Consortium (www.cineca.it)
 =========================================================================*/
-// To be included first because of wxWindows
-#ifdef __GNUG__
-    #pragma implementation "mafInteractor.cpp"
-#endif
 
-// For compilers that support precompilation, includes "wx/wx.h".
-#include "wx/wxprec.h"
 
-#include "mafInteractionDecl.h"
+#include "mafDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include "mmdRemoteMouse.h"
 #include "mafEventBase.h"
 #include "mafView.h"
 #include "mafSceneGraph.h"
 #include "mafRWIBase.h"
-#include "mmdRemoteMouse.h"
 
-#include "mflSmartPointer.h"
-#include "mflEventInteraction.h"
+#include "mafEventInteraction.h"
+#include "mmuIdFactory.h"
 
-#include "vtkMatrix4x4.h"
-#include "vtkAssemblyPath.h"
 #include "vtkRenderWindow.h"
 #include "vtkRenderWindowInteractor.h"
-#include "vtkRendererCollection.h"
-#include "vtkRenderer.h"
-#include "vtkCellPicker.h"
 
 //------------------------------------------------------------------------------
 // Events
 //------------------------------------------------------------------------------
-MFL_EVT_IMP(mmdRemoteMouse::MOUSE_2D_MOVE)
-MFL_EVT_IMP(mmdRemoteMouse::MOUSE_CHAR_EVENT)
+MAF_ID_IMP(mmdRemoteMouse::MOUSE_2D_MOVE)
+MAF_ID_IMP(mmdRemoteMouse::MOUSE_CHAR_EVENT)
+
+//------------------------------------------------------------------------------
+mafCxxTypeMacro(mmdRemoteMouse)
+//------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
 mmdRemoteMouse::mmdRemoteMouse()
 //------------------------------------------------------------------------------
 {
   SetThreaded(0);
-  LastPosition[0] = LastPosition[1] = 0;
-  ButtonState[0]  = ButtonState[1] = ButtonState[2] = 0;
-  SelectedView    = NULL;
-  SelectedRWI     = NULL;
-  Picker = vtkCellPicker::New();
+  m_LastPosition[0] = m_LastPosition[1] = 0;
+  m_ButtonState[0]  = m_ButtonState[1] = m_ButtonState[2] = 0;
+  m_SelectedView    = NULL;
+  m_SelectedRWI     = NULL;
 }
 
 //------------------------------------------------------------------------------
 mmdRemoteMouse::~mmdRemoteMouse()
 //------------------------------------------------------------------------------
 {
-  Picker->Delete();
 }
-
-/*//------------------------------------------------------------------------------
-void mmdRemoteMouse::CreateSettings()
 //------------------------------------------------------------------------------
-{
-  return; // NO Gui!
-}*/
-
-/*//------------------------------------------------------------------------------
-int mmdRemoteMouse::InternalInitialize()
-//------------------------------------------------------------------------------
-{
-  if (Superclass::InternalInitialize())
-    return -1;
-  return 0;
-}
-*/
-
-/*
-//------------------------------------------------------------------------------
-void mmdRemoteMouse::InternalShutdown()
-//------------------------------------------------------------------------------
-{
-  Superclass::InternalShutdown();
-}
-*/
-//------------------------------------------------------------------------------
-void mmdRemoteMouse::ProcessEvent(mflEvent *event,unsigned long channel)
+void mmdRemoteMouse::OnEvent(mafEventBase *event)
 //------------------------------------------------------------------------------
 {
   assert(event);
 
-  mafID id=event->GetID();
-
-  mflEventInteraction *e = (mflEventInteraction *)event;
-
+  mafID id = event->GetId();
+  mafEventInteraction *e = (mafEventInteraction *)event;
   if (id == MOUSE_2D_MOVE)
   {
     double pos[2];
     e->Get2DPosition(pos);
     SetLastPosition(pos[0],pos[1],e->GetModifiers());
   }
-  else if (id == ButtonDownEvent)
+  else if (id == BUTTON_DOWN)
   {
     // store the Selected RWI is needed for compounded view
-    e->Get2DPosition(LastPosition);
-    SelectedRWI = (mafRWIBase *)event->GetSender();
+    e->Get2DPosition(m_LastPosition);
+    m_SelectedRWI = (mafRWIBase *)event->GetSender();
     e->SetSender(this);
-    InvokeEvent(e,DeviceInputChannel);
+    InvokeEvent(e,MCH_INPUT);
   }
-  else if (id == ButtonUpEvent)
+  else if (id == BUTTON_UP)
   {
-    e->Get2DPosition(LastPosition);
+    e->Get2DPosition(m_LastPosition);
     e->SetSender(this);
-    InvokeEvent(e,DeviceInputChannel);
+    InvokeEvent(e,MCH_INPUT);
   }
-  else if (id == ViewSelectedEvent)
+  else if (id == VIEW_SELECT)
   {
-    SelectedView = ((mafEventBase *)event)->GetView();
+    mafEvent *e=mafEvent::SafeDownCast(event);
+    assert(e);
+    m_SelectedView = e->GetView();    
   }
   else if (id == MOUSE_CHAR_EVENT)
   {
-    unsigned char key = (unsigned char)((mafEventBase *)event)->GetArg();
-    mflSMART(mflEventInteraction,ev)(MOUSE_CHAR_EVENT,this);
-    ev->SetKey(key);
-    InvokeEvent(ev,DeviceInputChannel);
+    mafEvent *e=mafEvent::SafeDownCast(event);
+    assert(e);
+    unsigned char key = (unsigned char)e->GetArg();
+    mafEventInteraction ev(this,MOUSE_CHAR_EVENT);
+    ev.SetKey(key);
+    InvokeEvent(ev,MCH_INPUT);
   }
 }
 
@@ -132,52 +106,31 @@ void mmdRemoteMouse::ProcessEvent(mflEvent *event,unsigned long channel)
 void mmdRemoteMouse::SetLastPosition(double x,double y,unsigned long modifiers)
 //------------------------------------------------------------------------------
 {
-  LastPosition[0] = x;
-  LastPosition[1] = y;
+  m_LastPosition[0] = x;
+  m_LastPosition[1] = y;
   
   // create a new event with last position
-  mflSMART(mflEventInteraction,e)(MOUSE_2D_MOVE,this,x,y);
-  e->SetModifiers(modifiers);
-  InvokeEvent(e,DeviceInputChannel);
+  mafEventInteraction e(this,MOUSE_2D_MOVE,x,y);
+  e.SetModifiers(modifiers);
+  InvokeEvent(e,MCH_INPUT);
 }
 
 //------------------------------------------------------------------------------
-void mmdRemoteMouse::SendButtonEvent(mflEventInteraction *event)
+void mmdRemoteMouse::SendButtonEvent(mafEventInteraction *event)
 //------------------------------------------------------------------------------
 {
   event->Set2DPosition(GetLastPosition());
   Superclass::SendButtonEvent(event);
 }
 //------------------------------------------------------------------------------
-vtkAssemblyPath *mmdRemoteMouse::Pick(int mouse_screen_pos[2])
-//------------------------------------------------------------------------------
-{
-  vtkAssemblyPath *ap = NULL;
-
-  if (SelectedView)
-  {
-    vtkRendererCollection *rc = SelectedRWI->GetRenderWindow()->GetRenderers();
-    vtkRenderer *r = NULL;
-    assert(rc);
-    rc->InitTraversal();
-    while(r = rc->GetNextItem())
-    {
-      if( Picker->Pick(mouse_screen_pos[0],mouse_screen_pos[1],0,r) )
-      {
-        ap = Picker->GetPath();
-        break;
-      }
-    }
-  }
-
-  return ap;
-}
-//------------------------------------------------------------------------------
 vtkRenderer *mmdRemoteMouse::GetRenderer()
 //------------------------------------------------------------------------------
 {
   vtkRenderer *r = NULL;
-  r = SelectedRWI->FindPokedRenderer((int)LastPosition[0],(int)LastPosition[1]);
+  if (m_SelectedView)
+  {
+    r = m_SelectedRWI->FindPokedRenderer((int)m_LastPosition[0],(int)m_LastPosition[1]);
+  }
 
   return r;
 }
@@ -185,21 +138,20 @@ vtkRenderer *mmdRemoteMouse::GetRenderer()
 mafView *mmdRemoteMouse::GetView()
 //------------------------------------------------------------------------------
 {
-  return SelectedView;
-}
-//------------------------------------------------------------------------------
-vtkAbstractPropPicker *mmdRemoteMouse::GetPicker()
-//------------------------------------------------------------------------------
-{
-  return (vtkAbstractPropPicker *)Picker;
+  return m_SelectedView;
 }
 //------------------------------------------------------------------------------
 vtkRenderWindowInteractor *mmdRemoteMouse::GetInteractor()
 //------------------------------------------------------------------------------
 {
-  if (SelectedView)
-    return SelectedRWI->GetRenderWindow()->GetInteractor();
+  if (m_SelectedRWI)
+    return m_SelectedRWI->GetRenderWindow()->GetInteractor();
 
   return (vtkRenderWindowInteractor *)NULL;
 }
-
+//------------------------------------------------------------------------------
+mafRWIBase *mmdRemoteMouse::GetRWI()
+//------------------------------------------------------------------------------
+{
+  return m_SelectedRWI;
+}
--- Interaction/mmdRemoteMouse.h
+++ Interaction/mmdRemoteMouse.h
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mmdRemoteMouse.h,v $
   Language:  C++
-  Date:      $Date: 2005-04-30 14:34:57 $
-  Version:   $Revision: 1.1 $
+  Date:      $Date: 2006-02-28 14:47:40 $
+  Version:   $Revision: 1.2 $
   Authors:   Marco Petrone
 ==========================================================================
   Copyright (c) 2002/2004 
@@ -12,14 +12,6 @@
 
 #ifndef __mmdRemoteMouse_h
 #define __mmdRemoteMouse_h
-
-#ifdef __GNUG__
-    #pragma interface "mmdRemoteMouse.cpp"
-#endif
-
-#ifndef WX_PRECOMP
-    #include "wx/wx.h"
-#endif
 
 #include "mmdButtonsPad.h"
 
@@ -35,43 +27,35 @@ class vtkRenderWindowInteractor;
 class mafRWIBase;
 
 /** Tracking 2D device, i.e. "Mouse".
-  mmdRemoteMouse is a class implemnting interface for a Mouse. Current implemantation 
-  recevices its inputs by means of ProcessEvent and forward to all listeners. Any 
+  mmdRemoteMouse is a class implementing interface for a Mouse. Current implementation 
+  receives its inputs by means of ProcessEvent and forward to all listeners. Any 
   widget or windows toolkit can inject events into this centralized mouse.
   @sa mmdButtonsPad mafRWI
 */
 class mmdRemoteMouse : public mmdButtonsPad
 {
 public:
-  static mmdRemoteMouse *New() {return new mmdRemoteMouse;}
-  vtkTypeMacro(mmdRemoteMouse,mmdButtonsPad); 
-
-  // For factoring purpouses
-  static vtkObject *NewObjectInstance() {return mmdRemoteMouse::New();}
-  static const char *GetTypeName() {return "mmdRemoteMouse";}
+  mafTypeMacro(mmdRemoteMouse,mmdButtonsPad); 
 
   //------------------------------------------------------------------------------
   // Events
   //------------------------------------------------------------------------------
   /** @ingroup Events
       Issued when a mouse is moved */
-  MFL_EVT_DEC(MOUSE_2D_MOVE)
+  MAF_ID_DEC(MOUSE_2D_MOVE)
   /** @ingroup Events
       Issued when a keyboard char is pressed */
-  MFL_EVT_DEC(MOUSE_CHAR_EVENT)
+  MAF_ID_DEC(MOUSE_CHAR_EVENT)
     
   /**  Reimplemented to manage fusion of move events. */
-  virtual void ProcessEvent(mflEvent *event,mafID channel=mflAgent::DefaultChannel);
+  virtual void OnEvent(mafEventBase *event);
 
   /**  Set the last mouse position and send a 2D MoveEvent. */
   void SetLastPosition(double x,double y,unsigned long modifiers = 0);
 
   /**  Return the last position */
-  vtkGetVector2Macro(LastPosition,double);
-
-  /**
-  Perform mouse picking and return the selected assembly path picked, otherwise return NULL. */
-  vtkAssemblyPath *Pick(int mouse_screen_pos[2]);
+  const double *GetLastPosition() {return m_LastPosition;}
+  void GetLastPosition(double pos[2]) {pos[0]=m_LastPosition[0];pos[1]=m_LastPosition[1];}
 
   /**
   Return the renderer of the selected view. */
@@ -82,36 +66,26 @@ public:
   mafView *GetView();
 
   /** 
-  Return the default mouse picker. */
-  vtkAbstractPropPicker *GetPicker();
-
-  /** 
   Return the Interactor. */
   vtkRenderWindowInteractor *GetInteractor();
+
+  /** 
+  Return the RenderWindowInteractor used by mouse device */
+  mafRWIBase *GetRWI();
 
 protected:
   mmdRemoteMouse();
   virtual ~mmdRemoteMouse();
 
-  //virtual int InternalInitialize();
-  //virtual void InternalShutdown();
-
   /** add position to the event */ 
-  virtual void SendButtonEvent(mflEventInteraction *event);
+  virtual void SendButtonEvent(mafEventInteraction *event);
 
-  /** Currently Mouse device doesn't have any settings */
-  //virtual void CreateSettings();
-
-  double   LastPosition[2];///< stores the last position
-
-  mafView *SelectedView; ///< store the selected view to perform the mouse picking
-  mafRWIBase *SelectedRWI;
-  vtkCellPicker *Picker;
+  double       m_LastPosition[2];///< stores the last position
+  mafView     *m_SelectedView; ///< store the selected view to perform the mouse picking
+  mafRWIBase  *m_SelectedRWI;
 
 private:
   mmdRemoteMouse(const mmdRemoteMouse&);  // Not implemented.
   void operator=(const mmdRemoteMouse&);  // Not implemented.
 };
-
 #endif 
-
