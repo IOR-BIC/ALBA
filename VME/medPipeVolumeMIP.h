405dd34c84942dc78d572fb3d0fadf747aea9f19
aqd0 <aqd0>
2006-03-02 21:57:17 +0000

    minor changes on #ifdef

--- VME/medPipeVolumeMIP.h
+++ VME/medPipeVolumeMIP.h
@@ -2,16 +2,16 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: medPipeVolumeMIP.h,v $
   Language:  C++
-  Date:      $Date: 2005-12-06 14:21:40 $
-  Version:   $Revision: 1.2 $
+  Date:      $Date: 2006-03-02 21:57:17 $
+  Version:   $Revision: 1.3 $
   Authors:   Paolo Quadrani
 ==========================================================================
 Copyright (c) 2002/2004
 CINECA - Interuniversity Consortium (www.cineca.it) 
 =========================================================================*/
 
-#ifndef _medPipeVolumeMIP_H_
-#define _medPipeVolumeMIP_H_
+#ifndef __medPipeVolumeMIP_H__
+#define __medPipeVolumeMIP_H__
 
 //----------------------------------------------------------------------------
 // Include :
@@ -71,4 +71,4 @@ protected:
 
   vtkActor               *m_SelectionActor;
 };
-#endif // _medPipeVolumeMIP_H_
+#endif // __medPipeVolumeMIP_H__
