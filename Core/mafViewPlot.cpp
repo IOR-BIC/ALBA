6fdaceacea0dfa2629f9a296af385a759e95e4e8
aqd0 <aqd0>
2006-06-28 16:33:19 +0000

    Added mafViewPlot

---
+++ Core/mafViewPlot.cpp
@@ -0,0 +1,232 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: mafViewPlot.cpp,v $
+  Language:  C++
+  Date:      $Date: 2006-06-28 16:33:19 $
+  Version:   $Revision: 1.1 $
+  Authors:   Silvano Imboden - Paolo Quadrani
+==========================================================================
+  Copyright (c) 2002/2004
+  CINECA - Interuniversity Consortium (www.cineca.it) 
+=========================================================================*/
+
+
+#include "mafDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include "mafViewPlot.h"
+#include <wx/dc.h>
+#include "mafPipe.h"
+#include "mafPipeFactory.h"
+
+#include "mmdMouse.h"
+
+#include "mafVME.h"
+#include "mafVMEScalar.h"
+
+//----------------------------------------------------------------------------
+mafCxxTypeMacro(mafViewPlot);
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+mafViewPlot::mafViewPlot(wxString label)
+:mafView(label)
+//----------------------------------------------------------------------------
+{
+  m_Sg = NULL;
+}
+//----------------------------------------------------------------------------
+mafViewPlot::~mafViewPlot() 
+//----------------------------------------------------------------------------
+{
+  m_PipeMap.clear();
+  cppDEL(m_Sg);
+}
+//----------------------------------------------------------------------------
+void mafViewPlot::PlugVisualPipe(mafString vme_type, mafString pipe_type, long visibility)
+//----------------------------------------------------------------------------
+{
+  mafVisualPipeInfo plugged_pipe;
+  plugged_pipe.m_PipeName = pipe_type;
+  plugged_pipe.m_Visibility = visibility;
+  m_PipeMap[vme_type] = plugged_pipe;
+}
+//----------------------------------------------------------------------------
+mafView *mafViewPlot::Copy(mafObserver *Listener)
+//----------------------------------------------------------------------------
+{
+  mafViewPlot *v = new mafViewPlot(m_Label);
+  v->m_Listener = Listener;
+  v->m_Id = m_Id;
+  v->m_PipeMap = m_PipeMap;
+  v->Create();
+  return v;
+}
+//----------------------------------------------------------------------------
+void mafViewPlot::Create()
+//----------------------------------------------------------------------------
+{
+  m_Sg  = new mafSceneGraph(this,NULL);
+  m_Sg->SetListener(this);
+}
+//----------------------------------------------------------------------------
+void mafViewPlot::SetMouse(mmdMouse *mouse)
+//----------------------------------------------------------------------------
+{
+}
+
+//----------------------------------------------------------------------------
+void mafViewPlot::VmeAdd(mafNode *vme)
+//----------------------------------------------------------------------------
+{
+  assert(m_Sg); 
+  m_Sg->VmeAdd(vme);
+}
+//----------------------------------------------------------------------------
+void mafViewPlot::VmeShow(mafNode *vme, bool show)												{assert(m_Sg); m_Sg->VmeShow(vme,show);}
+void mafViewPlot::VmeUpdateProperty(mafNode *vme, bool fromTag)	        {assert(m_Sg); m_Sg->VmeUpdateProperty(vme,fromTag);}
+//----------------------------------------------------------------------------
+int  mafViewPlot::GetNodeStatus(mafNode *vme)
+//----------------------------------------------------------------------------
+{
+  int status = vme->IsMAFType(mafVMEScalar) ? NODE_VISIBLE_ON : NODE_NON_VISIBLE;
+  if (!m_PipeMap.empty())
+  {
+    mafString vme_type = vme->GetTypeName();
+    if(m_PipeMap[vme_type].m_Visibility == NON_VISIBLE)
+    {
+      status = NODE_NON_VISIBLE;
+    }
+    else if (m_PipeMap[vme_type].m_Visibility == MUTEX)
+    {
+      mafSceneNode *n = m_Sg->Vme2Node(vme);
+      n->m_Mutex = true;
+      status = m_Sg->GetNodeStatus(vme);
+    }
+  }
+  return status;
+}
+//----------------------------------------------------------------------------
+void mafViewPlot::VmeRemove(mafNode *vme)
+//----------------------------------------------------------------------------
+{
+  assert(m_Sg); 
+  m_Sg->VmeRemove(vme);
+}
+//----------------------------------------------------------------------------
+void mafViewPlot::VmeSelect(mafNode *vme, bool select)
+//----------------------------------------------------------------------------
+{
+  assert(m_Sg); 
+  m_Sg->VmeSelect(vme,select);
+}
+//----------------------------------------------------------------------------
+mafPipe* mafViewPlot::GetNodePipe(mafNode *vme)
+//----------------------------------------------------------------------------
+{
+   assert(m_Sg);
+   mafSceneNode *n = m_Sg->Vme2Node(vme);
+   if(!n) return NULL;
+   return n->m_Pipe;
+}
+//----------------------------------------------------------------------------
+void mafViewPlot::GetVisualPipeName(mafNode *node, mafString &pipe_name)
+//----------------------------------------------------------------------------
+{
+  assert(node->IsA("mafVME"));
+  mafVME *v = ((mafVME*)node);
+
+  mafString vme_type = v->GetTypeName();
+  if (!m_PipeMap.empty())
+  {
+    // pick up the visual pipe from the view's visual pipe map
+    pipe_name = m_PipeMap[vme_type].m_PipeName;
+  }
+  if(pipe_name.IsEmpty())
+  {
+    // pick up the default visual pipe from the vme
+    pipe_name = v->GetVisualPipe();
+  }
+}
+//----------------------------------------------------------------------------
+void mafViewPlot::VmeCreatePipe(mafNode *vme)
+//----------------------------------------------------------------------------
+{
+  mafString pipe_name = "";
+  GetVisualPipeName(vme, pipe_name);
+
+  if (pipe_name != "")
+  {
+    mafPipeFactory *pipe_factory  = mafPipeFactory::GetInstance();
+    assert(pipe_factory!=NULL);
+    mafObject *obj = NULL;
+    obj = pipe_factory->CreateInstance(pipe_name);
+    mafPipe *pipe = (mafPipe*)obj;
+    if (pipe)
+    {
+      pipe->SetListener(this);
+      mafSceneNode *n = m_Sg->Vme2Node(vme);
+      assert(n && !n->m_Pipe);
+      pipe->Create(n);
+      n->m_Pipe = (mafPipe*)pipe;
+    }
+    else
+    {
+      mafErrorMessage("Cannot create visual pipe object of type \"%s\"!",pipe_name.GetCStr());
+    }
+  }
+}
+//----------------------------------------------------------------------------
+void mafViewPlot::VmeDeletePipe(mafNode *vme)
+//----------------------------------------------------------------------------
+{
+  mafSceneNode *n = m_Sg->Vme2Node(vme);
+  assert(n && n->m_Pipe);
+  cppDEL(n->m_Pipe);
+}
+//-------------------------------------------------------------------------
+mmgGui *mafViewPlot::CreateGui()
+//-------------------------------------------------------------------------
+{
+  assert(m_Gui == NULL);
+  m_Gui = new mmgGui(this);
+
+  return m_Gui;
+}
+//----------------------------------------------------------------------------
+void mafViewPlot::OnEvent(mafEventBase *maf_event)
+//----------------------------------------------------------------------------
+{
+  if (mafEvent *e = mafEvent::SafeDownCast(maf_event))
+  {
+    switch(e->GetId()) 
+    {
+      default:
+        mafEventMacro(*maf_event);
+      break;
+    }
+  }
+  else
+  {
+    mafEventMacro(*maf_event);
+  }
+}
+//----------------------------------------------------------------------------
+void mafViewPlot::Print(wxDC *dc, wxRect margins)
+//----------------------------------------------------------------------------
+{
+  wxBitmap image;
+  GetImage(image/*, 2*/);
+  PrintBitmap(dc, margins, &image);
+}
+//----------------------------------------------------------------------------
+void mafViewPlot::GetImage(wxBitmap &bmp, int magnification)
+//----------------------------------------------------------------------------
+{
+//  bmp = *m_Rwi->m_RwiBase->GetImage(magnification);
+}
---
+++ Core/mafViewPlot.h
@@ -0,0 +1,122 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: mafViewPlot.h,v $
+  Language:  C++
+  Date:      $Date: 2006-06-28 16:33:19 $
+  Version:   $Revision: 1.1 $
+  Authors:   Silvano Imboden - Paolo Quadrani
+==========================================================================
+  Copyright (c) 2002/2004
+  CINECA - Interuniversity Consortium (www.cineca.it) 
+=========================================================================*/
+#ifndef __mafViewPlot_H__
+#define __mafViewPlot_H__
+
+//----------------------------------------------------------------------------
+// Include:
+//----------------------------------------------------------------------------
+#include "mafView.h"
+#include "mafSceneGraph.h"
+#include "mafSceneNode.h" //used in subclasses
+#include <map>
+
+//----------------------------------------------------------------------------
+// forward references :
+//----------------------------------------------------------------------------
+class mafVME;
+class mmdMouse;
+
+//----------------------------------------------------------------------------
+// mafViewPlot :
+//----------------------------------------------------------------------------
+/** 
+mafViewPlot is a View that got a mafXYPlot and plot curves on a plane.
+*/
+class mafViewPlot: public mafView
+{
+public:
+  mafViewPlot(wxString label = "plot View");
+  virtual ~mafViewPlot();
+
+  mafTypeMacro(mafViewPlot, mafView);
+
+  virtual mafView*  Copy(mafObserver *Listener);
+  virtual void      Create();
+  virtual void			OnEvent(mafEventBase *maf_event);
+
+  /** IDs for the GUI */
+/*  enum VIEW_VTK_WIDGET_ID
+  {
+    ID_SHOW_GRID = Superclass::ID_LAST,
+    ID_GRID_NORMAL,
+    ID_LAST
+  };*/
+
+  /** Add the vme to the view's scene-graph*/
+  virtual void VmeAdd(mafNode *vme);
+  
+  /** Remove the vme from the view's scene-graph*/
+  virtual void VmeRemove(mafNode *vme);
+
+  virtual void VmeSelect(mafNode *vme, bool select);
+
+  /** Called to show/hide vme*/
+  virtual void VmeShow(mafNode *vme, bool show);
+
+  /** 
+  Called to update visual pipe properties of the vme passed as argument. If the 'fromTag' flag is true,
+  the update is done by reading the visual parameters from tags.*/
+  virtual void VmeUpdateProperty(mafNode *vme, bool fromTag = false);
+
+  /** 
+  Set the visualization status for the node (visible, not visible, mutex, ...) \sa mafSceneGraph*/
+  virtual int GetNodeStatus(mafNode *vme);
+
+  /** 
+  Return a pointer to the visual pipe of the node passed as argument. 
+  It is used in mafSideBar to plug the visual pipe's GUI in the tabbed vme panel. \sa mafSideBar*/
+  virtual mafPipe*  GetNodePipe(mafNode *vme);
+  
+  /** 
+  Create the visual pipe for the node passed as argument. 
+  To create visual pipe first check in m_PipeMap if custom visual pipe is defined, 
+  otherwise ask to vme which is its visual pipe. */
+  virtual void VmeCreatePipe(mafNode *vme);
+
+  /** 
+  Delete vme's visual pipe. It is called when vme is removed from visualization.*/
+  virtual void VmeDeletePipe(mafNode *vme);
+
+  virtual mafSceneGraph *GetSceneGraph()	  {return m_Sg;}; 
+
+  /** Return a pointer to the image of the plot.*/
+  void GetImage(wxBitmap &bmp, int magnification = 1);
+
+  /** Struct containing information regarding visual pipe plugged into the view. */
+  struct mafVisualPipeInfo
+  {
+    mafString m_PipeName;
+    long      m_Visibility;
+  };
+  typedef std::map<mafString, mafVisualPipeInfo> mafPipeMap;
+
+  /** Plug a visual pipe for a particular vme. It is used also to plug custom pipe.*/
+  void PlugVisualPipe(mafString vme_type, mafString pipe_type, long visibility = VISIBLE);
+
+  mafPipeMap m_PipeMap; ///< Map used to store visual pipeline associated with vme types
+
+  /** Set the mouse device to use inside the view */
+  void SetMouse(mmdMouse *mouse);
+
+  /** Print this view.*/
+  virtual void Print(wxDC *dc, wxRect margins);
+
+protected:
+  mafSceneGraph *m_Sg;
+
+  virtual mmgGui *CreateGui();
+
+  /** Return the visual pipe's name.*/
+  void GetVisualPipeName(mafNode *node, mafString &pipe_name);
+};
+#endif
