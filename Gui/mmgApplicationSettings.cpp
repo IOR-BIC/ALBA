cd89305997b6d2f323a75770b9c90ae26c1aa976
aqd0 <aqd0>
2006-11-02 11:32:13 +0000

    Added settings for remote storage.

--- Gui/mmgApplicationSettings.cpp
+++ Gui/mmgApplicationSettings.cpp
@@ -2,8 +2,8 @@
 Program:   Multimod Application Framework
 Module:    $RCSfile: mmgApplicationSettings.cpp,v $
 Language:  C++
-Date:      $Date: 2006-07-24 15:48:27 $
-Version:   $Revision: 1.3 $
+Date:      $Date: 2006-11-02 11:32:13 $
+Version:   $Revision: 1.4 $
 Authors:   Paolo Quadrani
 ==========================================================================
 Copyright (c) 2001/2005 
@@ -21,6 +21,8 @@ CINECA - Interuniversity Consortium (www.cineca.it)
 
 #include "mmgApplicationSettings.h"
 #include <wx/config.h>
+#include "mafCrypt.h"
+
 #include "mafDecl.h"
 #include "mmgGui.h"
 
@@ -33,47 +35,137 @@ mmgApplicationSettings::mmgApplicationSettings(mafObserver *Listener)
   // Default values for the application.
   m_LogToFile   = 0;
   m_VerboseLog  = 0;
-  m_CacheFolder = wxGetCwd().c_str();
   m_LogFolder = wxGetCwd().c_str();
   
+  m_AnonymousFalg = true;
+  m_RemoteHostName = "ftp://ftp.wxwindows.org";
+  m_UserName = "anonymous";
+  m_Password = "anonymous@wxwindows.org";
+  m_Port = 21;
+  m_UseRemoteStorage = 0;
+  m_CacheFolder = wxGetCwd().c_str();
+  
+  m_UseDefaultPasPhrase = 1;
+  m_PassPhrase = mafDefaultPassPhrase();
+
   InitializeApplicationSettings();
 
   m_Gui = new mmgGui(this);
   m_Gui->Label(_("Application general settings"));
+  m_Gui->Bool(ID_USE_DEFAULT_PASSPHRASE,_("use default passphrase"),&m_UseDefaultPasPhrase,1);
+  m_Gui->String(ID_PASSPHRASE,_("passphrase"),&m_PassPhrase,"",false,true);
+  m_Gui->Divider(2);
+  m_Gui->Label(_("Default settings for remote storage"));
+  m_Gui->Bool(ID_STORAGE_TYPE,_("remote storage"),&m_UseRemoteStorage,1,_("Check if you want to use remote storage instead the local one."));
+  m_Gui->Bool(ID_ANONYMOUS_USER,_("anonymous connection"),&m_AnonymousFalg,1);
+  m_Gui->String(ID_HOST_NAME,"host",&m_RemoteHostName);
+  m_Gui->Integer(ID_PORT,_("port"),&m_Port,0);
+  m_Gui->String(ID_USERNAME,_("user"),&m_UserName);
+  m_Gui->String(ID_PASSWORD,_("pwd"),&m_Password,"",false,true);
+  m_Gui->DirOpen(ID_CACHE_FOLDER,_("cache"),&m_CacheFolder,_("set the local cache folder \nin which put downloaded files"));
+  m_Gui->Divider(2);
   m_Gui->Bool(ID_LOG_TO_FILE,_("log to file"),&m_LogToFile,1);
   m_Gui->Bool(ID_LOG_VERBOSE,_("log verbose"),&m_VerboseLog,1);
   m_Gui->DirOpen(ID_LOD_DIR,_("log dir"),&m_LogFolder);
   m_Gui->Divider(2);
-  m_Gui->DirOpen(ID_CACHE_FOLDER,"cache",&m_CacheFolder,_("set the local cache folder \nto put downloaded files"));
-  m_Gui->Label(_("changes will take effect when \nthe application restart"),false,true);
+  EnableItems();
+  m_Gui->Label(_("changes will take effect when the \napplication restart"),false,true);
   m_Gui->Label("");
 }
 //----------------------------------------------------------------------------
-mmgApplicationSettings::~mmgApplicationSettings() 
+mmgApplicationSettings::~mmgApplicationSettings()
 //----------------------------------------------------------------------------
 {
   m_Gui = NULL; // gui is destroyed by the dialog.
 }
 //----------------------------------------------------------------------------
+void mmgApplicationSettings::EnableItems()
+//----------------------------------------------------------------------------
+{
+  m_Gui->Enable(ID_LOG_VERBOSE,m_LogToFile != 0);
+  m_Gui->Enable(ID_LOD_DIR,m_LogToFile != 0);
+
+  m_Gui->Enable(ID_CACHE_FOLDER,m_UseRemoteStorage != 0);
+  m_Gui->Enable(ID_ANONYMOUS_USER,m_UseRemoteStorage != 0);
+  m_Gui->Enable(ID_HOST_NAME,m_UseRemoteStorage != 0);
+  m_Gui->Enable(ID_PORT,m_UseRemoteStorage != 0);
+  m_Gui->Enable(ID_USERNAME,m_UseRemoteStorage != 0 && m_AnonymousFalg == 0);
+  m_Gui->Enable(ID_PASSWORD,m_UseRemoteStorage != 0 && m_AnonymousFalg == 0);
+
+  m_Gui->Enable(ID_PASSPHRASE,m_UseDefaultPasPhrase == 0);
+}
+//----------------------------------------------------------------------------
 void mmgApplicationSettings::OnEvent(mafEventBase *maf_event)
 //----------------------------------------------------------------------------
 {
-	switch(maf_event->GetId())
+  wxConfig *config = new wxConfig(wxEmptyString);
+
+  switch(maf_event->GetId())
   {
     case ID_LOG_TO_FILE:
+      config->Write("LogToFile",m_LogToFile);
+    break;
     case ID_LOG_VERBOSE:
+      config->Write("LogVerbose",m_VerboseLog);
+    break;
     case ID_LOD_DIR:
+      config->Write("LogFolder",m_LogFolder.GetCStr());
+    break;
     case ID_CACHE_FOLDER:
+      config->Write("CacheFolder",m_CacheFolder.GetCStr());
+    break;
+    case ID_STORAGE_TYPE:
+      config->Write("UseRemoteStorage",m_UseRemoteStorage);
+    break;
+    case ID_ANONYMOUS_USER:
+      config->Write("AnonymousConnection",m_AnonymousFalg);
+    break;
+    case ID_HOST_NAME:
+      config->Write("RemoteHost",m_RemoteHostName.GetCStr());
+    break;
+    case ID_PORT:
+      config->Write("PortConnection",m_Port);
+    break;
+    case ID_USERNAME:
+    {
+      std::string cry_user;
+      cry_user.clear();
+      mafEncryptFromMemory(m_UserName.GetCStr(),m_UserName.Length(),cry_user,m_PassPhrase);
+      config->Write("User",cry_user.c_str());
+    }
+    break;
+    case ID_PASSWORD:
+    {
+      std::string cry_pwd;
+      cry_pwd.clear();
+      mafEncryptFromMemory(m_Password.GetCStr(),m_Password.Length(),cry_pwd,m_PassPhrase);
+      config->Write("Password",cry_pwd.c_str());
+    }
+    break;
+    case ID_PASSPHRASE:
+    break;
+    case ID_USE_DEFAULT_PASSPHRASE:
+      config->Write("UseDefaultPassphrase",m_UseDefaultPasPhrase);
+      if (m_UseDefaultPasPhrase != 0)
+      {
+        m_PassPhrase = mafDefaultPassPhrase();
+        wxMessageBox(_("Passphrase resetted to default one!"),_("Warning"));
+      }
+      else
+      {
+        m_PassPhrase = wxGetPasswordFromUser(_("Insert passphrase"),_("Passphrase"),wxEmptyString).c_str();
+      }
+      EnableItems();
+      m_Gui->Update();
+      OnEvent(&mafEvent(this,ID_USERNAME));
+      OnEvent(&mafEvent(this,ID_PASSWORD));
     break;
     default:
       mafEventMacro(*maf_event);
     break; 
   }
-  wxConfig *config = new wxConfig(wxEmptyString);
-  config->Write("LogToFile",m_LogToFile);
-  config->Write("LogVerbose",m_VerboseLog);
-  config->Write("CacheFolder",m_CacheFolder.GetCStr());
-  config->Write("LogFolder",m_LogFolder.GetCStr());
+  EnableItems();
+
   cppDEL(config);
 }
 //----------------------------------------------------------------------------
@@ -81,39 +173,109 @@ void mmgApplicationSettings::InitializeApplicationSettings()
 //----------------------------------------------------------------------------
 {
   wxConfig *config = new wxConfig(wxEmptyString);
-  long log_flags;
-  if(config->Read("LogToFile", &log_flags))
+  std::string crypted_user, crypted_pwd;
+  std::string decrypted_user, decrypted_pwd;
+  crypted_user.clear();
+  crypted_pwd.clear();
+  decrypted_user.clear();
+  decrypted_pwd.clear();
+  wxString string_item;
+  long long_item;
+  if(config->Read("LogToFile", &long_item))
   {
-    m_LogToFile = log_flags;
+    m_LogToFile = long_item;
   }
   else
   {
     config->Write("LogToFile",m_LogToFile);
   }
-  if(config->Read("LogVerbose", &log_flags))
+  if(config->Read("LogVerbose", &long_item))
   {
-    m_VerboseLog = log_flags;
+    m_VerboseLog = long_item;
   }
   else
   {
     config->Write("LogVerbose",m_VerboseLog);
   }
-  wxString cf;
-  if(config->Read("LogFolder", &cf))
+  if(config->Read("LogFolder", &string_item))
   {
-    m_LogFolder = cf.c_str();
+    m_LogFolder = string_item.c_str();
   }
   else
   {
     config->Write("LogFolder",m_LogFolder.GetCStr());
   }
-  if(config->Read("CacheFolder", &cf))
+  if(config->Read("UseRemoteStorage", &long_item))
   {
-    m_CacheFolder = cf.c_str();
+    m_UseRemoteStorage = long_item;
+  }
+  else
+  {
+    config->Write("UseRemoteStorage",m_UseRemoteStorage);
+  }
+  if(config->Read("PortConnection", &long_item))
+  {
+    m_Port = long_item;
+  }
+  else
+  {
+    config->Write("PortConnection",m_Port);
+  }
+  if(config->Read("AnonymousConnection", &long_item))
+  {
+    m_AnonymousFalg = long_item;
+  }
+  else
+  {
+    config->Write("AnonymousConnection",m_AnonymousFalg);
+  }
+  if(config->Read("CacheFolder", &string_item))
+  {
+    m_CacheFolder = string_item.c_str();
   }
   else
   {
     config->Write("CacheFolder",m_CacheFolder.GetCStr());
+  }
+  if(config->Read("RemoteHost", &string_item))
+  {
+    m_RemoteHostName = string_item.c_str();
+  }
+  else
+  {
+    config->Write("RemoteHost",m_RemoteHostName.GetCStr());
+  }
+  if (config->Read("UseDefaultPassphrase", &long_item))
+  {
+    m_UseDefaultPasPhrase = long_item;
+  }
+  else
+  {
+    config->Write("UseDefaultPassphrase",m_UseDefaultPasPhrase);
+  }
+  if (m_UseDefaultPasPhrase == 0)
+  {
+    m_PassPhrase = wxGetPasswordFromUser(_("Insert passphrase"),_("Passphrase"),wxEmptyString).c_str();
+  }
+  if(config->Read("User", &string_item))
+  {
+    mafDecryptInMemory(string_item.c_str(),decrypted_user,m_PassPhrase);
+    m_UserName = decrypted_user.c_str();
+  }
+  else
+  {
+    mafEncryptFromMemory(m_UserName.GetCStr(),m_UserName.Length(),crypted_user,m_PassPhrase);
+    config->Write("User",crypted_user.c_str());
+  }
+  if(config->Read("Password", &string_item))
+  {
+    mafDecryptInMemory(string_item.c_str(),decrypted_pwd,m_PassPhrase);
+    m_Password = decrypted_pwd.c_str();
+  }
+  else
+  {
+    mafEncryptFromMemory(m_Password.GetCStr(),m_Password.Length(),crypted_pwd,m_PassPhrase);
+    config->Write("Password",crypted_pwd.c_str());
   }
   
   cppDEL(config);
--- Gui/mmgApplicationSettings.h
+++ Gui/mmgApplicationSettings.h
@@ -2,8 +2,8 @@
 Program:   Multimod Application Framework
 Module:    $RCSfile: mmgApplicationSettings.h,v $
 Language:  C++
-Date:      $Date: 2006-07-24 15:48:27 $
-Version:   $Revision: 1.2 $
+Date:      $Date: 2006-11-02 11:32:13 $
+Version:   $Revision: 1.3 $
 Authors:   Paolo Quadrani
 ==========================================================================
 Copyright (c) 2001/2005 
@@ -36,7 +36,15 @@ public:
     ID_LOG_TO_FILE = MINID,
     ID_LOG_VERBOSE,
     ID_LOD_DIR,
-    ID_CACHE_FOLDER
+    ID_STORAGE_TYPE,
+    ID_CACHE_FOLDER,
+    ID_HOST_NAME,
+    ID_PORT,
+    ID_USERNAME,
+    ID_PASSWORD,
+    ID_ANONYMOUS_USER,
+    ID_USE_DEFAULT_PASSPHRASE,
+    ID_PASSPHRASE
   };
 
   /** Answer to the messages coming from interface. */
@@ -49,23 +57,84 @@ public:
   /** Return true if Log To File is enabled.*/
   bool GetLogToFileStatus() {return m_LogToFile != 0;};
 
+  void SetLogFileStatus(int log_status) {m_LogToFile = log_status;};
+
   /** Return true if Verbose Log is enabled.*/
   bool GetLogVerboseStatus() {return m_VerboseLog != 0;};
+  
+  void SetLogVerboseStatus(int log_verbose) {m_VerboseLog = log_verbose;};
 
   /** Return the folder in which store the log files.*/
   mafString &GetLogFolder() {return m_LogFolder;};
 
+  void SetLogFolder(mafString log_folder) {m_LogFolder = log_folder;};
+
+  /** Return true if Remote Storage flag is enabled.*/
+  bool UseRemoteStorage() {return m_UseRemoteStorage != 0;};
+
+  void SetUseRemoteStorage(int use_remote) {m_UseRemoteStorage = use_remote;};
+
   /** Return the path for the local cache in which download remote VMEs.*/
   mafString &GetCacheFolder() {return m_CacheFolder;};
+
+  void SetCacheFolder(mafString cache_folder) {m_CacheFolder = cache_folder;};
+
+  /** Return the path for the remote hostname.*/
+  mafString &GetRemoteHostName() {return m_RemoteHostName;};
+
+  void SetRemoteHostName(mafString host) {m_RemoteHostName = host;};
+
+  /** Return the port for the connection.*/
+  long GetRemotePort() {return m_Port;};
+
+  void SetRemotePort(long port) {m_Port = port;};
+
+  /** Return the username for the connection.*/
+  mafString &GetUserName() {return m_UserName;};
+
+  void SetUserName(mafString user) {m_UserName = user;};
+
+  /** Return the password for the connection.*/
+  mafString &GetPassword() {return m_Password;};
+
+  void SetPassword(mafString pwd) {m_Password = pwd;};
+
+  /** Return the status of the default passphrase usage.*/
+  bool UseDefaultPassPhrase() {return m_UseDefaultPasPhrase != 0;};
+
+  void SetUseDefaultPassPhrase(int use_default) {m_UseDefaultPasPhrase = use_default;};
+
+  /** Return the Passphrase used to encrypt/decrypt files.*/
+  mafString &GetPassPhrase() {return m_PassPhrase;};
+
+  void SetPassPhrase(mafString pass_phrase) {m_PassPhrase = pass_phrase;};
 
 protected:
   /** Initialize the application settings.*/
   void InitializeApplicationSettings();
+
+  /** Used to enable/disable items according to the current widgets state.*/
+  void EnableItems();
+
   mmgGui *m_Gui;
+  
+  // Log variables
   int     m_LogToFile;
   int     m_VerboseLog;
   mafString m_LogFolder;
+
+  // Remote storage variables
+  int       m_UseRemoteStorage;
+  int       m_AnonymousFalg;
+  int       m_Port;
   mafString m_CacheFolder;
+  mafString m_RemoteHostName;
+  mafString m_UserName;
+  mafString m_Password;
+  
+  // Encryption variables
+  int       m_UseDefaultPasPhrase;
+  mafString m_PassPhrase;
 
   mafObserver *m_Listener;
 };
