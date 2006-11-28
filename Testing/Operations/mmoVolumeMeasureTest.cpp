0765d19525249ef1901467154dfa2121099a98f9
ior04 <ior04>
2006-11-28 11:15:47 +0000

    add test class for volume measure

---
+++ Testing/Operations/mmoVolumeMeasureTest.cpp
@@ -0,0 +1,58 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: mmoVolumeMeasureTest.cpp,v $
+Language:  C++
+Date:      $Date: 2006-11-28 11:15:47 $
+Version:   $Revision: 1.1 $
+Authors:   Daniele Giunchi
+==========================================================================
+Copyright (c) 2002/2004 
+CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#include "mafDefines.h"
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include "mmoVolumeMeasureTest.h"
+#include "mmoVolumeMeasure.h"
+
+#include "mafString.h"
+#include "mafVMESurface.h"
+#include "vtkCubeSource.h"
+
+#include <string>
+#include <assert.h>
+
+//-----------------------------------------------------------
+void mmoVolumeMeasureTest::Test() 
+//-----------------------------------------------------------
+{
+  vtkCubeSource *cube = vtkCubeSource::New();
+  cube->SetXLength(10.0);
+  cube->SetYLength(20.0);
+  cube->SetZLength(30.0);
+  cube->Update();
+  	
+  mafVMESurface *vmeSurface;
+  mafNEW(vmeSurface);
+  vmeSurface->SetData(cube->GetOutput(), 0);
+  vmeSurface->Update();
+  	
+	mmoVolumeMeasure *volumeMeasureOp=new mmoVolumeMeasure("Volume Measure");
+	volumeMeasureOp->TestModeOn();
+  mafString result;
+  result = volumeMeasureOp->VolumeCompute(vmeSurface);
+	
+  //printf("%s", result);
+  CPPUNIT_ASSERT(result == mafString("6000"));
+
+  
+  mafDEL(vmeSurface);
+  vtkDEL(cube);
+  
+}
