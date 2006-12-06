9c3a976dc63286cc3fe0b2720a2c598231af2357
aqd0 <aqd0>
2006-12-06 09:44:47 +0000

    Specialized the OnEvent to reproduce the remote mouse behavior.

--- Interaction/mmdRemoteMouse.cpp
+++ Interaction/mmdRemoteMouse.cpp
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mmdRemoteMouse.cpp,v $
   Language:  C++
-  Date:      $Date: 2006-06-03 11:02:53 $
-  Version:   $Revision: 1.4 $
+  Date:      $Date: 2006-12-06 09:44:47 $
+  Version:   $Revision: 1.5 $
   Authors:   Marco Petrone
 ==========================================================================
   Copyright (c) 2002/2004 
@@ -32,12 +32,6 @@
 #include "vtkRenderWindowInteractor.h"
 
 //------------------------------------------------------------------------------
-// Events
-//------------------------------------------------------------------------------
-MAF_ID_IMP(mmdRemoteMouse::MOUSE_2D_MOVE)
-MAF_ID_IMP(mmdRemoteMouse::MOUSE_CHAR_EVENT)
-
-//------------------------------------------------------------------------------
 mafCxxTypeMacro(mmdRemoteMouse)
 //------------------------------------------------------------------------------
 
@@ -45,13 +39,7 @@ mafCxxTypeMacro(mmdRemoteMouse)
 mmdRemoteMouse::mmdRemoteMouse()
 //------------------------------------------------------------------------------
 {
-  SetThreaded(0);
-  m_LastPosition[0] = m_LastPosition[1] = 0;
-  m_ButtonState[0]  = m_ButtonState[1] = m_ButtonState[2] = 0;
-  m_SelectedView    = NULL;
-  m_SelectedRWI     = NULL;
 }
-
 //------------------------------------------------------------------------------
 mmdRemoteMouse::~mmdRemoteMouse()
 //------------------------------------------------------------------------------
@@ -64,92 +52,68 @@ void mmdRemoteMouse::OnEvent(mafEventBase *event)
   assert(event);
 
   mafID id = event->GetId();
-  mafEventInteraction *e = (mafEventInteraction *)event;
-  if (id == MOUSE_2D_MOVE)
+  mafEventInteraction *e = mafEventInteraction::SafeDownCast(event);
+
+  m_FromRemote = true;
+  double pos[2];
+  if (e)
   {
-    double pos[2];
     e->Get2DPosition(pos);
+  }
+
+  if (id == mmdMouse::MOUSE_2D_MOVE)
+  {
+    NormalizedDisplayToDisplay(pos);
     SetLastPosition(pos[0],pos[1],e->GetModifiers());
   }
   else if (id == BUTTON_DOWN)
   {
     // store the Selected RWI is needed for compounded view
-    e->Get2DPosition(m_LastPosition);
-    m_SelectedRWI = (mafRWIBase *)event->GetSender();
+    m_ButtonPressed = true;
+    if (m_SelectedView)
+    {
+      m_SelectedRWI = m_SelectedView->GetRWI();
+    }
+    NormalizedDisplayToDisplay(pos);
     e->SetSender(this);
+    m_LastPosition[0] = pos[0];
+    m_LastPosition[1] = pos[1];
+    e->Set2DPosition(m_LastPosition);
     InvokeEvent(e,MCH_INPUT);
   }
   else if (id == BUTTON_UP)
   {
-    e->Get2DPosition(m_LastPosition);
+    NormalizedDisplayToDisplay(pos);
+    m_LastPosition[0] = pos[0];
+    m_LastPosition[1] = pos[1];
+    e->Set2DPosition(m_LastPosition);
     e->SetSender(this);
     InvokeEvent(e,MCH_INPUT);
   }
   else if (id == VIEW_SELECT)
   {
-    mafEvent *ev=mafEvent::SafeDownCast(event);
-    m_SelectedView = ev->GetView();    
+    mafEvent *ev = mafEvent::SafeDownCast(event);
+    if (ev)
+    {
+      m_SelectedView = ev->GetView();
+    }
   }
-  else if (id == MOUSE_CHAR_EVENT)
+  else if (id == VIEW_DELETE)
   {
-    mafEvent *ev=mafEvent::SafeDownCast(event);
-    unsigned char key = (unsigned char)ev->GetArg();
-    mafEventInteraction ei(this,MOUSE_CHAR_EVENT);
-    ei.SetKey(key);
-    InvokeEvent(ei,MCH_INPUT);
+    m_SelectedView = NULL;
+    m_SelectedRWI  = NULL;
   }
-}
-
-//------------------------------------------------------------------------------
-void mmdRemoteMouse::SetLastPosition(double x,double y,unsigned long modifiers)
-//------------------------------------------------------------------------------
-{
-  m_LastPosition[0] = x;
-  m_LastPosition[1] = y;
-  
-  // create a new event with last position
-  mafEventInteraction e(this,MOUSE_2D_MOVE,x,y);
-  e.SetModifiers(modifiers);
-  InvokeEvent(e,MCH_INPUT);
-}
-
-//------------------------------------------------------------------------------
-void mmdRemoteMouse::SendButtonEvent(mafEventInteraction *event)
-//------------------------------------------------------------------------------
-{
-  event->Set2DPosition(GetLastPosition());
-  Superclass::SendButtonEvent(event);
-}
-//------------------------------------------------------------------------------
-vtkRenderer *mmdRemoteMouse::GetRenderer()
-//------------------------------------------------------------------------------
-{
-  vtkRenderer *r = NULL;
-  if (m_SelectedView)
+  else if (id == mmdMouse::MOUSE_CHAR_EVENT)
   {
-    r = m_SelectedRWI->FindPokedRenderer((int)m_LastPosition[0],(int)m_LastPosition[1]);
+    mafEvent *ev = mafEvent::SafeDownCast(event);
+    if (ev)
+    {
+      unsigned char key = (unsigned char)ev->GetArg();
+      mafEventInteraction ei(this, mmdMouse::MOUSE_CHAR_EVENT);
+      ei.SetKey(key);
+      InvokeEvent(ei,MCH_INPUT);
+    }
   }
 
-  return r;
-}
-//------------------------------------------------------------------------------
-mafView *mmdRemoteMouse::GetView()
-//------------------------------------------------------------------------------
-{
-  return m_SelectedView;
-}
-//------------------------------------------------------------------------------
-vtkRenderWindowInteractor *mmdRemoteMouse::GetInteractor()
-//------------------------------------------------------------------------------
-{
-  if (m_SelectedRWI)
-    return m_SelectedRWI->GetRenderWindow()->GetInteractor();
-
-  return (vtkRenderWindowInteractor *)NULL;
-}
-//------------------------------------------------------------------------------
-mafRWIBase *mmdRemoteMouse::GetRWI()
-//------------------------------------------------------------------------------
-{
-  return m_SelectedRWI;
+  m_FromRemote = false;
 }
