e286154f85a95be46a6a9b93a7400ce5f01969b2
aqd0 <aqd0>
2006-12-06 09:43:35 +0000

    Changed base class from mmdButtonPad to mmdMouse.

--- Interaction/mmdRemoteMouse.h
+++ Interaction/mmdRemoteMouse.h
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mmdRemoteMouse.h,v $
   Language:  C++
-  Date:      $Date: 2006-02-28 14:47:40 $
-  Version:   $Revision: 1.2 $
+  Date:      $Date: 2006-12-06 09:43:35 $
+  Version:   $Revision: 1.3 $
   Authors:   Marco Petrone
 ==========================================================================
   Copyright (c) 2002/2004 
@@ -13,7 +13,7 @@
 #ifndef __mmdRemoteMouse_h
 #define __mmdRemoteMouse_h
 
-#include "mmdButtonsPad.h"
+#include "mmdMouse.h"
 
 //----------------------------------------------------------------------------
 // forward declarations :
@@ -32,57 +32,17 @@ class mafRWIBase;
   widget or windows toolkit can inject events into this centralized mouse.
   @sa mmdButtonsPad mafRWI
 */
-class mmdRemoteMouse : public mmdButtonsPad
+class mmdRemoteMouse : public mmdMouse
 {
 public:
-  mafTypeMacro(mmdRemoteMouse,mmdButtonsPad); 
+  mafTypeMacro(mmdRemoteMouse, mmdMouse); 
 
-  //------------------------------------------------------------------------------
-  // Events
-  //------------------------------------------------------------------------------
-  /** @ingroup Events
-      Issued when a mouse is moved */
-  MAF_ID_DEC(MOUSE_2D_MOVE)
-  /** @ingroup Events
-      Issued when a keyboard char is pressed */
-  MAF_ID_DEC(MOUSE_CHAR_EVENT)
-    
   /**  Reimplemented to manage fusion of move events. */
   virtual void OnEvent(mafEventBase *event);
-
-  /**  Set the last mouse position and send a 2D MoveEvent. */
-  void SetLastPosition(double x,double y,unsigned long modifiers = 0);
-
-  /**  Return the last position */
-  const double *GetLastPosition() {return m_LastPosition;}
-  void GetLastPosition(double pos[2]) {pos[0]=m_LastPosition[0];pos[1]=m_LastPosition[1];}
-
-  /**
-  Return the renderer of the selected view. */
-  vtkRenderer *GetRenderer();
-
-  /** 
-  Return the selected view. */
-  mafView *GetView();
-
-  /** 
-  Return the Interactor. */
-  vtkRenderWindowInteractor *GetInteractor();
-
-  /** 
-  Return the RenderWindowInteractor used by mouse device */
-  mafRWIBase *GetRWI();
 
 protected:
   mmdRemoteMouse();
   virtual ~mmdRemoteMouse();
-
-  /** add position to the event */ 
-  virtual void SendButtonEvent(mafEventInteraction *event);
-
-  double       m_LastPosition[2];///< stores the last position
-  mafView     *m_SelectedView; ///< store the selected view to perform the mouse picking
-  mafRWIBase  *m_SelectedRWI;
 
 private:
   mmdRemoteMouse(const mmdRemoteMouse&);  // Not implemented.
