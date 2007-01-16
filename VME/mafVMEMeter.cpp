32de85be6a7dd30338d3378271179fafefb87e24
aqd0 <aqd0>
2007-01-16 12:38:38 +0000

    Commit for Giacomoni: Added Divider at the end of the interface.

--- VME/mafVMEMeter.cpp
+++ VME/mafVMEMeter.cpp
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mafVMEMeter.cpp,v $
   Language:  C++
-  Date:      $Date: 2006-12-14 12:10:55 $
-  Version:   $Revision: 1.26 $
+  Date:      $Date: 2007-01-16 12:38:38 $
+  Version:   $Revision: 1.27 $
   Authors:   Marco Petrone, Paolo Quadrani
 ==========================================================================
   Copyright (c) 2001/2005 
@@ -781,6 +781,7 @@ mmgGui* mafVMEMeter::CreateGui()
   if(GetMeterAttributes()->m_MeterMode == POINT_DISTANCE)
     m_Gui->Enable(ID_END2_METER_LINK,false);
 
+	m_Gui->Divider();
 	m_Gui->Divider();
 
   return m_Gui;
