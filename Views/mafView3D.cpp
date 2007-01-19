971a22c3878dae5bbd5e18a7ab5534906bb9081f
ior02 <ior02>
2007-01-19 15:26:20 +0000

    View3D

---
+++ Views/mafView3D.cpp
@@ -0,0 +1,209 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: mafView3D.cpp,v $
+  Language:  C++
+  Date:      $Date: 2007-01-19 15:26:20 $
+  Version:   $Revision: 1.1 $
+  Authors:   Matteo Giacomoni
+==========================================================================
+  Copyright (c) 2002/2004
+  CINECA - Interuniversity Consortium (www.cineca.it) 
+=========================================================================*/
+
+
+#include "mafDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include "mafView3D.h"
+#include "mafPipeFactory.h"
+#include "mafRWI.h"
+#include "mafSceneGraph.h"
+#include "mafAttachCamera.h"
+#include "mmgGui.h"
+#include "mafPipe.h"
+#include "mafPipeVolumeProjected.h"
+#include "mafVME.h"
+#include "mafVMEVolume.h"
+#include "mafVMELandmarkCloud.h"
+#include "mafVMELandmark.h"
+#include "mafVMESlicer.h"
+
+#include "vtkDataSet.h"
+#include "vtkRayCast3DPicker.h"
+#include "vtkCellPicker.h"
+
+enum ID_PIPE
+{
+	ID_PIPE_MIP = 0,
+	ID_PIPE_DRR,
+	ID_PIPE_VR
+};
+
+//----------------------------------------------------------------------------
+mafCxxTypeMacro(mafView3D);
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+mafView3D::mafView3D(wxString label, int camera_position, bool show_axes, bool show_grid, bool show_ruler, int stereo)
+:mafViewVTK(label,camera_position,show_axes,show_grid,show_ruler,stereo)
+//----------------------------------------------------------------------------
+{
+	m_Choose = ID_PIPE_MIP;
+}
+//----------------------------------------------------------------------------
+mafView3D::~mafView3D()
+//----------------------------------------------------------------------------
+{
+}
+//----------------------------------------------------------------------------
+mafView *mafView3D::Copy(mafObserver *Listener)
+//----------------------------------------------------------------------------
+{
+  mafView3D *v = new mafView3D(m_Label, m_CameraPosition, m_ShowAxes,m_ShowGrid, m_ShowRuler, m_StereoType);
+  v->m_Listener = Listener;
+  v->m_Id = m_Id;
+  v->m_PipeMap = m_PipeMap;
+  v->Create();
+  return v;
+}
+//----------------------------------------------------------------------------
+void mafView3D::OnEvent(mafEventBase *maf_event)
+//----------------------------------------------------------------------------
+{
+	if (mafEvent *e = mafEvent::SafeDownCast(maf_event))
+  {
+    switch(e->GetId()) 
+    {
+			case ID_COMBO_PIPE:
+				{
+					mafNode *selectedvme=this->GetSceneGraph()->GetSelectedVme();
+					if(((mafVME*)selectedvme)->GetVisualPipe())
+					{
+						this->VmeShow(selectedvme,false);
+						wxBusyCursor wait;
+						if(m_Choose == ID_PIPE_MIP)
+						{
+							this->PlugVisualPipe("mafVMEVolumeGray","medPipeVolumeMIP");
+						}
+						else if(m_Choose == ID_PIPE_DRR)
+							this->PlugVisualPipe("mafVMEVolumeGray","medPipeVolumeDRR");
+            else if(m_Choose == ID_PIPE_VR)
+              this->PlugVisualPipe("mafVMEVolumeGray","medPipeVolumeVR");
+						this->VmeShow(selectedvme,true);
+						mafEventMacro(mafEvent(this,VME_SELECT,selectedvme->GetParent()));
+            mafEventMacro(mafEvent(this,VME_SELECT,selectedvme));
+						//mafEventMacro(mafEvent(this,VME_SELECT,selectedvme,true));
+					}
+				}
+			break;
+      default:
+        mafEventMacro(*maf_event);
+      break;
+    }
+  }
+  else
+  {
+    mafEventMacro(*maf_event);
+  }
+}
+//----------------------------------------------------------------------------
+void mafView3D::Create()
+//----------------------------------------------------------------------------
+{
+  m_Rwi = new mafRWI(mafGetFrame(), TWO_LAYER, m_ShowGrid, m_ShowAxes, m_ShowRuler, m_StereoType);
+  m_Rwi->SetListener(this);
+  m_Rwi->CameraSet(m_CameraPosition);
+  m_Win = m_Rwi->m_RwiBase;
+
+  m_Sg  = new mafSceneGraph(this,m_Rwi->m_RenFront,m_Rwi->m_RenBack);
+  m_Sg->SetListener(this);
+  m_Rwi->m_Sg = m_Sg;
+
+  vtkNEW(m_Picker3D);
+  vtkNEW(m_Picker2D);
+  m_Picker2D->SetTolerance(0.005);
+  m_Picker2D->InitializePickList();
+
+	this->PlugVisualPipe("mafVMEVolumeGray","medPipeVolumeMIP");
+}
+/*//----------------------------------------------------------------------------
+void mafView3D::VmeCreatePipe(mafNode *vme)
+//----------------------------------------------------------------------------
+{
+  mafString pipe_name = "";
+  GetVisualPipeName(vme, pipe_name);
+
+  if (pipe_name != "")
+  {
+    m_NumberOfVisibleVme++;
+    mafPipeFactory *pipe_factory  = mafPipeFactory::GetInstance();
+    assert(pipe_factory!=NULL);
+    mafObject *obj = NULL;
+    obj = pipe_factory->CreateInstance(pipe_name);
+    mafPipe *pipe = (mafPipe*)obj;
+    if (pipe)
+    {
+      pipe->SetListener(this);
+      mafSceneNode *n = m_Sg->Vme2Node(vme);
+      assert(n && !n->m_Pipe);
+      pipe->Create(n);
+      n->m_Pipe = (mafPipe*)pipe;
+      if (m_NumberOfVisibleVme == 1)
+      {
+        CameraReset();
+      }
+      else
+      {
+        CameraUpdate();
+      }
+    }
+    else
+    {
+      mafErrorMessage(_("Cannot create visual pipe object of type \"%s\"!"),pipe_name.GetCStr());
+    }
+  }
+}
+//----------------------------------------------------------------------------
+void mafView3D::VmeDeletePipe(mafNode *vme)
+//----------------------------------------------------------------------------
+{
+  m_NumberOfVisibleVme--;
+  mafSceneNode *n = m_Sg->Vme2Node(vme);
+  assert(n && n->m_Pipe);
+  cppDEL(n->m_Pipe);
+}*/
+//-------------------------------------------------------------------------
+mmgGui *mafView3D::CreateGui()
+//-------------------------------------------------------------------------
+{
+  assert(m_Gui == NULL);
+  m_Gui = new mmgGui(this);
+	wxString choices[3] = {"MIP","DRR","VR"};
+	m_Gui->Combo(ID_COMBO_PIPE,_("Choose pipe"),&m_Choose,3,choices);
+	m_Gui->Divider();
+  return m_Gui;
+}
+//----------------------------------------------------------------------------
+void mafView3D::VmeSelect(mafNode *vme, bool select)
+//----------------------------------------------------------------------------
+{
+  assert(m_Sg); 
+  m_Sg->VmeSelect(vme,select);
+
+	if(m_Gui)
+	{
+		if(vme->IsA("mafVMEVolumeGray"))
+		{
+			m_Gui->Enable(ID_COMBO_PIPE,select);
+		}
+		else
+		{
+			m_Gui->Enable(ID_COMBO_PIPE,false);
+		}
+	}
+}
---
+++ Views/mafView3D.h
@@ -0,0 +1,72 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: mafView3D.h,v $
+  Language:  C++
+  Date:      $Date: 2007-01-19 15:26:20 $
+  Version:   $Revision: 1.1 $
+  Authors:   Matteo Giacomoni
+==========================================================================
+  Copyright (c) 2002/2004
+  CINECA - Interuniversity Consortium (www.cineca.it) 
+=========================================================================*/
+#ifndef __mafView3D_H__
+#define __mafView3D_H__
+
+//----------------------------------------------------------------------------
+// Include:
+//----------------------------------------------------------------------------
+#include "mafViewVTK.h"
+
+//----------------------------------------------------------------------------
+// forward references :
+//----------------------------------------------------------------------------
+class mafVME;
+class mafSceneNode;
+class mmgGui;
+
+//----------------------------------------------------------------------------
+// mafViewRX :
+//----------------------------------------------------------------------------
+/** 
+mafViewRX is a View that visualize volume as projection along x or y axis and 
+initialize the visual pipe according to the camera position that is passed through constructor
+\sa mafViewVTK
+*/
+class mafView3D: public mafViewVTK
+{
+public:
+  mafView3D(wxString label = "3D", int camera_position = CAMERA_RX_FRONT, bool show_axes = false, bool show_grid = false, bool show_ruler = false, int stereo = 0);
+  virtual ~mafView3D(); 
+
+  mafTypeMacro(mafView3D, mafViewVTK);
+
+  virtual mafView*  Copy(mafObserver *Listener);
+	virtual void			OnEvent(mafEventBase *maf_event);
+
+	void Create();
+
+
+  /** IDs for the GUI */
+  enum VIEW_RX_WIDGET_ID
+  {
+    ID_NONE = Superclass::ID_LAST,
+		ID_COMBO_PIPE,
+    ID_LAST
+  };
+
+	/** 
+  Create the visual pipe for the node passed as argument. 
+  To create visual pipe first check in m_PipeMap if custom visual pipe is defined, 
+  otherwise ask to vme which is its visual pipe. */
+  //virtual void VmeCreatePipe(mafNode *vme);
+
+	virtual void VmeSelect(mafNode *vme, bool select);
+
+protected:
+
+	virtual mmgGui *CreateGui();
+
+	int m_Choose;
+
+};
+#endif
