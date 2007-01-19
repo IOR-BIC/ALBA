cfcc27c4beaa77e456ceddd6394b305866fbd6aa
ior02 <ior02>
2007-01-19 15:24:32 +0000

    medVMEFactory

---
+++ VME/medVMEFactory.cpp
@@ -0,0 +1,104 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: medVMEFactory.cpp,v $
+  Language:  C++
+  Date:      $Date: 2007-01-19 15:24:32 $
+  Version:   $Revision: 1.2 $
+  Authors:   Daniele Giunchi
+==========================================================================
+  Copyright (c) 2001/2005 
+  CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#include "mafDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include "medVMEFactory.h"
+
+#include "mafVersion.h"
+
+#include "mafVMEAdvancedProber.h"
+
+
+#ifdef MAF_USE_ITK
+  
+#endif
+
+
+#include "mafIndent.h"
+#include <string>
+#include <ostream>
+
+medVMEFactory *medVMEFactory::m_Instance=NULL;
+
+mafCxxTypeMacro(medVMEFactory);
+
+//----------------------------------------------------------------------------
+// This is used to register the factory when linking statically
+int medVMEFactory::Initialize()
+//----------------------------------------------------------------------------
+{
+  if (m_Instance==NULL)
+  {
+    m_Instance=medVMEFactory::New();
+
+    if (m_Instance)
+    {
+      m_Instance->RegisterFactory(m_Instance);
+      return MAF_OK;  
+    }
+    else
+    {
+      return MAF_ERROR;
+    }
+  }
+  
+  return MAF_OK;
+}
+
+//------------------------------------------------------------------------
+medVMEFactory::medVMEFactory()
+//------------------------------------------------------------------------------
+{
+  //
+  // Plug here Nodes in this factory
+  //
+
+#ifdef MAF_USE_ITK
+  
+#endif
+  //mafPlugObjectMacro(mmaObject,"Object attributes");
+
+  mafPlugNodeMacro(mafVMEAdvancedProber,"VME for Advanced Probing");
+
+#ifdef MAF_USE_ITK
+  
+#endif
+  //mafPlugNodeMacro(mafVMEthing,"VME representing a thing");
+}
+
+//------------------------------------------------------------------------------
+const char* medVMEFactory::GetMAFSourceVersion() const
+//------------------------------------------------------------------------------
+{
+  return MAF_SOURCE_VERSION;
+}
+
+//------------------------------------------------------------------------------
+const char* medVMEFactory::GetDescription() const
+//------------------------------------------------------------------------------
+{
+  return "Factory for MAF VMEs";
+}
+
+//------------------------------------------------------------------------------
+mafVME *medVMEFactory::CreateVMEInstance(const char *type_name)
+//------------------------------------------------------------------------------
+{
+  return mafVME::SafeDownCast(Superclass::CreateInstance(type_name));
+}
---
+++ VME/medVMEFactory.h
@@ -0,0 +1,50 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: medVMEFactory.h,v $
+  Language:  C++
+  Date:      $Date: 2007-01-19 15:24:32 $
+  Version:   $Revision: 1.2 $
+  Authors:   Daniele Giunchi
+==========================================================================
+  Copyright (c) 2001/2005 
+  CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+#ifndef __medVMEFactory_h
+#define __medVMEFactory_h
+//----------------------------------------------------------------------------
+// includes :
+//----------------------------------------------------------------------------
+#include "mafVMEFactory.h"  
+#include "mafVME.h"
+
+/** Object factory for VMEs.
+  This is a simple specialization of mafNodeFactory. Notice that medVMEFactory is also
+  a mafNodeFactory this last does not need to be crated: you should create one or the other but
+  not both since the Instance would be overwritten and thus the Plug*<> methods would always
+  plug inside one of the two. */
+class MAF_EXPORT medVMEFactory : public mafVMEFactory
+{
+public: 
+  mafTypeMacro(medVMEFactory,mafVMEFactory);
+  virtual const char* GetMAFSourceVersion() const;
+  virtual const char* GetDescription() const;
+
+  static medVMEFactory *GetInstance() {if (!m_Instance) Initialize(); return m_Instance;}
+
+  /* Initialize the factory creating and registering a new instance */
+  static int Initialize();
+
+  mafVME *CreateVMEInstance(const char *type_name);
+  
+protected:
+  medVMEFactory();
+  ~medVMEFactory() { }
+
+	static medVMEFactory *m_Instance;
+  
+private:
+  medVMEFactory(const medVMEFactory&);  // Not implemented.
+  void operator=(const medVMEFactory&);  // Not implemented.
+};
+
+#endif
