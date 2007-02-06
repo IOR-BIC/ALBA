ee9d444946240fcf4539a7bca8e953035cdc5583
ior02 <ior02>
2007-02-06 10:17:41 +0000

    Minor Changes

--- Views/mafViewSingleSliceCompound.cpp
+++ Views/mafViewSingleSliceCompound.cpp
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mafViewSingleSliceCompound.cpp,v $
   Language:  C++
-  Date:      $Date: 2007-01-28 09:56:03 $
-  Version:   $Revision: 1.6 $
+  Date:      $Date: 2007-02-06 10:17:41 $
+  Version:   $Revision: 1.7 $
   Authors:   Daniele Giunchi
 ==========================================================================
   Copyright (c) 2002/2004
@@ -149,7 +149,7 @@ void mafViewSingleSliceCompound::PackageView()
 {
 	m_ViewSingleSlice = new mafViewSingleSlice("",CAMERA_CT);
 	m_ViewSingleSlice->PlugVisualPipe("mafVMESurface", "mafPipeSurfaceSlice");
-	m_ViewSingleSlice->PlugVisualPipe("mafVMEPolyline", "mafPipePolylineSlice");
+	//m_ViewSingleSlice->PlugVisualPipe("mafVMEPolyline", "mafPipePolylineSlice");
   m_ViewSingleSlice->PlugVisualPipe("mafVMESurfaceParametric", "mafPipeSurfaceSlice");
 	m_ViewSingleSlice->PlugVisualPipe("mafVMEVolumeGray", "mafPipeVolumeSlice");
 	
