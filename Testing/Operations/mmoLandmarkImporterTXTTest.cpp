0431059e06389cc0f8b27d0d01fd0ecbda84640a
mucci <mucci>
2007-03-05 16:43:59 +0000

    Test class for mmoLandmarkImporterTXT

---
+++ Testing/Operations/mmoLandmarkImporterTXTTest.cpp
@@ -0,0 +1,97 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: mmoLandmarkImporterTXTTest.cpp,v $
+Language:  C++
+Date:      $Date: 2007-03-05 16:43:59 $
+Version:   $Revision: 1.1 $
+Authors:   Roberto Mucci
+==========================================================================
+Copyright (c) 2002/2004 
+CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#include "mafDefines.h"
+#include "medDefines.h"
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include "mmoLandmarkImporterTXTTest.h"
+#include "mmoLandmarkImporterTXT.h"
+
+
+#include "mafString.h"
+#include "mafVMELandmarkCloud.h"
+#include "mafVMELandmark.h"
+#include "mafVMERoot.h"
+#include "mafVMEStorage.h"
+
+#include <string>
+#include <assert.h>
+
+#include <iostream>
+
+//-----------------------------------------------------------
+void mmoLandmarkImporterTXTTest::TestTXTImporter() 
+//-----------------------------------------------------------
+{
+	mmoLandmarkImporterTXT *importer=new mmoLandmarkImporterTXT("importer");
+	importer->TestModeOn();
+	mafString filename=MED_DATA_ROOT;
+  filename<<"/AL_File/cammino03.txt";
+	importer->SetFileName(filename.GetCStr());
+  importer->SetSkipColumn(1);
+	importer->Read();
+  mafVMELandmarkCloud *node=(mafVMELandmarkCloud *)importer->GetOutput();
+  
+  CPPUNIT_ASSERT(node->GetNumberOfLandmarks() == 18);
+  cppDEL(importer);
+}
+//-----------------------------------------------------------
+void mmoLandmarkImporterTXTTest::TestVisibility() 
+//-----------------------------------------------------------
+{
+  mmoLandmarkImporterTXT *importer=new mmoLandmarkImporterTXT("importer");
+  importer->TestModeOn();
+  mafString filename=MED_DATA_ROOT;
+  filename<<"/AL_File/cammino03.txt";
+  importer->SetFileName(filename.GetCStr());
+  importer->SetSkipColumn(1);
+  importer->Read();
+  mafVMELandmarkCloud *node=(mafVMELandmarkCloud *)importer->GetOutput();
+
+  bool visible = node->GetLandmarkVisibility(0,1.98);
+
+  CPPUNIT_ASSERT(visible == false);
+  cppDEL(importer);
+}
+
+//-----------------------------------------------------------
+void mmoLandmarkImporterTXTTest::TestCoordinates() 
+//-----------------------------------------------------------
+{
+  mmoLandmarkImporterTXT *importer=new mmoLandmarkImporterTXT("importer");
+  importer->TestModeOn();
+  mafString filename=MED_DATA_ROOT;
+  filename<<"/AL_File/cammino03.txt";
+  importer->SetFileName(filename.GetCStr());
+  importer->SetSkipColumn(1);
+  importer->Read();
+  mafVMELandmarkCloud *node=(mafVMELandmarkCloud *)importer->GetOutput();
+
+  double xyz[3], rot[3];
+  mafVMELandmark *lm = node->GetLandmark(0);
+  lm->GetOutput()->GetPose(xyz,rot,1.99);
+
+  CPPUNIT_ASSERT( fabs(xyz[0] - 144.13) < 0.01 &&
+    fabs(xyz[1] - -1103.8) < 0.01 &&
+    fabs(xyz[2] - 1547.2) < 0.01 );
+
+  //CPPUNIT_ASSERT( mafEquals(xyz[0],144.13) && mafEquals(xyz[1],-1103.8) && mafEquals(xyz[2],1547.2));
+  cppDEL(importer);
+}
+
+
---
+++ Testing/Operations/mmoLandmarkImporterTXTTest.h
@@ -0,0 +1,69 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: mmoLandmarkImporterTXTTest.h,v $
+Language:  C++
+Date:      $Date: 2007-03-05 16:43:59 $
+Version:   $Revision: 1.1 $
+Authors:   Roberto Mucci
+==========================================================================
+Copyright (c) 2002/2004 
+CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#ifndef CPP_UNIT_mmoLandmarkImporterTXTTEST_H
+#define CPP_UNIT_mmoLandmarkImporterTXTTEST_H
+
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
+
+
+class mmoLandmarkImporterTXTTest : public CPPUNIT_NS::TestFixture
+{
+  CPPUNIT_TEST_SUITE( mmoLandmarkImporterTXTTest );
+  CPPUNIT_TEST( TestTXTImporter );
+  CPPUNIT_TEST( TestVisibility );
+  CPPUNIT_TEST( TestCoordinates );
+ 
+  CPPUNIT_TEST_SUITE_END();
+
+  protected:
+    void TestTXTImporter();
+    void TestVisibility();
+    void TestCoordinates();
+};
+
+
+int 
+main( int argc, char* argv[] )
+{
+  // Create the event manager and test controller
+  CPPUNIT_NS::TestResult controller;
+
+  // Add a listener that colllects test result
+  CPPUNIT_NS::TestResultCollector result;
+  controller.addListener( &result );        
+
+  // Add a listener that print dots as test run.
+  CPPUNIT_NS::BriefTestProgressListener progress;
+  controller.addListener( &progress );      
+
+  // Add the top suite to the test runner
+  CPPUNIT_NS::TestRunner runner;
+  runner.addTest( mmoLandmarkImporterTXTTest::suite());
+  runner.run( controller );
+
+  // Print test in a compiler compatible format.
+  CPPUNIT_NS::CompilerOutputter outputter( &result, CPPUNIT_NS::stdCOut() );
+  outputter.write(); 
+
+  return result.wasSuccessful() ? 0 : 1;
+}
+
+
+
+#endif
