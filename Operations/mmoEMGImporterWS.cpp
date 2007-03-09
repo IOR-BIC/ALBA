848da30edf3f2e4d8269ddaa383072632e6f481f
mucci <mucci>
2007-03-09 11:32:52 +0000

    Importer for EMG data on WS files

---
+++ Operations/mmoEMGImporterWS.cpp
@@ -0,0 +1,194 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: mmoEMGImporterWS.cpp,v $
+  Language:  C++
+  Date:      $Date: 2007-03-09 11:32:52 $
+  Version:   $Revision: 1.1 $
+  Authors:   Roberto Mucci
+==========================================================================
+  Copyright (c) 2001/2005 
+  CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#include "mafDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include <wx/busyinfo.h>
+#include <wx/txtstrm.h>
+#include <wx/tokenzr.h>
+#include <wx/wfstream.h>
+
+#include "mmoEMGImporterWS.h"
+
+#include "mafDecl.h"
+#include "mafEvent.h"
+#include "mmgGui.h"
+
+#include "mafVME.h"
+#include "mafVMEScalar.h"
+#include "mafTagArray.h"
+#include "mafSmartPointer.h"
+
+
+
+#include <iostream>
+#include <fstream>
+
+//----------------------------------------------------------------------------
+// Constants :
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+mmoEMGImporterWS::mmoEMGImporterWS(wxString label) :
+mafOp(label)
+//----------------------------------------------------------------------------
+{
+	m_OpType	= OPTYPE_IMPORTER;
+	m_Canundo	= true;
+	m_File		= "";
+	m_FileDir = (mafGetApplicationDirectory() + "/Data/External/").c_str();
+
+  m_EmgScalar = NULL;
+  m_Start = 0;
+  m_Stop = 0;
+}
+//----------------------------------------------------------------------------
+mmoEMGImporterWS::~mmoEMGImporterWS()
+//----------------------------------------------------------------------------
+{
+  mafDEL(m_EmgScalar);
+}
+//----------------------------------------------------------------------------
+mafOp* mmoEMGImporterWS::Copy()   
+//----------------------------------------------------------------------------
+{
+	mmoEMGImporterWS *cp = new mmoEMGImporterWS(m_Label);
+	cp->m_Canundo = m_Canundo;
+	cp->m_OpType = m_OpType;
+	cp->m_Listener = m_Listener;
+
+	cp->m_File = m_File;
+	return cp;
+}
+//----------------------------------------------------------------------------
+void mmoEMGImporterWS::OpRun()   
+//----------------------------------------------------------------------------
+{
+	int result = OP_RUN_CANCEL;
+	m_File = "";
+	wxString pgd_wildc	= "EMG File (*.*)|*.*";
+  wxString f;
+  f = mafGetOpenFile(m_FileDir,pgd_wildc).c_str(); 
+	if(!f.IsEmpty() && wxFileExists(f))
+	{
+	  m_File = f;
+/*
+    m_Gui = new mmgGui(this);
+    m_Gui->Integer(ID_TYPE_FILE,"From Col",&m_Start,0,MAXINT,"Select the first column");
+    m_Gui->Integer(ID_TYPE_FILE,"To Col",&m_Stop,m_Start,MAXINT,"Select the last column");
+    m_Gui->OkCancel();
+    ShowGui();
+    */
+    Read();
+    result = OP_RUN_OK;
+  }
+  mafEventMacro(mafEvent(this,result));
+}
+//----------------------------------------------------------------------------
+void mmoEMGImporterWS::	OnEvent(mafEventBase *maf_event) 
+//----------------------------------------------------------------------------
+{
+  if (mafEvent *e = mafEvent::SafeDownCast(maf_event))
+  {
+    switch(e->GetId())
+    {
+      default:
+        mafEventMacro(*e);
+    }
+  }
+}
+//----------------------------------------------------------------------------
+void mmoEMGImporterWS::SetSkipColumn(int column)
+//----------------------------------------------------------------------------
+{
+  m_Start = column;
+}
+//----------------------------------------------------------------------------
+void mmoEMGImporterWS::Read()   
+//----------------------------------------------------------------------------
+{
+  if (!m_TestMode)
+    wxBusyInfo wait("Please wait, working...");
+  
+  mafNEW(m_EmgScalar);
+  wxString path, name, ext;
+  wxSplitPath(m_File.c_str(),&path,&name,&ext);
+  m_EmgScalar->SetName(name);
+
+  mafTagItem tag_Nature;
+  tag_Nature.SetName("VME_NATURE");
+  tag_Nature.SetValue("NATURAL");
+
+  mafString time, scalar;
+   
+  wxFileInputStream inputFile( m_File );
+  wxTextInputStream text( inputFile );
+
+  double emg_time; 
+  double val_scalar;
+  wxString line;
+
+  line = text.ReadLine(); 
+  line = text.ReadLine();
+  int comma = line.Find(',');
+  wxString freq = line.SubString(0,comma - 1); //Read frequency 
+  double freq_val;
+  freq_val = atof(freq.c_str());
+  
+  line = text.ReadLine();
+  line = text.ReadLine();
+  line = text.ReadLine();
+  line.Replace(","," ");
+
+  int space, num_tk;
+  unsigned i;
+  wxString frame;
+
+  do 
+  {
+    space = line.Find(' ');
+    frame = line.SubString(0,space - 1);
+    emg_time = atof(frame)/freq_val; 
+    wxStringTokenizer tkz(line,wxT(' '),wxTOKEN_RET_EMPTY_ALL);
+
+    tkz.GetNextToken(); //To skip the time value
+    num_tk = tkz.CountTokens();
+    i = 0;
+    
+    while (tkz.HasMoreTokens())
+    {
+      scalar = tkz.GetNextToken();
+      val_scalar = atof(scalar);
+      m_Emg_matrix.set_size(1,num_tk - 1);
+
+      m_Emg_matrix.put(0,i,val_scalar); //Add scalar value to the vnl_matrix
+
+      i++;     
+    }
+
+    m_EmgScalar->SetData(m_Emg_matrix, emg_time);
+
+    line = text.ReadLine();
+    line.Replace(","," ");
+
+  } while (!inputFile.Eof());
+
+  m_Output = m_EmgScalar;
+}
---
+++ Operations/mmoEMGImporterWS.h
@@ -0,0 +1,69 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: mmoEMGImporterWS.h,v $
+  Language:  C++
+  Date:      $Date: 2007-03-09 11:32:52 $
+  Version:   $Revision: 1.1 $
+  Authors:   Roberto Mucci
+==========================================================================
+Copyright (c) 2002/2004
+CINECA - Interuniversity Consortium (www.cineca.it) 
+=========================================================================*/
+
+#ifndef __mmoEMGImporterWS_H__
+#define __mmoEMGImporterWS_H__
+
+//----------------------------------------------------------------------------
+// Include :
+//----------------------------------------------------------------------------
+#include "mafOp.h"
+#include <vnl\vnl_matrix.h>
+
+//----------------------------------------------------------------------------
+// forward references :
+//----------------------------------------------------------------------------
+class mafEvent;
+class mafVMEScalar;
+
+//----------------------------------------------------------------------------
+// mmoEMGImporterWS :
+//----------------------------------------------------------------------------
+/** */
+class mmoEMGImporterWS : public mafOp
+{
+public:
+	mmoEMGImporterWS(wxString label);
+	~mmoEMGImporterWS(); 
+	mafOp* Copy();
+  virtual void OnEvent(mafEventBase *maf_event);
+
+	/** Return true for the acceptable vme type. */
+	bool Accept(mafNode* node) {return true;};
+
+	/** Builds operation's interface. */
+	void OpRun();
+
+  /** Read the file.
+  the format of the file admits some specifics.
+  1) Ignore the first line
+  2) Read the second line containing the Frequency
+  2) Ignore 2 lines
+  3) the first element of each line is Time, then n scalar values
+  4) Put the values in a mafVMEScalar item 
+  */
+  void Read();
+
+  /** Set the filename for the file to import */
+  void SetFileName(const char *file_name){m_File = file_name;};
+
+  /** Set number of column to skip */
+  void SetSkipColumn(int column);
+
+protected:
+  mafVMEScalar *m_EmgScalar;
+  vnl_matrix<double> m_Emg_matrix;
+  wxString m_FileDir;
+	wxString m_File;
+  int m_Start, m_Stop;
+};
+#endif
