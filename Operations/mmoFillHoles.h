f5e8f901877aba846113f174a65214a53ad182b4
ior02 <ior02>
2007-05-14 09:50:34 +0000

    FillHoles

---
+++ Operations/mmoFillHoles.h
@@ -0,0 +1,112 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: mmoFillHoles.h,v $
+Language:  C++
+Date:      $Date: 2007-05-14 09:50:34 $
+Version:   $Revision: 1.1 $
+Authors:   Matteo Giacomoni
+==========================================================================
+Copyright (c) 2002/2004
+CINECA - Interuniversity Consortium (www.cineca.it) 
+SCS s.r.l. - BioComputing Competence Centre (www.scsolutions.it - www.b3c.it)
+
+MafMedical Library use license agreement
+
+The software named MafMedical Library and any accompanying documentation, 
+manuals or data (hereafter collectively "SOFTWARE") is property of the SCS s.r.l.
+This is an open-source copyright as follows:
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+* Modified source versions must be plainly marked as such, and must not be misrepresented 
+as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR 
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+MafMedical is partially based on OpenMAF.
+=========================================================================*/
+
+#ifndef __mmoFillHoles_H__
+#define __mmoFillHoles_H__
+
+#include "mafOp.h"
+
+//----------------------------------------------------------------------------
+// forward references :
+//----------------------------------------------------------------------------
+
+class mmgDialog;
+class mafRWI;
+class mmiSelectPoint;
+class vtkFeatureEdges;
+class vtkHoleConnectivity;
+class vtkPolyDataMapper;
+class vtkActor;
+class vtkAppendPolyData;
+
+//----------------------------------------------------------------------------
+// mmoFillHoles :
+//----------------------------------------------------------------------------
+/** */
+class mmoFillHoles: public mafOp
+{
+public:
+	mmoFillHoles(const wxString &label = "Fill Holes");
+	~mmoFillHoles(); 
+
+	virtual void OnEvent(mafEventBase *maf_event);
+
+	mafTypeMacro(mmoFillHoles, mafOp);
+
+	mafOp* Copy();
+
+	/** Return true for the acceptable vme type. */
+	bool Accept(mafNode *node);
+
+	/** Builds operation's interface. */
+	void OpRun();
+
+	/** Execute the operation. */
+	void OpDo();
+
+	/** Makes the undo for the operation. */
+	void OpUndo();
+
+protected:
+	/** This method is called at the end of the operation and result contain the wxOK or wxCANCEL. */
+	void OpStop(int result);
+
+	void CreateOpDialog();
+	void DeleteOpDialog();
+	void CreatePolydataPipeline();
+
+	mmgDialog								*m_Dialog;
+	mafRWI									*m_Rwi;
+	mmiSelectPoint					*m_Picker;
+	vtkFeatureEdges					*m_FreeEdge;
+	vtkHoleConnectivity			*m_HoleConnectivity;
+	vtkPolyDataMapper				*m_MapperHoles;
+	vtkActor								*m_ActorHoles;
+
+	vtkPolyDataMapper				*m_MapperSurface;
+	vtkActor								*m_ActorSurface;
+
+	vtkAppendPolyData				*m_AppendPolydata;
+
+	double	m_MaxBound;
+
+};
+#endif
