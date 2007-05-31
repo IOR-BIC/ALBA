dc0669d48ad190e7f4b3c84c113e3d0a708a68c8
ior02 <ior02>
2007-05-31 08:14:50 +0000

    medLogicWithManagers

---
+++ Common/medLogicWithManagers.cpp
@@ -0,0 +1,87 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: medLogicWithManagers.cpp,v $
+Language:  C++
+Date:      $Date: 2007-05-31 08:14:50 $
+Version:   $Revision: 1.1 $
+Authors:   Matteo Giacomoni
+==========================================================================
+Copyright (c) 2002/2004
+CINECA - Interuniversity Consortium (www.cineca.it) 
+SCS s.r.l. - BioComputing Competence Centre (www.scsolutions.it - www.b3c.it)
+
+MafMedical Library use license agreement
+
+The software named MafMedical Library and any accompanying documentation, 
+manuals or data (hereafter collectively "SOFTWARE") is property of the SCS s.r.l.
+This is an open-source copyright as follows:
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+* Modified source versions must be plainly marked as such, and must not be misrepresented 
+as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR 
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+MafMedical is partially based on OpenMAF.
+=========================================================================*/
+
+#include "mafDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+
+#include "medLogicWithManagers.h"
+#include "mafDecl.h"
+
+//----------------------------------------------------------------------------
+medLogicWithManagers::medLogicWithManagers()
+: mafLogicWithManagers()
+//----------------------------------------------------------------------------
+{
+}
+//----------------------------------------------------------------------------
+medLogicWithManagers::~medLogicWithManagers()
+//----------------------------------------------------------------------------
+{
+}
+//----------------------------------------------------------------------------
+void medLogicWithManagers::OnEvent(mafEventBase *maf_event)
+//----------------------------------------------------------------------------
+{
+	if (mafEvent *e = mafEvent::SafeDownCast(maf_event))
+	{
+		switch(e->GetId())
+		{
+		case ABOUT_APPLICATION:
+			{
+				wxString message = m_AppTitle.GetCStr();
+				message += _(" Application ");
+				message += m_Revision;
+				wxMessageBox(message, "About Application");
+				mafLogMessage(wxString::Format("%s",m_Revision.GetCStr()));
+			}
+			break;
+		default:
+			mafLogicWithManagers::OnEvent(maf_event);
+			break; 
+		} // end switch case
+	} // end if SafeDowncast
+}
