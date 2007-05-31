8f1c960ed4287a1f217576a2d8245eec6c67930c
ior02 <ior02>
2007-05-31 08:14:01 +0000

    mmoExtrusionHoles

---
+++ Operations/mmoExtrusionHoles.cpp
@@ -0,0 +1,473 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: mmoExtrusionHoles.cpp,v $
+Language:  C++
+Date:      $Date: 2007-05-31 08:14:01 $
+Version:   $Revision: 1.1 $
+Authors:   Matteo Giacomoni
+==========================================================================
+Copyright (c) 2002/2004
+CINECA - Interuniversity Consortium (www.cineca.it) 
+SCS s.r.l. - BioComputing Competence Centre (www.scsolutions.it - www.b3c.it)
+
+MafMedical Library use license agreement
+
+The software named MafMedical Library and any accompanying documentation, 
+manuals or data (hereafter collectively "SOFTWARE") is property of the SCS s.r.l.
+This is an open-source copyright as follows:
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+* Modified source versions must be plainly marked as such, and must not be misrepresented 
+as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR 
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+MafMedical is partially based on OpenMAF.
+=========================================================================*/
+#include "mafDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include "mmoExtrusionHoles.h"
+#include "mafNode.h"
+
+#include "mmgDialog.h"
+#include "mmgGui.h"
+#include "mmgButton.h"
+#include "mmdMouse.h"
+#include "mmgValidator.h"
+#include "mafRWI.h"
+#include "mmiSelectPoint.h"
+#include "mafVMESurface.h"
+
+#include "vtkRenderWindow.h"
+#include "vtkFeatureEdges.h"
+#include "vtkHoleConnectivity.h"
+#include "vtkPolyData.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkActor.h"
+#include "vtkRenderer.h"
+#include "vtkSphereSource.h"
+#include "vtkGlyph3D.h"
+#include "vtkProperty.h"
+#include "vtkTextureMapToPlane.h"
+#include "vtkLinearExtrusionFilter.h"
+#include "vtkCleanPolyData.h"
+
+//----------------------------------------------------------------------------
+mafCxxTypeMacro(mmoExtrusionHoles);
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+mmoExtrusionHoles::mmoExtrusionHoles(const wxString &label) :
+mafOp(label)
+//----------------------------------------------------------------------------
+{
+	m_Canundo = true;
+	
+	m_ExtractFreeEdges	= NULL;
+	m_ExtractHole				= NULL;
+	m_ExtrusionFilter		= NULL;
+
+	m_SurfaceMapper			= NULL;
+	m_SurfaceActor			= NULL;
+	m_HolesMapper				= NULL;
+	m_HolesActor				= NULL;
+	m_SelectedHoleMapper= NULL;
+	m_SelectedHoleActor	= NULL;
+
+	m_Sphere						= NULL;
+	m_Glyph							= NULL;
+	m_ResultAfterExtrusion	= NULL;
+
+	m_OriginalPolydata	= NULL;
+	m_ResultPolydata		= NULL;
+
+	m_ExtrusionFactor		= 0.0;
+}
+//----------------------------------------------------------------------------
+mmoExtrusionHoles::~mmoExtrusionHoles()
+//----------------------------------------------------------------------------
+{
+	vtkDEL(m_ExtrusionFilter);
+	vtkDEL(m_Sphere);
+	vtkDEL(m_ExtractHole);
+	vtkDEL(m_ExtractFreeEdges);
+	vtkDEL(m_ResultAfterExtrusion);
+	vtkDEL(m_ResultPolydata);
+	vtkDEL(m_OriginalPolydata);
+}
+//----------------------------------------------------------------------------
+bool mmoExtrusionHoles::Accept(mafNode *node)
+//----------------------------------------------------------------------------
+{
+	return (node && node->IsA("mafVMESurface"));
+}
+//----------------------------------------------------------------------------
+mafOp *mmoExtrusionHoles::Copy()   
+//----------------------------------------------------------------------------
+{
+	return (new mmoExtrusionHoles(m_Label));
+}
+//----------------------------------------------------------------------------
+// Constants:
+//----------------------------------------------------------------------------
+enum FILTER_SURFACE_ID
+{
+	ID_OK = MINID,
+	ID_CANCEL,
+	ID_EXTRUSION_FACTOR,
+	ID_EXTRUDE,
+};
+//----------------------------------------------------------------------------
+void mmoExtrusionHoles::OpRun()   
+//----------------------------------------------------------------------------
+{
+	vtkNEW(m_OriginalPolydata);
+	vtkNEW(m_ResultPolydata);
+	vtkNEW(m_ResultAfterExtrusion);
+	vtkNEW(m_ExtractFreeEdges);
+	vtkNEW(m_ExtractHole);
+	vtkNEW(m_ExtrusionFilter);
+
+	m_OriginalPolydata->DeepCopy(((vtkPolyData*)((mafVMESurface*)m_Input)->GetOutput()->GetVTKData()));
+	m_OriginalPolydata->Update();
+	m_ResultPolydata->DeepCopy(((vtkPolyData*)((mafVMESurface*)m_Input)->GetOutput()->GetVTKData()));
+	m_ResultPolydata->Update();
+
+	if(!m_TestMode)
+	{
+		CreateOpDialog();
+
+		int ret_dlg = m_Dialog->ShowModal();
+		int result = OP_RUN_CANCEL;
+		if( ret_dlg == wxID_OK )
+		{
+			result = OP_RUN_OK;
+		}
+
+		DeleteOpDialog();
+
+		OpStop(result);
+	}
+}
+//----------------------------------------------------------------------------
+void mmoExtrusionHoles::OpDo()
+//----------------------------------------------------------------------------
+{
+	if(m_ResultPolydata)
+		((mafVMESurface*)m_Input)->SetData(m_ResultPolydata,((mafVME*)m_Input)->GetTimeStamp());
+
+	mafEventMacro(mafEvent(this,CAMERA_UPDATE));
+}
+//----------------------------------------------------------------------------
+void mmoExtrusionHoles::OpUndo()
+//----------------------------------------------------------------------------
+{
+	if(m_OriginalPolydata)
+		((mafVMESurface*)m_Input)->SetData(m_OriginalPolydata,((mafVME*)m_Input)->GetTimeStamp());
+
+	mafEventMacro(mafEvent(this,CAMERA_UPDATE));
+}
+//----------------------------------------------------------------------------
+void mmoExtrusionHoles::OnEvent(mafEventBase *maf_event)
+//----------------------------------------------------------------------------
+{
+	if (mafEvent *e = mafEvent::SafeDownCast(maf_event))
+	{
+		switch(e->GetId())
+		{
+		case VME_PICKED:
+			{
+				//Get the picked point
+				int pointID = e->GetArg();
+				if(pointID>=0)
+				{
+					SelectHole(pointID);
+					Extrude();
+				}
+			}
+			break;
+		case ID_EXTRUSION_FACTOR:
+			{
+				Extrude();
+			}
+			break;
+		case ID_EXTRUDE:
+			{
+				SaveExtrusion();
+			}
+			break;
+		case ID_OK:
+			m_Dialog->EndModal(wxID_OK);
+			break;
+		case ID_CANCEL:
+			m_Dialog->EndModal(wxID_CANCEL);
+			break;
+		default:
+			mafEventMacro(*e);
+		}
+	}
+}
+//----------------------------------------------------------------------------
+void mmoExtrusionHoles::SaveExtrusion()
+//----------------------------------------------------------------------------
+{
+	vtkCleanPolyData  *clean;
+	vtkNEW(clean);
+	clean->SetInput(m_ResultAfterExtrusion->GetOutput());
+	clean->Update();
+	m_ResultPolydata->DeepCopy(clean->GetOutput());
+	m_ResultPolydata->Update();
+	if(!m_TestMode)
+	{
+		m_SurfaceMapper->SetInput(m_ResultPolydata);
+		m_SurfaceMapper->Update();
+	}
+	if(!m_TestMode)
+	{
+		m_Rwi->m_RenFront->RemoveActor(m_SelectedHoleActor);
+		m_Rwi->CameraUpdate();
+	}
+	vtkDEL(clean);
+}
+//----------------------------------------------------------------------------
+void mmoExtrusionHoles::Extrude()
+//----------------------------------------------------------------------------
+{
+	vtkTextureMapToPlane *computeMedianPlane;
+	vtkNEW(computeMedianPlane);
+	computeMedianPlane->SetInput(m_ExtractHole->GetOutput());
+	computeMedianPlane->AutomaticPlaneGenerationOn();
+	computeMedianPlane->Update();
+	double normal[3];
+	computeMedianPlane->GetNormal(normal);
+
+	m_ExtrusionFilter->SetExtrusionTypeToVectorExtrusion();
+	m_ExtrusionFilter->SetVector(normal);
+	m_ExtrusionFilter->SetScaleFactor(m_ExtrusionFactor);
+	m_ExtrusionFilter->SetInput(m_ExtractHole->GetOutput());
+	m_ExtrusionFilter->Update();
+
+	m_ResultAfterExtrusion->RemoveAllInputs();
+	m_ResultAfterExtrusion->SetInput(m_ResultPolydata);
+	m_ResultAfterExtrusion->AddInput(m_ExtrusionFilter->GetOutput());
+	m_ResultAfterExtrusion->Update();
+
+	if(!m_TestMode)
+	{
+		m_SurfaceMapper->SetInput(m_ResultAfterExtrusion->GetOutput());
+		m_SurfaceMapper->Update();
+
+		m_Rwi->CameraUpdate();
+	}
+
+	vtkDEL(computeMedianPlane);
+}
+//----------------------------------------------------------------------------
+void mmoExtrusionHoles::SelectHole(int pointID)
+//----------------------------------------------------------------------------
+{
+	double	coordPointSelected[3];
+	m_ResultPolydata->GetPoint(pointID,coordPointSelected);
+
+	m_ExtractHole->SetInput(m_ExtractFreeEdges->GetOutput());
+	m_ExtractHole->Setpoint(coordPointSelected);
+	m_ExtractHole->Update();
+
+	if(!m_TestMode)
+	{
+		if(m_SelectedHoleActor)
+			m_Rwi->m_RenFront->RemoveActor(m_SelectedHoleActor);
+
+		vtkGlyph3D *glyph;
+		vtkNEW(glyph);
+		glyph->SetInput(m_ExtractHole->GetOutput());
+		glyph->SetSource(m_Sphere->GetOutput());
+
+		vtkNEW(m_SelectedHoleMapper);
+		m_SelectedHoleMapper->SetInput(glyph->GetOutput());
+
+		//Show the selected hole - selected hole is red
+		vtkNEW(m_SelectedHoleActor);
+		m_SelectedHoleActor->SetMapper(m_SelectedHoleMapper);
+		m_SelectedHoleActor->GetProperty()->SetColor(1,0,0);
+		m_SelectedHoleActor->PickableOff();
+
+		m_Rwi->m_RenFront->AddActor(m_SelectedHoleActor);
+		m_Rwi->m_RenderWindow->Render();
+		vtkDEL(glyph);
+	}
+}
+//----------------------------------------------------------------------------
+void mmoExtrusionHoles::OpStop(int result)
+//----------------------------------------------------------------------------
+{
+	mafEventMacro(mafEvent(this,result));        
+}
+//----------------------------------------------------------------------------
+void mmoExtrusionHoles::DeleteOpDialog()
+//----------------------------------------------------------------------------
+{
+	m_Mouse->RemoveObserver(m_Picker);
+
+	if(m_SurfaceActor)
+		m_Rwi->m_RenFront->RemoveActor(m_SurfaceActor);
+
+	if(m_HolesActor)
+		m_Rwi->m_RenFront->RemoveActor(m_HolesActor);
+
+	if(m_SelectedHoleActor)
+		m_Rwi->m_RenFront->RemoveActor(m_SelectedHoleActor);
+
+	vtkDEL(m_SelectedHoleActor);
+	vtkDEL(m_SelectedHoleMapper);
+	vtkDEL(m_HolesActor);
+	vtkDEL(m_HolesMapper);
+	vtkDEL(m_Sphere);
+	vtkDEL(m_SurfaceActor);
+	vtkDEL(m_SurfaceMapper);
+
+	mafDEL(m_Picker);
+	cppDEL(m_Rwi); 
+	cppDEL(m_Dialog);
+}
+//----------------------------------------------------------------------------
+void mmoExtrusionHoles::CreateOpDialog()
+//----------------------------------------------------------------------------
+{
+	wxBusyCursor wait;
+
+	//===== setup interface ====
+	m_Dialog = new mmgDialog("Extrusion", mafCLOSEWINDOW | mafRESIZABLE);
+
+	m_Rwi = new mafRWI(m_Dialog,ONE_LAYER,false);
+	m_Rwi->SetListener(this);
+	m_Rwi->CameraSet(CAMERA_PERSPECTIVE);
+	m_Rwi->m_RenderWindow->SetDesiredUpdateRate(0.0001f);
+	m_Rwi->SetSize(0,0,500,500);
+	m_Rwi->Show(true);
+	m_Rwi->m_RwiBase->SetMouse(m_Mouse);
+
+	mafNEW(m_Picker);
+	m_Picker->SetListener(this);
+	m_Mouse->AddObserver(m_Picker, MCH_INPUT);
+
+	wxPoint p = wxDefaultPosition;
+
+	m_ButtonOk = new mmgButton(m_Dialog, ID_OK,_("ok"), p, wxSize(80,20));
+	mmgButton  *b_cancel	= new mmgButton(m_Dialog, ID_CANCEL,_("cancel"), p, wxSize(80,20));
+
+
+	m_ButtonOk->SetValidator(mmgValidator(this,ID_OK,m_ButtonOk));
+	//m_ButtonOk->Enable(false);
+	b_cancel->SetValidator(mmgValidator(this,ID_CANCEL,b_cancel));
+
+	wxBoxSizer *h_sizer3 = new wxBoxSizer(wxHORIZONTAL);
+	h_sizer3->Add(m_ButtonOk,0,wxRIGHT);
+	h_sizer3->Add(b_cancel,0,wxRIGHT);
+
+	wxTextCtrl *extrusion = new wxTextCtrl(m_Dialog,ID_EXTRUSION_FACTOR, _("extrusion factor"),p,wxSize(50, 16 ), wxNO_BORDER );
+	mmgButton  *b_extrude	= new mmgButton(m_Dialog, ID_EXTRUDE,_("save extrusion"), p, wxSize(80,20));
+
+	b_extrude->SetValidator(mmgValidator(this,ID_EXTRUDE,b_extrude));
+	extrusion->SetValidator(mmgValidator(this,ID_EXTRUSION_FACTOR,extrusion,&m_ExtrusionFactor,-999.0,999.0));
+
+	wxBoxSizer *h_sizer2 = new wxBoxSizer(wxHORIZONTAL);
+	h_sizer2->Add(b_extrude,0,wxRIGHT);
+	h_sizer2->Add(extrusion,0,wxRIGHT);
+
+	wxBoxSizer *v_sizer =  new wxBoxSizer( wxVERTICAL );
+	v_sizer->Add(m_Rwi->m_RwiBase, 1,wxEXPAND);
+	v_sizer->Add(h_sizer2,     0,wxEXPAND | wxALL,5);
+	v_sizer->Add(h_sizer3,     0,wxEXPAND | wxALL,5);
+
+	m_Dialog->Add(v_sizer, 1, wxEXPAND);
+
+	int x_pos,y_pos,w,h;
+	mafGetFrame()->GetPosition(&x_pos,&y_pos);
+	m_Dialog->GetSize(&w,&h);
+	m_Dialog->SetSize(x_pos+5,y_pos+5,w,h);
+
+	CreatePolydataPipeline();
+
+	this->m_Rwi->CameraUpdate();
+}
+//----------------------------------------------------------------------------
+void mmoExtrusionHoles::ExtractFreeEdge()
+//----------------------------------------------------------------------------
+{
+	//Extract Holes from the input surface
+	m_ExtractFreeEdges->SetInput(m_ResultPolydata);
+	m_ExtractFreeEdges->SetBoundaryEdges(1);
+	m_ExtractFreeEdges->SetFeatureEdges(0);
+	m_ExtractFreeEdges->SetNonManifoldEdges(0);
+	m_ExtractFreeEdges->SetManifoldEdges(0);
+	m_ExtractFreeEdges->Update();
+}
+//----------------------------------------------------------------------------
+void mmoExtrusionHoles::CreatePolydataPipeline()
+//----------------------------------------------------------------------------
+{
+
+	double BoundingBox[6];
+	m_ResultPolydata->GetBounds(BoundingBox);
+	vtkNEW(m_SurfaceMapper);
+	m_SurfaceMapper->SetInput(m_ResultPolydata);
+	m_SurfaceMapper->ScalarVisibilityOff();
+
+	vtkNEW(m_SurfaceActor);
+	m_SurfaceActor->SetMapper(m_SurfaceMapper);
+
+	m_Rwi->m_RenFront->AddActor(m_SurfaceActor);
+
+	//Compute Max bound
+	double dimX, dimY, dimZ;
+	dimX = (BoundingBox[1] - BoundingBox[0]);
+	dimY = (BoundingBox[3] - BoundingBox[2]);
+	dimZ = (BoundingBox[5] - BoundingBox[4]);
+	double maxBounds = (dimX >= dimY) ? (dimX >= dimZ ? dimX : dimZ) : (dimY >= dimZ ? dimY : dimZ); 
+	m_Diameter = maxBounds / 100;
+
+	ExtractFreeEdge();
+
+	vtkNEW(m_Sphere);
+	m_Sphere->SetRadius(m_Diameter);
+
+	//Create a Glyph to highlight the holes
+	vtkGlyph3D *glyph;
+	vtkNEW(glyph);
+	glyph->SetInput(m_ExtractFreeEdges->GetOutput());
+	glyph->SetSource(m_Sphere->GetOutput());
+
+	vtkNEW(m_HolesMapper);
+	m_HolesMapper->SetInput(glyph->GetOutput());
+
+	vtkNEW(m_HolesActor);
+	m_HolesActor->SetMapper(m_HolesMapper);
+	m_HolesActor->GetProperty()->SetColor(0.0,1.0,0.0);
+	m_HolesActor->PickableOff();
+
+	m_Rwi->m_RenFront->AddActor(m_HolesActor);
+
+	m_Rwi->m_RenFront->ResetCamera(BoundingBox);
+	vtkDEL(glyph);
+}
---
+++ Operations/mmoExtrusionHoles.h
@@ -0,0 +1,149 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: mmoExtrusionHoles.h,v $
+Language:  C++
+Date:      $Date: 2007-05-31 08:14:01 $
+Version:   $Revision: 1.1 $
+Authors:   Matteo Giacomoni
+==========================================================================
+Copyright (c) 2002/2004
+CINECA - Interuniversity Consortium (www.cineca.it) 
+SCS s.r.l. - BioComputing Competence Centre (www.scsolutions.it - www.b3c.it)
+
+MafMedical Library use license agreement
+
+The software named MafMedical Library and any accompanying documentation, 
+manuals or data (hereafter collectively "SOFTWARE") is property of the SCS s.r.l.
+This is an open-source copyright as follows:
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+* Modified source versions must be plainly marked as such, and must not be misrepresented 
+as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR 
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+MafMedical is partially based on OpenMAF.
+=========================================================================*/
+#ifndef __mmoExtrusionHoles_H__
+#define __mmoExtrusionHoles_H__
+
+#include "mafOp.h"
+#include "vtkAppendPolyData.h"
+
+//----------------------------------------------------------------------------
+// forward references :
+//----------------------------------------------------------------------------
+
+class mmgDialog;
+class mmgButton;
+class mafRWI;
+class mmiSelectPoint;
+class mafVMESurface;
+class vtkFeatureEdges;
+class vtkHoleConnectivity;
+class vtkPolyDataMapper;
+class vtkActor;
+class vtkSphereSource;
+class vtkGlyph3D;
+class vtkLinearExtrusionFilter;
+class vtkAppendPolyData;
+class vtkPolyData;
+
+//----------------------------------------------------------------------------
+// mmoExtrusionHoles :
+//----------------------------------------------------------------------------
+/** 
+Perform extrusion of holes in a surface.
+CTRL modifier must be used in order to select a hole in render window while
+performing mouse dragging.
+*/
+class mmoExtrusionHoles: public mafOp
+{
+public:
+	mmoExtrusionHoles(const wxString &label = "Extrude Holes");
+	~mmoExtrusionHoles(); 
+
+	virtual void OnEvent(mafEventBase *maf_event);
+
+	mafTypeMacro(mmoExtrusionHoles, mafOp);
+
+	mafOp* Copy();
+
+	/** Return true for the acceptable vme type. */
+	bool Accept(mafNode *node);
+
+	/** Builds operation's interface. */
+	void OpRun();
+
+	/** Execute the operation. */
+	void OpDo();
+
+	/** Makes the undo for the operation. */
+	void OpUndo();
+
+	/** Highlight the hole selected with red color */
+	void SelectHole(int pointID);
+
+	/** Perform extrusion and visualize the result */
+	void Extrude();
+
+	/** Save the result polydata of extrusion in VME data */
+	void SaveExtrusion();
+
+	void ExtractFreeEdge();
+
+	void SetExtrusionFactor(double value){m_ExtrusionFactor = value;};
+	double GetExtrusionFactor(){return m_ExtrusionFactor;};
+
+	vtkPolyData* GetExtrutedSurface(){return m_ResultPolydata;};
+
+protected:
+	/** This method is called at the end of the operation and result contain the wxOK or wxCANCEL. */
+	void OpStop(int result);
+
+	void CreateOpDialog();
+	void DeleteOpDialog();
+	void CreatePolydataPipeline();
+
+	mmgDialog							*m_Dialog;
+	mmgButton							*m_ButtonOk;
+	mafRWI								*m_Rwi;
+	mmiSelectPoint				*m_Picker;
+
+	vtkPolyData						*m_ResultPolydata;
+	vtkPolyData						*m_OriginalPolydata;
+	mafVMESurface					*m_ResultSurface;
+	vtkAppendPolyData			*m_ResultAfterExtrusion;
+
+	double								m_Diameter;
+	double								m_ExtrusionFactor;
+
+	vtkPolyDataMapper			*m_SurfaceMapper;
+	vtkActor							*m_SurfaceActor;
+	vtkPolyDataMapper			*m_HolesMapper;
+	vtkActor							*m_HolesActor;
+	vtkPolyDataMapper			*m_SelectedHoleMapper;
+	vtkActor							*m_SelectedHoleActor;
+
+	vtkFeatureEdges						*m_ExtractFreeEdges;
+	vtkHoleConnectivity				*m_ExtractHole;
+	vtkLinearExtrusionFilter	*m_ExtrusionFilter;
+
+	vtkSphereSource				*m_Sphere;
+	vtkGlyph3D						*m_Glyph;
+};
+#endif
