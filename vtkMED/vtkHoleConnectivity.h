828eb2ea42295692bcb141030c8963f8afb7b5fe
ior02 <ior02>
2007-06-08 07:55:55 +0000

    minor changes

--- vtkMED/vtkHoleConnectivity.h
+++ vtkMED/vtkHoleConnectivity.h
@@ -1,43 +1,21 @@
 /*=========================================================================
-Program:   Multimod Application Framework
-Module:    $RCSfile: vtkHoleConnectivity.h,v $
+
+Program:   Visualization Toolkit
+Module:    vtkRemoveCellsFilter.h
 Language:  C++
-Date:      $Date: 2007-05-14 12:18:45 $
-Version:   $Revision: 1.1 $
-Authors:   Matteo Giacomoni
-==========================================================================
-Copyright (c) 2002/2007
-SCS s.r.l. - BioComputing Competence Centre (www.scsolutions.it - www.b3c.it)
+Version:   $Id: vtkHoleConnectivity.h,v 1.2 2007-06-08 07:55:55 ior02 Exp $
 
-MafMedical Library use license agreement
+Copyright (c) Goodwin Lawlor 2003-2004
+All rights reserved.
 
-The software named MafMedical Library and any accompanying documentation, 
-manuals or data (hereafter collectively "SOFTWARE") is property of the SCS s.r.l.
-This is an open-source copyright as follows:
-Redistribution and use in source and binary forms, with or without modification, 
-are permitted provided that the following conditions are met:
-* Redistributions of source code must retain the above copyright notice, 
-this list of conditions and the following disclaimer.
-* Redistributions in binary form must reproduce the above copyright notice, 
-this list of conditions and the following disclaimer in the documentation and/or 
-other materials provided with the distribution.
-* Modified source versions must be plainly marked as such, and must not be misrepresented 
-as being the original software.
+This software is distributed WITHOUT ANY WARRANTY; without even 
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+PURPOSE.  See the above copyright notice for more information.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' 
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR 
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+Some modifications by Matteo Giacomoni in order to make it work
+under MAF (www.openmaf.org)
 
-MafMedical is partially based on OpenMAF.
 =========================================================================*/
-
 #ifndef __vtkHoleConnectivity_h
 #define __vtkHoleConnectivity_h
 
