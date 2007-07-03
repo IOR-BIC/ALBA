a5b09784250efabf83b4d27144caf1ab0700d813
ior02 <ior02>
2007-07-03 10:13:09 +0000

    add medOPCreateEditSkeleton

---
+++ Operations/medOpCreateEditSkeleton.cpp
@@ -0,0 +1,165 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: medOpCreateEditSkeleton.cpp,v $
+Language:  C++
+Date:      $Date: 2007-07-03 10:13:09 $
+Version:   $Revision: 1.1 $
+Authors:   Matteo Giacomoni
+==========================================================================
+Copyright (c) 2002/2007
+SCS s.r.l. - BioComputing Competence Centre (www.scsolutions.it - www.b3c.it)
+
+MafMedical Library use license agreement
+
+The software named MafMedical Library and any accompanying documentation, 
+manuals or data (hereafter collectively "SOFTWARE") is property of the SCS s.r.l.
+This is an open-source copyright as follows:
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+* Modified source versions must be plainly marked as such, and must not be misrepresented 
+as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR 
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+MafMedical is partially based on OpenMAF.
+=========================================================================*/
+
+#include "mafDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include "medOpCreateEditSkeleton.h"
+
+#include "mafDecl.h"
+#include "mmgGui.h"
+
+#include "mafVME.h"
+#include "mafVMEVolumeGray.h"
+#include "medVMEPolylineGraph.h"
+#include "medVMEEditor.h"
+#include "medGeometryEditorPolylineGraph.h"
+
+#include "mmiPicker.h"
+#include "mafInteractor.h"
+
+#include "vtkPoints.h"
+
+//----------------------------------------------------------------------------
+mafCxxTypeMacro(medOpCreateEditSkeleton);
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+medOpCreateEditSkeleton::medOpCreateEditSkeleton(wxString label) :
+mafOp(label)
+//----------------------------------------------------------------------------
+{
+	m_OpType  = OPTYPE_OP;
+	m_Canundo = true;
+
+	m_Editor  = NULL;
+}
+//----------------------------------------------------------------------------
+medOpCreateEditSkeleton::~medOpCreateEditSkeleton()
+//----------------------------------------------------------------------------
+{
+	cppDEL(m_Editor);
+	mafDEL(m_Skeleton);
+}
+//----------------------------------------------------------------------------
+mafOp* medOpCreateEditSkeleton::Copy()
+//----------------------------------------------------------------------------
+{
+	/** return a copy of itself, needs to put it into the undo stack */
+	return new medOpCreateEditSkeleton(m_Label);
+}
+//----------------------------------------------------------------------------
+bool medOpCreateEditSkeleton::Accept(mafNode* vme)
+//----------------------------------------------------------------------------
+{
+	return vme != NULL && vme->IsMAFType(mafVMEVolumeGray);
+}
+//----------------------------------------------------------------------------
+void medOpCreateEditSkeleton::OpRun()
+//----------------------------------------------------------------------------
+{
+	mafNEW(m_Skeleton);
+
+	m_Editor = new medGeometryEditorPolylineGraph(mafVME::SafeDownCast(m_Input), this);
+	m_Editor->Show(true);
+
+	CreateGui();
+	ShowGui();
+}
+//----------------------------------------------------------------------------
+void medOpCreateEditSkeleton::OpDo()
+//----------------------------------------------------------------------------
+{
+	m_Skeleton->SetData((vtkDataSet*)m_Editor->GetOutput(),((mafVME*)m_Input)->GetTimeStamp());
+	m_Skeleton->ReparentTo(m_Input->GetRoot());
+}
+//----------------------------------------------------------------------------
+void medOpCreateEditSkeleton::OpUndo()
+//----------------------------------------------------------------------------
+{
+	m_Skeleton->ReparentTo(NULL);
+}
+//----------------------------------------------------------------------------
+void medOpCreateEditSkeleton::CreateGui()
+//----------------------------------------------------------------------------
+{
+	m_Gui = new mmgGui(this);
+	m_Gui->AddGui(m_Editor->GetGui());
+	m_Gui->OkCancel();
+}
+//----------------------------------------------------------------------------
+void medOpCreateEditSkeleton::OnEvent(mafEventBase *maf_event)
+//----------------------------------------------------------------------------
+{
+	if (mafEvent *e = mafEvent::SafeDownCast(maf_event))
+	{
+		switch(e->GetId())
+		{
+		case wxOK:
+			{
+				OpStop(OP_RUN_OK);
+			}
+			break;
+		case wxCANCEL:
+			{
+				OpStop(OP_RUN_CANCEL);
+			}
+			break;
+		default:
+			mafEventMacro(*e);
+			break; 
+		}
+	}
+}
+//----------------------------------------------------------------------------
+void medOpCreateEditSkeleton::OpStop(int result)
+//----------------------------------------------------------------------------
+{
+	m_Editor->Show(false);
+	cppDEL(m_Editor);
+
+	HideGui();
+	mafEventMacro(mafEvent(this,result));
+}
---
+++ Operations/medOpCreateEditSkeleton.h
@@ -0,0 +1,97 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: medOpCreateEditSkeleton.h,v $
+Language:  C++
+Date:      $Date: 2007-07-03 10:13:09 $
+Version:   $Revision: 1.1 $
+Authors:   Matteo Giacomoni
+==========================================================================
+Copyright (c) 2002/2007
+SCS s.r.l. - BioComputing Competence Centre (www.scsolutions.it - www.b3c.it)
+
+MafMedical Library use license agreement
+
+The software named MafMedical Library and any accompanying documentation, 
+manuals or data (hereafter collectively "SOFTWARE") is property of the SCS s.r.l.
+This is an open-source copyright as follows:
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+* Modified source versions must be plainly marked as such, and must not be misrepresented 
+as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR 
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+MafMedical is partially based on OpenMAF.
+=========================================================================*/
+
+#ifndef __medOpCreateEditSkeleton_H__
+#define __medOpCreateEditSkeleton_H__
+
+//----------------------------------------------------------------------------
+// Include :
+//----------------------------------------------------------------------------
+#include "mafOp.h"
+
+//----------------------------------------------------------------------------
+// forward references :
+//----------------------------------------------------------------------------
+class mafVME;
+class medVMEPolylineGraph;
+class medGeometryEditorPolylineGraph;
+
+//----------------------------------------------------------------------------
+// medOpCreateEditSkeleton :
+//----------------------------------------------------------------------------
+class medOpCreateEditSkeleton: public mafOp
+{
+public:
+
+	medOpCreateEditSkeleton(wxString label = "Create/Edit Skeleton");
+	~medOpCreateEditSkeleton(); 
+
+	mafTypeMacro(medOpCreateEditSkeleton, mafOp);
+
+	mafOp* Copy();
+
+	void OnEvent(mafEventBase *maf_event);
+
+	/** Return true for the acceptable vme type. */
+	bool Accept(mafNode* vme);
+
+	/** Builds operation's interface by calling CreateOpDialog() method. */
+	void OpRun();
+
+	/** Execute the operation. */
+	void OpDo();
+
+	/** Makes the undo for the operation. */
+	void OpUndo();
+
+protected:
+	
+	virtual void OpStop(int result);
+
+	void CreateGui();
+
+	medVMEPolylineGraph	*m_Skeleton;
+
+	medGeometryEditorPolylineGraph *m_Editor;
+
+	int m_Modality;
+
+};
+#endif
