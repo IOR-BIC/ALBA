79f631ceedab942fcb0997f442fe6942d6e1e4b7
ior02 <ior02>
2007-07-12 14:26:53 +0000

    Commited CreateSurface to convert Parametric surface into Normal surface

---
+++ Operations/medOpCreateSurface.cpp
@@ -0,0 +1,103 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: medOpCreateSurface.cpp,v $
+Language:  C++
+Date:      $Date: 2007-07-12 14:26:53 $
+Version:   $Revision: 1.1 $
+Authors:   Matteo Giacomoni
+==========================================================================
+Copyright (c) 2002/2007
+SCS s.r.l. - BioComputing Competence Centre (www.scsolutions.it - www.b3c.it)
+
+MafMedical Library use license agreement
+
+The software named MafMedical Library and any accompanying documentation, 
+manuals or data (hereafter collectively "SOFTWARE") is property of the SCS s.r.l.
+This is an open-source copyright as follows:
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+* Modified source versions must be plainly marked as such, and must not be misrepresented 
+as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR 
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+MafMedical is partially based on OpenMAF.
+=========================================================================*/
+
+#include "mafDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include "medOpCreateSurface.h"
+
+#include "mmgGui.h"
+
+#include "mafNode.h"
+#include "mafVMESurfaceParametric.h"
+#include "mafVMESurface.h"
+
+#include "vtkPolyData.h"
+
+//----------------------------------------------------------------------------
+mafCxxTypeMacro(medOpCreateSurface);
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+medOpCreateSurface::medOpCreateSurface(wxString label) :
+mafOp(label)
+//----------------------------------------------------------------------------
+{
+	m_OpType  = OPTYPE_OP;
+	m_Canundo = true;
+	m_InputPreserving = true;
+}
+//----------------------------------------------------------------------------
+medOpCreateSurface::~medOpCreateSurface()
+//----------------------------------------------------------------------------
+{
+
+}
+//----------------------------------------------------------------------------
+mafOp* medOpCreateSurface::Copy()
+//----------------------------------------------------------------------------
+{
+	/** return a copy of itself, needs to put it into the undo stack */
+	return new medOpCreateSurface(m_Label);
+}
+//----------------------------------------------------------------------------
+bool medOpCreateSurface::Accept(mafNode* vme)
+//----------------------------------------------------------------------------
+{
+	return vme != NULL && vme->IsMAFType(mafVMESurfaceParametric);
+}
+//----------------------------------------------------------------------------
+void medOpCreateSurface::OpRun()
+//----------------------------------------------------------------------------
+{
+	mafVMESurfaceParametric *inputSurface=mafVMESurfaceParametric::SafeDownCast(m_Input);
+	mafSmartPointer<mafVMESurface> newSurface;
+	newSurface->SetName(inputSurface->GetName());
+	newSurface->SetData(vtkPolyData::SafeDownCast(inputSurface->GetOutput()->GetVTKData()),inputSurface->GetTimeStamp());
+	newSurface->Update();
+
+	m_Output=newSurface;
+	OpStop(OP_RUN_OK);
+}
---
+++ Operations/medOpCreateSurface.h
@@ -0,0 +1,77 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: medOpCreateSurface.h,v $
+Language:  C++
+Date:      $Date: 2007-07-12 14:26:53 $
+Version:   $Revision: 1.1 $
+Authors:   Matteo Giacomoni
+==========================================================================
+Copyright (c) 2002/2007
+SCS s.r.l. - BioComputing Competence Centre (www.scsolutions.it - www.b3c.it)
+
+MafMedical Library use license agreement
+
+The software named MafMedical Library and any accompanying documentation, 
+manuals or data (hereafter collectively "SOFTWARE") is property of the SCS s.r.l.
+This is an open-source copyright as follows:
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+* Modified source versions must be plainly marked as such, and must not be misrepresented 
+as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR 
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+MafMedical is partially based on OpenMAF.
+=========================================================================*/
+
+#ifndef __medOpCreateSurface_H__
+#define __medOpCreateSurface_H__
+
+//----------------------------------------------------------------------------
+// Include :
+//----------------------------------------------------------------------------
+#include "mafOp.h"
+
+//----------------------------------------------------------------------------
+// forward references :
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+// medOpCreateSurface :
+//----------------------------------------------------------------------------
+/**Convert a Parametric Surface into a Normal Surface*/
+class medOpCreateSurface: public mafOp
+{
+public:
+
+	medOpCreateSurface(wxString label = "Create Surface");
+	~medOpCreateSurface(); 
+
+	mafTypeMacro(medOpCreateSurface, mafOp);
+
+	mafOp* Copy();
+
+	/** Return true for the acceptable vme type. */
+	bool Accept(mafNode* vme);
+
+	/** Builds operation's interface by calling CreateOpDialog() method. */
+	void OpRun();
+
+protected:
+
+};
+#endif
