07191ee70f095ef3646062cf3cb7ceb58bd68adb
ior02 <ior02>
2007-07-22 11:42:35 +0000

    modified the intialization of the importer (based on the label) modified the label in the GUI

--- Operations/mmoCTMRIImporter.cpp
+++ Operations/mmoCTMRIImporter.cpp
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mmoCTMRIImporter.cpp,v $
   Language:  C++
-  Date:      $Date: 2007-07-20 14:16:52 $
-  Version:   $Revision: 1.19 $
+  Date:      $Date: 2007-07-22 11:42:35 $
+  Version:   $Revision: 1.20 $
   Authors:   Paolo Quadrani    Stefano Perticoni
 ==========================================================================
   Copyright (c) 2002/2004
@@ -123,8 +123,8 @@ mmoCTMRIImporter::mmoCTMRIImporter(wxString label) : mafOp(label)
 	m_DICOM = 0;
 	m_DICOMType = -1;
 	
-	if(this->m_Label == "CT"  || this->m_Label == "3D-DRA") m_DICOM = 0;
-	else if(this->m_Label == "MRI") m_DICOM = 1;
+	if(this->m_Label.Contains("CT") ) m_DICOM = 0;
+	else if(this->m_Label.Contains("MR")) m_DICOM = 1;
 
   m_DictionaryFilename	= "";
   wxString dictionary = mafGetApplicationDirectory().c_str();
@@ -321,10 +321,12 @@ void mmoCTMRIImporter::CreateGui()
 	mafString wildcard = "DICT files (*.dic)|*.dic|All Files (*.*)|*.*";
 
 	m_Gui = new mmgGui(this);
-	m_Gui->SetListener(this);
+	m_Gui->SetListener(this); 
 
-	if(m_DICOM == 0) m_Gui->Label("CT / 3D-DRA Importer",true);
-	else if(m_DICOM == 1) m_Gui->Label("MRI Importer",true);
+	//if(m_DICOM == 0) m_Gui->Label("CT Importer",true);
+	//else if(m_DICOM == 0) m_Gui->Label("MRI Importer",true);
+	// this two lines have no sense: 
+	m_Gui->Label(m_Label+" Importer",true);
 
 	//m_Gui->Label("Type of DICOM",true);
 	//m_Gui->Combo(ID_TYPE_DICOM,"",&m_DICOM,1,TypeOfDICOM);
