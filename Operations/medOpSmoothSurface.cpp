6f26909c9427902089968f5e87e1349397d4d8e2
ior02 <ior02>
2007-10-17 14:37:43 +0000

    add medOpSmoothSurface

---
+++ Operations/medOpSmoothSurface.cpp
@@ -0,0 +1,269 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: medOpSmoothSurface.cpp,v $
+Language:  C++
+Date:      $Date: 2007-10-17 14:37:43 $
+Version:   $Revision: 1.1 $
+Authors:   Matteo Giacomoni
+==========================================================================
+Copyright (c) 2002/2004
+CINECA - Interuniversity Consortium (www.cineca.it) 
+SCS s.r.l. - BioComputing Competence Centre (www.scsolutions.it - www.b3c.it)
+
+MafMedical Library use license agreement
+
+The software named MafMedical Library and any accompanying documentation, 
+manuals or data (hereafter collectively "SOFTWARE") is property of the SCS s.r.l.
+This is an open-source copyright as follows:
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+* Modified source versions must be plainly marked as such, and must not be misrepresented 
+as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR 
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+MafMedical is partially based on OpenMAF.
+=========================================================================*/
+
+#include "mafDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include "medOpSmoothSurface.h"
+#include "mafDecl.h"
+#include "mafEvent.h"
+#include "mmgGui.h"
+
+#include "mafVMESurface.h"
+
+#include "vtkMAFSmartPointer.h"
+#include "vtkPointData.h"
+#include "vtkPolyData.h"
+#include "vtkCleanPolyData.h"
+#include "vtkSmoothPolyDataFilter.h"
+#include "vtkDecimatePro.h"
+#include "vtkStripper.h"
+#include "vtkTriangleFilter.h"
+#include "vtkPolyDataNormals.h"
+#include "vtkPolyDataConnectivityFilter.h"
+
+//----------------------------------------------------------------------------
+mafCxxTypeMacro(medOpSmoothSurface);
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+medOpSmoothSurface::medOpSmoothSurface(const wxString &label) :
+mafOp(label)
+//----------------------------------------------------------------------------
+{
+	m_OpType	= OPTYPE_OP;
+	m_Canundo	= true;
+
+	m_InputPreserving = false;
+
+	m_PreviewResultFlag	= false;
+	m_ClearInterfaceFlag= false;
+
+	m_Iterations	= 50;
+
+	m_ResultPolydata	  = NULL;
+	m_OriginalPolydata  = NULL;
+}
+//----------------------------------------------------------------------------
+medOpSmoothSurface::~medOpSmoothSurface()
+//----------------------------------------------------------------------------
+{
+	vtkDEL(m_ResultPolydata);
+	vtkDEL(m_OriginalPolydata);
+}
+//----------------------------------------------------------------------------
+bool medOpSmoothSurface::Accept(mafNode *node)
+//----------------------------------------------------------------------------
+{
+	return (node && node->IsMAFType(mafVMESurface));
+}
+//----------------------------------------------------------------------------
+mafOp *medOpSmoothSurface::Copy()   
+//----------------------------------------------------------------------------
+{
+	return (new medOpSmoothSurface(m_Label));
+}
+//----------------------------------------------------------------------------
+// Constants:
+//----------------------------------------------------------------------------
+enum SMOOTH_SURFACE_ID
+{
+	ID_SMOOTH = MINID,
+	ID_PREVIEW,
+	ID_CLEAR,
+	ID_ITERACTION,
+};
+//----------------------------------------------------------------------------
+void medOpSmoothSurface::OpRun()   
+//----------------------------------------------------------------------------
+{  
+	vtkNEW(m_ResultPolydata);
+	m_ResultPolydata->DeepCopy((vtkPolyData*)((mafVME *)m_Input)->GetOutput()->GetVTKData());
+
+	vtkNEW(m_OriginalPolydata);
+	m_OriginalPolydata->DeepCopy((vtkPolyData*)((mafVME *)m_Input)->GetOutput()->GetVTKData());
+
+	// interface:
+	m_Gui = new mmgGui(this);
+
+	m_Gui->Label("");
+	m_Gui->Label("smooth",true);
+	m_Gui->Slider(ID_ITERACTION,"n.iteraction: ",&m_Iterations,0,500);
+	m_Gui->Button(ID_SMOOTH,"apply smooth");
+
+	m_Gui->Divider(2);
+	m_Gui->Label("");
+	m_Gui->Button(ID_PREVIEW,"preview");
+	m_Gui->Button(ID_CLEAR,"clear");
+	m_Gui->OkCancel();
+	m_Gui->Enable(wxOK,false);
+
+	m_Gui->Enable(ID_PREVIEW,false);
+	m_Gui->Enable(ID_CLEAR,false);
+
+	m_Gui->Divider();
+
+	ShowGui();
+}
+//----------------------------------------------------------------------------
+void medOpSmoothSurface::OpDo()
+//----------------------------------------------------------------------------
+{
+	((mafVMESurface *)m_Input)->SetData(m_ResultPolydata,((mafVME *)m_Input)->GetTimeStamp());
+	mafEventMacro(mafEvent(this, CAMERA_UPDATE));
+}
+//----------------------------------------------------------------------------
+void medOpSmoothSurface::OpUndo()
+//----------------------------------------------------------------------------
+{
+	((mafVMESurface *)m_Input)->SetData(m_OriginalPolydata,((mafVME *)m_Input)->GetTimeStamp());
+	mafEventMacro(mafEvent(this, CAMERA_UPDATE));
+}
+//----------------------------------------------------------------------------
+void medOpSmoothSurface::OnEvent(mafEventBase *maf_event)
+//----------------------------------------------------------------------------
+{
+	if (mafEvent *e = mafEvent::SafeDownCast(maf_event))
+	{
+		switch(e->GetId())
+		{	
+		case ID_SMOOTH:
+			OnSmooth();
+			break;
+		case ID_PREVIEW:
+			OnPreview(); 
+			break;
+		case ID_CLEAR:
+			OnClear(); 
+			break;
+		case wxOK:
+			if(m_PreviewResultFlag)
+				OnPreview();
+			OpStop(OP_RUN_OK);        
+			break;
+		case wxCANCEL:
+			if(m_ClearInterfaceFlag)
+				OnClear();
+			OpStop(OP_RUN_CANCEL);        
+			break;
+		}
+	}
+}
+//----------------------------------------------------------------------------
+void medOpSmoothSurface::OpStop(int result)
+//----------------------------------------------------------------------------
+{
+	HideGui();
+	mafEventMacro(mafEvent(this,result));        
+}
+//----------------------------------------------------------------------------
+void medOpSmoothSurface::OnSmooth()
+//----------------------------------------------------------------------------
+{
+	wxBusyCursor wait;
+	m_Gui->Enable(ID_SMOOTH,false);
+	m_Gui->Enable(ID_ITERACTION,false);
+	m_Gui->Update();
+
+	vtkMAFSmartPointer<vtkSmoothPolyDataFilter> smoothFilter;
+	smoothFilter->SetInput(m_ResultPolydata);
+	smoothFilter->SetNumberOfIterations(m_Iterations);
+	smoothFilter->FeatureEdgeSmoothingOn();
+	smoothFilter->Update();
+
+	m_ResultPolydata->DeepCopy(smoothFilter->GetOutput());
+
+	m_Gui->Enable(ID_SMOOTH,true);
+	m_Gui->Enable(ID_ITERACTION,true);
+
+	m_Gui->Enable(ID_PREVIEW,true);
+	m_Gui->Enable(ID_CLEAR,true);
+	m_Gui->Enable(wxOK,true);
+
+	m_PreviewResultFlag = true;
+}
+//----------------------------------------------------------------------------
+void medOpSmoothSurface::OnPreview()
+//----------------------------------------------------------------------------
+{
+	wxBusyCursor wait;
+
+	((mafVMESurface *)m_Input)->SetData(m_ResultPolydata,((mafVME *)m_Input)->GetTimeStamp());
+
+	m_Gui->Enable(ID_PREVIEW,false);
+	m_Gui->Enable(ID_CLEAR,true);
+	m_Gui->Enable(wxOK,true);
+
+	m_PreviewResultFlag   = false;
+	m_ClearInterfaceFlag	= true;
+
+	mafEventMacro(mafEvent(this, CAMERA_UPDATE));
+}
+//----------------------------------------------------------------------------
+void medOpSmoothSurface::OnClear()  
+//----------------------------------------------------------------------------
+{
+	wxBusyCursor wait;
+
+	((mafVMESurface *)m_Input)->SetData(m_OriginalPolydata,((mafVME *)m_Input)->GetTimeStamp());
+
+	m_ResultPolydata->DeepCopy(m_OriginalPolydata);
+
+	m_Iterations	= 50;
+
+	m_Gui->Enable(ID_SMOOTH,true);
+	m_Gui->Enable(ID_ITERACTION,true);
+
+	m_Gui->Enable(ID_PREVIEW,false);
+	m_Gui->Enable(ID_CLEAR,false);
+	m_Gui->Enable(wxOK,false);
+	m_Gui->Update();
+
+	m_PreviewResultFlag = false;
+	m_ClearInterfaceFlag= false;
+
+	mafEventMacro(mafEvent(this, CAMERA_UPDATE));
+}
