b9ef7b7e7d013daac99530d6946da06332963dbd
aqd0 <aqd0>
2007-12-06 09:14:00 +0000

    Commit for Giunchi:  vtkMAFPolyDataToSinglePolyLineTest.

---
+++ vtkMAF/Testing/vtkMAFPolyDataToSinglePolyLineTest.h
@@ -0,0 +1,75 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: vtkMAFPolyDataToSinglePolyLineTest.h,v $
+Language:  C++
+Date:      $Date: 2007-12-06 09:14:00 $
+Version:   $Revision: 1.1 $
+Authors:   Daniele Giunchi
+==========================================================================
+Copyright (c) 2002/2004 
+CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#ifndef __CPP_UNIT_vtkMAFPolyDataToSinglePolyLineTEST_H__
+#define __CPP_UNIT_vtkMAFPolyDataToSinglePolyLineTEST_H__
+
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
+
+class vtkMAFPolyDataToSinglePolyLineTest : public CPPUNIT_NS::TestFixture
+{
+  public:
+  
+    // CPPUNIT fixture: executed before each test
+    void setUp();
+
+    // CPPUNIT fixture: executed after each test
+    void tearDown();
+
+    CPPUNIT_TEST_SUITE( vtkMAFPolyDataToSinglePolyLineTest );
+    CPPUNIT_TEST( TestFixture );
+    CPPUNIT_TEST( TestConversion );
+    CPPUNIT_TEST_SUITE_END();
+
+  protected:
+
+    void Test();
+    void TestFixture();
+    void TestConversion();
+
+};
+
+
+int 
+main( int argc, char* argv[] )
+{
+
+  // Create the event manager and test controller
+  CPPUNIT_NS::TestResult controller;
+
+  // Add a listener that colllects test result
+  CPPUNIT_NS::TestResultCollector result;
+  controller.addListener( &result );        
+
+  // Add a listener that print dots as test run.
+  CPPUNIT_NS::BriefTestProgressListener progress;
+  controller.addListener( &progress );      
+
+  // Add the top suite to the test runner
+  CPPUNIT_NS::TestRunner runner;
+  runner.addTest( vtkMAFPolyDataToSinglePolyLineTest::suite());
+  runner.run( controller );
+
+  // Print test in a compiler compatible format.
+  CPPUNIT_NS::CompilerOutputter outputter( &result, CPPUNIT_NS::stdCOut() );
+  outputter.write(); 
+
+  return result.wasSuccessful() ? 0 : 1;
+}
+
+#endif
