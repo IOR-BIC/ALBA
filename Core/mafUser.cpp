f84fe343ff2b781648a94e40bed4d52e4ef8432c
aqd0 <aqd0>
2007-12-17 11:45:58 +0000

    Added mafUser class.

---
+++ Core/mafUser.cpp
@@ -0,0 +1,227 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: mafUser.cpp,v $
+Language:  C++
+Date:      $Date: 2007-12-17 11:45:58 $
+Version:   $Revision: 1.1 $
+Authors:   Paolo Quadrani
+==========================================================================
+Copyright (c) 2002/2004
+CINECA - Interuniversity Consortium (www.cineca.it) 
+=========================================================================*/
+
+#include "mafDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include "mafUser.h"
+#include <wx/stdpaths.h>
+#include <wx/tokenzr.h>
+
+#include "mafDecl.h"
+#include "mafGUIDialogLogin.h"
+#include "mafCrypt.h"
+
+#include <fstream>
+
+//----------------------------------------------------------------------------
+mafUser::mafUser()
+//----------------------------------------------------------------------------
+{
+  m_Username = "";
+  m_Password = "";
+  m_UserHome = "";
+  m_UserInfoFile = "/.usrInfo";
+  m_Initialized = false;
+  m_RememberCredentials = false;
+}
+//----------------------------------------------------------------------------
+mafUser::~mafUser()
+//----------------------------------------------------------------------------
+{
+}
+//----------------------------------------------------------------------------
+void mafUser::ShowLoginDialog()
+//----------------------------------------------------------------------------
+{
+  if (!m_Initialized)
+  {
+    InitializeUserInformations();
+  }
+  mafGUIDialogLogin login_dialog(_("User authentication"));
+  login_dialog.SetUserCredentials(m_Username, m_Password, m_RememberCredentials);
+  int result = login_dialog.ShowModal();
+  if(result != wxID_OK) return;
+  m_Username = login_dialog.GetUser();
+  m_Password = login_dialog.GetPwd();
+  m_RememberCredentials = login_dialog.GetRememberUserCredentials();
+  UpdateUserCredentialsFile();
+}
+//----------------------------------------------------------------------------
+bool mafUser::CheckUserCredentials()
+//----------------------------------------------------------------------------
+{
+  if (!m_Initialized)
+  {
+    InitializeUserInformations();
+  }
+  if (m_Username.IsEmpty() && m_Password.IsEmpty())
+  {
+    ShowLoginDialog();
+  }
+  // Check only username and not password.
+  // This can be valid for anonymous (or guest) user without password
+  bool res = !m_Username.IsEmpty();
+  return res;
+}
+//----------------------------------------------------------------------------
+void mafUser::InitializeUserInformations()
+//----------------------------------------------------------------------------
+{
+  // if not initialized, m_UserHome will be initialized to the LocalSettings user folder.
+  InitUserInfoHome();
+
+  mafString credentials = "";
+  if (wxFileExists(m_UserInfoFile.GetCStr()))
+  {
+    char usr[256];
+    std::ifstream f_in;
+    f_in.open(m_UserInfoFile.GetCStr(), std::ifstream::in| std::ifstream::binary);
+#ifdef MAF_USE_CRYPTO
+    while(!f_in.eof())
+    {
+      f_in.getline(usr,256);
+      credentials << usr;
+    }
+    std::string decrypted_input;
+    mafDefaultDecryptInMemory(credentials.GetCStr(), decrypted_input);
+    credentials = decrypted_input.c_str();
+#else
+    f_in.getline(usr,256);
+    credentials << usr;
+    while(!f_in.eof())
+    {
+      credentials << "\n";
+      f_in.getline(usr, 256);
+      credentials << usr;
+    }
+#endif
+    f_in.close();
+  }
+
+  if (credentials.Length() > 0)
+  {
+    mafString usr;
+    mafString pwd;
+
+    wxStringTokenizer tkz(credentials.GetCStr(), "\n");
+    usr = tkz.GetNextToken().c_str();
+    if (tkz.HasMoreTokens())
+    {
+      pwd = tkz.GetNextToken().c_str();
+    }
+    m_Username = usr;
+    m_Password = pwd;
+    m_RememberCredentials = 1;
+  }
+  else
+  {
+    m_Username = wxGetUserName().c_str();
+    m_Password = "";
+    m_RememberCredentials = 0;
+  }
+
+  m_Initialized = true;
+}
+//----------------------------------------------------------------------------
+bool mafUser::SetCredentials(mafString &name, mafString &pwd, int &remember_me)
+//----------------------------------------------------------------------------
+{
+  m_Username = name;
+  m_Password = pwd;
+  m_RememberCredentials = remember_me;
+  
+  // empty username is not accepted!!
+  m_Initialized = !m_Username.IsEmpty();
+  if (m_Initialized) 
+  {
+    if (m_UserHome.IsEmpty())
+    {
+      InitUserInfoHome();
+    }
+    UpdateUserCredentialsFile();
+  }
+  return m_Initialized;
+}
+//----------------------------------------------------------------------------
+mafString &mafUser::GetName()
+//----------------------------------------------------------------------------
+{
+  if (!m_Initialized)
+  {
+    InitializeUserInformations();
+  }
+  return m_Username;
+}
+//----------------------------------------------------------------------------
+mafString &mafUser::GetPwd()
+//----------------------------------------------------------------------------
+{
+  if (!m_Initialized)
+  {
+    InitializeUserInformations();
+  }
+  return m_Password;
+}
+//----------------------------------------------------------------------------
+int mafUser::GetRememberUserCredentials()
+//----------------------------------------------------------------------------
+{
+  if (!m_Initialized)
+  {
+    InitializeUserInformations();
+  }
+  return m_RememberCredentials;
+}
+//----------------------------------------------------------------------------
+void mafUser::UpdateUserCredentialsFile()
+//----------------------------------------------------------------------------
+{
+  if (m_RememberCredentials != 0)
+  {
+    mafString credentials;
+    credentials = m_Username;
+    credentials << "\n";
+    credentials << m_Password;
+#ifdef MAF_USE_CRYPTO
+    std::string encrypted_output;
+    mafDefaultEncryptFromMemory(credentials.GetCStr(), credentials.Length(), encrypted_output);
+    credentials = encrypted_output.c_str();
+#endif
+    std::ofstream f_out;
+    f_out.open(m_UserInfoFile.GetCStr(), std::ofstream::out | std::ofstream::binary);
+    f_out.write(credentials.GetCStr(), credentials.Length());
+    f_out.close();
+  }
+  else
+  {
+    wxRemoveFile(m_UserInfoFile.GetCStr());
+  }
+}
+//----------------------------------------------------------------------------
+void mafUser::InitUserInfoHome()
+//----------------------------------------------------------------------------
+{
+  wxStandardPaths std_paths;
+  m_UserHome = std_paths.GetUserLocalDataDir().c_str();
+  if (!wxDirExists(m_UserHome.GetCStr()))
+  {
+    wxMkdir(m_UserHome.GetCStr());
+  }
+  m_UserInfoFile = m_UserHome;
+  m_UserInfoFile << "/.usrInfo";
+}
---
+++ Core/mafUser.h
@@ -0,0 +1,73 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: mafUser.h,v $
+Language:  C++
+Date:      $Date: 2007-12-17 11:45:58 $
+Version:   $Revision: 1.1 $
+Authors:   Paolo Quadrani
+==========================================================================
+Copyright (c) 2002/2004
+CINECA - Interuniversity Consortium (www.cineca.it) 
+=========================================================================*/
+#ifndef __mafUser_H__
+#define __mafUser_H__
+
+//----------------------------------------------------------------------------
+// Include:
+//----------------------------------------------------------------------------
+#include "mafString.h"
+
+//----------------------------------------------------------------------------
+// forward declarations
+//----------------------------------------------------------------------------
+
+/** mafUser - Used to manage username and password; store them and give basic function to check user's credentials.
+This is intended as starting point for user managing. Method for Check User Credentials can be redefined to be
+customized at application level according to custom requests.
+*/
+class mafUser
+{
+public:
+  mafUser();
+  virtual ~mafUser();
+
+  /** Allows to show login dialog and get credentials from the application's user.*/
+  void ShowLoginDialog();
+
+  /** Function to be customized at application level.
+  By default open Login Dialog if the user did not inserted any information.*/
+  virtual bool CheckUserCredentials();
+
+  /** Set the user's credentials. 
+  Return 'true' if the users' credentials are correctly initialized, otherwise 'flase' value is returned. */
+  bool SetCredentials(mafString &name, mafString &pwd, int &remember_me);
+
+  /** Return the information on user name.*/
+  mafString &GetName();
+
+  /** Return the information on users' password.*/
+  mafString &GetPwd();
+
+  /** Return information to allow saving or not users' information.*/
+  int GetRememberUserCredentials();
+
+protected:
+  /** Retrieve user information saved into an encrypted file.*/
+  void InitializeUserInformations();
+
+  /** Update user credentials into an encrypted file or remove the file if
+  the user don't want to save its credentials.*/
+  void UpdateUserCredentialsFile();
+
+  /** Set the folder in which store encrypted user credentials.*/
+  void InitUserInfoHome();
+
+  // User credentials
+  mafString m_Username;
+  mafString m_Password;
+  mafString m_UserHome;
+  mafString m_UserInfoFile;
+  int m_RememberCredentials;
+  bool m_Initialized; ///< Flag used to verify if users' information has been initialized.
+};
+#endif
