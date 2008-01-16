a5e639aed915b1604df445043288c871539f4bff
aqd0 <aqd0>
2008-01-16 11:04:59 +0000

    Commit for Mucci: bug fix #742

--- VME/mafVMEExternalData.cpp
+++ VME/mafVMEExternalData.cpp
@@ -2,9 +2,9 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mafVMEExternalData.cpp,v $
   Language:  C++
-  Date:      $Date: 2006-09-20 12:24:03 $
-  Version:   $Revision: 1.1 $
-  Authors:   Marco Petrone
+  Date:      $Date: 2008-01-16 11:04:59 $
+  Version:   $Revision: 1.2 $
+  Authors:   Marco Petrone - Roberto Mucci
 ==========================================================================
   Copyright (c) 2002/2004 
   CINECA - Interuniversity Consortium (www.cineca.it)
@@ -21,8 +21,13 @@
 //----------------------------------------------------------------------------
 
 #include "mafVMEExternalData.h"
-#include "mafTagArray.h"
+
 #include <vtkIndent.h>
+#include <wx/filefn.h>
+
+#include "mafTagArray.h"
+#include "mafStorage.h"
+#include "mafEventIO.h"
 
 //-------------------------------------------------------------------------
 mafCxxTypeMacro(mafVMEExternalData)
@@ -39,6 +44,7 @@ mafVMEExternalData::~mafVMEExternalData()
 
 //-------------------------------------------------------------------------
 void mafVMEExternalData::SetExtension(const char *ext)
+//-------------------------------------------------------------------------
 {
   mafTagItem item;
 
@@ -49,47 +55,76 @@ void mafVMEExternalData::SetExtension(const char *ext)
 
 //-------------------------------------------------------------------------
 void mafVMEExternalData::SetFileName(const char *filename)
+//-------------------------------------------------------------------------
 {
   mafTagItem item;
-
+  
   item.SetName("EXTDATA_FILENAME");
   item.SetValue(filename);
   this->GetTagArray()->SetTag(item);
 }
 
 //-------------------------------------------------------------------------
-const char *
-mafVMEExternalData::GetExtension()
+const char* mafVMEExternalData::GetExtension()
+//-------------------------------------------------------------------------
 {
   mafTagItem *item=this->GetTagArray()->GetTag("EXTDATA_EXTENSION");
   return item->GetValue();
 }
 
 //-------------------------------------------------------------------------
-const char *
-mafVMEExternalData::GetFileName()
+const char *mafVMEExternalData::GetFileName()
+//-------------------------------------------------------------------------
 {
-  mafTagItem *item=this->GetTagArray()->GetTag("EXTDATA_FILENAME");
+  mafTagItem* item=this->GetTagArray()->GetTag("EXTDATA_FILENAME");
   return item->GetValue();
 }
 
 //-------------------------------------------------------------------------
 mafString mafVMEExternalData::GetAbsoluteFileName()
+//-------------------------------------------------------------------------
 {
-  mafString filename=this->CurrentPath;
+  mafString filename = m_MSFPath;
+
+  if (m_MSFPath.IsEmpty())
+  {
+    InitializeCurrentPath();
+    filename = m_MSFPath;
+   }
+
+  // if m_MSFPath is still empty, than the file is not stored yet and I must use TmpPath
+  if (m_MSFPath.IsEmpty())
+  {
+    GetTmpPath();
+    filename = m_TmpPath;
+  }
+
+  filename.Append("/");
   filename.Append(this->GetFileName());
+  filename.Append(".");
+  filename.Append(this->GetExtension());
 
   return filename;
 }
 
 //-------------------------------------------------------------------------
+mafString mafVMEExternalData::GetTmpPath()
+//-------------------------------------------------------------------------
+{
+  mafString tmpPath = this->m_TmpPath;
+  return tmpPath;
+}
+
+//-------------------------------------------------------------------------
 void mafVMEExternalData::GetAbsoluteFileName(char *fname)
+//-------------------------------------------------------------------------
 {
   mafString::Copy(fname,this->GetAbsoluteFileName().GetCStr());
 }
 
 //-------------------------------------------------------------------------
 void mafVMEExternalData::SetMimeType(const char *mimetype)
+//-------------------------------------------------------------------------
 {
   mafTagItem item;
   item.SetName("EXTDATA_MIMETYPE");
@@ -98,18 +133,75 @@ void mafVMEExternalData::SetMimeType(const char *mimetype)
 }
  
 //-------------------------------------------------------------------------
-const char *
-mafVMEExternalData::GetMimeType()
+const char* mafVMEExternalData::GetMimeType()
+//-------------------------------------------------------------------------
 {
   mafTagItem *item=this->GetTagArray()->GetTag("EXTDATA_MIMETYPE");
   return item->GetValue();  
 }
+
+//-----------------------------------------------------------------------
+int mafVMEExternalData::InternalStore(mafStorageElement *parent)
+//-----------------------------------------------------------------------
+{
+  InitializeCurrentPath();
+
+  wxString path, pathTarget, name, ext;
+  
+  mafTagItem *t = GetTagArray()->GetTag("EXTDATA_FILENAME");
+  wxString nameNew = t->GetValue();
+
+  mafTagItem *item=this->GetTagArray()->GetTag("EXTDATA_EXTENSION");
+  wxString extNew = item->GetValue();
+
+  wxSplitPath(nameNew, &pathTarget, &name, &ext);
  
+  mafString fileNameOrigin =  GetTmpPath() + "/" + GetFileName() + "." + GetExtension();
+  mafString fileNameTarget = m_MSFPath + "/" + name + "." + extNew;
+
+  bool copySuccess = wxCopyFile(fileNameOrigin.GetCStr(), fileNameTarget.GetCStr());
+  if (copySuccess)
+  {
+    t->SetValue(name);
+  }
+  Superclass::InternalStore(parent);
+
+  return MAF_OK;
+}
+
+//-----------------------------------------------------------------------
+int mafVMEExternalData::InternalRestore(mafStorageElement *parent)
+//-----------------------------------------------------------------------
+{
+  Superclass::InternalRestore(parent);
+  m_MSFPath = "";
+  
+  return MAF_OK;
+}
+
+//-----------------------------------------------------------------------
+void mafVMEExternalData::InitializeCurrentPath()
+//-----------------------------------------------------------------------
+{
+  mafEventIO e(this,NODE_GET_STORAGE);
+  ForwardUpEvent(e);
+  mafStorage *storage=e.GetStorage();
+  m_MSFPath = storage->GetURL();
+
+  m_MSFPath.ExtractPathName();
+}
+
 //-------------------------------------------------------------------------
 void mafVMEExternalData::PrintSelf(std::ostream& os,const int indent)
+//-------------------------------------------------------------------------
 {
+  if (m_MSFPath.IsEmpty())
+  {
+    InitializeCurrentPath();
+  }
+
   mafVME::Print(os,indent);
-  os << indent << "CurrentPath: "  << this->CurrentPath.GetCStr() << "\n";
+  os << indent << "m_MSFPath: "  << this->m_MSFPath.GetCStr() << "\n";
   os << indent << "FileName: " << this->GetFileName() << "\n";
   os << indent << "File Extension: " << this->GetExtension() << "\n" ;
 	os << indent << "MIME Type: " << this->GetMimeType() << "\n";
--- VME/mafVMEExternalData.h
+++ VME/mafVMEExternalData.h
@@ -2,9 +2,9 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mafVMEExternalData.h,v $
   Language:  C++
-  Date:      $Date: 2006-09-20 12:24:03 $
-  Version:   $Revision: 1.1 $
-  Authors:   Marco Petrone
+  Date:      $Date: 2008-01-16 11:04:59 $
+  Version:   $Revision: 1.2 $
+  Authors:   Marco Petrone - Roberto Mucci
 ==========================================================================
   Copyright (c) 2002/2004 
   CINECA - Interuniversity Consortium (www.cineca.it)
@@ -28,53 +28,65 @@ public:
 
 void PrintSelf(std::ostream& os,const int indent);
 
-  /**
-  Set the file extension */
+  /**  Set the file extension */
   void SetExtension(const char *ext);
 
-  /**
-  Get the file extension */
+  /**  Get the file extension */
   const char *GetExtension();
 
-  /**
-  Set the file name*/
+  /**  Set the file name*/
   void SetFileName(const char *filename);
 
 
-  /**
-  Get the file name */
+  /**  Get the file name */
   const char *GetFileName();
 
-	/**
-	Set the mime type*/
+	/**	Set the mime type*/
 	void SetMimeType(const char *mimetype);
   
-	/**
-	Get the mime type*/
+	/**	Get the mime type*/
 	const char *GetMimeType();
 
   /**
   Return the current absolute filename for the file: this file name is
-  based on the CurrentPath, computed at runtime by the mflVMEStorage class,
+  based on the m_CurrentPath, computed at runtime by the mflVMEStorage class,
   which knows about the full path for the MSF file itself.*/
   mafString GetAbsoluteFileName();
   void GetAbsoluteFileName(char *fname);
 
-  void SetCurrentPath(mafString &path) {this->CurrentPath=path;};
-  void SetCurrentPath(const char *path) {this->CurrentPath=path;};
+  void SetCurrentPath(mafString &path) {this->m_TmpPath=path;};
+  void SetCurrentPath(const char *path) {this->m_TmpPath=path;};
 
 protected:
+
+  /** This is used to allow nested serialization of subclasses.
+  This function is called by Store and is reimplemented in subclasses.
+  Each subclass can store its own subelements which are
+  closed inside the "Device" element. Reimplemented functions
+  should first call Superclass implementation. */
+  int InternalStore(mafStorageElement *parent);
+
+  /** 
+  This function fills in the device with settings restored from the node.
+  Subclasses should reimplement it to restore custom settings. Reimplemented
+  functions should first call Superclass implementation. */
+  int InternalRestore(mafStorageElement *parent);
+
+  /** Get path of the msf file from storage */
+  void InitializeCurrentPath();
+
+  /** Get path of the external file imported not stored yet */
+  mafString GetTmpPath();
+
   mafVMEExternalData();
   virtual ~mafVMEExternalData();
 
-  mafString CurrentPath;
+  mafString m_MSFPath;
+  mafString m_TmpPath;
 
 private:
   mafVMEExternalData(const mafVMEExternalData&); // Not implemented
   void operator=(const mafVMEExternalData&); // Not implemented
-
- 
-  
 };
 
 #endif
