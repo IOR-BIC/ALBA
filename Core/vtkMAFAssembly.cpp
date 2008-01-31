afef687555e0451bdee93a2d446987ee5962b232
aqd0 <aqd0>
2008-01-31 15:58:06 +0000

    Changed VTL_LARGE_FLOAT with VTK_DOUBLE_MAX/MIN to compute bounds of visible props.

--- Core/vtkMAFAssembly.cpp
+++ Core/vtkMAFAssembly.cpp
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: vtkMAFAssembly.cpp,v $
   Language:  C++
-  Date:      $Date: 2005-11-18 14:51:26 $
-  Version:   $Revision: 1.4 $
+  Date:      $Date: 2008-01-31 15:58:06 $
+  Version:   $Revision: 1.5 $
   Authors:   Silvano Imboden
 ==========================================================================
   Copyright (c) 2002/2004
@@ -29,54 +29,64 @@
 #include "vtkActor.h"
 #include "vtkVolume.h"
 
-vtkCxxRevisionMacro(vtkMAFAssembly, "$Revision: 1.4 $");
+vtkCxxRevisionMacro(vtkMAFAssembly, "$Revision: 1.5 $");
 vtkStandardNewMacro(vtkMAFAssembly);
 
 // Construct object with no children.
+//----------------------------------------------------------------------------
 vtkMAFAssembly::vtkMAFAssembly()
+//----------------------------------------------------------------------------
 {
   this->Parts = vtkProp3DCollection::New();
 }
 
+//----------------------------------------------------------------------------
 vtkMAFAssembly::~vtkMAFAssembly()
+//----------------------------------------------------------------------------
 {
   this->Parts->Delete();
   this->Parts = NULL;
 }
 
 // Add a part to the list of Parts.
+//----------------------------------------------------------------------------
 void vtkMAFAssembly::AddPart(vtkProp3D *prop)
+//----------------------------------------------------------------------------
 {
   if ( ! this->Parts->IsItemPresent(prop) )
-    {
+  {
     this->Parts->AddItem(prop);
     this->Modified();
-    } 
+  } 
 }
 
 // Remove a part from the list of parts,
+//----------------------------------------------------------------------------
 void vtkMAFAssembly::RemovePart(vtkProp3D *prop)
+//----------------------------------------------------------------------------
 {
   if ( this->Parts->IsItemPresent(prop) )
-    {
+  {
     this->Parts->RemoveItem(prop);
     this->Modified();
-    } 
+  } 
 }
 
 // Shallow copy another assembly.
+//----------------------------------------------------------------------------
 void vtkMAFAssembly::ShallowCopy(vtkProp *prop)
+//----------------------------------------------------------------------------
 {
   vtkMAFAssembly *a = vtkMAFAssembly::SafeDownCast(prop);
   if ( a != NULL )
-    {
+  {
     this->Parts->RemoveAllItems();
     a->Parts->InitTraversal();
     for (int i=0; i<0; i++)
-      {
+    {
       this->Parts->AddItem(a->Parts->GetNextProp3D());
-      }
     }
+  }
 
   // Now do superclass
   this->vtkProp3D::ShallowCopy(prop);
@@ -160,18 +170,20 @@ int vtkMAFAssembly::RenderOpaqueGeometry(vtkViewport *ren)
 }
 */
 
+//----------------------------------------------------------------------------
 void vtkMAFAssembly::ReleaseGraphicsResources(vtkWindow *renWin)
+//----------------------------------------------------------------------------
 {
   vtkProp3D *prop3D;
-
-  for ( this->Parts->InitTraversal(); 
-        (prop3D = this->Parts->GetNextProp3D()); )
-    {
+  for ( this->Parts->InitTraversal(); prop3D = this->Parts->GetNextProp3D(); )
+  {
     prop3D->ReleaseGraphicsResources(renWin);
-    }
+  }
 }
 
+//----------------------------------------------------------------------------
 void vtkMAFAssembly::GetActors(vtkPropCollection *ac)
+//----------------------------------------------------------------------------
 {
   vtkProp3D *prop3D;
   vtkActor *actor;
@@ -179,16 +191,18 @@ void vtkMAFAssembly::GetActors(vtkPropCollection *ac)
 
   this->UpdatePaths();
   for ( this->Paths->InitTraversal(); (path = this->Paths->GetNextItem()); )
-    {
+  {
     prop3D = (vtkProp3D *)path->GetLastNode()->GetProp();
     if ( (actor = vtkActor::SafeDownCast(prop3D)) != NULL )
-      {
+    {
       ac->AddItem(actor);
-      }
     }
+  }
 }
 
+//----------------------------------------------------------------------------
 void vtkMAFAssembly::GetVolumes(vtkPropCollection *ac)
+//----------------------------------------------------------------------------
 {
   vtkProp3D *prop3D;
   vtkVolume *volume;
@@ -196,16 +210,18 @@ void vtkMAFAssembly::GetVolumes(vtkPropCollection *ac)
 
   this->UpdatePaths();
   for ( this->Paths->InitTraversal(); (path = this->Paths->GetNextItem()); )
-    {
+  {
     prop3D = (vtkProp3D *)path->GetLastNode()->GetProp();
     if ( (volume = vtkVolume::SafeDownCast(prop3D)) != NULL )
-      {
+    {
       ac->AddItem(volume);
-      }
     }
+  }
 }
 
+//----------------------------------------------------------------------------
 void vtkMAFAssembly::InitPathTraversal()
+//----------------------------------------------------------------------------
 {
   this->UpdatePaths();
   this->Paths->InitTraversal();
@@ -213,19 +229,23 @@ void vtkMAFAssembly::InitPathTraversal()
 
 // Return the next part in the hierarchy of assembly Parts.  This method 
 // returns a properly transformed and updated actor.
+//----------------------------------------------------------------------------
 vtkAssemblyPath *vtkMAFAssembly::GetNextPath()
+//----------------------------------------------------------------------------
 {
   if ( this->Paths )
-    {
+  {
     return this->Paths->GetNextItem();
-    }
+  }
   else
-    {
+  {
     return NULL;
-    }
+  }
 }
 
+//----------------------------------------------------------------------------
 int vtkMAFAssembly::GetNumberOfPaths()
+//----------------------------------------------------------------------------
 {
   this->UpdatePaths();
   return this->Paths->GetNumberOfItems();
@@ -297,7 +317,9 @@ void vtkMAFAssembly::BuildPaths(vtkAssemblyPaths *paths, vtkAssemblyPath *path)
 */
 
 // Get the bounds for the assembly as (Xmin,Xmax,Ymin,Ymax,Zmin,Zmax).
+//----------------------------------------------------------------------------
 double *vtkMAFAssembly::GetBounds()
+//----------------------------------------------------------------------------
 {
   vtkProp3D *prop3D;
   vtkAssemblyPath *path;
@@ -308,23 +330,23 @@ double *vtkMAFAssembly::GetBounds()
   this->UpdatePaths();
 
   // now calculate the new bounds
-  this->Bounds[0] = this->Bounds[2] = this->Bounds[4] = VTK_LARGE_FLOAT;
-  this->Bounds[1] = this->Bounds[3] = this->Bounds[5] = -VTK_LARGE_FLOAT;
+  this->Bounds[0] = this->Bounds[2] = this->Bounds[4] = VTK_DOUBLE_MAX;
+  this->Bounds[1] = this->Bounds[3] = this->Bounds[5] = VTK_DOUBLE_MIN;
 
   for ( this->Paths->InitTraversal(); (path = this->Paths->GetNextItem()); )
-    {
+  {
     prop3D = (vtkProp3D *)path->GetLastNode()->GetProp();
     if ( prop3D->GetVisibility() )
-      {
+    {
       propVisible = 1;
       prop3D->PokeMatrix(path->GetLastNode()->GetMatrix());
       bounds = prop3D->GetBounds();
       prop3D->PokeMatrix(NULL);
 
-      // if bounds is invalid skip merging
-      if (bounds[0]==VTK_LARGE_FLOAT && bounds[1]==-VTK_LARGE_FLOAT && \
-        bounds[2]==VTK_LARGE_FLOAT && bounds[3]==-VTK_LARGE_FLOAT &&
-        bounds[4]==VTK_LARGE_FLOAT && bounds[5]==-VTK_LARGE_FLOAT)
+      // if bounds are invalid skip merging
+      if (bounds[0] == VTK_DOUBLE_MAX && bounds[1]==VTK_DOUBLE_MIN && \
+        bounds[2]==VTK_DOUBLE_MAX && bounds[3]==VTK_DOUBLE_MIN &&
+        bounds[4]==VTK_DOUBLE_MAX && bounds[5]==VTK_DOUBLE_MIN)
       {
         continue;
       }
@@ -340,47 +362,51 @@ double *vtkMAFAssembly::GetBounds()
       bbox[21] = bounds[0]; bbox[22] = bounds[3]; bbox[23] = bounds[4];
 
       for (i = 0; i < 8; i++)
-        {
+      {
         for (n = 0; n < 3; n++)
-          {
+        {
           if (bbox[i*3+n] < this->Bounds[n*2])
-            {
+          {
             this->Bounds[n*2] = bbox[i*3+n];
-            }
+          }
           if (bbox[i*3+n] > this->Bounds[n*2+1])
-            {
+          {
             this->Bounds[n*2+1] = bbox[i*3+n];
-            }
-          }//for each coordinate axis
-        }//for each point of box
-      }//if visible && prop3d
-    }//for each path
+          }
+        }//for each coordinate axis
+      }//for each point of box
+    }//if visible && prop3d
+  }//for each path
 
   if ( ! propVisible )
-    {
+  {
     this->Bounds[0] = this->Bounds[2] = this->Bounds[4] = -1.0;
     this->Bounds[1] = this->Bounds[3] = this->Bounds[5] =  1.0;
-    }
+  }
 
   return this->Bounds;
 }
 
+//----------------------------------------------------------------------------
 unsigned long int vtkMAFAssembly::GetMTime()
+//----------------------------------------------------------------------------
 {
   unsigned long mTime=this->vtkProp3D::GetMTime();
   unsigned long time;
   vtkProp3D *prop;
 
   for (this->Parts->InitTraversal(); (prop = this->Parts->GetNextProp3D()); )
-    {
+  {
     time = prop->GetMTime();
     mTime = ( time > mTime ? time : mTime );
-    }
+  }
 
   return mTime;
 }
 
+//----------------------------------------------------------------------------
 void vtkMAFAssembly::PrintSelf(ostream& os, vtkIndent indent)
+//----------------------------------------------------------------------------
 {
   this->Superclass::PrintSelf(os,indent);
 
@@ -401,7 +427,9 @@ void vtkMAFAssembly::PrintSelf(ostream& os, vtkIndent indent)
 //-----------------------------------------------------------------------------
 // this method was reimplemented from vtkAssembly to correct div-by-zero bug
 //-----------------------------------------------------------------------------
-int vtkMAFAssembly::RenderTranslucentGeometry(vtkViewport *ren) {
+int vtkMAFAssembly::RenderTranslucentGeometry(vtkViewport *ren) 
+//----------------------------------------------------------------------------
+{
   vtkAssemblyPath *path;
   int renderedSomething = 0;
   
@@ -412,24 +440,27 @@ int vtkMAFAssembly::RenderTranslucentGeometry(vtkViewport *ren) {
     / (float)(this->Paths->GetNumberOfItems() > 0 ? this->Paths->GetNumberOfItems() : 1);
   
   // render the Paths
-  for ( this->Paths->InitTraversal(); (path = this->Paths->GetNextItem()); ) {
+  for ( this->Paths->InitTraversal(); (path = this->Paths->GetNextItem()); ) 
+  {
     vtkProp3D *prop3D = (vtkProp3D *)path->GetLastNode()->GetProp();
-    if ( prop3D->GetVisibility()) {
+    if ( prop3D->GetVisibility()) 
+    {
       prop3D->SetAllocatedRenderTime(fraction, ren);
       prop3D->PokeMatrix(path->GetLastNode()->GetMatrix());
       renderedSomething += prop3D->RenderTranslucentGeometry(ren);
       prop3D->PokeMatrix(NULL);
-      }
     }
+  }
   
   return renderedSomething > 0;
-  }
-
+}
 
 //-----------------------------------------------------------------------------
 // this method was reimplemented from vtkAssembly to correct div-by-zero bug
 //-----------------------------------------------------------------------------
-int vtkMAFAssembly::RenderOpaqueGeometry(vtkViewport *ren) {
+int vtkMAFAssembly::RenderOpaqueGeometry(vtkViewport *ren) 
+//----------------------------------------------------------------------------
+{
   vtkAssemblyPath *path;
   int renderedSomething = 0;
   
@@ -440,18 +471,20 @@ int vtkMAFAssembly::RenderOpaqueGeometry(vtkViewport *ren) {
     / (float)(this->Paths->GetNumberOfItems() > 0 ? this->Paths->GetNumberOfItems() : 1);
   
   // render the Paths
-  for ( this->Paths->InitTraversal(); (path = this->Paths->GetNextItem()); ) {
+  for ( this->Paths->InitTraversal(); (path = this->Paths->GetNextItem()); ) 
+  {
     vtkProp3D *prop3D = (vtkProp3D *)path->GetLastNode()->GetProp();
-    if ( prop3D->GetVisibility()) {
+    if ( prop3D->GetVisibility()) 
+    {
       prop3D->SetAllocatedRenderTime(fraction, ren);
       prop3D->PokeMatrix(path->GetLastNode()->GetMatrix());
       renderedSomething += prop3D->RenderOpaqueGeometry(ren);
       prop3D->PokeMatrix(NULL);
-      }
     }
+  }
   
   return renderedSomething > 0;
-  }
+}
 
 
 //-----------------------------------------------------------------------------
@@ -460,15 +493,16 @@ int vtkMAFAssembly::RenderOpaqueGeometry(vtkViewport *ren) {
 // of the hierarchy; otherwise UpdatePaths() is called.
 //-----------------------------------------------------------------------------
 void vtkMAFAssembly::UpdatePaths()
+//----------------------------------------------------------------------------
 {
   if ( this->GetMTime() > this->PathTime ||
     (this->Paths != NULL && this->Paths->GetMTime() > this->PathTime) )
-    {
+  {
     if ( this->Paths != NULL )
-      {
+    {
       this->Paths->Delete();
       this->Paths = NULL;
-      }
+    }
 
     // Create the list to hold all the paths
     this->Paths = vtkAssemblyPaths::New();
@@ -500,13 +534,15 @@ void vtkMAFAssembly::UpdatePaths()
 
     path->Delete();
     this->PathTime.Modified();
-    }
+  }
 }
 
 
 // Build assembly paths from this current assembly. A path consists of
 // an ordered sequence of props, with transformations properly concatenated.
+//----------------------------------------------------------------------------
 void vtkMAFAssembly::BuildPaths(vtkAssemblyPaths *paths, vtkAssemblyPath *path)
+//----------------------------------------------------------------------------
 {
   vtkProp3D *prop3D;
 
