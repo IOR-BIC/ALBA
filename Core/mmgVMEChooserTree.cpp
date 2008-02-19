91555e23372084a7190742cbe053194432619e3e
aqd0 <aqd0>
2008-02-19 08:46:53 +0000

    Extended to manage multiple selection of nodes.

--- Core/mmgVMEChooserTree.cpp
+++ Core/mmgVMEChooserTree.cpp
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mmgVMEChooserTree.cpp,v $
   Language:  C++
-  Date:      $Date: 2007-12-17 11:45:10 $
-  Version:   $Revision: 1.9 $
+  Date:      $Date: 2008-02-19 08:46:53 $
+  Version:   $Revision: 1.10 $
   Authors:   Paolo Quadrani
 ==========================================================================
   Copyright (c) 2001/2005 
@@ -30,6 +30,7 @@
 #include "mafVMERoot.h"
 
 #include <vector>
+#include <algorithm>
 
 //----------------------------------------------------------------------------
 // EVENT_TABLE
@@ -39,19 +40,26 @@ BEGIN_EVENT_TABLE(mmgVMEChooserTree,wxPanel)
 END_EVENT_TABLE()
 
 //----------------------------------------------------------------------------
-mmgVMEChooserTree::mmgVMEChooserTree( wxWindow *parent, mmgCheckTree *tree, ValidateCallBackType vme_accept_function,wxWindowID id, bool CloseButton, bool HideTitle, long style)
+mmgVMEChooserTree::mmgVMEChooserTree( wxWindow *parent, mmgCheckTree *tree, ValidateCallBackType vme_accept_function,wxWindowID id, bool CloseButton, bool HideTitle, long style, bool multiSelect)
 :mmgCheckTree(parent, id, CloseButton, HideTitle)
 //----------------------------------------------------------------------------
 {
   m_ChoosedNode       = NULL;
   m_ChooserTreeStyle  = style;
+  m_CheckedNode.clear();
 
+  m_MultipleSelection = multiSelect;
+ 
   if(vme_accept_function == 0)
     m_ValidateFunction = NULL;
   else
     m_ValidateFunction = vme_accept_function;
   
-  InitializeImageList();
+  if (!multiSelect)
+    InitializeImageList();
+  else
+    InitializeImageListMulti();
+  
 
   CloneSubTree(tree, &tree->GetTree()->GetRootItem(), (wxTreeItemId *)NULL);
 }
@@ -85,11 +93,24 @@ int mmgVMEChooserTree::GetVmeStatus(mafNode *node)
   image_id = ClassNameToIcon(node->GetTypeName()) + NODE_NON_VISIBLE;
   return image_id;
 }
+
+//----------------------------------------------------------------------------
+std::vector<mafNode*> mmgVMEChooserTree::GetChoosedNode()
+//----------------------------------------------------------------------------
+{
+  if (!m_MultipleSelection)
+  {
+    m_CheckedNode.clear();
+    m_CheckedNode.push_back(m_ChoosedNode);
+  }
+  return m_CheckedNode;
+}
+
 //----------------------------------------------------------------------------
 void mmgVMEChooserTree::InitializeImageList()
 //----------------------------------------------------------------------------
 {
-  // pourpose:
+  // purpose:
   // each vme-picture is combined with each state-picture,
   // all the combined picture are inserted in the image-list.
   // given a vme-class-name and a vme-state
@@ -136,12 +157,81 @@ void mmgVMEChooserTree::InitializeImageList()
   {
     wxString name = v[i];
     m_MapClassNameToIcon[name]=i*num_of_status;
-    
+
     for( int s=0; s<num_of_status; s++)
     {
       wxBitmap vmeico = mafPics.GetVmePic(name);
       if(s==0) vmeico = mafGrayScale(vmeico);
       imgs->Add(vmeico);
+    }
+  }
+  SetImageList(imgs);
+}
+//----------------------------------------------------------------------------
+void mmgVMEChooserTree::InitializeImageListMulti()
+//----------------------------------------------------------------------------
+{
+  // purpose:
+  // each vme-picture is combined with each state-picture,
+  // all the combined picture are inserted in the image-list.
+  // given a vme-class-name and a vme-state
+  // the corresponding icon index can be retrieved as 
+  // ClassNameToIcon(vme-class-name) + vme-state
+
+  std::vector<wxString>  v;
+  mafPics.GetVmeNames(v);
+
+  const int num_of_status = 5; 
+  int num_types = v.size();
+  int num_icons = num_types * (num_of_status * 2); // Added the status "Data not available"
+
+  if(num_types <= 0)
+  {
+    wxLogMessage("mmgCheckTree:  Warning - no vme-icons defined");
+    return;
+  }
+  //retrieve state icons
+  //I assume all state-icon to have the same size
+  wxBitmap state_ico[num_of_status];
+  state_ico[NODE_NON_VISIBLE] = mafPics.GetBmp("DISABLED");
+  state_ico[NODE_VISIBLE_OFF] = mafPics.GetBmp("CHECK_OFF");
+  state_ico[NODE_VISIBLE_ON]  = mafPics.GetBmp("CHECK_ON");
+  state_ico[NODE_MUTEX_OFF]   = mafPics.GetBmp("RADIO_OFF");
+  state_ico[NODE_MUTEX_ON]    = mafPics.GetBmp("RADIO_ON");
+  int sw = state_ico[0].GetWidth();
+  int sh = state_ico[0].GetHeight();
+
+  //get icon size 
+  //I assume all vme-icon to have the same size
+  wxBitmap bmp = mafPics.GetVmePic(v[0]);
+  assert(bmp != wxNullBitmap);
+  int w = bmp.GetWidth();
+  int h = bmp.GetHeight();
+  assert(w>0 && h>0);
+
+  // create the ImageList 
+  int mw = sw+w; 
+  int mh = (sh>h) ? sh : h;
+  wxImageList *imgs = new wxImageList(mw,mh,FALSE,num_icons);
+
+  for(int i=0; i<num_types; i++)
+  {
+    wxString name = v[i];
+    m_MapClassNameToIcon[name]=i*(num_of_status * 2); // Paolo 18/12/2006
+
+    int s;
+    for( s=0; s<num_of_status; s++)
+    {
+      wxBitmap vmeico = mafPics.GetVmePic(name);
+      if(s==0) vmeico = mafGrayScale(vmeico);
+      wxBitmap merged = MergeIcons(state_ico[s],vmeico);
+      imgs->Add(merged);
+
+      // Icons for missing data
+      if(s!=0) vmeico = mafGrayScale(vmeico);
+      vmeico = mafRedScale(vmeico);
+      wxBitmap missingData = MergeIcons(state_ico[s],vmeico); // Same icon as above, but represent a 
+      imgs->Add(missingData);                                 // node with no data available.
     }
   }
   SetImageList(imgs);
@@ -161,16 +251,29 @@ void mmgVMEChooserTree::OnSelectionChanged(wxTreeEvent& event)
   event.Skip();
 
   int status = GetVmeStatus(m_ChoosedNode);
-  mafEventMacro(mafEvent(this,VME_SELECTED,status != NODE_NON_VISIBLE));
+  bool enable_ok = false;
+  if (!m_MultipleSelection)
+  {
+    if (m_ValidateFunction)
+    {
+      enable_ok = m_ValidateFunction(m_ChoosedNode);
+    }
+    else
+    {
+      enable_ok = status != NODE_NON_VISIBLE;
+    }
+    mafEventMacro(mafEvent(this,VME_SELECTED, enable_ok));
+  }
 }
 //----------------------------------------------------------------------------
 void mmgVMEChooserTree::OnIconClick(wxTreeItemId item)
 //----------------------------------------------------------------------------
 {
-/*  mafNode* vme = (mafNode*) (NodeFromItem(item));
-  int status = GetVmeStatus(vme);
+  mafNode* vme = (mafNode*) (NodeFromItem(item));
+  VmeUpdateIcon(vme);
 
-  mafEventMacro(mafEvent(this,VME_SELECTED,status != NODE_NON_VISIBLE));*/
+  bool enable_ok = GetChoosedNode().size() > 0;
+  mafEventMacro(mafEvent(this,VME_SELECTED, enable_ok));
 }
 //----------------------------------------------------------------------------
 void mmgVMEChooserTree::ShowContextualMenu(wxMouseEvent& event)
@@ -231,4 +334,33 @@ void mmgVMEChooserTree::CloneSubTree(mmgCheckTree *source_tree, wxTreeItemId *so
     m_NodeTree->Expand(current_item);
     m_NodeTree->SortChildren(current_item);
   }
+  mmgTreeTableElement *el = new mmgTreeTableElement( current_item );  
+  m_NodeTable->Put(node, el);
+}
+//----------------------------------------------------------------------------
+void mmgVMEChooserTree::VmeUpdateIcon(mafNode *vme)
+//----------------------------------------------------------------------------
+{
+  int dataStatus = 1;
+  int icon_index;
+  int nodeSatus;
+
+  bool checked = IsIconChecked(ItemFromNode((long)vme));
+  if (!checked)
+  {
+    nodeSatus = NODE_VISIBLE_ON*2;
+    icon_index = ClassNameToIcon(vme->GetTypeName()) + nodeSatus;
+    m_CheckedNode.push_back(vme);
+  }
+  else
+  {
+    nodeSatus = NODE_VISIBLE_ON;
+    icon_index = ClassNameToIcon(vme->GetTypeName()) + nodeSatus;
+    std::vector<mafNode *>::iterator found = std::find(m_CheckedNode.begin(), m_CheckedNode.end(), vme);
+    if (found != m_CheckedNode.end())
+    {
+      m_CheckedNode.erase(found);
+    }
+  }
+  SetNodeIcon( (long)vme, icon_index );
 }
--- Core/mmgVMEChooserTree.h
+++ Core/mmgVMEChooserTree.h
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mmgVMEChooserTree.h,v $
   Language:  C++
-  Date:      $Date: 2007-09-07 08:35:58 $
-  Version:   $Revision: 1.3 $
+  Date:      $Date: 2008-02-19 08:46:53 $
+  Version:   $Revision: 1.4 $
   Authors:   Paolo Quadrani
 ==========================================================================
   Copyright (c) 2001/2005 
@@ -34,16 +34,21 @@ typedef bool (*ValidateCallBackType) (mafNode *);
 //----------------------------------------------------------------------------
 // mmgVMEChooserTree :
 //----------------------------------------------------------------------------
-/** 
+/** Widget representing the tree that allow the user to choose a VME. 
+This widget has the possibility to manage validation callback passed through a constructor that allows to say if a VME can be choose or not.
+Another feature of the widget is to show also a checkbox near the VME's icon to allow a multiple selection.
 */
 class mmgVMEChooserTree: public mmgCheckTree
 {
 public:
-                 mmgVMEChooserTree (wxWindow *parent, mmgCheckTree *tree, ValidateCallBackType vme_accept_function = 0, wxWindowID id=-1, bool CloseButton = false, bool HideTitle = false, long style = REPRESENTATION_AS_TREE); 
+                 mmgVMEChooserTree (wxWindow *parent, mmgCheckTree *tree, ValidateCallBackType vme_accept_function = 0, wxWindowID id=-1, bool CloseButton = false, bool HideTitle = false, long style = REPRESENTATION_AS_TREE, bool multiSelect = false); 
   virtual       ~mmgVMEChooserTree();
 
   /** Return the choosed node*/
-  mafNode *GetChoosedNode() {return m_ChoosedNode;};
+  std::vector<mafNode*> GetChoosedNode();
+
+  /** Called by the Custom-Tree-Event-Handler - via OnMouseDown*/
+  virtual void OnIconClick(wxTreeItemId item);
 
 protected:
   /** Return the status of the node according to the vme visibility. */
@@ -51,11 +56,14 @@ protected:
 
   void InitializeImageList();
 
+  /** Initialize images for multi selection dialog. */
+  void InitializeImageListMulti();
+
+  /** Update the vme nodes icon. */
+  void VmeUpdateIcon(mafNode *n);
+
   /** clone in tree a subtree of source_item */
   void CloneSubTree(mmgCheckTree *tree, wxTreeItemId *source_item, wxTreeItemId *dest_parent_item);
-
-  /** Called by the Custom-Tree-Event-Handler - via OnMouseDown*/
-  void OnIconClick(wxTreeItemId item);
 
   /** Called by the Custom-Tree-Event-Handler */
   void ShowContextualMenu(wxMouseEvent& event);
@@ -65,8 +73,10 @@ protected:
 
   ValidateCallBackType m_ValidateFunction;
 
-  mafNode   *m_ChoosedNode;
+  mafNode   *m_ChoosedNode; ///< Pointer to the selected node in single selection.
   long       m_ChooserTreeStyle;
+  std::vector<mafNode*> m_CheckedNode; ///< Vector of checked node (used in multi-selection)
+  bool m_MultipleSelection; ///< Flag that manage the single or multi selection of the nodes inside the tree.
 
   DECLARE_EVENT_TABLE()
 };
