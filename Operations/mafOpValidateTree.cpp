613e4f8349fea1ea65dad7dc4b976892b14959cf
aqd0 <aqd0>
2008-03-07 12:50:24 +0000

    Added mafOpValidateTree operation.

---
+++ Operations/mafOpValidateTree.cpp
@@ -0,0 +1,220 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: mafOpValidateTree.cpp,v $
+  Language:  C++
+  Date:      $Date: 2008-03-07 12:50:24 $
+  Version:   $Revision: 1.1 $
+  Authors:   Paolo Quadrani
+==========================================================================
+  Copyright (c) 2001/2005 
+  CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#include "mafDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+
+#include "mafOpValidateTree.h"
+#include "mafDecl.h"
+#include "mafEvent.h"
+#include "mmgGui.h"
+
+#include "mafStorage.h"
+#include "mafEventIO.h"
+#include "mafDataVector.h"
+#include "mafVMEItem.h"
+#include "mafVMEGenericAbstract.h"
+#include "mafVMERoot.h"
+#include "mafNodeIterator.h"
+
+//----------------------------------------------------------------------------
+// Constants :
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+mafCxxTypeMacro(mafOpValidateTree);
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+mafOpValidateTree::mafOpValidateTree(const wxString &label) :
+mafOp(label)
+//----------------------------------------------------------------------------
+{
+  m_OpType	= OPTYPE_OP;
+  m_Canundo = true;
+  m_MSFPath = "";
+}
+//----------------------------------------------------------------------------
+mafOpValidateTree::~mafOpValidateTree()
+//----------------------------------------------------------------------------
+{
+}
+//----------------------------------------------------------------------------
+mafOp* mafOpValidateTree::Copy()   
+//----------------------------------------------------------------------------
+{
+	return new mafOpValidateTree(m_Label);
+}
+//----------------------------------------------------------------------------
+void mafOpValidateTree::OpRun()
+//----------------------------------------------------------------------------
+{
+  if (ValidateTree())
+  {
+    wxMessageBox(_("Tree validation terminated succesfully!!."), _("Info"));
+  }
+  else
+  {
+    wxMessageBox(_("Tree invalid!! In log area you can find details."), _("Warning"));
+  }
+  mafEventMacro(mafEvent(this,OP_RUN_OK));
+}
+//----------------------------------------------------------------------------
+bool mafOpValidateTree::ValidateTree()
+//----------------------------------------------------------------------------
+{
+  bool result = true;
+  mafNode *node, *root = m_Input->GetRoot();
+  mafNodeIterator *iter = root->NewIterator();
+  try
+  {
+    for (node = iter->GetFirstNode(); node; node = iter->GetNextNode())
+    {
+      // check node ID
+      bool valid = node->IsValid();
+      if (!valid && !node->IsMAFType(mafVMERoot))
+      {
+        ErrorLog(mafOpValidateTree::INVALID_NODE, node->GetName());
+        result = false;
+      }
+      int numLinks = node->GetNumberOfLinks();
+      if (numLinks > 0)
+      {
+        // check node links
+        mafNode::mafLinksMap::iterator lnk_it;
+        for (lnk_it = node->GetLinks()->begin(); lnk_it != node->GetLinks()->end(); lnk_it++)
+        {
+          valid = lnk_it->second.m_Node->IsValid();
+          if (!valid && !lnk_it->second.m_Node->IsMAFType(mafVMERoot))
+          {
+            ErrorLog(mafOpValidateTree::INVALID_NODE, lnk_it->second.m_Node->GetName());
+            result = false;
+          }
+          valid = root->IsInTree(lnk_it->second.m_Node);
+          if (!valid)
+          {
+            ErrorLog(mafOpValidateTree::LINK_NOT_PRESENT, lnk_it->second.m_Node->GetName());
+            result = false;
+          }
+        }
+      }
+      // check data (binary data files are associated to that file that 
+      // have the DataVector => inherit from mafVMEGenericAbstract
+      mafString urlString = "";
+      mafString archiveFilename = "";
+      mafString absFilename = "";
+      mafVMEItem *item = NULL;
+      bool singleFileMode = false;
+      mafVMEGenericAbstract *vme = mafVMEGenericAbstract::SafeDownCast(node);
+      if (vme)
+      {
+        mafDataVector *dv = vme->GetDataVector();
+        if (dv)
+        {
+          for (int t = 0; t < dv->GetNumberOfItems(); t++)
+          {
+            if (m_MSFPath.IsEmpty())
+            {
+              mafEventIO e(this,NODE_GET_STORAGE);
+              vme->ForwardUpEvent(e);
+              mafStorage *storage = e.GetStorage();
+              if (storage != NULL)
+              {
+                m_MSFPath = storage->GetURL();
+                m_MSFPath.ExtractPathName();
+              }
+            }
+            // binary files exists => VME is animated on data
+            item = dv->GetItemByIndex(t);
+            if (item == NULL)
+            {
+              ErrorLog(mafOpValidateTree::ITEM_NOT_PRESENT, vme->GetName());
+              result = false;
+            }
+            else
+            {
+              urlString = item->GetURL();
+              if (urlString.IsEmpty())
+              {
+                ErrorLog(mafOpValidateTree::URL_EMPTY, vme->GetName());
+                result = false;
+              }
+              archiveFilename = item->GetArchiveFileName();
+              singleFileMode = dv->GetSingleFileMode();
+              if (singleFileMode && archiveFilename.IsEmpty())
+              {
+                ErrorLog(mafOpValidateTree::ARCHIVE_FILE_NOT_PRESENT, vme->GetName());
+                result = false;
+              }
+              absFilename = m_MSFPath;
+              absFilename << "/";
+              if (singleFileMode)
+              {
+                absFilename << archiveFilename;
+              }
+              else
+              {
+                absFilename << urlString;
+              }
+              if (!wxFileExists(absFilename.GetCStr()))
+              {
+                ErrorLog(mafOpValidateTree::BINARY_FILE_NOT_PRESENT, absFilename.GetCStr());
+                result = false;
+              }
+            } // item != NULL
+          } // for() on items
+        } // if(dv)
+      } // if(vme) vme is inherited from a mafVMEGenericAbstract => has binary data file
+    } // for () on node into the tree
+  } // try
+  catch (...)
+  {
+    ErrorLog(mafOpValidateTree::EXCEPTION_ON_ITERATOR, iter->GetCurrentNode()->GetName());
+  }
+  iter->Delete();
+  return result;
+}
+//----------------------------------------------------------------------------
+void mafOpValidateTree::ErrorLog(int error_num, const char *node_name)
+//----------------------------------------------------------------------------
+{
+  switch(error_num)
+  {
+    case mafOpValidateTree::INVALID_NODE:
+      mafLogMessage(_("node %s has an invalid ID"), node_name);
+    break;
+    case mafOpValidateTree::LINK_NOT_PRESENT:
+      mafLogMessage(_("link node %s is not present into the tree"), node_name);
+    break;
+    case mafOpValidateTree::EXCEPTION_ON_ITERATOR:
+      mafLogMessage(_("exception occurred during iteration on node %s"), node_name);
+    break;
+    case mafOpValidateTree::ITEM_NOT_PRESENT:
+      mafLogMessage(_("Item not present in node %s"), node_name);
+    break;
+    case mafOpValidateTree::URL_EMPTY:
+      mafLogMessage(_("URL referring to binary data not present in node %s"), node_name);
+    break;
+    case mafOpValidateTree::BINARY_FILE_NOT_PRESENT:
+      mafLogMessage(_("Binary data file '%s' not present"), node_name);
+    break;
+    case mafOpValidateTree::ARCHIVE_FILE_NOT_PRESENT:
+      mafLogMessage(_("Archive data file not present for node %s"), node_name);
+    break;
+  }
+}
---
+++ Operations/mafOpValidateTree.h
@@ -0,0 +1,67 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: mafOpValidateTree.h,v $
+  Language:  C++
+  Date:      $Date: 2008-03-07 12:50:24 $
+  Version:   $Revision: 1.1 $
+  Authors:   Paolo Quadrani
+==========================================================================
+Copyright (c) 2002/2004
+CINECA - Interuniversity Consortium (www.cineca.it) 
+=========================================================================*/
+
+#ifndef __mafOpValidateTree_H__
+#define __mafOpValidateTree_H__
+
+#include "mafOp.h"
+
+//----------------------------------------------------------------------------
+// forward references :
+//----------------------------------------------------------------------------
+class mafNode;
+
+//----------------------------------------------------------------------------
+// mafOpValidateTree :
+//----------------------------------------------------------------------------
+/** Class used to validate the correctness of the data tree loaded. 
+The tree to be validated has to be saved, otherwise some elements could not 
+have binary files associated with the VME. This class perform checks on VME id, 
+binary data if exists, correctness of TagArray and links.*/
+class mafOpValidateTree: public mafOp
+{
+public:
+  mafOpValidateTree(const wxString &label = "Validate Tree");
+  ~mafOpValidateTree(); 
+
+  mafTypeMacro(mafOpValidateTree, mafOp);
+
+  /** Return a copy of the operation.*/
+  mafOp* Copy();
+
+  /** This operation accept everything as input.*/
+  bool Accept(mafNode *node) {return true;};
+  
+  /** Create the user interface and initialize variables.*/
+  void OpRun();
+
+protected: 
+  /** iterate to all nodes and check correctness for each one.*/
+  bool ValidateTree();
+
+  /** Log errors into the log area according to the error number reported.*/
+  void ErrorLog(int error_num, const char *node_name);
+
+  enum ERROR_IDS
+  {
+    INVALID_NODE = 0,
+    LINK_NOT_PRESENT,
+    EXCEPTION_ON_ITERATOR,
+    ITEM_NOT_PRESENT,
+    URL_EMPTY,
+    BINARY_FILE_NOT_PRESENT,
+    ARCHIVE_FILE_NOT_PRESENT,
+  };
+
+  mafString m_MSFPath;
+};
+#endif
