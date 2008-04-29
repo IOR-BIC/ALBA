4fcce19f84762070000030ae9933b2a5e8a7e839
aqd0 <aqd0>
2008-04-29 10:47:46 +0000

    Changed inheritance from mafVMEGenericAbstract to mafVME.

--- VME/mafVMEGroup.h
+++ VME/mafVMEGroup.h
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mafVMEGroup.h,v $
   Language:  C++
-  Date:      $Date: 2008-02-08 13:48:27 $
-  Version:   $Revision: 1.5 $
+  Date:      $Date: 2008-04-29 10:47:46 $
+  Version:   $Revision: 1.6 $
   Authors:   Marco Petrone
 ==========================================================================
   Copyright (c) 2001/2005 
@@ -15,7 +15,7 @@
 //----------------------------------------------------------------------------
 // Include:
 //----------------------------------------------------------------------------
-#include "mafVMEGenericAbstract.h"
+#include "mafVME.h"
 #include "mafVMEOutputNULL.h"
 
 //----------------------------------------------------------------------------
@@ -28,19 +28,34 @@ class mafTransform;
   mafVMEGroup is a specialized VME inheriting the VME-Generic features to internally
   store pose matrices, used to create assemblies of VMEs and direct cinematic. 
   @sa mafVME mafMatrixVector */
-class MAF_EXPORT mafVMEGroup : public mafVMEGenericAbstract
+class MAF_EXPORT mafVMEGroup : public mafVME
 {
 public:
-  mafTypeMacro(mafVMEGroup,mafVMEGenericAbstract);
+  mafTypeMacro(mafVMEGroup,mafVME);
+
+  /** Copy the contents of another mafVMEProber into this one. */
+  /*virtual*/ int DeepCopy(mafNode *a);
+
+  /** Compare with another mafVMEProber. */
+  /*virtual*/ bool Equals(mafVME *vme);
 
   /** Precess events coming from other objects */ 
-  virtual void OnEvent(mafEventBase *maf_event);
+  /*virtual*/ void OnEvent(mafEventBase *maf_event);
 
   /** Return the right type of output.*/  
   mafVMEOutputNULL *GetVTKOutput() {return (mafVMEOutputNULL *)GetOutput();}
 
   /** Return the output. This create the output object on demand. */  
-  virtual mafVMEOutput *GetOutput();
+  /*virtual*/ mafVMEOutput *GetOutput();
+
+  /** Set the pose matrix for the mafVMEGroup.*/
+  void SetMatrix(const mafMatrix &mat);
+
+  /** Fill the timestamp vector with the timestamp of the pose matrix.*/
+  void GetLocalTimeStamps(std::vector<mafTimeStamp> &kframes);
+
+  /** Print the information of the pose matrix.*/
+  /*virtual*/ void Print(std::ostream& os, const int indent=0);
 
   /** return icon */
   static char** GetIcon();
@@ -52,11 +67,13 @@ protected:
   /** Internally used to create a new instance of the GUI.*/
   virtual mmgGui *CreateGui();
 
+  /*virtual*/ int InternalStore(mafStorageElement *parent);
+  /*virtual*/ int InternalRestore(mafStorageElement *node);
+
   mafTransform *m_Transform;
 
 private:
   mafVMEGroup(const mafVMEGroup&); // Not implemented
   void operator=(const mafVMEGroup&); // Not implemented
 };
-
 #endif
