7b8c8c8707cbbbfa096cb6241e9f058b64f8edf8
aqd0 <aqd0>
2008-04-29 14:19:21 +0000

    Reverted inheritance changes.

--- VME/mafVMEGroup.cpp
+++ VME/mafVMEGroup.cpp
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mafVMEGroup.cpp,v $
   Language:  C++
-  Date:      $Date: 2008-04-29 10:47:46 $
-  Version:   $Revision: 1.9 $
+  Date:      $Date: 2008-04-29 14:19:21 $
+  Version:   $Revision: 1.10 $
   Authors:   Marco Petrone
 ==========================================================================
   Copyright (c) 2001/2005 
@@ -23,11 +23,10 @@
 #include "mafVMEGroup.h"
 #include "mmgGui.h"
 
-#include "mafIndent.h"
 #include "mafTransform.h"
+#include "mafMatrixVector.h"
 #include "mafNode.h"
 #include "mafVMEOutputNULL.h"
-#include "mafStorageElement.h"
 
 //-------------------------------------------------------------------------
 mafCxxTypeMacro(mafVMEGroup)
@@ -40,6 +39,7 @@ mafVMEGroup::mafVMEGroup()
   // The output is created on demand in GetOutput() to avoid
   // subclasses to have to destroy base class output
   mafNEW(m_Transform);
+  m_MatrixVector->SetMatrix(m_Transform->GetMatrix());
 }
 
 //-------------------------------------------------------------------------
@@ -50,76 +50,13 @@ mafVMEGroup::~mafVMEGroup()
   // data pipe destroyed in mafVME
   // data vector destroyed in mafVMEGenericAbstract
 }
-//-------------------------------------------------------------------------
-int mafVMEGroup::DeepCopy(mafNode *a)
-//-------------------------------------------------------------------------
-{
-  if (Superclass::DeepCopy(a) == MAF_OK)
-  {
-    mafVMEGroup *group = mafVMEGroup::SafeDownCast(a);
-    m_Transform->SetMatrix(group->m_Transform->GetMatrix());
-    return MAF_OK;
-  }
-  return MAF_ERROR;
-}
-//-------------------------------------------------------------------------
-bool mafVMEGroup::Equals(mafVME *vme)
-//-------------------------------------------------------------------------
-{
-  bool ret = false;
-  if (Superclass::Equals(vme))
-  {
-    ret = m_Transform->GetMatrix() == ((mafVMEGroup *)vme)->m_Transform->GetMatrix();
-  }
-  return ret;
-}
-//-------------------------------------------------------------------------
-void mafVMEGroup::SetMatrix(const mafMatrix &mat)
-//-------------------------------------------------------------------------
-{
-  m_Transform->SetMatrix(mat);
-}
 
-//-------------------------------------------------------------------------
-int mafVMEGroup::InternalStore(mafStorageElement *parent)
-//-------------------------------------------------------------------------
-{
-  if (Superclass::InternalStore(parent)==MAF_OK)
-  {
-    parent->StoreMatrix("Transform",&m_Transform->GetMatrix());
-    return MAF_OK;
-  }
-  return MAF_ERROR;
-}
-//-------------------------------------------------------------------------
-int mafVMEGroup::InternalRestore(mafStorageElement *node)
-//-------------------------------------------------------------------------
-{
-  if (Superclass::InternalRestore(node)==MAF_OK)
-  {
-    mafMatrix matrix;
-    if (node->RestoreMatrix("Transform",&matrix)==MAF_OK)
-    {
-      m_Transform->SetMatrix(matrix);
-      return MAF_OK;
-    }
-  }
-
-  return MAF_ERROR;
-}
-//-------------------------------------------------------------------------
-void mafVMEGroup::GetLocalTimeStamps(std::vector<mafTimeStamp> &kframes)
-//-------------------------------------------------------------------------
-{
-  kframes.clear();
-  kframes.push_back(m_Transform->GetTimeStamp());
-}
 //-------------------------------------------------------------------------
 mafVMEOutput *mafVMEGroup::GetOutput()
 //-------------------------------------------------------------------------
 {
   // allocate the right type of output on demand
-  if (m_Output == NULL)
+  if (m_Output==NULL)
   {
     SetOutput(mafVMEOutputNULL::New()); // create the output
   }
@@ -180,16 +117,6 @@ void mafVMEGroup::OnEvent(mafEventBase *maf_event)
     ForwardDownEvent(maf_event);
     return;
   }
-}
-//----------------------------------------------------------------------------
-void mafVMEGroup::Print(std::ostream& os, const int indent)
-//----------------------------------------------------------------------------
-{
-  Superclass::Print(os,indent);
-
-  mafIndent the_indent(indent);
-  os << the_indent << "Matrix:" << std::endl;
-  m_Transform->Print(os,the_indent.GetNextIndent());
 }
 
 //-------------------------------------------------------------------------
