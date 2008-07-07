b2a0a3893088b54276a07e4f0cc9e0bf58e779b8
ior01 <ior01>
2008-07-07 14:34:57 +0000

    coding convention fix

--- Operations/mafVMEMeshAnsysTextExporter.h
+++ Operations/mafVMEMeshAnsysTextExporter.h
@@ -2,8 +2,8 @@
 Program:   Multimod Application Framework
 Module:    $RCSfile: mafVMEMeshAnsysTextExporter.h,v $
 Language:  C++
-Date:      $Date: 2008-07-07 14:30:06 $
-Version:   $Revision: 1.2 $
+Date:      $Date: 2008-07-07 14:34:57 $
+Version:   $Revision: 1.3 $
 Authors:   Stefano Perticoni
 ==========================================================================
 Copyright (c) 2002/2004 
@@ -72,7 +72,7 @@ public:
   /** Apply abs matrix to geometry */
   void ApplyMatrixOn() {m_ApplyMatrixFlag = 1;};
   void ApplyMatrixOff() {m_ApplyMatrixFlag = 0;};
-  void SetApplyMatrix(int apply_matrix) {m_ApplyMatrixFlag = apply_matrix;};
+  void SetApplyMatrix(int applyMatrix) {m_ApplyMatrixFlag = applyMatrix;};
 
   /** Write output files  */
   void Write();
