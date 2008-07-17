374b193941c5e876e6d6c1eaed7835dfff94c186
aqd0 <aqd0>
2008-07-17 16:01:04 +0000

    Commit for Giunchi: improved WiiMote device interface

--- Interaction/mafDeviceTrackerWIIMote.cpp
+++ Interaction/mafDeviceTrackerWIIMote.cpp
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mafDeviceTrackerWIIMote.cpp,v $
   Language:  C++
-  Date:      $Date: 2008-02-27 17:20:16 $
-  Version:   $Revision: 1.1 $
+  Date:      $Date: 2008-07-17 16:01:04 $
+  Version:   $Revision: 1.2 $
   Authors:   Daniele Giunchi
 ==========================================================================
   Copyright (c) 2002/2004 
@@ -19,8 +19,8 @@
 
 //include wiimote drivers
 #include "Console.h"
-#include "Utils.h"
-#include "Wiimote.h"
+/*#include "Utils.h"
+#include "Wiimote.h"*/
 
 #include "mafMatrix.h"
 
@@ -37,17 +37,9 @@ mafDeviceTrackerWIIMote::mafDeviceTrackerWIIMote()
 {
   this->SetThreaded(1);
   mafNEW(m_TmpPose);
-  /*m_IndexValue  = 0;
-  m_MiddleValue = 0;
-  m_RingValue   = 0;
-  m_PinkyValue  = 0;
-  m_ThumbValue  = 0;
-  m_IndexSensitivity  = 0;
-  m_MiddleSensitivity = 0;
-  m_RingSensitivity   = 0;
-  m_PinkySensitivity  = 0;
-  m_ThumbSensitivity  = 0;
-  SetNumberOfButtons(5);*/
+  
+	m_XTranslation = m_YTranslation = m_ZTranslation = 0.;
+	m_TranslationCalibration = 1.;
 }
 
 //------------------------------------------------------------------------------
@@ -61,54 +53,6 @@ mafDeviceTrackerWIIMote::~mafDeviceTrackerWIIMote()
 int mafDeviceTrackerWIIMote::InternalInitialize()
 //------------------------------------------------------------------------------
 {
-  /*if (m_P5==NULL)
-  {
-    m_P5 = new CP5DLL;
-  }
-
-  if (!m_P5->P5_Init())
-	{
-	  mafErrorMacro("P5 Tracker Not Found");
-    delete m_P5;
-    m_P5=NULL;
-    return -1;
-	}
-	else
-	{
-    if (m_P5->m_nNumP5==0)
-    {
-      mafErrorMacro("0 gloves connected to P5, shutdown...");
-      m_P5->P5_Close();
-      delete m_P5;
-      m_P5=NULL;
-      return -1;
-    }
-    
-		RECT region;
-
-		P5Motion_Init(m_P5, 0);
-
-		P5Motion_InvertMouse(P5MOTION_INVERTAXIS, P5MOTION_NORMALAXIS, P5MOTION_NORMALAXIS);
-#ifdef WIN32
-    GetClipCursor(&region);
-		P5Motion_SetClipRegion(region.left, region.right, region.top, region.bottom, region.top, region.bottom);
-#endif
-
-    // initilize the bend utility routine
-    P5Bend_Init(m_P5, 0);
-
-    // set sensitivity (notice P5 lib has a global sensitivity variable)
-    SetThumbSensitivity(nBendSensitivity[P5_THUMB]);
-	  SetIndexSensitivity(nBendSensitivity[P5_INDEX]);
-	  SetMiddleSensitivity(nBendSensitivity[P5_MIDDLE]);
-	  SetRingSensitivity(nBendSensitivity[P5_RING]);
-	  SetPinkySensitivity(nBendSensitivity[P5_PINKY]);
-
-    // disable Mouse modality for P5 0
-		m_P5->P5_SetMouseState(0, FALSE);
-
-    mafLogMessage("P5 Tracker Found & Initialized");
-	}*/
   wiimotes = HIDDevice::ConnectToHIDDevices<Wiimote>();
 
 	if (wiimotes.size() <= 0) // Need to add a user fri
@@ -148,73 +92,49 @@ void mafDeviceTrackerWIIMote::InternalShutdown()
 int mafDeviceTrackerWIIMote::InternalUpdate()
 //------------------------------------------------------------------------------
 { 
-  //float xyz[3];
-  //float pyr[3];
 
-	/*if( m_P5 && m_P5->m_P5Devices != NULL)
-	{
-    
-    P5Data glove = m_P5->m_P5Devices[0];
-    P5Motion_Process();
-
-    m_TmpPose->Identity();
-    mafTransform::SetPosition(*m_TmpPose,fFilterX, fFilterY, -fFilterZ);
-    
-    // todo: to be checked
-    mafTransform::RotateZ(*m_TmpPose,-fAbsRollPos, PRE_MULTIPLY);
-    mafTransform::RotateX(*m_TmpPose,fAbsPitchPos, PRE_MULTIPLY);
-    mafTransform::RotateY(*m_TmpPose,-fAbsYawPos, PRE_MULTIPLY);
-		
-    
-    // the matrix is automatically copied and timestamped
-    SetLastPoseMatrix(*m_TmpPose);
-    
-    unsigned char value;
-
-    value=((unsigned char)glove.m_byBendSensor_Data[P5_INDEX]);
-		m_IndexValue = value;
-		
-		value=((unsigned char)glove.m_byBendSensor_Data[P5_MIDDLE]);
-		m_MiddleValue = value;
-		
-		value=((unsigned char)glove.m_byBendSensor_Data[P5_RING]);
-		m_RingValue = value;
-		
-		value=((unsigned char)glove.m_byBendSensor_Data[P5_PINKY]);
-		m_PinkyValue = value;
-
-		value=((unsigned char)glove.m_byBendSensor_Data[P5_THUMB]);
-		m_ThumbValue = value;
-
-    // update bend sensors sensitivity
-		P5Bend_SetClickSensitivity(P5_THUMB, m_ThumbSensitivity);
-		P5Bend_SetClickSensitivity(P5_INDEX, m_IndexSensitivity);
-		P5Bend_SetClickSensitivity(P5_MIDDLE, m_MiddleSensitivity);
-		P5Bend_SetClickSensitivity(P5_RING, m_RingSensitivity);
-		P5Bend_SetClickSensitivity(P5_PINKY, m_PinkySensitivity);
-
-		P5Bend_Process();
-
-    SetButtonState(0,bP5ClickLevel[P5_INDEX]);
-    SetButtonState(1,bP5ClickLevel[P5_MIDDLE]);
-    SetButtonState(2,bP5ClickLevel[P5_RING]);
-    SetButtonState(3,bP5ClickLevel[P5_PINKY]);
-    SetButtonState(4,bP5ClickLevel[P5_THUMB]);
-    
-    Sleep(25);
-    
-    return 0;
-	}
-	else
-  {
-    return -1; // stop UpdateLoop
-  }*/
 	if(wiimotes.size() > 0)
 	{
 		MotionData p = wiimotes[0].GetLastMotionData();
-		Button & b = wiimotes[0].GetButton("A");
-		Sleep(525);
-		mafLogMessage("%d %d %d - button : %s" , p.x, p.y, p.z, b.Pressed()?"pressed":"");
+		Button & buttonA = wiimotes[0].GetButton("A");
+		Button & buttonB = wiimotes[0].GetButton("B");
+		//Button & buttonC = wiimotes[0].GetButton("C");
+		//Button & buttonZ = wiimotes[0].GetButton("Z");
+		
+
+		mafLogMessage("%d %d %d - button : %s %s" , p.x, p.y, p.z, \
+			buttonA.Pressed()?"pressedA":"", buttonB.Pressed()?"pressedB":"");
+
+		/*
+		//with nunchuck
+		mafLogMessage("%d %d %d - button : %s %s %s %s" , p.x, p.y, p.z, \
+			            buttonA.Pressed()?"pressedA":"", buttonB.Pressed()?"pressedB":"",buttonC.Pressed()?"pressedC":"",buttonZ.Pressed()?"pressedZ":"");
+    */
+
+		m_TranslationCalibration =  .1;
+		
+		
+		SetTranlationAndRotation(p);
+		
+		
+		m_TmpPose->Identity();
+		mafTransform::SetPosition(*m_TmpPose,m_XTranslation, m_YTranslation, m_ZTranslation);
+
+		// todo: to be checked
+		/*mafTransform::RotateZ(*m_TmpPose,-fAbsRollPos, PRE_MULTIPLY);
+		mafTransform::RotateX(*m_TmpPose,fAbsPitchPos, PRE_MULTIPLY);
+		mafTransform::RotateY(*m_TmpPose,-fAbsYawPos, PRE_MULTIPLY);*/
+
+		mafLogMessage("MATRIX");
+		mafLogMessage("%.2f", m_TmpPose->GetElement(0,3));
+		mafLogMessage("%.2f", m_TmpPose->GetElement(1,3));
+		mafLogMessage("%.2f", m_TmpPose->GetElement(2,3));
+		mafLogMessage("END MATRIX");
+    
+		// the matrix is automatically copied and timestamped
+		SetLastPoseMatrix(*m_TmpPose);
+
+		Sleep(25);
 		return 0;
 	}
 	else
@@ -222,4 +142,32 @@ int mafDeviceTrackerWIIMote::InternalUpdate()
 		return -1;
 	}
 }
+//------------------------------------------------------------------------------
+void mafDeviceTrackerWIIMote::SetTranlationAndRotation(MotionData p)
+//------------------------------------------------------------------------------
+{
+	if (last_positions.size() <= 2)
+	{	
+		last_positions.push_back(p);
+		return;
+	}
 
+	last_positions.erase(last_positions.begin());
+
+	/*if (p.y > 30) p.y = 30; // Restrict the values to -30 to 30
+	if (p.y < -30) p.y = -30;*/
+
+	last_positions.push_back(p);
+
+	m_XTranslation = (last_positions[1].x - last_positions[0].x) * m_TranslationCalibration;
+	m_YTranslation = (last_positions[1].y - last_positions[0].y) * m_TranslationCalibration;
+	m_ZTranslation = (last_positions[1].z - last_positions[0].z) * m_TranslationCalibration;
+
+	m_Angle = m_TranslationCalibration * (last_positions[0].x + last_positions[1].x + last_positions[2].x) / 3.0;
+}
+//------------------------------------------------------------------------------
+void mafDeviceTrackerWIIMote::Calibration()
+//------------------------------------------------------------------------------
+{
+
+}
--- Interaction/mafDeviceTrackerWIIMote.h
+++ Interaction/mafDeviceTrackerWIIMote.h
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mafDeviceTrackerWIIMote.h,v $
   Language:  C++
-  Date:      $Date: 2008-02-27 17:20:16 $
-  Version:   $Revision: 1.1 $
+  Date:      $Date: 2008-07-17 16:01:04 $
+  Version:   $Revision: 1.2 $
   Authors:   Daniele Giunchi
 ==========================================================================
   Copyright (c) 2002/2004 
@@ -14,6 +14,10 @@
 #define __mafDeviceTrackerWIIMote_h
 
 #include "mmdTracker.h"
+
+#include "Utils.h"
+#include "Wiimote.h"
+
 #include <vector>
 using namespace std;
 //----------------------------------------------------------------------------
@@ -22,56 +26,15 @@ using namespace std;
 class mafMatrix;
 class Wiimote;
 
-/** class supporting device P5 glove from Essential Reality Inc.
+/** class supporting device WII Mote from Nintendo WII Console.
   .SECTION Description
-  This class implements Device P5 management as a Tracker device. It currently
-  support only one P5 device!
-  @sa  mmdTracker mafDevice
-  @todo
-  - create base class for gloves
-  - create GUI with specific settings
-  - buttons support
-  - mouse mode support
-  - improve stability
-  - support multiple P5 gloves
+  This class implements Device WII Mote management as a Tracker device. It currently
+  support only one WII Mote!
 */
 class mafDeviceTrackerWIIMote : public mmdTracker
 {
 public:
   mafTypeMacro(mafDeviceTrackerWIIMote,mmdTracker);
-
-  /** Get the finger bend sensor current value, range [0,63] */
-  int GetIndexValue() {return m_IndexValue;}
-  /** Get the finger bend sensor current value, range [0,63] */
-  int GetMiddleValue() {return m_MiddleValue;}
-  /** Get the finger bend sensor current value, range [0,63] */
-  int GetRingValue() {return m_RingValue;}
-  /** Get the finger bend sensor current value, range [0,63] */
-  int GetPinkyValue() {return m_PinkyValue;}
-  /** Get the finger bend sensor current value, range [0,63] */
-  int GetThumbValue() {return m_ThumbValue;}
-
-  /** Set the finger bend sensor sensitivity, range [0,30] */
-  void SetIndexSensitivity(int value) {m_IndexSensitivity=value;}
-  /** Set the finger bend sensor sensitivity, range [0,30] */
-  void SetMiddleSensitivity(int value) {m_MiddleSensitivity=value;}
-  /** Set the finger bend sensor sensitivity, range [0,30] */
-  void SetRingSensitivity(int value) {m_RingSensitivity=value;}
-  /** Set the finger bend sensor sensitivity, range [0,30] */
-  void SetPinkySensitivity(int value) {m_PinkySensitivity=value;}
-  /** Set the finger bend sensor sensitivity, range [0,30] */
-  void SetThumbSensitivity(int value) {m_ThumbSensitivity=value;}
-
-  /** Get the finger bend sensor sensitivity, range [0,30] */
-  int GetIndexSensitivity(int value) {m_IndexSensitivity=value;}
-  /** Get the finger bend sensor sensitivity, range [0,30] */
-  int GetMiddleSensitivity(int value) {m_MiddleSensitivity=value;}
-  /** Get the finger bend sensor sensitivity, range [0,30] */
-  int GetRingSensitivity(int value) {m_RingSensitivity=value;}
-  /** Get the finger bend sensor sensitivity, range [0,30] */
-  int GetPinkySensitivity(int value) {m_PinkySensitivity=value;}
-  /** Get the finger bend sensor sensitivity, range [0,30] */
-  int GetThumbSensitivity(int value) {m_ThumbSensitivity=value;}
 
 protected:
   mafDeviceTrackerWIIMote();
@@ -85,20 +48,21 @@ protected:
   /** close P5DLL */
   virtual void InternalShutdown();
 
-  int m_IndexValue;
-  int m_MiddleValue;
-  int m_RingValue;
-  int m_PinkyValue;
-  int m_ThumbValue;
-  
-  int m_IndexSensitivity;
-  int m_MiddleSensitivity;
-  int m_RingSensitivity;
-  int m_PinkySensitivity;
-  int m_ThumbSensitivity;
+	void Calibration();
+	void SetTranlationAndRotation(MotionData p);
   
   mafMatrix  *m_TmpPose;
 	vector<Wiimote> wiimotes;
+
+	
+	double m_XTranslation, m_YTranslation, m_ZTranslation;
+  double m_TranslationCalibration;
+
+	double m_Angle;
+
+	MotionData p;
+	vector<MotionData> last_positions;
+
 
 private:
   mafDeviceTrackerWIIMote(const mafDeviceTrackerWIIMote&);  // Not implemented.
