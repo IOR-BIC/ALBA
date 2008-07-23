03225cd9e54db0c521ae82bc2481d06cf51b5342
ior04 <ior04>
2008-07-23 10:32:08 +0000

    Add Test for CT View

---
+++ Testing/Views/mafViewCTTest.cpp
@@ -0,0 +1,40 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: mafViewCTTest.cpp,v $
+Language:  C++
+Date:      $Date: 2008-07-23 10:32:08 $
+Version:   $Revision: 1.1 $
+Authors:   Daniele Giunchi
+==========================================================================
+Copyright (c) 2002/2004 
+CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#include "mafDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include <cppunit/config/SourcePrefix.h>
+#include "mafViewCT.h"
+#include "mafViewCTTest.h"
+
+#include "mafVMESurface.h"
+#include "mafSceneGraph.h"
+
+#include <iostream>
+
+//-------------------------------------------------------------------------
+void mafViewCTTest::CreateDestroyTest()
+//-------------------------------------------------------------------------
+{
+  mafViewCT *ctView = new mafViewCT();
+  ctView->Delete();
+   
+  CPPUNIT_ASSERT(true);
+}
+
+  
