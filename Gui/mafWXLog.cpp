4a2a320c4b7296f66b062e1ca422b0ce694831c7
aqd0 <aqd0>
2008-10-17 11:51:19 +0000

    Added return a value to SetFileName that return MAF_OK only if filename can be created correctly.

--- Gui/mafWXLog.cpp
+++ Gui/mafWXLog.cpp
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mafWXLog.cpp,v $
   Language:  C++
-  Date:      $Date: 2005-04-11 11:22:19 $
-  Version:   $Revision: 1.2 $
+  Date:      $Date: 2008-10-17 11:51:19 $
+  Version:   $Revision: 1.2.22.1 $
   Authors:   Silvano Imboden
 ==========================================================================
   Copyright (c) 2002/2004
@@ -19,6 +19,7 @@
 // "Failure#0: The value of ESP was not properly saved across a function call"
 //----------------------------------------------------------------------------
 #include "mafWXLog.h"
+#include <wx/datetime.h>
 
 // ----------------------------------------------------------------------------
 mafWXLog::mafWXLog(wxTextCtrl *pTextCtrl)
@@ -39,7 +40,9 @@ void mafWXLog::DoLogString(const wxChar *szString, time_t WXUNUSED(t))
 // ----------------------------------------------------------------------------
 {
   wxString msg;
-  TimeStamp(&msg);
+  //TimeStamp(&msg);
+  wxDateTime log_time = wxDateTime::UNow();
+  msg = wxString::Format("%02d:%02d:%02d:%03d ",log_time.GetHour(), log_time.GetMinute(),log_time.GetSecond(), log_time.GetMillisecond());
   msg << szString << wxT('\n');
   m_pTextCtrl->AppendText(msg);
     
@@ -56,10 +59,11 @@ void mafWXLog::LogToFile(bool on)
   m_LogToFile = on;
 }
 // ----------------------------------------------------------------------------
-void mafWXLog::SetFileName(wxString filename)
+int mafWXLog::SetFileName(wxString filename)
 // ----------------------------------------------------------------------------
 {
  	if(m_fp) fclose(m_fp);
   m_fp = NULL;
   m_fp = fopen(filename.c_str(),"w");
+  return m_fp != NULL ? MAF_OK : MAF_ERROR;
 }
