20aaba9b41d23b209609d1e617aca14d70ed46d4
aqd0 <aqd0>
2008-12-02 15:07:38 +0000

    Commit for Giunchi: Send for floadslider also the event type. In this way high logic level it can be discriminate when update data (always or only when the mouse button is released)

--- Gui/mafGUIValidator.cpp
+++ Gui/mafGUIValidator.cpp
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mafGUIValidator.cpp,v $
   Language:  C++
-  Date:      $Date: 2008-09-04 10:23:38 $
-  Version:   $Revision: 1.2 $
+  Date:      $Date: 2008-12-02 15:07:38 $
+  Version:   $Revision: 1.2.2.1 $
   Authors:   Silvano Imboden
 ==========================================================================
   Copyright (c) 2002/2004
@@ -943,7 +943,7 @@ void mafGUIValidator::OnScrollEvent(wxScrollEvent& event)
 				TransferFromWindow();
 				s.Printf("%g",*m_DoubleVar);
 				m_TextCtrl->SetValue(s);
-				mafEventMacro(mafEvent(m_FloatSlider, m_ModuleId, m_FloatSlider));
+				mafEventMacro(mafEvent(m_FloatSlider, m_ModuleId, m_FloatSlider,event.GetEventType()));
       break;
 		}
   }
