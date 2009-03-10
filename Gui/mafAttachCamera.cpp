d80f5153e62fa47c6ca92b412ea3e9469bebce32
ior01 <ior01>
2009-03-10 14:55:25 +0000

    mafAttachCameraTest

--- Gui/mafAttachCamera.cpp
+++ Gui/mafAttachCamera.cpp
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mafAttachCamera.cpp,v $
   Language:  C++
-  Date:      $Date: 2008-07-25 07:03:23 $
-  Version:   $Revision: 1.20 $
+  Date:      $Date: 2009-03-10 14:55:25 $
+  Version:   $Revision: 1.20.2.1 $
   Authors:   Paolo Quadrani
 ==========================================================================
   Copyright (c) 2002/2004
@@ -37,18 +37,23 @@
 mafAttachCamera::mafAttachCamera(wxWindow* parent, mafRWI *rwi, mafObserver *Listener)
 //----------------------------------------------------------------------------
 {
-	m_Rwi = rwi;
+  m_Gui = NULL;
+
+  m_RenderWindow = rwi;
 	
 	m_Listener		= Listener;
 	m_ParentPanel = parent;
 
-  m_CameraAttach = 0;
+  m_EnableAttachCamera = 0;
   
   m_AttachedVme       = NULL;
   m_AttachedVmeMatrix = NULL;
   vtkNEW(m_StartingMatrix);
 
-	CreateGui();
+  if (parent)
+  {
+    CreateGui();
+  }
 }
 //----------------------------------------------------------------------------
 mafAttachCamera::~mafAttachCamera() 
@@ -78,7 +83,7 @@ void mafAttachCamera::CreateGui()
 {
 	m_Gui = new mafGUI(this);
 	m_Gui->Show(true);
-  m_Gui->Bool(ID_CAMERA_ATTACH,"attach",&m_CameraAttach,0,"Attach the camera to selected vme");
+  m_Gui->Bool(ID_CAMERA_ATTACH,"attach",&m_EnableAttachCamera,0,"Attach the camera to selected vme");
 	m_Gui->Divider();
 	m_Gui->Update();
 }
@@ -91,12 +96,12 @@ void mafAttachCamera::OnEvent(mafEventBase *maf_event)
     switch(e->GetId())
     {
       case ID_CAMERA_ATTACH:
-        if(m_CameraAttach)
+        if(m_EnableAttachCamera)
         {
-          mafVME *sel_vme = mafVME::SafeDownCast(m_Rwi->m_Sg->GetSelectedVme());
+          mafVME *sel_vme = mafVME::SafeDownCast(m_RenderWindow->m_Sg->GetSelectedVme());
           if(sel_vme == NULL) 
           {
-            m_CameraAttach = 0;
+            m_EnableAttachCamera = 0;
             m_Gui->Update();
             return;
           }
@@ -117,7 +122,7 @@ void mafAttachCamera::OnEvent(mafEventBase *maf_event)
       case NODE_DETACHED_FROM_TREE:
       case NODE_DESTROYED:
         // the VME observed will be removed on the destructor.
-        m_CameraAttach = 0;
+        m_EnableAttachCamera = 0;
         m_Gui->Update();
       break;
       case VME_TIME_SET:
@@ -138,7 +143,7 @@ void mafAttachCamera::SetVme(mafNode *node)
     }
     vtkDEL(m_AttachedVmeMatrix);
     m_AttachedVme = NULL;
-    m_CameraAttach = 0;
+    m_EnableAttachCamera = 0;
     if (m_Gui != NULL)
     {
       m_Gui->Update();
@@ -160,9 +165,12 @@ void mafAttachCamera::SetVme(mafNode *node)
   delta->PreMultiply();
   delta->Concatenate(m_StartingMatrix);
 
-  m_Rwi->m_Camera->ApplyTransform(delta);
+  if (m_RenderWindow)
+  {
+    m_RenderWindow->m_Camera->ApplyTransform(delta);
+    m_RenderWindow->CameraUpdate();
+  }
 
-  m_Rwi->CameraUpdate();
   m_AttachedVme->GetEventSource()->AddObserver(this);
 }
 //----------------------------------------------------------------------------
@@ -176,7 +184,7 @@ void mafAttachCamera::SetStartingMatrix(mafMatrix *matrix)
 void mafAttachCamera::UpdateCameraMatrix()
 //----------------------------------------------------------------------------
 {
-  if (m_CameraAttach == 0)
+  if (m_EnableAttachCamera == 0)
   {
     return;
   }
@@ -192,7 +200,75 @@ void mafAttachCamera::UpdateCameraMatrix()
     delta->Concatenate(new_matrix);
     delta->Concatenate(m_AttachedVmeMatrix);
 
-    m_Rwi->m_Camera->ApplyTransform(delta);
+    m_RenderWindow->m_Camera->ApplyTransform(delta);
     m_AttachedVmeMatrix->DeepCopy(new_matrix);
   }
+}
+
+void mafAttachCamera::PrintSelf( ostream& os)
+{
+  os << "-------------------------------------------------------" << std::endl;
+  os << "mafAttachCamera " << this << " PrintSelf:" << std::endl;
+  
+  os << "Camera attach enabled flag m_EnableAttachCamera: " << m_EnableAttachCamera << std::endl;
+  os << "Vme to follow with camera m_AttachedVme: " << (m_AttachedVme ? m_AttachedVme->GetName() : "NULL") << std::endl; 
+  os << "m_AttachedVmeMatrix: " << m_AttachedVmeMatrix << std::endl;
+  if (m_AttachedVmeMatrix)
+  {
+    m_AttachedVmeMatrix->PrintSelf(os, NULL);
+  }
+
+
+  os << "m_StartingMatrix: " << m_StartingMatrix << std::endl;
+  
+  if (m_StartingMatrix)
+  {
+    m_StartingMatrix->PrintSelf(os,NULL);
+  }
+
+  os << "m_Listener: " << m_Listener << std::endl;
+  os << "m_Gui: " << m_Gui << std::endl;
+  os << "m_RenderWindow: " << m_RenderWindow << std::endl;
+  os << "m_ParentPanel: " << m_ParentPanel << std::endl;
+  os << "-------------------------------------------------------" << std::endl;
+}
+
+mafMatrix mafAttachCamera::GetStartingMatrix()
+{
+  return mafMatrix(m_StartingMatrix);
+}
+
+void mafAttachCamera::SetListener( mafObserver *Listener )
+{
+  m_Listener = Listener;
+}
+
+mafObserver * mafAttachCamera::GetListener()
+{
+  return m_Listener;
+}
+
+void mafAttachCamera::EnableAttachCamera()
+{
+  m_EnableAttachCamera = TRUE;
+}
+
+void mafAttachCamera::DisableAttachCamera()
+{
+  m_EnableAttachCamera = FALSE;
+}
+
+int mafAttachCamera::GetEnableAttachCamera()
+{
+  return m_EnableAttachCamera;
+}
+
+void mafAttachCamera::SetEnableAttachCamera( int enable )
+{
+  m_EnableAttachCamera = enable;
+}
+
+mafNode * mafAttachCamera::GetVme()
+{
+  return m_AttachedVme;
 }
--- Gui/mafAttachCamera.h
+++ Gui/mafAttachCamera.h
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mafAttachCamera.h,v $
   Language:  C++
-  Date:      $Date: 2008-07-25 07:03:23 $
-  Version:   $Revision: 1.7 $
+  Date:      $Date: 2009-03-10 14:55:25 $
+  Version:   $Revision: 1.7.2.1 $
   Authors:   Paolo Quadrani
 ==========================================================================
   Copyright (c) 2002/2004
@@ -18,6 +18,9 @@
 //----------------------------------------------------------------------------
 #include "mafEvent.h"
 #include "mafObserver.h"
+#include <ostream>
+
+using namespace std;
 
 //----------------------------------------------------------------------------
 // forward references :
@@ -33,41 +36,57 @@ class vtkMatrix4x4;
 /**
 mafAttachCamera is tool to attach the camera present in mafRWI to the selected VME.
 This tool has to be updated by calling UpdateCameraMatrix() during CameraUpdate into a view like mafViewVTK
-\sa mafRWI mafViewVTK
+
+@sa mafViewVTK for an example on how to use it, mafRWI
+
 */
 class mafAttachCamera : public mafObserver
 {
 public:
+
+  /** Attach camera is disabled by default: use EnableAttachCamera() to enable attaching */
 	mafAttachCamera(wxWindow* parent, mafRWI *rwi, mafObserver *Listener = NULL);
 	~mafAttachCamera(); 
-	
-	void OnEvent(mafEventBase *maf_event);
-	void SetListener(mafObserver *Listener) {m_Listener = Listener;};
-
-	/** 
-  Returns the mafAttachCamera's GUI */
-	mafGUI *GetGui() {return m_Gui;};
+  
+  /** Enable/Disable attach camera to vme */
+  void EnableAttachCamera();
+  void DisableAttachCamera();
+  void SetEnableAttachCamera(int enable);
+  int GetEnableAttachCamera();
 
   /** 
-  Update the camera according to the absolute position of the attached VME.*/
+  Update the camera according to the absolute position of the attached VME.
+  To be used in client view overridden CameraUpdate() method*/
   void UpdateCameraMatrix();
 
   /** Set the vme to follow with camera. 
   This method is optional; in alternative the vme to follow is set on the attach camera activation keeping the selected vme.*/
   void SetVme(mafNode *node);
+  mafNode *GetVme();
 
-  /**Set an initial transform matrix before attaching camera, this method must be called before SetVme*/
+  /**Set an initial transform matrix before attaching camera. 
+  this method must be called before SetVme*/
   void SetStartingMatrix(mafMatrix *matrix);
+  mafMatrix GetStartingMatrix();
 
-  void EnableAttachCamera(){m_CameraAttach = TRUE;};
-  void DisableAttachCamera(){m_CameraAttach = FALSE;};
+	void OnEvent(mafEventBase *maf_event);
+  
+	void SetListener(mafObserver *Listener);
+  mafObserver *GetListener();
+
+	/** 
+  Returns the mafAttachCamera's GUI */
+	mafGUI *GetGui() {return m_Gui;};
+
+  void PrintSelf(ostream& os);
 
 protected:
+  
   /** 
   Create GUI for AttachCamera module.*/
   void CreateGui();
 
-  int						 m_CameraAttach; ///< Flag to turn On/Off the camera attaching on a particular VME
+  int						 m_EnableAttachCamera; ///< Flag to turn On/Off the camera attaching on a particular VME
   mafVME				*m_AttachedVme; ///< VME on which the camera is attached when the attach-camera option is 'On'
   vtkMatrix4x4	*m_AttachedVmeMatrix; ///< Matrix given to the Camera to be moved together with m_AttachedVme
   vtkMatrix4x4	*m_StartingMatrix; ///< Matrix given to the Camera to be moved together with m_AttachedVme
@@ -75,7 +94,7 @@ protected:
 
   mafObserver	*m_Listener;
 	mafGUI			*m_Gui;
-	mafRWI      *m_Rwi;
+	mafRWI      *m_RenderWindow;
 	wxWindow	  *m_ParentPanel;
 };
 #endif
