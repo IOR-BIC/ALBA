0b0431f2473177b4ce39e8c91e00f76b3f4cf70d
lut01 <lut01>
2009-06-03 15:31:10 +0000

    add test() method to have a test

--- Operations/medOpExporterWrappedMeter.cpp
+++ Operations/medOpExporterWrappedMeter.cpp
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: medOpExporterWrappedMeter.cpp,v $
   Language:  C++
-  Date:      $Date: 2009-05-29 15:57:34 $
-  Version:   $Revision: 1.3.2.1 $
+  Date:      $Date: 2009-06-03 15:31:10 $
+  Version:   $Revision: 1.3.2.2 $
   Authors:   Daniele Giunchi
 ==========================================================================
   Copyright (c) 2001/2005 
@@ -27,6 +27,15 @@
 #include "medVMEWrappedMeter.h"
 #include "medVMEOutputWrappedMeter.h"
 #include "medVMEComputeWrapping.h"
+
+#include "medOpImporterLandmark.h"
+#include "medDefines.h"
+#include "mafVMELandmarkCloud.h"
+#include "mafVMERoot.h"
+#include "medVMEWrappedMeter.h"
+#include "mafVMELandmarkCloud.h"
+#include "mafVMELandmark.h"
+#include "vtkDataSet.h"
 
 #include <fstream>
 
@@ -108,7 +117,9 @@ void medOpExporterWrappedMeter::OnEvent(mafEventBase *maf_event)
 
           if (m_File == "") return;
 
-				  ExportWrappedMeterCoordinates();
+		  //Test();		  //using this method to try test case
+		  ExportWrappedMeterCoordinates();
+				  
 				  OpStop(OP_RUN_OK);
 				}
       break;
@@ -209,5 +220,109 @@ void medOpExporterWrappedMeter::ExportWrappedMeterCoordinates()
   }*/
   
   outputFile.close();
+  
+
+}
+
+//-----------------------------------------------------------
+void medOpExporterWrappedMeter::Test() 
+//-----------------------------------------------------------
+{
+/*	mafVMEStorage *storage = mafVMEStorage::New();
+	storage->GetRoot()->SetName("root");
+	storage->GetRoot()->Initialize();
+
+*/
+	//create landmarks and relative landmark cloud
+	medOpImporterLandmark *importer=new medOpImporterLandmark("importer");
+	importer->TestModeOn();
+	//importer->SetInput(storage->GetRoot());
+	//mafString filename=MED_DATA_ROOT;
+	mafString filename = "C:\\MAF\\Medical\\Testing\\unittestData\\RAW_MAL\\cloud_to_be_imported";
+	//filename<<"../unittestData";
+	//filename<<"/RAW_MAL/cloud_to_be_imported";
+	importer->SetFileName(filename.GetCStr());
+	importer->Read();
+	
+	mafVMELandmarkCloud *cloud=(mafVMELandmarkCloud *)importer->GetOutput();
+	cloud->Open();
+
+
+	medVMEComputeWrapping *wrappedMeter;
+	mafNEW(wrappedMeter);
+	//mafNode *n = mafNode::SafeDownCast(cloud);
+//wrappedMeter->SetMeterLink(wrappedMeter,);
+
+	wrappedMeter->SetMeterLink("StartVME",cloud->GetLandmark(0));
+	wrappedMeter->SetMeterLink("EndVME1",cloud->GetLandmark(1));
+
+	wrappedMeter->SetMeterLink(cloud->GetLandmark(2)->GetName(),cloud->GetLandmark(2));
+	wrappedMeter->SetMeterLink(cloud->GetLandmark(3)->GetName(),cloud->GetLandmark(3));
+	wrappedMeter->SetMeterLink(cloud->GetLandmark(4)->GetName(),cloud->GetLandmark(4));
+	//wrappedMeter->PushIdVector(cloud->GetId()); // for landmark middlepoint is memorized as sequence of cloud id and interal id of the landmark
+	//wrappedMeter->PushIdVector(2); //this is for the vector syncronized with the gui widget, that is not used in gui test
+
+
+	wrappedMeter->SetParent(cloud);
+	wrappedMeter->GetOutput()->GetVTKData()->Update();
+	wrappedMeter->Modified();
+	wrappedMeter->Update();
+
+	double l1[3], l2[3], l3[3]; 
+	cloud->GetLandmark(2)->GetPoint(l1);
+	cloud->GetLandmark(3)->GetPoint(l2);
+	cloud->GetLandmark(4)->GetPoint(l3);
+
+	const short int controlDimension = 9;
+	double controlValues[controlDimension];
+	controlValues[0] = l1[0];controlValues[1] = l1[1];controlValues[2] = l1[2];
+	controlValues[3] = l2[0];controlValues[4] = l2[1];controlValues[5] = l2[2];
+	controlValues[6] = l3[0];controlValues[7] = l3[1];controlValues[8] = l3[2];
+
+	//printf("\n%.2f %.2f %.2f\n", l1[0] , l1[1], l1[2]);
+	//printf("\n%.2f %.2f %.2f\n", l2[0] , l2[1], l2[2]);
+	//printf("\n%.2f %.2f %.2f\n", l3[0] , l3[1], l3[2]);
+
+	//Inizialize exporter
+	medOpExporterWrappedMeter *exporter=new medOpExporterWrappedMeter("test exporter");
+	exporter->SetInput(wrappedMeter);
+	mafString fileExp=MED_DATA_ROOT;
+	fileExp<<"/RAW_MAL/ExportWrappedMeter.txt";
+	exporter->TestModeOn();
+	exporter->SetFileName(fileExp);
+	exporter->ExportWrappedMeterCoordinates();
+
+	std::fstream control(fileExp);
+
+	int result = MAF_OK;
+	double pos1, pos2, pos3;
+
+	short int counter = 0;
+	while(counter < controlDimension / 3)
+	{
+		control >> pos1;
+		control >> pos2;
+		control >> pos3;
+
+		if((pos1-controlValues[counter*3]  > 0.01)   || 
+			(pos2-controlValues[counter*3+1] > 0.01)   ||
+			(pos3-controlValues[counter*3+2] > 0.01)   )
+			result = MAF_ERROR;
+
+		counter++;
+	}
+
+	if(result == MAF_OK){
+
+	}
+	control.close();
+
+	wrappedMeter->SetParent(NULL);
+
+	mafDEL(exporter);
+	mafDEL(wrappedMeter);
+	cppDEL(importer);
+
+	//mafDEL(storage);
 
 }
