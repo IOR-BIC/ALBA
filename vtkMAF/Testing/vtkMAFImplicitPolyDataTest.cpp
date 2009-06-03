e83694f2133b2807632bdef5c46f31edbb2c7437
aqd0 <aqd0>
2009-06-03 12:29:56 +0000

    Commit for Perticoni: test class for vtkMAFImplicitPolyData.

---
+++ vtkMAF/Testing/vtkMAFImplicitPolyDataTest.cpp
@@ -0,0 +1,367 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: vtkMAFImplicitPolyDataTest.cpp,v $
+Language:  C++
+Date:      $Date: 2009-06-03 12:29:56 $
+Version:   $Revision: 1.1.2.1 $
+Authors:   Stefano Perticoni
+==========================================================================
+Copyright (c) 2002/2004 
+CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#include "mafDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+#include <cppunit/config/SourcePrefix.h>
+
+#include "vtkMAFImplicitPolyDataTest.h"
+#include "vtkMAFSmartPointer.h"
+
+#include "vtkRenderer.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkPolyData.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkSphereSource.h"
+#include "vtkCamera.h"
+#include "vtkTriangleFilter.h"
+#include "vtkLinearSubdivisionFilter.h"
+#include "vtkMAFImplicitPolyData.h"
+#include "vtkClipPolyData.h"
+#include "vtkSampleFunction.h"
+#include "vtkContourFilter.h"
+#include "vtkImageData.h"
+#include "vtkImplicitBoolean.h"
+
+void vtkMAFImplicitPolyDataTest::setUp()
+{
+ 
+}
+
+void vtkMAFImplicitPolyDataTest::tearDown()
+{
+
+}
+
+void vtkMAFImplicitPolyDataTest::TestFixture()
+{
+
+}
+
+void vtkMAFImplicitPolyDataTest::RenderData( vtkPolyData *data )
+{
+  vtkMAFSmartPointer<vtkRenderer> renderer;
+  renderer->SetBackground(0.1, 0.1, 0.1);
+  
+  vtkMAFSmartPointer<vtkRenderWindow> renderWindow;
+  renderWindow->AddRenderer(renderer);
+  renderWindow->SetSize(640, 480);
+  renderWindow->SetPosition(400,0);
+  
+  vtkMAFSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor;
+  renderWindowInteractor->SetRenderWindow(renderWindow);
+
+  vtkMAFSmartPointer<vtkPolyDataMapper> mapper;
+  mapper->SetInput(data);
+  mapper->ScalarVisibilityOn();
+  
+  vtkMAFSmartPointer<vtkActor> actor;
+  actor->SetMapper(mapper);
+  
+  renderer->AddActor(actor);
+  renderWindow->Render();
+
+  renderWindowInteractor->Start();
+
+  mafSleep(1000);
+
+}
+
+void vtkMAFImplicitPolyDataTest::TestClipSphere1PolydataWithSphere2PolydataUsedAsImplicitFunction()
+{
+  // Clip one polygonal sphere with another.
+
+  // polygonal sphere 1, this will be clipped by sphere 2
+  vtkSphereSource *s1 = vtkSphereSource::New();
+  s1->SetThetaResolution( 6 );
+  s1->SetPhiResolution (  6 );
+  s1->SetCenter( 0, 0, 0 );
+  s1->SetRadius( 1.0 );
+  
+
+  // triangulate sphere 1 for input to subdivision filter
+  vtkTriangleFilter *tf1 = vtkTriangleFilter::New();
+  tf1->SetInput( s1->GetOutput() );
+
+  // subdivide triangles in sphere 1 to get better clipping
+  vtkLinearSubdivisionFilter *lsf1 = vtkLinearSubdivisionFilter::New();
+  lsf1->SetInput( tf1->GetOutput() );
+  lsf1->SetNumberOfSubdivisions( 1 );	// use this (0-3+) to see improvement in clipping
+
+  // polygonal sphere 2, this will clip sphere 1
+  vtkSphereSource *s2 = vtkSphereSource::New();
+  s2->SetThetaResolution( 6 );	// vary resolution (3+) to get different clipper shapes
+  s2->SetPhiResolution (  6 );
+  s2->SetCenter( 0, 1, 0 );	// offset position from sphere 1
+  s2->SetRadius( 1.0 );
+
+  // use sphere 2 polydata as an implicit function
+  vtkMAFImplicitPolyData *ip2 = vtkMAFImplicitPolyData::New();
+  ip2->SetInput( s2->GetOutput() );
+
+  // clip sphere 1 with sphere 2 polydata
+  vtkClipPolyData *cpd1 = vtkClipPolyData::New();
+  cpd1->SetInput( lsf1->GetOutput() );
+  cpd1->SetClipFunction( ip2 );
+  cpd1->SetGenerateClipScalars ( 0 );	// 0 outputs input data scalars, 1 outputs implicit function values
+  cpd1->SetInsideOut( 0 );		// use 0/1 to reverse sense of clipping
+  cpd1->SetValue( 0 );			// use this to control distance from implicit surface
+
+  cpd1->Update();
+  int cellsNumber = cpd1->GetOutput()->GetNumberOfCells();
+  CPPUNIT_ASSERT(cellsNumber == 186);
+  
+  // Rendering for debug purposes...
+  // RenderData(cpd1->GetOutput())  ;
+
+  s1->Delete();
+  tf1->Delete();
+  lsf1->Delete();
+  s2->Delete();
+  ip2->Delete();
+  cpd1->Delete();
+}
+
+void vtkMAFImplicitPolyDataTest::TestEvaluateGradient()
+{
+  vtkSphereSource *s = vtkSphereSource::New();
+  s->SetCenter( 0, 0, 0 );
+  s->SetRadius( 1.0 );
+  s->Update();
+
+  vtkMAFImplicitPolyData *ip = vtkMAFImplicitPolyData::New();
+  ip->SetInput( s->GetOutput() );
+  
+  double gradient[3] = {-9999,-9999,-9999};
+  double point[3] = {0,0,1};
+  ip->EvaluateGradient(point,gradient);
+
+  CPPUNIT_ASSERT(gradient[0] == 0);
+  CPPUNIT_ASSERT(gradient[1] == 0);
+  CPPUNIT_ASSERT(gradient[2] == 1);
+  
+  ip->Delete();
+  s->Delete();
+}
+
+
+void vtkMAFImplicitPolyDataTest::TestEvaluateFunction()
+{
+
+  vtkSphereSource *s = vtkSphereSource::New();
+  s->SetThetaResolution( 6 );
+  s->SetPhiResolution (  6 );
+  s->SetCenter( 0, 0, 0 );	
+  s->SetRadius( 1.0 );
+  s->Update();
+
+  vtkMAFImplicitPolyData *ip = vtkMAFImplicitPolyData::New();
+  ip->SetInput( s->GetOutput() );
+
+  double pointOnSphere[3] = {0,0,1};
+  double ret = ip->EvaluateFunction(pointOnSphere);
+  CPPUNIT_ASSERT(ret == 0);
+
+  double pointOutOfSphere[3] = {0,0,2};
+  ret = ip->EvaluateFunction(pointOutOfSphere);
+  CPPUNIT_ASSERT(ret > 0);
+
+  double pointInsideSphere[3] = {0,0,0};
+  ret = ip->EvaluateFunction(pointInsideSphere);
+  CPPUNIT_ASSERT(ret <0);
+
+  ip->Delete(); 
+  s->Delete();
+}
+
+void vtkMAFImplicitPolyDataTest::TestSetInput()
+{
+  vtkMAFImplicitPolyData *ip = vtkMAFImplicitPolyData::New();
+  CPPUNIT_ASSERT(ip->Input == NULL);
+  
+  vtkSphereSource *s = vtkSphereSource::New();
+  s->Update();
+  ip->SetInput(s->GetOutput());
+  
+  CPPUNIT_ASSERT(ip->Input !=  NULL);
+
+  s->Delete();
+  ip->Delete();
+}
+
+void vtkMAFImplicitPolyDataTest::TestConstructorDestructor()
+{
+  vtkMAFImplicitPolyData *ip = vtkMAFImplicitPolyData::New();
+  CPPUNIT_ASSERT(ip != NULL);
+  ip->Delete();
+}
+
+void vtkMAFImplicitPolyDataTest::TestSetGetNoValue()
+{
+  vtkMAFImplicitPolyData *ip = vtkMAFImplicitPolyData::New();
+  ip->SetNoValue(10);
+  CPPUNIT_ASSERT(ip->GetNoValue() == 10);
+  ip->Delete();
+}
+
+void vtkMAFImplicitPolyDataTest::TestSetGetNoGradient()
+{
+  vtkMAFImplicitPolyData *ip = vtkMAFImplicitPolyData::New();
+  double inGradient[3] = {1,2,3};
+  ip->SetNoGradient(inGradient);
+  
+  double outGradient[3] = {0,0,0};
+  ip->GetNoGradient(outGradient);
+
+  for (int i = 0; i < 3; i++) 
+  {
+    CPPUNIT_ASSERT(inGradient[i] == outGradient[i]);
+  }
+  
+  ip->Delete();
+}
+
+void vtkMAFImplicitPolyDataTest::TestGenerateOffsetSurfaceFromPolydata()
+{
+  vtkSphereSource *s = vtkSphereSource::New();
+  s->SetThetaResolution( 6 );
+  s->SetPhiResolution (  6 );
+  s->SetCenter( 0, 0, 0 );	
+  s->SetRadius( 1.0 );
+  s->Update();
+
+  float offset = 5.0;     // desired surface offset distance
+  int resolution = 10;    // xyz resolution for sampling surface
+
+  // use users polydata as an implicit function
+  vtkMAFImplicitPolyData *ip1 = vtkMAFImplicitPolyData::New();
+  ip1->SetInput( s->GetOutput() ); 
+
+  // determine enlarged volume to accommodate offset surface
+  double bounds[6], buffer = offset*1.5;
+  s->GetOutput()->GetBounds( bounds );
+  bounds[0] -= buffer;
+  bounds[1] += buffer;
+  bounds[2] -= buffer;
+  bounds[3] += buffer;
+  bounds[4] -= buffer;
+  bounds[5] += buffer;
+
+  // sample a volume around the surface
+  vtkSampleFunction *sf1 = vtkSampleFunction::New();
+  sf1->SetImplicitFunction( ip1 );
+  sf1->SetModelBounds( bounds );
+  sf1->SetSampleDimensions( resolution, resolution, resolution );    // define xyz resolution
+  sf1->ComputeNormalsOff();
+
+  vtkContourFilter *cf1 = vtkContourFilter::New();
+  cf1->SetInput( sf1->GetOutput() );
+  cf1->SetValue( 0, offset );
+  
+  int numCells = cf1->GetOutput()->GetNumberOfCells();
+  CPPUNIT_ASSERT(numCells == 0);
+
+  cf1->Update();
+  
+  numCells = cf1->GetOutput()->GetNumberOfCells();
+  CPPUNIT_ASSERT(numCells == 380);
+
+  // rendering for debug purposes...
+  // RenderData(cf1->GetOutput());
+
+  s->Delete();
+  ip1->Delete();
+  sf1->Delete();
+  cf1->Delete();
+}
+
+void vtkMAFImplicitPolyDataTest::TestUnionBetweenTwoPolydata()
+{
+  // Determine difference between two sets of PolyData using vtkImplicitBoolean.
+
+  int resolution = 10;    // xyz resolution for sampling surface
+  
+  vtkSphereSource *s1 = vtkSphereSource::New();
+  s1->SetThetaResolution( 6 );
+  s1->SetPhiResolution (  6 );
+  s1->SetCenter( 0, 0, 0 );
+  s1->SetRadius( 1.0 );
+
+  vtkSphereSource *s2 = vtkSphereSource::New();
+  s2->SetThetaResolution( 6 );
+  s2->SetPhiResolution (  6 );
+  s2->SetCenter( 0.7, 0, 0 );
+  s2->SetRadius( 1.0 );
+
+  // treat users polydata as an implicit function
+  vtkMAFImplicitPolyData *ip1 = vtkMAFImplicitPolyData::New();
+  ip1->SetInput(s1->GetOutput()); // user might have to use ->GetOutput() from an upstream filter in vtk
+
+  // treat users polydata as an implicit function
+  vtkMAFImplicitPolyData *ip2 = vtkMAFImplicitPolyData::New();
+  ip2->SetInput(s2->GetOutput()); // user might have to use ->GetOutput() from an upstream filter in vtk
+
+  // make a boolean combination of the two surfaces
+  vtkImplicitBoolean * ib1 = vtkImplicitBoolean::New();
+  ib1->SetOperationTypeToUnion(); // see notes below
+  ib1->AddFunction( ip1 );
+  ib1->AddFunction( ip2 );
+
+  // determine enlarged volume to accommodate both poydata sets
+  double bounds1[6], bounds2[6];
+  s1->GetOutput()->GetBounds( bounds1 );
+  s2->GetOutput()->GetBounds( bounds2 );
+  if( bounds2[0] < bounds1[0] ) bounds1[0] = bounds2[0];
+  if( bounds2[1] > bounds1[1] ) bounds1[1] = bounds2[1];
+  if( bounds2[2] < bounds1[2] ) bounds1[2] = bounds2[2];
+  if( bounds2[3] > bounds1[3] ) bounds1[3] = bounds2[3];
+  if( bounds2[4] < bounds1[4] ) bounds1[4] = bounds2[4];
+  if( bounds2[5] > bounds1[5] ) bounds1[5] = bounds2[5];
+
+  // sample a volume around the implicit surface
+  vtkSampleFunction *sf1 = vtkSampleFunction::New();
+  sf1->SetImplicitFunction( ib1 );
+  sf1->SetModelBounds( bounds1 );
+  sf1->SetSampleDimensions( resolution, resolution, resolution );    // define xyz resolution
+  sf1->ComputeNormalsOff();
+
+  double offset = 0;
+
+  vtkContourFilter *cf1 = vtkContourFilter::New();
+  cf1->SetInput( sf1->GetOutput() );
+  cf1->SetValue( 0, offset );
+  
+  int numCells = cf1->GetOutput()->GetNumberOfCells();
+  CPPUNIT_ASSERT(numCells == 0);
+
+  cf1->Update();
+
+  numCells = cf1->GetOutput()->GetNumberOfCells();
+  CPPUNIT_ASSERT(numCells == 716);
+
+  // rendering for debug purposes...
+  // RenderData(cf1->GetOutput());
+
+  s1->Delete();
+  s2->Delete();
+  ip1->Delete();
+  ip2->Delete();
+  ib1->Delete();
+  sf1->Delete();
+  cf1->Delete();
+}
