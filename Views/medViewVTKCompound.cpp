77a6237127c0bb2c837bde0448da3a606ba189f3
ior11 <ior11>
2009-07-16 09:06:45 +0000

    A view using mafViewVTK supporting windowing on images.

---
+++ Views/medViewVTKCompound.cpp
@@ -0,0 +1,142 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: medViewVTKCompound.cpp,v $
+  Language:  C++
+  Date:      $Date: 2009-07-16 09:06:45 $
+  Version:   $Revision: 1.1.2.1 $
+  Authors:   Eleonora Mambrini
+==========================================================================
+  Copyright (c) 2002/2004
+  CINECA - Interuniversity Consortium (www.cineca.it) 
+=========================================================================*/
+
+
+#include "mafDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include "medViewVTKCompound.h"
+#include "mafViewImage.h"
+#include "mafViewVTK.h"
+#include "mafRWI.h"
+#include "mafSceneGraph.h"
+#include "mafSceneNode.h"
+#include "mafPipeImage3D.h"
+#include "mafGUIViewWin.h"
+#include "mafGUI.h"
+#include "mafGUILutSlider.h"
+#include "mafGUILutSwatch.h"
+#include "mafNode.h"
+#include "mafNodeIterator.h"
+#include "mafVMEImage.h"
+#include "mafVMEOutputVolume.h"
+#include "mafGUIFloatSlider.h"
+#include "mafVMEOutputImage.h"
+#include "vtkImageData.h"
+#include "vtkTexture.h"
+
+#include "vtkLookupTable.h"
+#include "vtkWindowLevelLookupTable.h"
+#include "vtkDataSet.h"
+
+#include "mmaVolumeMaterial.h"
+
+
+//----------------------------------------------------------------------------
+// constants:
+//----------------------------------------------------------------------------
+
+enum SUBVIEW_ID
+{
+  ID_VIEW_VTK = 0,
+};
+
+//----------------------------------------------------------------------------
+mafCxxTypeMacro(medViewVTKCompound);
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+medViewVTKCompound::medViewVTKCompound( wxString label, int num_row, int num_col)
+: medViewCompoundWindowing(label,num_row,num_col)
+//----------------------------------------------------------------------------
+{
+  m_ViewVTK = NULL;
+}
+//----------------------------------------------------------------------------
+medViewVTKCompound::~medViewVTKCompound()
+//----------------------------------------------------------------------------
+{
+	/*m_ColorLUT = NULL;
+	cppDEL(m_LutWidget);
+	cppDEL(m_LutSlider);*/
+}
+//----------------------------------------------------------------------------
+mafView *medViewVTKCompound::Copy(mafObserver *Listener)
+//----------------------------------------------------------------------------
+{
+  medViewVTKCompound *v = new medViewVTKCompound(m_Label, m_ViewRowNum, m_ViewColNum);
+  v->m_Listener = Listener;
+  v->m_Id = m_Id;
+  for (int i=0;i<m_PluggedChildViewList.size();i++)
+  {
+    v->m_PluggedChildViewList.push_back(m_PluggedChildViewList[i]->Copy(this));
+  }
+  v->m_NumOfPluggedChildren = m_NumOfPluggedChildren;
+  v->Create();
+  return v;
+}
+
+//-------------------------------------------------------------------------
+mafGUI* medViewVTKCompound::CreateGui()
+//-------------------------------------------------------------------------
+{
+	assert(m_Gui == NULL);
+	m_Gui = new mafGUI(this);
+  //m_Gui->AddGui(((mafViewVTK*)m_ChildViewList[ID_VIEW_VTK])->GetGui());
+	m_LutWidget = m_Gui->Lut(ID_LUT_CHOOSER,"lut",m_ColorLUT);
+	m_LutWidget->Enable(false);
+	m_Gui->Divider();
+	m_Gui->FitGui();
+	m_Gui->Update();
+	return m_Gui;
+}
+//-------------------------------------------------------------------------
+void medViewVTKCompound::PackageView()
+//-------------------------------------------------------------------------
+{
+	m_ViewVTK = new mafViewVTK("",CAMERA_PERSPECTIVE);	
+	PlugChildView(m_ViewVTK);
+}
+
+bool medViewVTKCompound::ActivateWindowing(mafNode *node)
+{
+  bool conditions     = false;
+  
+  if(((mafVME *)node)->IsA("mafVMEImage")){
+    
+    conditions = true;
+
+    for(int i=0; i<m_NumOfChildView; i++) {
+      //m_ChildViewList[i]->VmeSelect(node, select);
+
+      mafPipeImage3D *pipe = (mafPipeImage3D *)m_ChildViewList[i]->GetNodePipe(node);
+      conditions = (conditions && (pipe && pipe->IsGrayImage()));
+    }
+  }
+
+  return conditions;
+}
+
+//----------------------------------------------------------------------------
+void medViewVTKCompound::AddVisualPipe(mafString vme_type, mafString pipe_type, long visibility)
+//----------------------------------------------------------------------------
+{
+  if(m_ViewVTK != NULL){
+
+    m_ViewVTK->PlugVisualPipe(vme_type, pipe_type, visibility);
+  }
+}
---
+++ Views/medViewVTKCompound.h
@@ -0,0 +1,83 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: medViewVTKCompound.h,v $
+  Language:  C++
+  Date:      $Date: 2009-07-16 09:06:45 $
+  Version:   $Revision: 1.1.2.1 $
+  Authors:   Eleonora Mambrini
+==========================================================================
+  Copyright (c) 2002/2004
+  CINECA - Interuniversity Consortium (www.cineca.it) 
+=========================================================================*/
+
+#ifndef __medViewVTKCompound_H__
+#define __medViewVTKCompound_H__
+
+//----------------------------------------------------------------------------
+// Include:
+//----------------------------------------------------------------------------
+#include "mafViewCompound.h"
+#include "medViewCompoundWindowing.h"
+
+#include "mafViewVTK.h"
+
+//----------------------------------------------------------------------------
+// forward references :
+//----------------------------------------------------------------------------
+class mafViewVTK;
+class mafSceneGraph;
+class mafGUI;
+class mafRWIBase;
+class mafViewImage;
+class mafGUILutSlider;
+class mafGUILutSwatch;
+class mafGUIFloatSlider;
+class vtkLookupTable;
+class vtkWindowLevelLookupTable;
+
+/**
+  Class Name: medViewVTKCompound.
+  Class that contain mafViewVTK. Derived by medViewCompoundWindowing in order to add gui view widget like windowing buttons.
+
+*/
+class medViewVTKCompound: public medViewCompoundWindowing
+{
+public:
+  /** constructor. */
+  medViewVTKCompound(wxString label = "View Compound with Windowing", int num_row = 1, int num_col = 1);
+  /** destructor. */
+  virtual ~medViewVTKCompound(); 
+
+  /** RTTI macro.*/
+  mafTypeMacro(medViewVTKCompound, medViewCompoundWindowing);
+
+
+  /**  Redefine this method to package the compounded view */
+  virtual void PackageView();
+
+  /** Function that clones instance of the object. */
+  virtual mafView *Copy(mafObserver *Listener);
+
+  /** Function to be called after PackageView() to plug the right visual pipes. */
+  virtual void AddVisualPipe(mafString vme_type, mafString pipe_type, long visibility);
+
+
+protected:
+  /**
+  Internally used to create a new instance of the GUI. This function should be
+  overridden by subclasses to create specialized GUIs. Each subclass should append
+  its own widgets and define the enum of IDs for the widgets as an extension of
+  the superclass enum. The last id value must be defined as "LAST_ID" to allow the 
+  subclass to continue the ID enumeration from it. For appending the widgets in the
+  same pannel GUI, each CreateGUI() function should first call the superclass' one.*/
+  virtual mafGUI  *CreateGui();
+
+   /** A function with windowing activate/deactivate conditions*/
+  virtual bool ActivateWindowing(mafNode *node);
+
+	mafViewVTK *m_ViewVTK;
+
+};
+#endif
+
+
