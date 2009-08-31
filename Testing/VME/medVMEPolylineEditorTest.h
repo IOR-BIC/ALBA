3ff6f4c87d976f4b7322883c9d42e064e4b55153
ior02 <ior02>
2009-08-31 11:51:48 +0000

    added medVMEPolylineEditorTest

---
+++ Testing/VME/medVMEPolylineEditorTest.h
@@ -0,0 +1,74 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: medVMEPolylineEditorTest.h,v $
+Language:  C++
+Date:      $Date: 2009-08-31 11:51:48 $
+Version:   $Revision: 1.1.2.1 $
+Authors:   Matteo Giacomoni
+==========================================================================
+Copyright (c) 2009
+CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#ifndef __CPP_UNIT_medVMEPolylineEditorTest_H__
+#define __CPP_UNIT_medVMEPolylineEditorTest_H__
+
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
+
+
+class medVMEPolylineEditorTest : public CPPUNIT_NS::TestFixture
+{
+public: 
+  // CPPUNIT fixture: executed before each test
+  void setUp();
+
+  // CPPUNIT fixture: executed after each test
+  void tearDown();
+
+  CPPUNIT_TEST_SUITE( medVMEPolylineEditorTest );
+  CPPUNIT_TEST( TestDynamicAllocation );
+  CPPUNIT_TEST( TestSetData );
+  CPPUNIT_TEST( TestSetGetVisualPipe );
+  CPPUNIT_TEST_SUITE_END();
+
+protected:
+  void TestDynamicAllocation();
+  void TestSetData();
+  void TestSetGetVisualPipe();
+  bool m_Result;
+};
+
+
+int
+main( int argc, char* argv[] )
+{
+  // Create the event manager and test controller
+  CPPUNIT_NS::TestResult controller;
+
+  // Add a listener that colllects test result
+  CPPUNIT_NS::TestResultCollector result;
+  controller.addListener( &result );        
+
+  // Add a listener that print dots as test run.
+  CPPUNIT_NS::BriefTestProgressListener progress;
+  controller.addListener( &progress );      
+
+  // Add the top suite to the test runner
+  CPPUNIT_NS::TestRunner runner;
+  runner.addTest( medVMEPolylineEditorTest::suite());
+  runner.run( controller );
+
+  // Print test in a compiler compatible format.
+  CPPUNIT_NS::CompilerOutputter outputter( &result, CPPUNIT_NS::stdCOut() );
+  outputter.write(); 
+
+  return result.wasSuccessful() ? 0 : 1;
+}
+
+#endif
