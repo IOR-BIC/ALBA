6b674ddc218ec19e976d9b1cdf0d857ef2b79e53
ior04 <ior04>
2009-10-05 14:23:15 +0000

    test for BES pipe slice

---
+++ Testing/VME/mafPipeMeshSlice_BESTest.h
@@ -0,0 +1,103 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: mafPipeMeshSlice_BESTest.h,v $
+Language:  C++
+Date:      $Date: 2009-10-05 14:23:15 $
+Version:   $Revision: 1.1.2.1 $
+Authors:   Daniele Giunchi
+==========================================================================
+Copyright (c) 2002/2004 
+CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#ifndef __CPP_UNIT_mafPipeMeshSlice_BESTest_H__
+#define __CPP_UNIT_mafPipeMeshSlice_BESTest_H__
+
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
+
+class vtkPolyData;
+class vtkRenderer;
+class vtkRenderWindow;
+class vtkRenderWindowInteractor;
+class vtkProp;
+class vtkPropCollection;
+
+class mafPipeMeshSlice_BESTest : public CPPUNIT_NS::TestFixture
+{
+  public: 
+  // CPPUNIT fixture: executed before each test
+  void setUp();
+
+  // CPPUNIT fixture: executed after each test
+  void tearDown();
+
+	CPPUNIT_TEST_SUITE( mafPipeMeshSlice_BESTest );
+  CPPUNIT_TEST(TestFixture); // just to test that the fixture has no leaks
+	CPPUNIT_TEST( TestPipeExecution );
+  CPPUNIT_TEST( TestPipeExecution_Wireframe );
+  CPPUNIT_TEST( TestPipeExecution_WiredActorVisibility );
+  CPPUNIT_TEST( TestPipeExecution_FlipNormal );
+  CPPUNIT_TEST( TestPipeExecution_UseVTKProperty );
+  CPPUNIT_TEST( TestPipeExecution_Thickness_PickActor );
+	CPPUNIT_TEST_SUITE_END();
+
+protected:
+  void TestFixture();
+	void TestPipeExecution();
+  void TestPipeExecution_Wireframe();
+  void TestPipeExecution_WiredActorVisibility();
+  void TestPipeExecution_FlipNormal();
+  void TestPipeExecution_UseVTKProperty();
+  void TestPipeExecution_Thickness_PickActor();
+
+
+  vtkRenderer *m_Renderer;
+  vtkRenderWindow *m_RenderWindow;
+  vtkRenderWindowInteractor *m_RenderWindowInteractor;
+
+  /**Compare a list of already saved images (control images) with renderized image from render window
+     @scalarIndex: index X of the image. Original Image is imageX.jpg, created imaged is compX.jpg
+  */
+  void CompareImages(int scalarIndex = 0);
+
+  /**Compare Mapper scalarange with controlled values */
+  void ProceduralControl(double controlRangeMapper[2] , vtkProp *propToControl);
+
+  /** Select The Actor that will be controlled*/
+  vtkProp *SelectActorToControl(vtkPropCollection* propList, int index);
+};
+
+
+int
+main( int argc, char* argv[] )
+{
+	// Create the event manager and test controller
+	CPPUNIT_NS::TestResult controller;
+
+	// Add a listener that colllects test result
+	CPPUNIT_NS::TestResultCollector result;
+	controller.addListener( &result );        
+
+	// Add a listener that print dots as test run.
+	CPPUNIT_NS::BriefTestProgressListener progress;
+	controller.addListener( &progress );      
+
+	// Add the top suite to the test runner
+	CPPUNIT_NS::TestRunner runner;
+	runner.addTest( mafPipeMeshSlice_BESTest::suite());
+	runner.run( controller );
+
+	// Print test in a compiler compatible format.
+	CPPUNIT_NS::CompilerOutputter outputter( &result, CPPUNIT_NS::stdCOut() );
+	outputter.write(); 
+
+	return result.wasSuccessful() ? 0 : 1;
+}
+
+#endif
