89ac15e226f935325a2cfa3a65a07a018422e7d1
aqd0 <aqd0>
2009-10-15 07:51:00 +0000

    Commit for Giunchi: upgrade documentation.

--- Operations/mafOpValidateTree.h
+++ Operations/mafOpValidateTree.h
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mafOpValidateTree.h,v $
   Language:  C++
-  Date:      $Date: 2009-09-16 15:27:56 $
-  Version:   $Revision: 1.3.2.1 $
+  Date:      $Date: 2009-10-15 07:51:00 $
+  Version:   $Revision: 1.3.2.2 $
   Authors:   Paolo Quadrani
 ==========================================================================
 Copyright (c) 2002/2004
@@ -23,7 +23,9 @@ class mafNode;
 //----------------------------------------------------------------------------
 // mafOpValidateTree :
 //----------------------------------------------------------------------------
-/** Class used to validate the correctness of the data tree loaded. 
+/** 
+  Class Name: mafOpValidateTree
+Class used to validate the correctness of the data tree loaded. 
 The tree to be validated has to be saved, otherwise some elements could not 
 have binary files associated with the VME. This class perform checks on VME id, 
 binary data if exists, correctness of TagArray and links.
@@ -36,11 +38,12 @@ Sample usage:
   
 Check returnValue against VALIDATE_RETURN_VALUES
 */
-
 class mafOpValidateTree: public mafOp
 {
 public:
+  /** constructor */
   mafOpValidateTree(const wxString &label = "Validate Tree");
+  /** destructor */
   ~mafOpValidateTree(); 
   
   /** Iterate on all tree nodes and check the consistency for each one: 
@@ -55,6 +58,7 @@ public:
     VALIDATE_WARNING,
   };
 
+  /** RTTI macro */
   mafTypeMacro(mafOpValidateTree, mafOp);
 
   /** Return a copy of the operation.*/
@@ -71,6 +75,7 @@ protected:
   /** Log errors into the log area according to the error number reported.*/
   void ErrorLog(int error_num, const char *node_name, const char *description = NULL);
 
+  /** validate report ids*/
   enum VALIDATE_REPORT_IDS
   {
     INVALID_NODE = 0,
