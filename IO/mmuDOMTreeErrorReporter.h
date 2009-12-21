0cf954f856d0e3321dfc75c4eac3f8cc2492d45b
aqd0 <aqd0>
2009-12-21 15:39:30 +0000

    Commit for Giunchi: Added documentation.

--- IO/mmuDOMTreeErrorReporter.h
+++ IO/mmuDOMTreeErrorReporter.h
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mmuDOMTreeErrorReporter.h,v $
   Language:  C++
-  Date:      $Date: 2009-02-23 14:47:48 $
-  Version:   $Revision: 1.1.2.1 $
+  Date:      $Date: 2009-12-21 15:39:30 $
+  Version:   $Revision: 1.1.2.2 $
   Authors:   Marco Petrone
 ==========================================================================
   Copyright (c) 2001/2005 
@@ -19,24 +19,35 @@
 #include <xercesc/sax/SAXParseException.hpp>
 
 
-//------------------------------------------------------------------------------
-// mmuDOMTreeErrorReporter
-//------------------------------------------------------------------------------
-/** Utility class to catch XML error rised while parsing XML */
+/** 
+class name: mmuDOMTreeErrorReporter
+  Utility class to catch XML error rised while parsing XML 
+*/
+
 class mmuDOMTreeErrorReporter : public XERCES_CPP_NAMESPACE_QUALIFIER ErrorHandler
 {
 public:
-    mmuDOMTreeErrorReporter() : m_SawErrors(false) {}
+    /** constructor */
+    mmuDOMTreeErrorReporter() : m_SawErrors(false), m_TestFlag(false) {}
+    /** destructor */
     ~mmuDOMTreeErrorReporter() {}
 
-    /** Implementation of the error handler interface */
+    /** Implementation of the warning handler interface */
     void warning(const XERCES_CPP_NAMESPACE_QUALIFIER SAXParseException& toCatch);
+    /** Implementation of the error handler interface */
     void error(const XERCES_CPP_NAMESPACE_QUALIFIER SAXParseException& toCatch);
+    /** Implementation of the fatal error handler interface */
     void fatalError(const XERCES_CPP_NAMESPACE_QUALIFIER SAXParseException& toCatch);
+    /** reset error flag */
     void resetErrors();
 
+    /** retrieve error flag */
     bool GetSawErrors() const {return m_SawErrors;}
+    /** set test modality in order to skip, problematic log messages*/
+    void SetTestMode(bool enable){m_TestFlag = enable;}
+
 private:
     bool    m_SawErrors; ///< Set if we get any errors, used by the main code to suppress output if there are errors.
+    bool m_TestFlag;
 };
 #endif // _mmuDOMTreeErrorReporter_h_
