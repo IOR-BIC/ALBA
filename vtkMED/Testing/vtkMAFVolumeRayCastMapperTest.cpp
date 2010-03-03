f1a803d6cc447495201341737e4a5474d22bbcc3
ior04 <ior04>
2010-03-03 09:44:36 +0000

    add test for vtkMAFVolumeRayCastMapper

---
+++ vtkMED/Testing/vtkMAFVolumeRayCastMapperTest.cpp
@@ -0,0 +1,265 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: vtkMAFVolumeRayCastMapperTest.cpp,v $
+Language:  C++
+Date:      $Date: 2010-03-03 09:44:36 $
+Version:   $Revision: 1.1.2.1 $
+Authors:   Daniele Giunchi
+==========================================================================
+Copyright (c) 2002/2008
+CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+
+#include "mafDefines.h" 
+#include "medDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include <cppunit/config/SourcePrefix.h>
+#include "vtkMAFVolumeRayCastMapperTest.h"
+#include "vtkMAFVolumeRayCastMapper.h"
+
+#include "vtkMAFVolumeRayCastMapper.h"
+#include "vtkVolume.h"
+#include "vtkVolumeProperty.h"
+#include "vtkVolumeRayCastMIPFunction.h"
+
+
+#include "vtkMapper.h"
+#include "vtkJPEGWriter.h"
+#include "vtkJPEGReader.h"
+#include "vtkWindowToImageFilter.h"
+#include "vtkImageMathematics.h"
+#include "vtkImageData.h"
+#include "vtkPointData.h"
+#include "vtkDataSetReader.h"
+#include "mafString.h"
+
+// render window stuff
+#include "vtkRenderer.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
+
+
+#include <iostream>
+
+
+//----------------------------------------------------------------------------
+void vtkMAFVolumeRayCastMapperTest::TestFixture()
+//----------------------------------------------------------------------------
+{
+}
+//----------------------------------------------------------------------------
+void vtkMAFVolumeRayCastMapperTest::setUp()
+//----------------------------------------------------------------------------
+{
+  vtkNEW(m_Renderer);
+  vtkNEW(m_RenderWindow);
+  vtkNEW(m_RenderWindowInteractor);
+}
+//----------------------------------------------------------------------------
+void vtkMAFVolumeRayCastMapperTest::tearDown()
+//----------------------------------------------------------------------------
+{
+  vtkDEL(m_Renderer);
+  vtkDEL(m_RenderWindow);
+  vtkDEL(m_RenderWindowInteractor);
+}
+//----------------------------------------------------------------------------
+void vtkMAFVolumeRayCastMapperTest::TestPipeExecution()
+//----------------------------------------------------------------------------
+{
+  ///////////////// render stuff /////////////////////////
+
+  m_Renderer->SetBackground(0.1, 0.1, 0.1);
+  m_RenderWindow->AddRenderer(m_Renderer);
+  m_RenderWindow->SetSize(320, 240);
+  m_RenderWindow->SetPosition(400,0);
+
+  m_RenderWindowInteractor->SetRenderWindow(m_RenderWindow);
+
+  ///////////// end render stuff /////////////////////////
+
+
+  ////// import vtkData ////////////////////
+  vtkDataSetReader *Importer;
+  vtkNEW(Importer);
+  mafString filename=MED_DATA_ROOT;
+  filename<<"/VTK_Volumes/volume.vtk";
+  Importer->SetFileName(filename);
+  Importer->Update();
+  
+  vtkVolumeProperty *volumeProperty = NULL;
+  vtkNEW(volumeProperty);  
+  //volumeProperty->SetColor(m_ColorTransferFunction);
+  volumeProperty->SetInterpolationTypeToLinear();
+
+  vtkVolumeRayCastMIPFunction *MIPFunction = NULL;
+  vtkNEW(MIPFunction);
+  MIPFunction->SetMaximizeMethodToOpacity();
+
+  vtkMAFVolumeRayCastMapper *volumeMapper;
+  vtkNEW(volumeMapper);
+  volumeMapper->SetInput(vtkImageData::SafeDownCast(Importer->GetOutput()));
+
+  volumeMapper->SetVolumeRayCastFunction(MIPFunction);
+  volumeMapper->SetCroppingRegionPlanes(0, 1, 0, 1, 0, 1);
+  volumeMapper->SetImageSampleDistance(1);
+  volumeMapper->SetMaximumImageSampleDistance(10);
+  volumeMapper->SetMinimumImageSampleDistance(1);
+  volumeMapper->SetNumberOfThreads(1);
+  volumeMapper->SetSampleDistance(1);
+
+  volumeMapper->Update();	
+  
+  vtkVolume *volume;
+  vtkNEW(volume);
+  volume->SetMapper(volumeMapper);
+  volume->SetProperty(volumeProperty);
+  volume->PickableOff();
+
+  m_Renderer->AddVolume(volume);
+  m_RenderWindow->Render();
+  CompareImages(0);
+
+  vtkDEL(volumeProperty);
+  vtkDEL(MIPFunction);
+  vtkDEL(volume);
+  vtkDEL(volumeMapper);
+  vtkDEL(MIPFunction);
+  vtkDEL(Importer);
+  
+  delete wxLog::SetActiveTarget(NULL);
+}
+//----------------------------------------------------------------------------
+void vtkMAFVolumeRayCastMapperTest::CompareImages(int scalarIndex)
+//----------------------------------------------------------------------------
+{
+  char *file = __FILE__;
+  std::string name(file);
+  int slashIndex =  name.find_last_of('\\');
+
+  name = name.substr(slashIndex+1);
+
+  int pointIndex =  name.find_last_of('.');
+  name = name.substr(0, pointIndex);
+
+  mafString controlOriginFile=MED_DATA_ROOT;
+  controlOriginFile<<"/Test_vtkMAFVolumeRayCastMapper/";
+  controlOriginFile<<name.c_str();
+  controlOriginFile<<"_";
+  controlOriginFile<<"image";
+  controlOriginFile<<scalarIndex;
+  controlOriginFile<<".jpg";
+
+  fstream controlStream;
+  controlStream.open(controlOriginFile.GetCStr()); 
+
+  // visualization control
+	m_RenderWindow->OffScreenRenderingOn();
+  vtkWindowToImageFilter *w2i;
+  vtkNEW(w2i);
+  w2i->SetInput(m_RenderWindow);
+  //w2i->SetMagnification(magnification);
+  w2i->Update();
+	m_RenderWindow->OffScreenRenderingOff();
+
+  //write comparing image
+  vtkJPEGWriter *w;
+  vtkNEW(w);
+  w->SetInput(w2i->GetOutput());
+  mafString imageFile=MED_DATA_ROOT;
+
+  if(!controlStream)
+  {
+    imageFile<<"/Test_vtkMAFVolumeRayCastMapper/";
+    imageFile<<name.c_str();
+    imageFile<<"_";
+    imageFile<<"image";
+  }
+  else
+  {
+    imageFile<<"/Test_vtkMAFVolumeRayCastMapper/";
+    imageFile<<name.c_str();
+    imageFile<<"_";
+    imageFile<<"comp";
+  }
+  
+  imageFile<<scalarIndex;
+  imageFile<<".jpg";
+  w->SetFileName(imageFile.GetCStr());
+  w->Write();
+
+  if(!controlStream)
+  {
+    controlStream.close();
+    vtkDEL(w);
+    vtkDEL(w2i);
+
+    return;
+  }
+  controlStream.close();
+
+  //read original Image
+  vtkJPEGReader *rO;
+  vtkNEW(rO);
+  mafString imageFileOrig=MED_DATA_ROOT;
+  imageFileOrig<<"/Test_vtkMAFVolumeRayCastMapper/";
+  imageFileOrig<<name.c_str();
+  imageFileOrig<<"_";
+  imageFileOrig<<"image";
+  imageFileOrig<<scalarIndex;
+  imageFileOrig<<".jpg";
+  rO->SetFileName(imageFileOrig.GetCStr());
+  rO->Update();
+
+  vtkImageData *imDataOrig = rO->GetOutput();
+
+  //read compared image
+  vtkJPEGReader *rC;
+  vtkNEW(rC);
+  rC->SetFileName(imageFile.GetCStr());
+  rC->Update();
+
+  vtkImageData *imDataComp = rC->GetOutput();
+
+
+  vtkImageMathematics *imageMath = vtkImageMathematics::New();
+  imageMath->SetInput1(imDataOrig);
+  imageMath->SetInput2(imDataComp);
+  imageMath->SetOperationToSubtract();
+  imageMath->Update();
+
+  double srR[2] = {-1,1};
+  imageMath->GetOutput()->GetPointData()->GetScalars()->GetRange(srR);
+
+  CPPUNIT_ASSERT(srR[0] == 0.0 && srR[1] == 0.0);
+
+  // end visualization control
+  vtkDEL(imageMath);
+  vtkDEL(rC);
+  vtkDEL(rO);
+
+  vtkDEL(w);
+  vtkDEL(w2i);
+}
+//----------------------------------------------------------------------------
+vtkProp *vtkMAFVolumeRayCastMapperTest::SelectActorToControl(vtkPropCollection *propList, int index)
+//----------------------------------------------------------------------------
+{
+  propList->InitTraversal();
+  vtkProp *actor = propList->GetNextProp();
+  int count = 0;
+  while(actor)
+  {   
+    if(count == index) return actor;
+    actor = propList->GetNextProp();
+    count ++;
+  }
+  return NULL;
+}
