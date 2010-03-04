15b369e3e7df4cd0dd5e92efc2db6c81f2c41edc
ior11 <ior11>
2010-03-04 08:23:41 +0000

    Rollback: back to mafViewGlobalSlice with mafViewVTK as superclass. (1.29.2.2)

--- Views/mafViewGlobalSlice.h
+++ Views/mafViewGlobalSlice.h
@@ -1,143 +1,142 @@
 /*=========================================================================
-Program:   VPH2
-Module:    $RCSfile: mafViewGlobalSlice.h,v $
-Language:  C++
-Date:      $Date: 2010-02-24 16:52:33 $
-Version:   $Revision: 1.9.2.3 $
-Authors:   Eleonora Mambrini
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: mafViewGlobalSlice.h,v $
+  Language:  C++
+  Date:      $Date: 2010-03-04 08:23:41 $
+  Version:   $Revision: 1.9.2.4 $
+  Authors:   Matteo Giacomoni
 ==========================================================================
-Copyright (c) 2002/2004
-CINECA - Interuniversity Consortium (www.cineca.it) 
+  Copyright (c) 2002/2004
+  CINECA - Interuniversity Consortium (www.cineca.it) 
 =========================================================================*/
+
 #ifndef __mafViewGlobalSlice_H__
 #define __mafViewGlobalSlice_H__
 
 //----------------------------------------------------------------------------
 // Include:
 //----------------------------------------------------------------------------
-#include "mafViewSlice.h"
+#include "mafViewVTK.h"
+#include <vector>
 
 //----------------------------------------------------------------------------
 // forward references :
 //----------------------------------------------------------------------------
-class mafVME;
+class mafView;
+class mafObserver;
+class mafSceneNode;
+class mafEventBase;
+class mafString;
+class vtkActor2D;
+class vtkTextMapper;
+class vtkPlaneSource;
+class vtkOutlineCornerFilter;
+class vtkProperty;
+class vtkPolyDataMapper;
+class vtkActor;
 
-//----------------------------------------------------------------------------
-// mafViewGlobalSlice :
-//----------------------------------------------------------------------------
-/** 
-mafViewGlobalSlice is a View that visualize volume as slices and 
-initialize the visual pipe according to the camera position that is passed through constructor.
-This is an experimental component with rotated volumes interaction
-and visualization enabled.
+/**
+  Class Name: mafViewGlobalSlice.
+  View class that visualize actors sliced from an implicit plane that operates in all
+  the 3d world.
 */
 
-class mafViewGlobalSlice: public mafViewSlice
+class mafViewGlobalSlice: public mafViewVTK
 {
 public:
-  mafViewGlobalSlice(wxString label = "Slice", int camera_position = CAMERA_CT, bool show_axes = false, bool show_grid = false, bool show_ruler = false, int stereo = 0,bool showTICKs=false);
+  /** constructor. */
+  mafViewGlobalSlice(wxString label = "Global Slice", int camera_position = CAMERA_PERSPECTIVE, bool show_axes = false, bool show_grid = false, bool show_ruler = false, int stereo = 0);
+  /** destructor. */
   virtual ~mafViewGlobalSlice(); 
 
-  mafTypeMacro(mafViewGlobalSlice, mafViewSlice);
+  /** RTTI macro. */
+  mafTypeMacro(mafViewGlobalSlice, mafViewVTK);
 
-  virtual mafView*  Copy(mafObserver *Listener);
-  virtual void			OnEvent(mafEventBase *maf_event);
-
-  //void Create();
-
-  /** IDs for the view GUI */
-  enum VIEW_NEW_GLOBAL_SLICE_WIDGET_ID
+	/** IDs for the view GUI */
+  enum VIEW_SLICE_WIDGET_ID
   {
     ID_POS_SLIDER = Superclass::ID_LAST,
-    ID_CHANGE_VIEW,
-    ID_OPACITY_SLIDER,
-    ID_SHOW_TEXT,
-    ID_TEXT_MODE,
+		ID_CHANGE_VIEW,
+		ID_OPACITY_SLIDER,
     ID_LAST
   };
+  
+  /** Function that clones instance of the object. */
+	virtual mafView *Copy(mafObserver *Listener);
 
-  /** 
+  /** Function that creates renderwindow, scenegraph, specific actors and interactors. */
+	virtual void Create();
+
+	/** 
   Create the visual pipe for the node passed as argument. 
   To create visual pipe first check in m_PipeMap if custom visual pipe is defined, 
   otherwise ask to vme which is its visual pipe. */
   virtual void VmeCreatePipe(mafNode *vme);
 
-  /** 
-  Delete vme's visual pipe. It is called when vme is removed from visualization.*/
+	/** Delete vme's visual pipe. It is called when vme is removed from visualization.*/
   virtual void VmeDeletePipe(mafNode *vme);
 
-  /** Set slicer parameter to generate the slice. */
-  virtual void SetSlice(double origin[3], float xVect[3], float yVect[3]);
-
-  /** Set the origin and normal of the slice 
-  Both, Origin and Normal may be NULL, if the current value is to be preserved */
-  virtual void SetSlice(double* Origin, double* Normal = NULL) {Superclass::SetSlice(Origin, Normal);};
-
-  /** Calculate for every actor the correct slice. */
-  virtual void UpdateSlice();
-
-  /** Update slice position along current axis. */
-  double UpdateSlicePos(double pos = -1);
-
-  /** Refresh text in CT View**/
-  virtual void UpdateText(int ID = 1);
-
-  /** Public method to be called to change slice axis. */
-  void ChangeView(int viewIndex);
-
-  /** Show on/off slice and frame numbers. */
-  virtual void ShowText(bool show);
-
-  /**  Change text mode. */
-  virtual void SetTextMode(int textMode);
+  /** Function that handles events sent from other objects. */
+	virtual void OnEvent(mafEventBase *maf_event);
 
   /** Function called when select a vme different from selected one.*/
-  virtual void VmeSelect(mafNode *node, bool select);
+	virtual void VmeSelect(mafNode *node, bool select);
 
-  /** Override superclass. */
+	/** Set slicer parameter to generate the slice. */
+  virtual void SetSlice(double origin[3], float xVect[3], float yVect[3]);
+	
+	/** Set slicer parameter to generate the slice. */
+	virtual void SetSlice(double origin[3], double dn = 0.0);
+
+	/** Show/Hide VMEs into plugged sub-views*/
   virtual void VmeShow(mafNode *node, bool show);
 
-  /** pPrint a dump of this object */
-  virtual void Print(std::ostream& os, const int tabs=0);// const;
-
-  /** Override superclass. */
-  virtual void CameraUpdate();
+  /** Force the updating of the camera. */
+	virtual void CameraUpdate();
 
 protected:
-
-  /** Override superclass. */
-  virtual mafGUI *CreateGui();
-
-  /** Internally called to update information about current slice number.*/
-  void UpdateSliceIndex();
-
   /** Force the updating of several slice parameters like origin, vectors and gui components. */
-  virtual void UpdateSliceParameters();
+	virtual void UpdateSliceParameters();
+  /** Calculate for every actor the correct slice. */
+	virtual void UpdateSlice();
+  /** Update the correct value corresponding to specific slice; this value is visualized with an actor.*/
+	virtual void UpdateText();
+  /** Initialize position of the plane and actor properties.*/
+	virtual void InizializePlane();
+  /** Update origin, point1 and point2 of the vtkPlaneSource. */
+	virtual void UpdatePlane();
 
-  /** Internally called to update information about current volume data bounds..*/
-  virtual void UpdateBounds();
+  /** Create gui widgets that will be attached in view settings tab in side bar.*/
+	virtual mafGUI  *CreateGui();
 
-  double		m_GlobalBounds[6];
-  double		m_SliderOldOrigin;
-  double		m_SliderOrigin;
-  double		m_Opacity;
-  //int				m_SliceMode;
-  int				m_ViewIndex;
-  double		m_Dn;
-
-  mafGUIFloatSlider	*m_GlobalSlider;
-  mafGUIFloatSlider	*m_OpacitySlider;
-
-  bool			m_GlobalBoundsInitialized;
+	double		m_SliceOrigin[3];
+	float			m_SliceXVector[3];
+	float			m_SliceYVector[3];
+	float			m_SliceNormal[3];
+	double		m_GlobalBounds[6];
+	double		m_SliderOldOrigin;
+	double		m_SliderOrigin;
+	double		m_Opacity;
+	int				m_IDPlane;
+	int				m_SliceMode;
+	int				m_ViewIndex;
+	double		m_Dn;
+	bool			m_GlobalBoundsInitialized;
   bool      m_GlobalBoundsValid;
 
-  int m_NumberOfSlices[3];
-  int m_SliceIndex;
-  mafTimeStamp m_CurrentTimeStamp;
-  int m_NumberOfFrames;
-  int m_FrameIndex;
-  int m_ShowText;
-  int m_TextMode;
+	mafSceneNode		*m_SelectedVolume;
+	mafGUIFloatSlider	*m_GlobalSlider;
+	mafGUIFloatSlider	*m_OpacitySlider;
 
+	vtkActor2D					*m_TextActor;
+	vtkTextMapper				*m_TextMapper;
+	mafString						 m_Text;
+	double							 m_TextColor[3];
+
+	vtkPlaneSource					*m_BoundsPlane;
+	vtkOutlineCornerFilter	*m_BoundsOutlineBox;
+	vtkProperty             *m_BoundsOutlineProperty;
+	vtkPolyDataMapper       *m_BoundsOutlineMapper;
+	vtkActor	              *m_BoundsOutlineActor;
 };
 #endif
