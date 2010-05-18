602e3a63d61bb17e5a0c6359a254a917b12a529f
aqd0 <aqd0>
2010-05-18 07:16:21 +0000

    Added initialization for m_Cstr in the default constructor.

--- IO/mafXMLString.h
+++ IO/mafXMLString.h
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mafXMLString.h,v $
   Language:  C++
-  Date:      $Date: 2006-06-14 14:46:33 $
-  Version:   $Revision: 1.6 $
+  Date:      $Date: 2010-05-18 07:16:21 $
+  Version:   $Revision: 1.6.10.1 $
   Authors:   Rick Parrish (rfmobile@swbell.net) adapted by Marco Petrone (CINECA)
 ==========================================================================
   Copyright (c) 2001/2005 
@@ -25,7 +25,7 @@
 #endif
 
 /** string type for converting const char * to and from XMLString types
-  This is an internal string type to be used only in conjuction with XercecC XML library.
+  This is an internal string type to be used only in conjunction with XercecC XML library.
   This object is able to convert const char* to and from XMLStrings. This code has been
   adapted from example code "class XercesString" found in articles "Make the most of Xerces-C++"
   by Rick Parrish (rfmobile@swbell.net) that can be found at "www.ibm.com/developerworks/xml".
@@ -33,7 +33,7 @@
 class MAF_EXPORT mafXMLString
 {
 public:
-	mafXMLString() : m_WStr(0L) { };
+	mafXMLString() : m_WStr(0L), m_CStr(NULL) { };
 	mafXMLString(const char *str);
 	mafXMLString(XMLCh *wstr);
 	mafXMLString(const XMLCh *wstr);
