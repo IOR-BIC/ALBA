749e795d65f452c248b110cb5c5d9586f500a6fa
mucci <mucci>
2010-11-17 16:04:14 +0000

    test class for mmiInfoImage

---
+++ Testing/Interaction/mmiInfoImageTest.cpp
@@ -0,0 +1,74 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: mmiInfoImageTest.cpp,v $
+Language:  C++
+Date:      $Date: 2010-11-17 16:04:14 $
+Version:   $Revision: 1.1.2.1 $
+Authors:   Roberto Mucci
+==========================================================================
+Copyright (c) 2002/2004 
+CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+
+#include "mafDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include "mmiInfoImageTest.h"
+
+#include <cppunit/config/SourcePrefix.h>
+#include <iostream>
+#include "mmiInfoImage.h"
+#include "mafEventInteraction.h"
+#include "mafDeviceButtonsPadMouse.h"
+#include "mafVMESurface.h"
+
+void mmiInfoImageTest::setUp()
+{
+  
+}
+
+void mmiInfoImageTest::tearDown()
+{
+
+}
+
+void mmiInfoImageTest::TestFixture()
+{
+	
+}
+
+void mmiInfoImageTest::TestConstructorDestructor()
+{
+	mmiInfoImage *info = mmiInfoImage::New();
+	info->Delete();
+}
+
+void mmiInfoImageTest::TestOnEvent()
+{
+  mmiInfoImage *info = mmiInfoImage::New();
+  CPPUNIT_ASSERT(info);
+
+  mafDeviceButtonsPadMouse *dummyDevice = mafDeviceButtonsPadMouse::New();
+  dummyDevice->UpdateRWIDuringMotionOn();
+
+  mafVMESurface *dummy = mafVMESurface::New();
+  info->OnVmeSelected(dummy);
+
+  mafEventInteraction dummyMouse2DMoveEvent;
+  dummyMouse2DMoveEvent.SetChannel(MCH_INPUT);
+  dummyMouse2DMoveEvent.SetSender(dummyDevice);
+  dummyMouse2DMoveEvent.SetId(mafDeviceButtonsPadMouse::MOUSE_2D_MOVE);
+
+  info->OnEvent(&dummyMouse2DMoveEvent);
+  dummyDevice->Delete();
+  dummy->Delete();
+  info->Delete();
+  CPPUNIT_ASSERT(true);
+}
+
