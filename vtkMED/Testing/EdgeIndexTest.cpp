9392911e83f4a1dfcf794ddc7bc9a2f399e12e43
ior02 <ior02>
2010-11-19 09:51:55 +0000

    added EdgeIndexTest

---
+++ vtkMED/Testing/EdgeIndexTest.cpp
@@ -0,0 +1,39 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: EdgeIndexTest.cpp,v $
+Language:  C++
+Date:      $Date: 2010-11-19 09:51:55 $
+Version:   $Revision: 1.1.2.1 $
+Authors:   Matteo Giacomoni
+==========================================================================
+Copyright (c) 2002/2004 
+CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#include "medDefines.h"
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include <cppunit/config/SourcePrefix.h>
+#include "EdgeIndexTest.h"
+
+#include "vtkMEDPoissonSurfaceReconstruction.h"
+
+//-------------------------------------------------------------------------
+void EdgeIndexTest::TestDynamicAllocation()
+//-------------------------------------------------------------------------
+{
+  CoredPointIndex *coredPointIndex = new CoredPointIndex();
+
+  delete coredPointIndex;
+}
+//-------------------------------------------------------------------------
+void EdgeIndexTest::TestStaticAllocation()
+//-------------------------------------------------------------------------
+{
+  CoredPointIndex coredPointIndex;
+}
