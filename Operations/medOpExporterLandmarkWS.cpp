093751be3077721fdfcfcc8e4f8174f026b788a1
ior12 <ior12>
2010-11-26 09:25:52 +0000

    Added Trajectories exporter in the Medical.

---
+++ Operations/medOpExporterLandmarkWS.cpp
@@ -0,0 +1,189 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: medOpExporterLandmarkWS.cpp,v $
+  Language:  C++
+  Date:      $Date: 2010-11-26 09:25:53 $
+  Version:   $Revision: 1.1.2.1 $
+  Authors:   Simone Brazzale
+==========================================================================
+  Copyright (c) 2001/2005 
+  CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#include "mafDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+#include "medOpExporterLandmarkWS.h"
+
+#include <wx/busyinfo.h>
+
+#include <fstream>
+#include <iostream>
+
+#include "mafVMELandmarkCloud.h"
+#include "mafVMELandmark.h"
+
+using namespace std;
+
+#define TAG_FORMAT "TRAJECTORIES"
+#define FREQ 1.00
+
+//----------------------------------------------------------------------------
+medOpExporterLandmarkWS::medOpExporterLandmarkWS(const wxString &label) :
+mafOp(label)
+//----------------------------------------------------------------------------
+{
+	m_OpType	= OPTYPE_EXPORTER;
+	m_Canundo	= true;
+	m_File		= "";
+
+  m_Cloud = NULL;
+}
+//----------------------------------------------------------------------------
+medOpExporterLandmarkWS::~medOpExporterLandmarkWS()
+//----------------------------------------------------------------------------
+{
+}
+//----------------------------------------------------------------------------
+bool medOpExporterLandmarkWS::Accept(mafNode *node)
+//----------------------------------------------------------------------------
+{
+  return (node && node->IsA("mafVMELandmarkCloud"));
+}
+//----------------------------------------------------------------------------
+mafOp* medOpExporterLandmarkWS::Copy()   
+//----------------------------------------------------------------------------
+{
+	medOpExporterLandmarkWS *cp = new medOpExporterLandmarkWS(m_Label);
+	cp->m_File = m_File;
+	return cp;
+}
+//----------------------------------------------------------------------------
+void medOpExporterLandmarkWS::OpRun()   
+//----------------------------------------------------------------------------
+{
+	wxString proposed = mafGetApplicationDirectory().c_str();
+  proposed += "/Data/External/";
+	proposed += m_Input->GetName();
+  proposed += "_TRAJECTORIES";
+	proposed += ".csv";
+	
+  wxString wildc = "ASCII CSV file (*.csv)|*.csv";
+	wxString f = mafGetSaveFile(proposed,wildc).c_str(); 
+
+	int result = OP_RUN_CANCEL;
+	if(!f.IsEmpty())
+	{
+		m_File = f;
+		Write();
+		result = OP_RUN_OK;
+	}
+	mafEventMacro(mafEvent(this,result));
+}
+
+//----------------------------------------------------------------------------
+void medOpExporterLandmarkWS::Write()   
+//----------------------------------------------------------------------------
+{
+  if (!m_TestMode)
+  {
+    wxSetCursor(wxCursor(wxCURSOR_WAIT));
+	  mafEventMacro(mafEvent(this,PROGRESSBAR_SHOW));
+  }
+  
+  m_Cloud = mafVMELandmarkCloud::SafeDownCast(m_Input);
+  bool statusOpen = m_Cloud->IsOpen();
+  if (!statusOpen)
+  {
+    m_Cloud->Open();
+    statusOpen = true;
+  }
+
+  std::vector<mafString> points_name;
+  int numberLandmark = m_Cloud->GetNumberOfLandmarks();
+
+  // Get points names
+  for (int i=0; i<numberLandmark; i++)
+  {
+    points_name.push_back(m_Cloud->GetLandmarkName(i));
+  }
+
+  std::vector<mafTimeStamp> timeStamps;
+  m_Cloud->GetTimeStamps(timeStamps);
+  mafString lmName = "";
+  double pos[3] = {0.0,0.0,0.0};
+  double ori[3] = {0.0,0.0,0.0};
+  double t;
+  
+  std::ofstream f_Out(m_File);
+  if (!f_Out.bad())
+  {
+    // Add TRAJECTORIES tag
+    f_Out << TAG_FORMAT << "\n";
+
+    // Add the first row containing the frequency
+    f_Out << FREQ << ",(DUMMY)\n";
+
+    // Add the third line containing the points names
+    f_Out << ","; // This dummy comma is necessary for compatibility with the Importer
+    for (int i=0; i<numberLandmark; i++)
+    {
+      f_Out << points_name.at(i);
+      if (i!=numberLandmark-1)
+      {
+        f_Out << ",,,"; // These dummy commas are necessary for compatibility with the Importer
+      }
+    }
+    f_Out << "\n";
+
+    // Add the line with user information
+    f_Out << "Time";
+    for (int i=0; i<numberLandmark-1; i++)
+    {
+      f_Out << ",X,Y,Z";
+    }
+    f_Out << "\n";
+
+    for (int i=0; i<timeStamps.size(); i++)
+    {
+      t = timeStamps[i];
+      f_Out << t;
+
+      for(int j = 0; j < numberLandmark; j++)
+      {
+        m_Cloud->GetLandmark(j)->GetOutput()->GetAbsPose(pos,ori,t);
+        if (pos[0]!=0 && pos[1]!=0 && pos[2]!=0)
+        {
+          f_Out << "," << pos[0] << "," << pos[1] << "," << pos[2];
+        }
+        else
+        {
+          f_Out << ",,,"; // These dummy commas are necessary for compatibility with the importer.
+        }
+      }
+      f_Out << "\n";
+
+      if (!m_TestMode)
+      {
+        mafEventMacro(mafEvent(this,PROGRESSBAR_SET_VALUE,(long)(((double) i)/((double) timeStamps.size())*100.)));
+      }
+    }
+
+    f_Out.close();
+  }
+
+  if (statusOpen)
+  {
+    m_Cloud->Close();
+  }
+
+  if (!m_TestMode)
+  {
+    mafEventMacro(mafEvent(this,PROGRESSBAR_HIDE));
+    wxSetCursor(wxCursor(wxCURSOR_DEFAULT));
+  }
+}
---
+++ Operations/medOpExporterLandmarkWS.h
@@ -0,0 +1,78 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: medOpExporterLandmarkWS.h,v $
+  Language:  C++
+  Date:      $Date: 2010-11-26 09:25:53 $
+  Version:   $Revision: 1.1.2.1 $
+  Authors:   Simone Brazzale
+==========================================================================
+Copyright (c) 2002/2004
+CINECA - Interuniversity Consortium (www.cineca.it) 
+=========================================================================*/
+
+#ifndef __medOpExporterLandmarkWS_H__
+#define __medOpExporterLandmarkWS_H__
+
+//----------------------------------------------------------------------------
+// Include :
+//----------------------------------------------------------------------------
+#include "mafOp.h"
+
+//----------------------------------------------------------------------------
+// forward references :
+//----------------------------------------------------------------------------
+
+class mafVMELandmarkCloud;
+
+/** 
+class name: medOpExporterLandmarkWS
+/** Export trajectories in a cvs file */
+class medOpExporterLandmarkWS : public mafOp
+{
+public:
+	medOpExporterLandmarkWS(const wxString &label = "Trajectories Exporter");
+	~medOpExporterLandmarkWS(); 
+	mafOp* Copy();
+
+	/** Return true for the acceptable vme type. */
+	bool Accept(mafNode* node);
+
+	/** Builds operation's interface. */
+	void OpRun();
+
+  /** Write the file.
+  File format:
+
+  TRAJECTORIES
+  Frequency,Hz
+  PointName1,PointName2,..,PointName(n-1)
+  Time, x,y,z, ...
+  time0 x0,y0,z0,x0,y0,z0, .. 
+  time1 x1,y1,z1,x1,y1,z1, ..
+  ..
+  time(m-1) x(m-1),y(m-1),z(m-1), ..
+
+  the format of the file admits some specifics:
+  1) The first line should contains the tag TRAJECTORIES.
+  2) The second line contains the Frequency, which in this case is a dummy value (see below).
+  3) The third line contains the points names.
+  4) The fourth line contains tags.
+  5) The other lines contain time and values. Time is always the first row.
+
+  this operation does as follows:
+  1) Checks for the tag TRAJECTORIES in the first line
+  2) Sets Frequency to 1 --> This is done since the exporter puts time instead of sample in the file.
+  3) Writes the points names
+  4) Write the user tags
+  5) Writes time and values 
+  */
+  void Write();
+
+  /** Set the filename for the file to export */
+  void SetFileName(const char *file_name){m_File = file_name;};
+
+protected:
+	wxString m_File;
+	mafVMELandmarkCloud  *m_Cloud;
+};
+#endif
