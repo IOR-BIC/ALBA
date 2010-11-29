d46c450da4d62c8972728bdd203fa836891f4dd3
ior04 <ior04>
2010-11-29 13:19:57 +0000

    add test for medPipeCompoundVolumeVR and MIP

---
+++ Testing/VME/medPipeCompoundVolumeVRTest.cpp
@@ -0,0 +1,137 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: medPipeCompoundVolumeVRTest.cpp,v $
+Language:  C++
+Date:      $Date: 2010-11-29 13:19:57 $
+Version:   $Revision: 1.1.2.1 $
+Authors:   Daniele Giunchi
+==========================================================================
+Copyright (c) 2002/2008
+CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#include "medDefines.h" 
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include <cppunit/config/SourcePrefix.h>
+#include "medPipeCompoundVolumeFixedScalars.h"
+#include "medPipeCompoundVolumeVRTest.h"
+#include "mafSceneNode.h"
+#include "medGUIDynamicVP.h"
+
+#include "mafVMEVolumeGray.h"
+
+
+#include <iostream>
+
+class DummyPipeCompoundVolumeVR : public medPipeCompoundVolumeVR
+{
+public:
+  mafTypeMacro(DummyPipeCompoundVolumeVR,medPipeCompoundVolumeVR);
+  DummyPipeCompoundVolumeVR(){}
+  void Create(mafSceneNode *n);
+protected:
+  // redefinition for avoiding gui components
+  mafGUI *CreateGui() {return NULL;}
+  void    UpdateGUILayout(bool bForce){}
+  void    CreatePageGroups(){}
+};
+
+//----------------------------------------------------------------------------
+void DummyPipeCompoundVolumeVR::Create(mafSceneNode *n)
+//----------------------------------------------------------------------------
+{
+  //remove wxCursor busy;
+
+  mafPipe::Create(n);
+  m_SceneNode = n; 
+
+  //creates description of groups
+  CreatePageGroups();
+
+  //creates initial pages for each group
+  CreatePages();
+
+  //AddActor();
+}
+
+//----------------------------------------------------------------------------
+mafCxxTypeMacro(DummyPipeCompoundVolumeVR);
+//----------------------------------------------------------------------------
+
+
+//----------------------------------------------------------------------------
+void medPipeCompoundVolumeVRTest::TestFixture()
+//----------------------------------------------------------------------------
+{
+}
+//----------------------------------------------------------------------------
+void medPipeCompoundVolumeVRTest::setUp()
+//----------------------------------------------------------------------------
+{
+
+}
+//----------------------------------------------------------------------------
+void medPipeCompoundVolumeVRTest::tearDown()
+//----------------------------------------------------------------------------
+{
+
+}
+
+//----------------------------------------------------------------------------
+void medPipeCompoundVolumeVRTest::TestAllocation()
+//----------------------------------------------------------------------------
+{
+  DummyPipeCompoundVolumeVR *pipe = new DummyPipeCompoundVolumeVR;
+  mafDEL(pipe);
+}
+//----------------------------------------------------------------------------
+void medPipeCompoundVolumeVRTest::TestCreateSceneNode()
+//----------------------------------------------------------------------------
+{
+  mafVMEVolumeGray *volume;
+  mafNEW(volume);
+
+  //Assembly will be create when instancing mafSceneNode
+  mafSceneNode *sceneNode;
+  sceneNode = new mafSceneNode(NULL,NULL,volume, NULL);
+
+  /////////// Pipe Instance and Creation ///////////
+  DummyPipeCompoundVolumeVR *pipe = new DummyPipeCompoundVolumeVR;
+  pipe->Create(sceneNode);
+
+  delete pipe;
+  delete sceneNode;
+  mafDEL(volume);
+
+  delete wxLog::SetActiveTarget(NULL);
+}
+
+//----------------------------------------------------------------------------
+void medPipeCompoundVolumeVRTest::TestGetCurrentScalarVisualPipe()
+//----------------------------------------------------------------------------
+{
+  mafVMEVolumeGray *volume;
+  mafNEW(volume);
+
+  //Assembly will be create when instancing mafSceneNode
+  mafSceneNode *sceneNode;
+  sceneNode = new mafSceneNode(NULL,NULL,volume, NULL);
+
+  /////////// Pipe Instance and Creation ///////////
+  DummyPipeCompoundVolumeVR *pipe = new DummyPipeCompoundVolumeVR;
+  pipe->Create(sceneNode);
+
+  //smoke test
+  CPPUNIT_ASSERT(pipe->GetCurrentScalarVisualPipe() == NULL);
+
+  delete pipe;
+  delete sceneNode;
+  mafDEL(volume);
+
+}
