12b99c41d6f7949fa37d0a73ce389659fbbc4f14
ior02 <ior02>
2011-02-14 13:39:09 +0000

    added medGUIWizardPageTest

---
+++ Testing/Gui/medGUIWizardPageTest.h
@@ -0,0 +1,92 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: medGUIWizardPageTest.h,v $
+Language:  C++
+Date:      $Date: 2011-02-14 13:39:09 $
+Version:   $Revision: 1.1.2.1 $
+Authors:   Matteo Giacomoni
+==========================================================================
+Copyright (c) 2002/2004 
+CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#ifndef __CPP_UNIT_medGUIWizardPageTest_H__
+#define __CPP_UNIT_medGUIWizardPageTest_H__
+
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
+
+class TestApp;
+class mafGUIFrame;
+
+class medGUIWizardPageTest : public CPPUNIT_NS::TestFixture
+{
+public: 
+  // CPPUNIT fixture: executed before each test
+  void setUp();
+
+  // CPPUNIT fixture: executed after each test
+  void tearDown();
+
+  CPPUNIT_TEST_SUITE( medGUIWizardPageTest );
+  CPPUNIT_TEST( TestDynamicAllocation );
+  CPPUNIT_TEST( TestAddGuiLowerLeft );
+  CPPUNIT_TEST( TestAddGuiLowerRight );
+  CPPUNIT_TEST( TestAddGuiLowerCenter );
+  CPPUNIT_TEST( TestRemoveGuiLowerLeft );
+  CPPUNIT_TEST( TestRemoveGuiLowerRight );
+  CPPUNIT_TEST( TestRemoveGuiLowerCenter );
+  CPPUNIT_TEST( TestSetNextPage );
+  CPPUNIT_TEST_SUITE_END();
+
+protected:
+
+  void TestDynamicAllocation();
+  void TestAddGuiLowerLeft();
+  void TestAddGuiLowerRight();
+  void TestAddGuiLowerCenter();
+  void TestRemoveGuiLowerLeft();
+  void TestRemoveGuiLowerRight();
+  void TestRemoveGuiLowerCenter();
+  void TestSetNextPage();
+
+  bool m_Result;
+
+  mafGUIFrame *m_Win;
+
+  TestApp *m_App;
+};
+
+
+int
+main( int argc, char* argv[] )
+{
+  // Create the event manager and test controller
+  CPPUNIT_NS::TestResult controller;
+
+  // Add a listener that colllects test result
+  CPPUNIT_NS::TestResultCollector result;
+  controller.addListener( &result );        
+
+  // Add a listener that print dots as test run.
+  CPPUNIT_NS::BriefTestProgressListener progress;
+  controller.addListener( &progress );      
+
+  // Add the top suite to the test runner
+  CPPUNIT_NS::TestRunner runner;
+  runner.addTest( medGUIWizardPageTest::suite());
+  runner.run( controller );
+
+  // Print test in a compiler compatible format.
+  CPPUNIT_NS::CompilerOutputter outputter( &result, CPPUNIT_NS::stdCOut() );
+  outputter.write(); 
+
+  return result.wasSuccessful() ? 0 : 1;
+}
+
+#endif
