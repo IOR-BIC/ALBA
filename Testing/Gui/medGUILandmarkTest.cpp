5e59d3ed23e886aabe47310ceac26d952d5077c2
mucci <mucci>
2011-02-14 14:25:29 +0000

    Removed unused lines

--- Testing/Gui/medGUILandmarkTest.cpp
+++ Testing/Gui/medGUILandmarkTest.cpp
@@ -2,8 +2,8 @@
 Program:   Multimod Application Framework
 Module:    $RCSfile: medGUILandmarkTest.cpp,v $
 Language:  C++
-Date:      $Date: 2011-02-14 13:49:41 $
-Version:   $Revision: 1.1.2.1 $
+Date:      $Date: 2011-02-14 14:25:29 $
+Version:   $Revision: 1.1.2.2 $
 Authors:   Roberto Mucci
 ==========================================================================
 Copyright (c) 2002/2004 
@@ -23,38 +23,8 @@ CINECA - Interuniversity Consortium (www.cineca.it)
 #include "medGUILandmark.h"
 #include "mafVMESurfaceParametric.h"
 #include "mafVMESurface.h"
-//#include "wx/module.h"
 #include "vtkDataSet.h"
 
-/*
-// Helper class used to build a fake application needed by the wxConfig
-// to store settings into the registry with the same name of the application
-// ===============================================================================
-class TestApp : public wxApp
-  // ===============================================================================
-{
-public:
-  bool OnInit();
-  int  OnExit();
-};
-DECLARE_APP(TestApp)
-
-IMPLEMENT_APP(TestApp)
-//--------------------------------------------------------------------------------
-bool TestApp::OnInit()
-//--------------------------------------------------------------------------------
-{
-  return TRUE;
-}
-//--------------------------------------------------------------------------------
-int TestApp::OnExit()
-//--------------------------------------------------------------------------------
-{
-  wxApp::CleanUp();
-  return 0;
-}
-// ===============================================================================
-*/
 
 #define TEST_RESULT CPPUNIT_ASSERT(m_Result)
 
