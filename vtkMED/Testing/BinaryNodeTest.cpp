89b9f4b5191b185e5e18f75c867841f35f40c8a9
ior04 <ior04>
2011-03-14 13:45:48 +0000

    add binarynode test, remove symmetric sparse matrix (unused and bugged class)

---
+++ vtkMED/Testing/BinaryNodeTest.cpp
@@ -0,0 +1,104 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: BinaryNodeTest.cpp,v $
+Language:  C++
+Date:      $Date: 2011-03-14 13:45:48 $
+Version:   $Revision: 1.1.2.1 $
+Authors:   Daniele Giunchi
+==========================================================================
+Copyright (c) 2002/2004 
+CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#include "medDefines.h"
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include <cppunit/config/SourcePrefix.h>
+#include "BinaryNodeTest.h"
+
+#include "vtkMEDPoissonSurfaceReconstruction.h"
+
+//-------------------------------------------------------------------------
+void BinaryNodeTest::TestDynamicAllocation()
+//-------------------------------------------------------------------------
+{
+  BinaryNode<Real> *binaryNode = new BinaryNode<Real>();
+
+  delete binaryNode;
+}
+//-------------------------------------------------------------------------
+void BinaryNodeTest::TestStaticAllocation()
+//-------------------------------------------------------------------------
+{
+  BinaryNode<Real> binaryNode;
+}
+
+//-------------------------------------------------------------------------
+void BinaryNodeTest:: TestCenterCount()
+//-------------------------------------------------------------------------
+{
+    int centerCount = BinaryNode<Real>::CenterCount(3);
+    CPPUNIT_ASSERT(centerCount == 8);
+}
+//-------------------------------------------------------------------------
+void BinaryNodeTest:: TestCumulativeCenterCount()
+//-------------------------------------------------------------------------
+{
+    int centerCount = BinaryNode<Real>::CumulativeCenterCount(5);
+    CPPUNIT_ASSERT(centerCount == 63);
+}
+//-------------------------------------------------------------------------
+void BinaryNodeTest:: TestIndex()
+//-------------------------------------------------------------------------
+{
+    int index = BinaryNode<Real>::Index(2, 1);
+    CPPUNIT_ASSERT(index == 4);
+}
+//-------------------------------------------------------------------------
+void BinaryNodeTest:: TestCornerIndex()
+//-------------------------------------------------------------------------
+{
+    int cornerIndex = BinaryNode<Real>::CornerIndex(5,2,1,1);
+    CPPUNIT_ASSERT(cornerIndex == 16);
+}
+//-------------------------------------------------------------------------
+void BinaryNodeTest:: TestCornerIndexPosition()
+//-------------------------------------------------------------------------
+{
+    Real cip = BinaryNode<Real>::CornerIndexPosition(0, 2);
+    CPPUNIT_ASSERT(cip == 0);
+}
+//-------------------------------------------------------------------------
+void BinaryNodeTest:: TestWidth()
+//-------------------------------------------------------------------------
+{
+    Real width = BinaryNode<Real>::Width(3);
+    CPPUNIT_ASSERT(width == 0.125);
+}
+//-------------------------------------------------------------------------
+void BinaryNodeTest:: TestCenterAndWidth()
+//-------------------------------------------------------------------------
+{
+    Real center, width;
+    BinaryNode<Real>::CenterAndWidth(2, 1, center, width);
+    
+    CPPUNIT_ASSERT(center == 0.375 && width == 0.25);
+    
+    BinaryNode<Real>::CenterAndWidth(2, center, width);
+    
+    CPPUNIT_ASSERT(center == 0.75 && width == 0.5);
+}
+//-------------------------------------------------------------------------
+void BinaryNodeTest:: TestDepthAndOffset()
+//-------------------------------------------------------------------------
+{
+    int depth, offset;
+    BinaryNode<Real>::DepthAndOffset(3, depth, offset);
+    
+    CPPUNIT_ASSERT(depth == 2 && offset == 0);
+}
