45ef7010c51abc04ec94aa10a1549a5c12b1fd11
mucci <mucci>
2011-03-23 10:50:24 +0000

    test class for StartingPlynomial

---
+++ vtkMED/Testing/StartingPolynomialTest.cpp
@@ -0,0 +1,48 @@
+/*=========================================================================
+Program:   Multimod Application Framework
+Module:    $RCSfile: StartingPolynomialTest.cpp,v $
+Language:  C++
+Date:      $Date: 2011-03-23 10:50:24 $
+Version:   $Revision: 1.1.2.1 $
+Authors:   Roberto Mucci
+==========================================================================
+Copyright (c) 2002/2004 
+CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#include "medDefines.h"
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include <cppunit/config/SourcePrefix.h>
+#include "StartingPolynomialTest.h"
+
+#include "vtkMEDPoissonSurfaceReconstruction.h"
+#include "vtkMEDPoissonSurfaceReconstruction.cxx"
+
+//-------------------------------------------------------------------------
+void StartingPolynomialTest::TestAllMethods()
+//-------------------------------------------------------------------------
+{
+
+/** scale operation*/
+// StartingPolynomial<Degree> StartingPolynomial<Degree>::scale(const double& s) const
+
+	StartingPolynomial<1> sp1;
+  sp1.start = 50;
+  StartingPolynomial<1> s1 = sp1.scale(2);
+  CPPUNIT_ASSERT(s1.start == sp1.start*2);
+
+
+/** shift operation*/
+// StartingPolynomial<Degree> StartingPolynomial<Degree>::shift(const double& s) const
+
+  StartingPolynomial<1> sp2(sp1);
+  StartingPolynomial<1> s2 = sp2.shift(10);
+  CPPUNIT_ASSERT(s2.start == sp2.start+10);
+}
+
