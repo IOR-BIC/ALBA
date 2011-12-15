fea4e072b87c324b735b6e5bac5001c8b042af42
ior02 <ior02>
2011-12-15 10:32:31 +0000

    added mafUtility.h

---
+++ Base/mafUtility.h
@@ -0,0 +1,27 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: mafUtility.h,v $
+  Language:  C++
+  Date:      $Date: 2011-12-15 10:32:31 $
+  Version:   $Revision: 1.1.2.1 $
+  Authors:   Marco Petrone
+==========================================================================
+  Copyright (c) 2001/2005 
+  CINECA - Interuniversity Consortium (www.cineca.it)
+=========================================================================*/
+
+#ifndef __mafUtility_h
+#define __mafUtility_h
+
+#include "mafConfigure.h"
+
+
+/** mafUtility - the pourpose of this class is just to groups utilities classes
+*/
+
+class MAF_EXPORT mafUtility 
+{
+};
+
+#endif
+
