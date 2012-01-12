223b7b73604f1bfb5535520646755d5a3cdab097
ior02 <ior02>
2012-01-12 13:22:34 +0000

    added gui comments qa rules

--- qa/Rules/GuiCommentsRules/GuiCommentsRules.py
+++ qa/Rules/GuiCommentsRules/GuiCommentsRules.py
@@ -8,31 +8,32 @@ class GuiCommentsRules(AbstractRule):
         AbstractRule.__init__(self)
         self.DictionaryList = []
         
-    def CheckFile(self,filename,match,num,minNumElem):
-      r = open(filename, 'r')
+    def CheckFile(self,filename,match,num):
+        r = open(filename, 'r')
+        lines = r.readlines()
+        r.close()
+        
+        lineNumber = 0
+        for line in lines:
+            lineNumber = lineNumber + 1
+            x = re.compile(match)
+            if(re.search(x, line)):
 
-      lines = r.readlines()
-      lineNumber = 0
-      for line in lines:
-        lineNumber = lineNumber + 1
-        x = re.compile(match)
-        if(re.search(x, line)):
+                founded =  re.findall('".+"\b*,|_\(".+"\b*\),|".+"\b*\);',re.split(x, line)[1])#sUBDIVIDE ELEMENTS OF THE GUI METHOD
+                for f in founded:
+                    line = line.replace(re.findall('".*"',f)[0],re.findall('".*"',f)[0].replace(',',' '))#REMOVE ','
 
-          founded =  re.findall('".+"\b*,|_\(".+"\b*\),|".+"\b*\);',re.split(x, line)[1])
-          for f in founded:
-            line = line.replace(re.findall('".*"',f)[0],re.findall('".*"',f)[0].replace(',',' '))
+                if (len(re.split(',',re.split(x, line)[1])) != num):#CHECK IF THE GUI METHOD HAS A NUMBER OF ELEMENTS SUFFICIENT FOR TOOLTIP
+                    # print filename + ' : ' + line
+                    self.MarkedList.append("<item><class>" + (os.path.split(self.FullPathInputFile)[-1]) + "</class><line>" + str(lineNumber) + "</line>" + "</item>")          
 
-          if (len(re.split(',',re.split(x, line)[1])) != num and len(re.split(',',re.split(x, line)[1])) >= minNumElem):
-            # print filename + ' : ' + line
-            self.MarkedList.append("<item><class>" + (os.path.split(self.FullPathInputFile)[-1]) + "</class><line>" + str(lineNumber) + "</line>" + "</item>")          
-
-      r.close()
 
     def execute(self):
-      f = open("./Rules/GuiCommentsRules/" + self.ParameterList[0], 'r')
-      lines = f.readlines()
-      for line in lines:
-        self.CheckFile( self.FullPathInputFile, line.split()[0], int(line.split()[1]), int(line.split()[2]) )
-            
-      return self.MarkedList
+        f = open("./Rules/GuiCommentsRules/" + self.ParameterList[0], 'r')
+        lines = f.readlines()
+        f.close()
+        for line in lines:
+            self.CheckFile( self.FullPathInputFile, line.split()[0], int(line.split()[1]) )
+        
+        return self.MarkedList
                   
