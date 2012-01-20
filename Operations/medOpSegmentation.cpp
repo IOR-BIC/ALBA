b430450b577a2e704db6e00d79228eaa2f9434fd
ior10 <ior10>
2012-01-20 15:28:41 +0000

    Modify op. to use the fill holes remove islands filter for the refinement step

--- Operations/medOpSegmentation.cpp
+++ Operations/medOpSegmentation.cpp
@@ -2,8 +2,8 @@
 Program:   LHP
 Module:    $RCSfile: medOpSegmentation.cpp,v $
 Language:  C++
-Date:      $Date: 2012-01-19 09:43:30 $
-Version:   $Revision: 1.1.2.23 $
+Date:      $Date: 2012-01-20 15:28:41 $
+Version:   $Revision: 1.1.2.24 $
 Authors:   Eleonora Mambrini - Matteo Giacomoni, Gianluigi Crimi
 ==========================================================================
 Copyright (c) 2007
@@ -84,11 +84,7 @@ SCS s.r.l. - BioComputing Competence Centre (www.scsolutions.it - www.b3c.it)
 #include "vtkConnectivityFilter.h"
 #include "vtkUnstructuredGrid.h"
 #include "vtkPointData.h"
-// #include "vtkPolyData.h"
-// #include "vtkPoints.h"
-// #include "vtkCellArray.h"
-// #include "vtkMatrix4x4.h"
-// #include "mafVMEPolyline.h"
+#include "vtkMEDImageFillHolesRemoveIslands.h"
 
 #include "itkImage.h"
 #include "itkImageToVTKImageFilter.h"
@@ -872,96 +868,21 @@ bool medOpSegmentation::Refinement()
 bool medOpSegmentation::ApplyRefinementFilter2(vtkStructuredPoints *inputImage, vtkStructuredPoints *outputImage)
 //----------------------------------------------------------------------------
 {
-  outputImage->DeepCopy(inputImage);
-  int imgDims[3];
-  outputImage->GetDimensions(imgDims);
-
-  vtkUnsignedCharArray* imgScalars = (vtkUnsignedCharArray*)outputImage->GetPointData()->GetScalars();
-  
-  int recognitionSquareEdge = m_RefinementRegionsSize + 2; // Number of pixels of the recognition square
-  // @ToDo: test image bounds?
-
-
-  // @Todo: OPTIMIZATION:
-  // Start from first coordinates (xs,ys) where the value is different from discrimination value (not necessary depending on the same pixel)
-  // End from last coordinates (xl,yl) where the value is different from discrimination value (not necessary depending on the same pixel)
-
-  double discriminationValue = 0;
-
+  vtkMEDImageFillHolesRemoveIslands *filter = vtkMEDImageFillHolesRemoveIslands::New();
+  filter->SetInput(inputImage);
+  filter->SetEdgeSize(m_RefinementRegionsSize);
   if(m_RefinementSegmentationAction == ID_REFINEMENT_HOLES_FILL)
   {
-     discriminationValue = 255;
+    filter->SetAlgorithmToFillHoles();
   }
-
-  while(recognitionSquareEdge >= 3)
+  else if(m_RefinementSegmentationAction == ID_REFINEMENT_ISLANDS_REMOVE)
   {
-    // (x0, y0) origin of the recognition square
-    for(int y0 = 0; y0 < imgDims[1] - recognitionSquareEdge; y0++)
-    {
-
-      for(int x0 = 0; x0 < imgDims[0] - recognitionSquareEdge; x0++)
-      {
-        // imgScalars->SetTuple1(y0 * imgDims[0] + x0, 255);
-        bool isolatedRegion = true;
-
-        // check "y" sides
-        for(int y = y0; y < y0 + recognitionSquareEdge; y = y + recognitionSquareEdge - 1)
-        {
-          for(int x = x0; x < x0 + recognitionSquareEdge; x++)
-          {
-            if(imgScalars->GetTuple1(y * imgDims[0] + x) != discriminationValue)
-            {
-              isolatedRegion = false;
-              //mafLogMessage("Square x0,y0 %d,%d is NOT an island",x0,y0);
-              break;
-            }
-          }
-          if(!isolatedRegion)
-          {
-            break;
-          }
-        }
-        // check x sides
-        if(isolatedRegion)
-        {
-          for(int x = x0; x < x0 + recognitionSquareEdge; x = x + recognitionSquareEdge - 1)
-          {
-            for(int y = y0; y < y0 + recognitionSquareEdge; y++)
-            {
-              if(imgScalars->GetTuple1(y * imgDims[0] + x) != discriminationValue)
-              {
-                isolatedRegion = false;
-                //mafLogMessage("Square x0,y0 %d,%d is NOT an island",x0,y0);
-                break;
-              }
-            }
-            if(!isolatedRegion)
-            {
-              break;
-            }
-          }
-        }
-        if(isolatedRegion)
-        {
-          //mafLogMessage("Square x0,y0 %d,%d IS an island",x0,y0);
-          // fill all the recognition square with discrimination value
-          for(int y = y0; y < y0 + recognitionSquareEdge; y++)
-          {
-            for(int x = x0; x < x0 + recognitionSquareEdge; x++)
-            {
-              imgScalars->SetTuple1(y * imgDims[0] + x, discriminationValue);
-            }
-          }
-        }
-      }
-    }
-    recognitionSquareEdge --;
+    filter->SetAlgorithmToRemoveIslands();
   }
-  outputImage->GetPointData()->SetScalars(imgScalars);
-  outputImage->GetPointData()->Modified();
-  outputImage->GetPointData()->Update();
+  filter->Update();
+  outputImage->DeepCopy(filter->GetOutput());
   outputImage->Update();
-
+  filter->Delete();
   return true;
 }
 //----------------------------------------------------------------------------
