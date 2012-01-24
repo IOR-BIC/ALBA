8260b4c10a2c972477efd2711acff4e84853bfa3
ior02 <ior02>
2012-01-24 07:31:39 +0000

    increase streght of the rule

--- qa/Rules/GuiCommentsRules/GuiCommentsRules.py
+++ qa/Rules/GuiCommentsRules/GuiCommentsRules.py
@@ -18,14 +18,20 @@ class GuiCommentsRules(AbstractRule):
             lineNumber = lineNumber + 1
             x = re.compile(match)
             if(re.search(x, line)):
-
                 founded =  re.findall('".+"\b*,|_\(".+"\b*\),|".+"\b*\);',re.split(x, line)[1])#sUBDIVIDE ELEMENTS OF THE GUI METHOD
                 for f in founded:
                     line = line.replace(re.findall('".*"',f)[0],re.findall('".*"',f)[0].replace(',',' '))#REMOVE ','
-
-                if (len(re.split(',',re.split(x, line)[1])) != num):#CHECK IF THE GUI METHOD HAS A NUMBER OF ELEMENTS SUFFICIENT FOR TOOLTIP
-                    # print filename + ' : ' + line
-                    self.MarkedList.append("<item><class>" + (os.path.split(self.FullPathInputFile)[-1]) + "</class><line>" + str(lineNumber) + "</line>" + "</item>")          
+                
+                foundCorrectValue = False
+                for el in num:
+                    if (len(re.split(',',re.split(x, line)[1])) >= el):#CHECK IF THE GUI METHOD HAS A NUMBER OF ELEMENTS SUFFICIENT FOR TOOLTIP
+                        if(line[len(line)-3:-1] == ');'):#REMOVE , IF PRESENT, ');' AT THE END OF THE LINE
+                            line = line[:-3]
+                        if(re.search('".*"',re.split(',',re.split(x, line)[1])[el-1]) != None ):
+                            foundCorrectValue = True
+                
+                if(foundCorrectValue == False):
+                    self.MarkedList.append("<item><class>" + (os.path.split(self.FullPathInputFile)[-1]) + "</class><line>" + str(lineNumber) + "</line>" + "</item>")
 
 
     def execute(self):
@@ -33,7 +39,13 @@ class GuiCommentsRules(AbstractRule):
         lines = f.readlines()
         f.close()
         for line in lines:
-            self.CheckFile( self.FullPathInputFile, line.split()[0], int(line.split()[1]) )
+            num = []
+            num = line.split()[1:]
+            numInt = []
+            for el in num:
+                numInt.append(int(el))
+                
+            found = self.CheckFile( self.FullPathInputFile, line.split()[0], numInt )
+            
         
         return self.MarkedList
-                  
