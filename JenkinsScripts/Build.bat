4234b7ffbf6d8917f19b636045ced0d82cdfa8e9
stefano perticoni <s.perticoni@scsitaly.com>
2012-03-27 13:07:43 +0200

    Changed build directories

--- JenkinsScripts/Build.bat
+++ JenkinsScripts/Build.bat
@@ -6,14 +6,13 @@ IF "%1" == "LIB_VS2010_DEBUG" GOTO LIB_VS2010_DEBUG
 
 :LIB_VS2010_DEBUG
 
-cd ../MAF2.Build
+cd ../Build
 
 CALL "%PROGRAMFILES%/Microsoft Visual Studio 10.0/Common7/Tools/vsvars32.bat"
 devenv MAF.sln /project ALL_BUILD.vcxproj /build debug /out build_log.txt
 
-cd ../MAF2.Source
+cd ../Source
   
 GOTO END
 
 :END
-
