83f0d92a3226bed65d0886f7fa5d045ec0d11ebd
stefano perticoni <s.perticoni@scsitaly.com>
2012-04-02 16:02:02 +0200

    wip jenkins build scripts

--- JenkinsScripts/Build.bat
+++ JenkinsScripts/Build.bat
@@ -3,8 +3,11 @@ REM Build scripts
 REM -----------------------------------------------------------------------------
 
 IF "%1" == "LIB_VS2010_DEBUG" GOTO LIB_VS2010_DEBUG
+IF "%1" == "LIB_VS2010_RELEASE" GOTO LIB_VS2010_RELEASE
 
+REM -----------------------------------------------------------------------------
 :LIB_VS2010_DEBUG
+REM -----------------------------------------------------------------------------
 
 cd Build
 
@@ -14,5 +17,20 @@ devenv MED.sln /project ALL_BUILD.vcxproj /build debug /out build_log.txt
 cd ..
 
 GOTO END
+
+REM -----------------------------------------------------------------------------
+:LIB_VS2010_RELEASE
+REM -----------------------------------------------------------------------------
+
+cd Build
+
+CALL "%PROGRAMFILES%/Microsoft Visual Studio 10.0/Common7/Tools/vsvars32.bat"
+devenv MED.sln /project ALL_BUILD.vcxproj /build release /out build_log.txt
+
+cd ..
+
+GOTO END
+
+REM -----------------------------------------------------------------------------
 
 :END
