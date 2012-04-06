ec7c0939fda420fb513d1bc07c14ef5c149507f6
ior02 <ior02>
2012-04-06 07:54:31 +0000

    added classe to allow DLL of Medical's projects

---
+++ Common/itkRawMotionImporterUtility.cxx
@@ -0,0 +1,41 @@
+/*=========================================================================
+
+ Program:   Visualization Toolkit
+ Module:    $RCSfile: itkRawMotionImporterUtility.cxx,v $
+ Language:  C++
+ Date:      $Date: 2012-04-06 07:54:31 $
+ Version:   $Revision: 1.1.2.1 $
+
+=========================================================================*/
+#include "itkRawMotionImporterUtility.h"
+
+#include <vcl_fstream.h>
+
+//----------------------------------------------------------------------------
+int itkRawMotionImporterUtility::ReadMatrix()
+{
+//   //Read raw motion data
+//   vcl_ifstream v_raw_matrix(fname, std::ios::in);
+// 
+// 	
+// 	if(v_raw_matrix.is_open() != 0)
+// 	{	
+// 		M.read_ascii(v_raw_matrix);
+//     return 0;
+// 	}
+
+ 	return 1;
+}
+
+itkRawMotionImporterUtility::itkRawMotionImporterUtility()
+{
+
+}
+
+itkRawMotionImporterUtility::~itkRawMotionImporterUtility()
+{
+
+}
+
+
+
---
+++ Common/medGUIContextualMenu.h
@@ -0,0 +1,55 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: medGUIContextualMenu.h,v $
+  Language:  C++
+  Date:      $Date: 2012-04-06 07:54:31 $
+  Version:   $Revision: 1.1.2.1 $
+  Authors:   Daniele Giunchi    
+==========================================================================
+  Copyright (c) 2002/2004
+  CINECA - Interuniversity Consortium (www.cineca.it) 
+=========================================================================*/
+
+#ifndef __medGUIContextualMenu_H__
+#define __medGUIContextualMenu_H__
+
+#include "medCommonDefines.h"
+
+//----------------------------------------------------------------------------
+// forward references;
+//----------------------------------------------------------------------------
+class mafView;
+class mafEvent;
+class mafObserver;
+
+/**
+  Class Name: medGUIContextualMenu.
+  Represents the contexctual menu' which compares when click right button 
+  over a viewport.
+*/
+class MED_COMMON_EXPORT medGUIContextualMenu : public wxMenu
+{
+public:
+  /** constructor. */
+  medGUIContextualMenu();
+  /** destructor. */
+  virtual ~medGUIContextualMenu();
+  /** Set the listener object, i.e. the object receiving events sent by this object */
+  void SetListener(mafObserver *Listener) {m_Listener = Listener;};
+
+	/** 
+  Visualize contextual menu for the MDI child and selected view. */
+  void ShowContextualMenu(wxFrame *child, mafView *view, bool vme_menu);		
+
+protected:
+  wxFrame     *m_ChildViewActive;
+  mafView     *m_ViewActive;
+  mafObserver *m_Listener;
+
+	/** Answer contextual menu's selection. */
+	void OnContextualViewMenu(wxCommandEvent& event);
+
+  /** Event Table Declaration. */
+  DECLARE_EVENT_TABLE()
+};
+#endif
