35b222df63eb5695e0c2a6ff34fa7c0b62f21b43
ior02 <ior02>
2012-04-19 07:32:54 +0000

    changing in way to compile DLLs under VS 2010

--- Base/mafTransformBase.h
+++ Base/mafTransformBase.h
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: mafTransformBase.h,v $
   Language:  C++
-  Date:      $Date: 2011-05-25 09:30:12 $
-  Version:   $Revision: 1.13.4.1 $
+  Date:      $Date: 2012-04-19 07:32:54 $
+  Version:   $Revision: 1.13.4.2 $
   Authors:   Marco Petrone
 ==========================================================================
   Copyright (c) 2001/2005 
@@ -25,6 +25,10 @@
 class mafEventSource;
 class vtkLinearTransform;
 class vtkMAFToLinearTransform;
+
+#ifdef MAF_EXPORTS
+template class MAF_EXPORT mafAutoPointer<mafMatrix>;
+#endif
 
 /**  Superclass for Homogeneous transformations.
   mafTransformBase is the superclass for MAF geometric, and currently homogeneous
@@ -142,9 +146,6 @@ public:
 #endif
 
 protected:
-#ifdef MAF_EXPORTS
-  template class MAF_EXPORT mafAutoPointer<mafMatrix>;
-#endif
   mafAutoPointer<mafMatrix> m_Matrix;        ///< internally stored matrix.
   mafMTime        m_MTime;           ///< modification time
   mafMTime        m_UpdateTime;      ///< We need to record the time of the last update
