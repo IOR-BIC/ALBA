0f100d6e70f19eff6a6a3c06bb6ab20074eba84f
Matteo Giacomoni <m.giacomoni@scsitaly.com>
2012-05-22 14:48:44 +0200

    added DLL_VS2010_RELEASE modality

--- JenkinsScripts/CMake.bat
+++ JenkinsScripts/CMake.bat
@@ -6,6 +6,7 @@ REM ----------------------------------------------------------------------------
 IF "%1" == "LIB_VS2010_DEBUG" GOTO LIB_VS2010_DEBUG
 IF "%1" == "LIB_VS2010_RELEASE" GOTO LIB_VS2010_RELEASE
 IF "%1" == "DLL_VS2010_DEBUG" GOTO DLL_VS2010_DEBUG
+IF "%1" == "DLL_VS2010_RELEASE" GOTO DLL_VS2010_RELEASE
 
 REM -----------------------------------------------------------------------------
 :LIB_VS2010_DEBUG
@@ -57,6 +58,22 @@ cmake.exe  ../Source ^
   -DCPPUNIT_INCLUDE_DIR:PATH="C:/cppunit-1.12.0_VS2010_BUILD/cppunit-1.12.0/include" ^
   -DCPPUNIT_LIBRARY:FILEPATH="C:/cppunit-1.12.0_VS2010_BUILD/cppunit-1.12.0/lib/cppunitd.lib" ^
   -DMAF_BINARY_PATH:PATH="d:/MAF2Libs/VS2010D_DLL/Build/"
+  
+cd ..
+
+GOTO END
+
+REM -----------------------------------------------------------------------------
+:DLL_VS2010_RELEASE
+REM -----------------------------------------------------------------------------
+
+mkdir Build
+cd Build
+
+cmake.exe  ../Source ^
+  -G"Visual Studio 10" ^
+  -DMED_BUILD_MEDDLL:BOOL=ON ^
+  -DMAF_BINARY_PATH:PATH=d:\MAF2Libs\VS2010R_DLL\Build\
   
 cd ..
 
