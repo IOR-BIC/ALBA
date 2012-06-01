361438f7709fcd114e5454eacd9dca14840d5810
sperticoni <s.perticoni@scsitaly.com>
2012-06-01 14:33:01 +0200

    Working on Doxygen documentation embed into Jenkins

--- JenkinsScripts/CMake.bat
+++ JenkinsScripts/CMake.bat
@@ -6,6 +6,7 @@ IF "%1" == "LIB_VS2010_DEBUG" GOTO LIB_VS2010_DEBUG
 IF "%1" == "LIB_VS2010_RELEASE" GOTO LIB_VS2010_RELEASE
 IF "%1" == "DLL_VS2010_DEBUG" GOTO DLL_VS2010_DEBUG
 IF "%1" == "DLL_VS2010_RELEASE" GOTO DLL_VS2010_RELEASE
+IF "%1" == "DOXYGEN_DOCUMENTATION" GOTO DOXYGEN_DOCUMENTATION
 
 REM -----------------------------------------------------------------------------
 :LIB_VS2010_DEBUG
@@ -74,7 +75,23 @@ cd ..
 GOTO END
 
 REM -----------------------------------------------------------------------------
+:DOXYGEN_DOCUMENTATION
+REM -----------------------------------------------------------------------------
 
+mkdir Build
+cd Build
+
+cmake.exe  ../Source  -G"Visual Studio 10" ^
+  -DBUILD_DOCUMENTATION:BOOL=ON ^
+  -DBUILD_TESTING:BOOL=OFF ^
+  -DBUILD_EXAMPLES:BOOL=OFF ^
+
+cd ..
+  
+GOTO END
+
+REM -----------------------------------------------------------------------------
 :END
+REM -----------------------------------------------------------------------------
 
 
