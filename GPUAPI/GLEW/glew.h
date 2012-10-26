ea786787fb46ec5e1eeb90c891e788cb8c02f0a0
besoft <besoft@kiv.zcu.cz>
2012-10-26 11:27:34 +0200

    Added GLEW_STATIC into glew.h for simplify backward compatibility

--- GPUAPI/GLEW/glew.h
+++ GPUAPI/GLEW/glew.h
@@ -80,6 +80,8 @@
 #define __glew_h__
 #define __GLEW_H__
 
+#define GLEW_STATIC	//BES: 26.10.2012 - in MAF2, we are always using STATIC version
+
 #if defined(__gl_h_) || defined(__GL_H__) || defined(__X_GL_H)
 #error gl.h included before glew.h
 #endif
