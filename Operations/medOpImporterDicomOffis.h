f07b71adc599134645b4d8265749ab0b3e8909b7
Simone Bna <s.bn@cineca.it>
2013-08-20 12:32:54 +0200

    Build Dicom Volume

--- Operations/medOpImporterDicomOffis.h
+++ Operations/medOpImporterDicomOffis.h
@@ -1,16 +1,16 @@
 /*=========================================================================
 
- Program: MAF2Medical
- Module: medOpImporterDicomOffis
- Authors: Matteo Giacomoni, Roberto Mucci , Stefano Perticoni, Gianluigi Crimi
- 
- Copyright (c) B3C
- All rights reserved. See Copyright.txt or
- http://www.scsitaly.com/Copyright.htm for details.
+Program: MAF2Medical
+Module: medOpImporterDicomOffis
+Authors: Matteo Giacomoni, Roberto Mucci , Stefano Perticoni, Gianluigi Crimi
 
- This software is distributed WITHOUT ANY WARRANTY; without even
- the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- PURPOSE.  See the above copyright notice for more information.
+Copyright (c) B3C
+All rights reserved. See Copyright.txt or
+http://www.scsitaly.com/Copyright.htm for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notice for more information.
 
 =========================================================================*/
 
@@ -141,32 +141,32 @@ public:
 
 protected:
 
-  enum DICOM_IMPORTER_GUI_ID
-  {
-    ID_FIRST = medGUIWizard::ID_LAST,
-    ID_STUDY_SELECT,
-    ID_SERIES_SELECT,
-    ID_CROP,
-    ID_UNDO_CROP,
-    ID_BUILD_STEP,
-    ID_BUILD_BUTTON,
-    ID_RS_STEP,
-    ID_RS_BUTTON,
-    ID_CANCEL,
-    ID_PATIENT_NAME,
-    ID_PATIENT_ID,
-    ID_SURGEON_NAME,
-    ID_SCAN_TIME,
-    ID_SCAN_SLICE,
-    ID_VOLUME_NAME,
-    ID_VOLUME_SIDE,
-    ID_VME_TYPE,
-    ID_SORT_AXIS,
-    ID_RS_SELECT,
-    ID_RS_SWAP,
-    ID_RS_SWAPALL,
-    ID_RS_APPLYTOALL,
-  };
+	enum DICOM_IMPORTER_GUI_ID
+	{
+		ID_FIRST = medGUIWizard::ID_LAST,
+		ID_STUDY_SELECT,
+		ID_SERIES_SELECT,
+		ID_CROP,
+		ID_UNDO_CROP,
+		ID_BUILD_STEP,
+		ID_BUILD_BUTTON,
+		ID_RS_STEP,
+		ID_RS_BUTTON,
+		ID_CANCEL,
+		ID_PATIENT_NAME,
+		ID_PATIENT_ID,
+		ID_SURGEON_NAME,
+		ID_SCAN_TIME,
+		ID_SCAN_SLICE,
+		ID_VOLUME_NAME,
+		ID_VOLUME_SIDE,
+		ID_VME_TYPE,
+		ID_SORT_AXIS,
+		ID_RS_SELECT,
+		ID_RS_SWAP,
+		ID_RS_SWAPALL,
+		ID_RS_APPLYTOALL,
+	};
 
 	/** OnEvent helper functions */
 	void OnScanTime();
@@ -175,13 +175,13 @@ protected:
 	void OnMouseDown( mafEvent * e );
 	void OnMouseMove( mafEvent * e );
 	void OnMouseUp();
-  void OnWizardChangePage( mafEvent * e );
-  void OnSeriesSelect();
+	void OnWizardChangePage( mafEvent * e );
+	void OnSeriesSelect();
 	void OnStudySelect();
 	void OnVmeTypeSelected();
-  void OnReferenceSystemSelected();
-  void OnSwapReferenceSystemSelected();
-  void UpdateReferenceSystemVariables();
+	void OnReferenceSystemSelected();
+	void OnSwapReferenceSystemSelected();
+	void UpdateReferenceSystemVariables();
 
 	/** Create load page and his GUI for the wizard. */
 	void CreateLoadPage();
@@ -192,17 +192,17 @@ protected:
 	/** Create build page and his GUI for the wizard. */
 	void CreateBuildPage();
 
-  /** Create reference system page and his GUI for the wizard.\n
-  This page is shown only if VME image is created as output\n
-  This page allow to select reference system (xy, xz or yz) for the
-  selected images */
-  void CreateReferenceSystemPage();
+	/** Create reference system page and his GUI for the wizard.\n
+	This page is shown only if VME image is created as output\n
+	This page allow to select reference system (xy, xz or yz) for the
+	selected images */
+	void CreateReferenceSystemPage();
 
 	/** Reset the list of files and all the structures that own images information. */
 	void ResetStructure();
 
-  /** gets the range of the dicom by walking thought the slices */
-  void GetDicomRange(double *range);
+	/** gets the range of the dicom by walking thought the slices */
+	void GetDicomRange(double *range);
 
 	/** Reset the slider that allow to scan the slices. */
 	void ResetSliders();
@@ -276,11 +276,11 @@ protected:
 	/** Rescale to 16 Bit */
 	void RescaleTo16Bit(vtkImageData *dataSet);
 
-  /** Reference system page is shown only if image is the vme output type */
-  void UpdateReferenceSystemPageConnection();
+	/** Reference system page is shown only if image is the vme output type */
+	void UpdateReferenceSystemPageConnection();
 
-  /** Apply transform according to the specified reference system */
-  void ApplyReferenceSystem();
+	/** Apply transform according to the specified reference system */
+	void ApplyReferenceSystem();
 
 	vtkDirectory			*m_DICOMDirectoryReader; 
 	vtkWindowLevelLookupTable	*m_SliceLookupTable;
@@ -303,7 +303,7 @@ protected:
 	medGUIWizardPageNew	*m_LoadPage;
 	medGUIWizardPageNew	*m_CropPage;
 	medGUIWizardPageNew	*m_BuildPage;
-  medGUIWizardPageNew	*m_ReferenceSystemPage; ///< Wizard step to choose reference system
+	medGUIWizardPageNew	*m_ReferenceSystemPage; ///< Wizard step to choose reference system
 	mafVMEMesh        *m_Mesh;
 	mafVMEGroup       *m_ImagesGroup;
 
@@ -311,12 +311,13 @@ protected:
 	mafGUI	*m_LoadGuiUnderLeft;
 	mafGUI	*m_CropGuiLeft;
 	mafGUI	*m_CropGuiCenter;
+	mafGUI *m_LoadGuiCenter;
 	mafGUI	*m_BuildGuiLeft;      ///< Left gui for RS step
 	mafGUI	*m_BuildGuiUnderLeft; ///< Underleft gui for RS step
 	mafGUI	*m_BuildGuiCenter;    ///< Center gui for RS step
-  mafGUI	*m_ReferenceSystemGuiLeft;
-  mafGUI	*m_ReferenceSystemGuiUnderLeft;
-  //mafGUI	*m_ReferenceSystemGuiCenter;
+	mafGUI	*m_ReferenceSystemGuiLeft;
+	mafGUI	*m_ReferenceSystemGuiUnderLeft;
+	//mafGUI	*m_ReferenceSystemGuiCenter;
 
 	mafGUI  *m_LoadGuiUnderCenter;
 
@@ -327,7 +328,7 @@ protected:
 	mafString m_SurgeonName;
 	mafString	m_Identifier;
 	mafString m_PatientPosition;
-    mafString m_VolumeName;
+	mafString m_VolumeName;
 
 	int				m_BuildStepValue;
 	int				m_DicomReaderModality; ///<Type DICOM Read from file
@@ -336,7 +337,9 @@ protected:
 	int				m_SortAxes;
 	int				m_NumberOfTimeFrames;
 
-  int m_SkipAllNoPosition;
+	int m_SkipAllNoPosition;
+
+	std::map <mafString,int> m_dcm_dim;
 
 	medDicomSeriesSliceList	*m_SelectedSeriesSlicesList; ///< Selected study slices list
 
@@ -348,6 +351,8 @@ protected:
 
 	std::map<std::vector<mafString>,int> m_SeriesIDstringToSeriesIDint;
 
+	std::map<std::vector<mafString>,std::map<mafString,int>> m_SeriesIDstringToSeriesDimensionMap;
+
 	std::map<std::vector<mafString>,medDicomCardiacMRIHelper*> m_SeriesIDToCardiacMRIHelperMap; ///< StudyUID-SeriesUIDWithPlanesNumber-SeriesUIDWithoutPlanesNumber vector to slices list map 
 
 	std::map<std::vector<mafString>,bool> m_SeriesIDContainsRotationsMap; ///< StudyUID-SeriesUIDWithPlanesNumber-SeriesUIDWithoutPlanesNumber vector to boolean map 
@@ -358,8 +363,8 @@ protected:
 	int				 m_NumberOfStudies; ///<Number of study present in the DICOM directory
 	int				 m_NumberOfSlices;
 	wxListBox	*m_StudyListbox;
-//	wxListCtrl  *m_StudyListctrl;
-//	wxListBox	*m_SeriesListbox;
+	//	wxListCtrl  *m_StudyListctrl;
+	//	wxListBox	*m_SeriesListbox;
 	wxListCtrl  *m_SeriesListctrl;
 
 	int           m_ZCropBounds[2];
@@ -367,13 +372,13 @@ protected:
 	wxSlider		 *m_SliceScannerLoadPage;
 	wxSlider		 *m_SliceScannerCropPage;
 	wxSlider		 *m_SliceScannerBuildPage;
-  wxSlider		 *m_SliceScannerReferenceSystemPage; ///< Slice slider for RS page
+	wxSlider		 *m_SliceScannerReferenceSystemPage; ///< Slice slider for RS page
 
 	int						m_CurrentTime;
 	wxSlider		 *m_TimeScannerLoadPage;
 	wxSlider		 *m_TimeScannerCropPage;
 	wxSlider		 *m_TimeScannerBuildPage;
-  wxSlider		 *m_TimeScannerReferenceSystemPage; ///< Time slider for RS page
+	wxSlider		 *m_TimeScannerReferenceSystemPage; ///< Time slider for RS page
 
 	mafTagArray	*m_TagArray;
 
@@ -393,24 +398,24 @@ protected:
 	int m_ResampleFlag;
 	int m_DiscardPosition;
 	int m_RescaleTo16Bit;
-  int m_SelectedReferenceSystem;  ///< Specify the reference system (xy xz yx)
-  int m_SwapReferenceSystem;      ///< Specify the if the reference system is swapped or not (e.g xy to yx)
-  int m_SwapAllReferenceSystem;   ///< Specify the if the reference system is swapped or not (e.g xy to yx)
-  int m_ApplyToAllReferenceSystem;///< Specify if the current refernce system is applyed to all images
-  int m_GlobalReferenceSystem;    ///< Global reference system if apply to all is selected
+	int m_SelectedReferenceSystem;  ///< Specify the reference system (xy xz yx)
+	int m_SwapReferenceSystem;      ///< Specify the if the reference system is swapped or not (e.g xy to yx)
+	int m_SwapAllReferenceSystem;   ///< Specify the if the reference system is swapped or not (e.g xy to yx)
+	int m_ApplyToAllReferenceSystem;///< Specify if the current refernce system is applyed to all images
+	int m_GlobalReferenceSystem;    ///< Global reference system if apply to all is selected
 
 	mafVMEImage				*m_Image;
 	mafVMEVolumeGray	*m_Volume;
 
 	mafGUICheckListBox *m_DicomModalityListBox;
-  int m_CurrentImageID;
+	int m_CurrentImageID;
 
-  double m_TotalDicomRange[2]; ///< contains the scalar range og the full dicom
-  double m_TotalDicomSubRange[2]; ///< contains the scalar range og the full dicom
+	double m_TotalDicomRange[2]; ///< contains the scalar range og the full dicom
+	double m_TotalDicomSubRange[2]; ///< contains the scalar range og the full dicom
 
 	/** destructor */
 	~medOpImporterDicomOffis();
- 
+
 };
 
 /**
@@ -424,28 +429,28 @@ http://www.cmake.org/pipermail/insight-users/2005-September/014711.html
 class MED_OPERATION_EXPORT medDicomSlice
 {
 public:
-  /** Enumerate reference system modalities */
-  enum ID_REFERENCE_SYSTEM
-  {
-    ID_RS_XY = 0,
-    ID_RS_XZ,
-    ID_RS_YZ,
-  };
+	/** Enumerate reference system modalities */
+	enum ID_REFERENCE_SYSTEM
+	{
+		ID_RS_XY = 0,
+		ID_RS_XZ,
+		ID_RS_YZ,
+	};
 
 	/** constructor */
 	medDicomSlice() 
 	{
-    m_PatientBirthdate = '###';
-    m_PatientName = '###';
-    m_Description = "###";
-    m_Date = "###";
+		m_PatientBirthdate = '###';
+		m_PatientName = '###';
+		m_Description = "###";
+		m_Date = "###";
 		m_SliceABSFileName = "";
 		m_DcmImagePositionPatient[0] = -9999;
 		m_DcmImagePositionPatient[1] = -9999;
 		m_DcmImagePositionPatient[2] = -9999;
-    m_DcmImagePositionPatientOriginal[0] = m_DcmImagePositionPatient[0];
-    m_DcmImagePositionPatientOriginal[1] = m_DcmImagePositionPatient[1];
-    m_DcmImagePositionPatientOriginal[2] = m_DcmImagePositionPatient[2];
+		m_DcmImagePositionPatientOriginal[0] = m_DcmImagePositionPatient[0];
+		m_DcmImagePositionPatientOriginal[1] = m_DcmImagePositionPatient[1];
+		m_DcmImagePositionPatientOriginal[2] = m_DcmImagePositionPatient[2];
 		m_DcmImageOrientationPatient[0] = 0.0;
 		m_DcmImageOrientationPatient[1] = 0.0; 
 		m_DcmImageOrientationPatient[2] = 0.0; 
@@ -456,26 +461,26 @@ public:
 		m_DcmTriggerTime = -1.0;
 		m_DcmCardiacNumberOfImages = -1;
 		m_Data = NULL;
-    m_ReferenceSystem = ID_RS_XY;
-    SetSwapReferenceSystem(FALSE);
+		m_ReferenceSystem = ID_RS_XY;
+		SetSwapReferenceSystem(FALSE);
 	};
 
 	/** overloaded constructor */
 	medDicomSlice(mafString sliceABSFilename,double dcmImagePositionPatient[3], double dcmImageOrientationPatient[6],\
 		vtkImageData *data , mafString description , mafString date , mafString patientName , mafString patientBirthdate ,int dcmInstanceNumber=-1, int dcmCardiacNumberOfImages=-1,\
-    double dcmTtriggerTime=-1.0, int referenceSystem = ID_RS_XY)  
+		double dcmTtriggerTime=-1.0, int referenceSystem = ID_RS_XY)  
 	{
-    m_PatientBirthdate = patientBirthdate;
-    m_PatientName = patientName;
-    m_Description = description;
-    m_Date = date;
+		m_PatientBirthdate = patientBirthdate;
+		m_PatientName = patientName;
+		m_Description = description;
+		m_Date = date;
 		m_SliceABSFileName = sliceABSFilename;
 		m_DcmImagePositionPatient[0] = dcmImagePositionPatient[0];
 		m_DcmImagePositionPatient[1] = dcmImagePositionPatient[1];
 		m_DcmImagePositionPatient[2] = dcmImagePositionPatient[2];
-    m_DcmImagePositionPatientOriginal[0] = m_DcmImagePositionPatient[0];
-    m_DcmImagePositionPatientOriginal[1] = m_DcmImagePositionPatient[1];
-    m_DcmImagePositionPatientOriginal[2] = m_DcmImagePositionPatient[2];
+		m_DcmImagePositionPatientOriginal[0] = m_DcmImagePositionPatient[0];
+		m_DcmImagePositionPatientOriginal[1] = m_DcmImagePositionPatient[1];
+		m_DcmImagePositionPatientOriginal[2] = m_DcmImagePositionPatient[2];
 		m_DcmImageOrientationPatient[0] = dcmImageOrientationPatient[0];
 		m_DcmImageOrientationPatient[1] = dcmImageOrientationPatient[1];
 		m_DcmImageOrientationPatient[2] = dcmImageOrientationPatient[2];
@@ -495,18 +500,18 @@ public:
 			m_Data = NULL;
 		}
 
-    SetReferenceSystem(referenceSystem);
-    SetSwapReferenceSystem(FALSE);
+		SetReferenceSystem(referenceSystem);
+		SetSwapReferenceSystem(FALSE);
 	};
 
 	/** destructor */
 	~medDicomSlice() {vtkDEL(m_Data);};
 
-  /** Return patient birthday */
-  mafString GetPatientBirthday(){return m_PatientBirthdate;};
+	/** Return patient birthday */
+	mafString GetPatientBirthday(){return m_PatientBirthdate;};
 
-  /** Return patient name */
-  mafString GetPatientName(){return m_PatientName;};
+	/** Return patient name */
+	mafString GetPatientName(){return m_PatientName;};
 
 	/** Return the filename of the corresponding dicom slice. */
 	const char *GetSliceABSFileName() const {return m_SliceABSFileName.GetCStr();};
@@ -554,13 +559,13 @@ public:
 		dcmImagePositionPatient[2]=m_DcmImagePositionPatient[2];
 	};
 
-  /** Get the DcmImagePositionPatient tag original for the slice */
-  void GetDcmImagePositionPatientOriginal(double dcmImagePositionPatient[3])
-  {
-    dcmImagePositionPatient[0]=m_DcmImagePositionPatientOriginal[0];
-    dcmImagePositionPatient[1]=m_DcmImagePositionPatientOriginal[1];
-    dcmImagePositionPatient[2]=m_DcmImagePositionPatientOriginal[2];
-  };
+	/** Get the DcmImagePositionPatient tag original for the slice */
+	void GetDcmImagePositionPatientOriginal(double dcmImagePositionPatient[3])
+	{
+		dcmImagePositionPatient[0]=m_DcmImagePositionPatientOriginal[0];
+		dcmImagePositionPatient[1]=m_DcmImagePositionPatientOriginal[1];
+		dcmImagePositionPatient[2]=m_DcmImagePositionPatientOriginal[2];
+	};
 
 
 	/** Set the DcmImageOrientationPatient tag for the slice*/
@@ -588,69 +593,69 @@ public:
 	/** 
 	Write dicom slice orientation on matrix*/
 	void GetOrientation( vtkMatrix4x4 * matrix );
- 
-  /** return the description */
-  mafString GetDescription(){return m_Description;};
 
-  /** return the date */
-  mafString GetDate(){return m_Date;};
+	/** return the description */
+	mafString GetDescription(){return m_Description;};
 
-  /** Set the information about the selected reference system (xy, xz, yx). see ID_REFERENCE_SYSTEM enum */
-  bool SetReferenceSystem(int referenceSystem)
-  {
-    if(referenceSystem>= ID_RS_XY && referenceSystem <= ID_RS_YZ)
-    {
-      m_ReferenceSystem = referenceSystem;
-      return true;
-    }
-    else
-    {
-      CheckReferenceSystem();
-    }
-    return false;
-  };
+	/** return the date */
+	mafString GetDate(){return m_Date;};
 
-  /** Get the information about the selected reference system (xy, xz, yx). see ID_REFERENCE_SYSTEM enum */
-  int GetReferenceSystem()
-  {
-    CheckReferenceSystem();
-    return m_ReferenceSystem;
-  };
+	/** Set the information about the selected reference system (xy, xz, yx). see ID_REFERENCE_SYSTEM enum */
+	bool SetReferenceSystem(int referenceSystem)
+	{
+		if(referenceSystem>= ID_RS_XY && referenceSystem <= ID_RS_YZ)
+		{
+			m_ReferenceSystem = referenceSystem;
+			return true;
+		}
+		else
+		{
+			CheckReferenceSystem();
+		}
+		return false;
+	};
 
-  /** Utility function to check reference system integrity */
-  void CheckReferenceSystem()
-  {
-    if(!(m_ReferenceSystem>= ID_RS_XY && m_ReferenceSystem <= ID_RS_YZ)) // Initialization (m_ReferenceSystem have an invalid value)
-    {
-      m_ReferenceSystem = ID_RS_XY;
-    } // Otherwise keep the old value
-  };
+	/** Get the information about the selected reference system (xy, xz, yx). see ID_REFERENCE_SYSTEM enum */
+	int GetReferenceSystem()
+	{
+		CheckReferenceSystem();
+		return m_ReferenceSystem;
+	};
 
-  /** Set if reference system is swapped */
-  void SetSwapReferenceSystem(int swap)
-  {
-    if(swap != FALSE)
-    {
-      swap = TRUE;
-    }
-    m_SwapReferenceSystem = swap;
-  };
+	/** Utility function to check reference system integrity */
+	void CheckReferenceSystem()
+	{
+		if(!(m_ReferenceSystem>= ID_RS_XY && m_ReferenceSystem <= ID_RS_YZ)) // Initialization (m_ReferenceSystem have an invalid value)
+		{
+			m_ReferenceSystem = ID_RS_XY;
+		} // Otherwise keep the old value
+	};
 
-  /** Set if reference system is swapped */
-  int GetSwapReferenceSystem()
-  {
-    return m_SwapReferenceSystem;
-  };
+	/** Set if reference system is swapped */
+	void SetSwapReferenceSystem(int swap)
+	{
+		if(swap != FALSE)
+		{
+			swap = TRUE;
+		}
+		m_SwapReferenceSystem = swap;
+	};
+
+	/** Set if reference system is swapped */
+	int GetSwapReferenceSystem()
+	{
+		return m_SwapReferenceSystem;
+	};
 
 protected:
 	double m_DcmImagePositionPatient[3];
-  double m_DcmImagePositionPatientOriginal[3];
+	double m_DcmImagePositionPatientOriginal[3];
 	double m_DcmImageOrientationPatient[6];
 	mafString m_SliceABSFileName;
-  mafString m_Description;
-  mafString m_Date;
-  mafString m_PatientName;
-  mafString m_PatientBirthdate;
+	mafString m_Description;
+	mafString m_Date;
+	mafString m_PatientName;
+	mafString m_PatientBirthdate;
 
 	double m_DcmTriggerTime;
 	int m_DcmInstanceNumber;
@@ -658,7 +663,7 @@ protected:
 
 	vtkImageData *m_Data;
 
-  int m_ReferenceSystem;  ///< Store information about the selected reference system (xy, xz, yx). see ID_REFERENCE_SYSTEM enum
-  int m_SwapReferenceSystem;
+	int m_ReferenceSystem;  ///< Store information about the selected reference system (xy, xz, yx). see ID_REFERENCE_SYSTEM enum
+	int m_SwapReferenceSystem;
 };
 #endif
