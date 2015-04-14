71249d66333e9a7a36bf9ed45043e276c3f0944a
Gianluigi Crimi <g.g.crimi@gmail.com>
2015-04-14 17:45:38 +0200

    Merge views tests

--- Testing/Views/mafViewRXCTTest.h
+++ Testing/Views/mafViewRXCTTest.h
@@ -14,8 +14,8 @@
 
 =========================================================================*/
 
-#ifndef __CPP_UNIT_MAFVIEWRXCTTEST_H__
-#define __CPP_UNIT_MAFVIEWRXCTTEST_H__
+#ifndef __CPP_UNIT_mafViewRXCTTest_H__
+#define __CPP_UNIT_mafViewRXCTTest_H__
 
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/BriefTestProgressListener.h>
@@ -34,31 +34,5 @@ class mafViewRXCTTest : public CPPUNIT_NS::TestFixture
   protected:
     void CreateDestroyTest();
 };
-
-int
-main( int argc, char* argv[] )
-{
-  // Create the event manager and test controller
-  CPPUNIT_NS::TestResult controller;
-
-  // Add a listener that colllects test result
-  CPPUNIT_NS::TestResultCollector result;
-  controller.addListener( &result );        
-
-  // Add a listener that print dots as test run.
-  CPPUNIT_NS::BriefTestProgressListener progress;
-  controller.addListener( &progress );      
-
-  // Add the top suite to the test runner
-  CPPUNIT_NS::TestRunner runner;
-  runner.addTest( mafViewRXCTTest::suite());
-  runner.run( controller );
-
-  // Print test in a compiler compatible format.
-  CPPUNIT_NS::CompilerOutputter outputter( &result, CPPUNIT_NS::stdCOut() );
-  outputter.write(); 
-
-  return result.wasSuccessful() ? 0 : 1;
-}
 
 #endif
--- Testing/Views/mafViewSliceGlobalTest.h
+++ Testing/Views/mafViewSliceGlobalTest.h
@@ -14,8 +14,8 @@
 
 =========================================================================*/
 
-#ifndef __CPP_UNIT_MAFVIEWRXCTTEST_H__
-#define __CPP_UNIT_MAFVIEWRXCTTEST_H__
+#ifndef __CPP_UNIT_mafViewSliceGlobalTest_H__
+#define __CPP_UNIT_mafViewSliceGlobalTest_H__
 
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/BriefTestProgressListener.h>
@@ -36,32 +36,5 @@ class mafViewSliceGlobalTest : public CPPUNIT_NS::TestFixture
   protected:
     void CreateDestroyTest();
 };
-
-
-int
-main( int argc, char* argv[] )
-{
-  // Create the event manager and test controller
-  CPPUNIT_NS::TestResult controller;
-
-  // Add a listener that colllects test result
-  CPPUNIT_NS::TestResultCollector result;
-  controller.addListener( &result );        
-
-  // Add a listener that print dots as test run.
-  CPPUNIT_NS::BriefTestProgressListener progress;
-  controller.addListener( &progress );      
-
-  // Add the top suite to the test runner
-  CPPUNIT_NS::TestRunner runner;
-  runner.addTest( mafViewSliceGlobalTest::suite());
-  runner.run( controller );
-
-  // Print test in a compiler compatible format.
-  CPPUNIT_NS::CompilerOutputter outputter( &result, CPPUNIT_NS::stdCOut() );
-  outputter.write(); 
-
-  return result.wasSuccessful() ? 0 : 1;
-}
 
 #endif
