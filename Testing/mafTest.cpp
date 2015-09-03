0ec762325f9abec728f4f69986995375073c9c78
Gianluigi Crimi <g.g.crimi@gmail.com>
2015-09-03 16:49:05 +0200

    added mafTest.cpp and mafTest.h

---
+++ Testing/mafTest.cpp
@@ -0,0 +1,67 @@
+/*=========================================================================
+
+ Program: MAF2
+ Module: mafGUIWizardPageTest
+ Authors: Matteo Giacomoni
+ 
+ Copyright (c) B3C
+ All rights reserved. See Copyright.txt or
+ http://www.scsitaly.com/Copyright.htm for details.
+
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include "mafDefines.h"
+#include "mafDecl.h"
+//----------------------------------------------------------------------------
+// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
+// This force to include Window,wxWidgets and VTK exactly in this order.
+// Failing in doing this will result in a run-time error saying:
+// "Failure#0: The value of ESP was not properly saved across a function call"
+//----------------------------------------------------------------------------
+
+#include <cppunit/config/SourcePrefix.h>
+#include "mafTest.h"
+
+
+IMPLEMENT_APP(TestApp)
+//--------------------------------------------------------------------------------
+bool TestApp::OnInit()
+//--------------------------------------------------------------------------------
+{
+	return TRUE;
+}
+
+//--------------------------------------------------------------------------------
+int TestApp::OnExit()
+//--------------------------------------------------------------------------------
+{
+	wxApp::CleanUp();
+	return 0;
+}
+
+//----------------------------------------------------------------------------
+void mafTest::setUp()
+{
+	m_App = new TestApp();  // Instantiate the application class
+	m_App->argc = 0;        // set the number of input argument to 0
+	m_App->argv = NULL;     // set to NULL the input argument's parameters
+	wxTheApp->SetAppName("mafUserTest"); // Set the name for the application
+	
+	wxLog::EnableLogging();
+
+	//Run Test Specific Stuff
+	BeforeTest();
+}
+//----------------------------------------------------------------------------
+void mafTest::tearDown()
+{
+	//Clean Test Spefic Stuff
+	AfterTest();
+
+	cppDEL(m_App);  // Destroy the application
+	wxAppConsole::SetInstance(NULL);
+}
---
+++ Testing/mafTest.h
@@ -0,0 +1,58 @@
+/*=========================================================================
+
+ Program: MAF2
+ Module: mafGUIWizardPageTest
+ Authors: Matteo Giacomoni
+ 
+ Copyright (c) B3C
+ All rights reserved. See Copyright.txt or
+ http://www.scsitaly.com/Copyright.htm for details.
+
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#ifndef __CPP_UNIT_mafTests_H__
+#define __CPP_UNIT_mafTests_H__
+
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestResult.h>
+#include "mafIncludeWX.h"
+
+// Helper class used to build a fake application needed by the wxConfig
+// to store settings into the registry with the same name of the application
+// ===============================================================================
+class TestApp : public wxApp
+	// ===============================================================================
+{
+public:
+	bool OnInit();
+	int  OnExit();
+};
+DECLARE_APP(TestApp)
+
+
+class mafTest : public CPPUNIT_NS::TestFixture
+{
+public: 
+  /** CPPUNIT fixture: executed before each test, prepares an application for test runs
+			DO NOT OVERLOAD THIS METHOD, use BeforeTest instead */
+  void setUp();
+
+	/** CPPUNIT fixture: executed after each test, clean an application for test runs
+			DO NOT OVERLOAD THIS METHOD, use AfterTest instead */
+  void tearDown();
+
+	/** Test specific stuff executed before each test */
+	virtual void BeforeTest() {};
+
+	/** Test specific stuff executed after each test */
+	virtual void AfterTest() {};
+
+private:
+  TestApp *m_App;
+};
+
+#endif
