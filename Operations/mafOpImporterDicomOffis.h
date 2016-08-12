47137db4495e698cdf08f2f904fbc79080a037cb
Gianluigi Crimi <g.g.crimi@gmail.com>
2016-08-12 11:59:17 +0200

    Solved Bug on slice slider

--- Operations/mafOpImporterDicomOffis.h
+++ Operations/mafOpImporterDicomOffis.h
@@ -384,9 +384,8 @@ class MAF_EXPORT mafDicomSlice
 public:
 
 	/** Constructor */
-	mafDicomSlice(mafString sliceABSFilename, double dcmImagePositionPatient[3], double dcmImageOrientationPatient[6], \
-		vtkImageData *data, mafString description, mafString date, mafString patientName, mafString patientBirthdate, int dcmInstanceNumber = -1, int dcmCardiacNumberOfImages = -1, \
-		double dcmTtriggerTime = -1.0)
+	mafDicomSlice(mafString sliceABSFilename, double dcmImageOrientationPatient[6], vtkImageData *data, mafString description, mafString date,
+								mafString patientName, mafString patientBirthdate, int dcmInstanceNumber = -1, int dcmCardiacNumberOfImages = -1, double dcmTtriggerTime = -1.0)
 	{
 		m_PatientBirthdate = patientBirthdate;
 		m_PatientName = patientName;
@@ -397,10 +396,7 @@ public:
 		m_DcmCardiacNumberOfImages = dcmCardiacNumberOfImages;
 		m_DcmTriggerTime = dcmTtriggerTime;
 		m_ImageData = data;
-		SetDcmImagePositionPatient(dcmImagePositionPatient);
-		SetDcmImagePositionPatientOriginal(dcmImagePositionPatient);
 		SetDcmImageOrientationPatient(dcmImageOrientationPatient);
-		CalculateUnrotatedOrigin();
 	};
 
 	/** destructor */
@@ -439,44 +435,6 @@ public:
 	/** Retrieve image data */
 	vtkImageData* GetVTKImageData() { return m_ImageData; };
 
-	/** Set the DcmImagePositionPatient tag for the slice */
-	void SetDcmImagePositionPatient(double dcmImagePositionPatient[3])
-	{
-		m_DcmImagePositionPatient[0] = dcmImagePositionPatient[0];
-		m_DcmImagePositionPatient[1] = dcmImagePositionPatient[1];
-		m_DcmImagePositionPatient[2] = dcmImagePositionPatient[2];
-	};
-
-	/** Get the DcmImagePositionPatient tag for the slice */
-	void GetDcmImagePositionPatient(double dcmImagePositionPatient[3])
-	{
-		dcmImagePositionPatient[0] = m_DcmImagePositionPatient[0];
-		dcmImagePositionPatient[1] = m_DcmImagePositionPatient[1];
-		dcmImagePositionPatient[2] = m_DcmImagePositionPatient[2];
-	};
-
-	/** Get the DcmImagePositionPatient tag for the slice */
-	const double *GetDcmImagePositionPatient() {return m_DcmImagePositionPatient;};
-
-	/** Set the DcmImagePositionPatient tag for the slice */
-	void SetDcmImagePositionPatientOriginal(double dcmImagePositionPatient[3])
-	{
-		m_DcmImagePositionPatientOriginal[0] = dcmImagePositionPatient[0];
-		m_DcmImagePositionPatientOriginal[1] = dcmImagePositionPatient[1];
-		m_DcmImagePositionPatientOriginal[2] = dcmImagePositionPatient[2];
-	};
-
-	/** Get the DcmImagePositionPatient tag original for the slice */
-	void GetDcmImagePositionPatientOriginal(double dcmImagePositionPatient[3])
-	{
-		dcmImagePositionPatient[0] = m_DcmImagePositionPatientOriginal[0];
-		dcmImagePositionPatient[1] = m_DcmImagePositionPatientOriginal[1];
-		dcmImagePositionPatient[2] = m_DcmImagePositionPatientOriginal[2];
-	};
-
-	/** Get the DcmImagePositionPatient tag for the slice */
-	const double *GetDcmImagePositionPatientOriginal() { return m_DcmImagePositionPatientOriginal; };
-
 	/** Set the DcmImageOrientationPatient tag for the slice */
 	void SetDcmImageOrientationPatient(double dcmImageOrientationPatient[6])
 	{
@@ -486,6 +444,7 @@ public:
 		m_DcmImageOrientationPatient[3] = dcmImageOrientationPatient[3];
 		m_DcmImageOrientationPatient[4] = dcmImageOrientationPatient[4];
 		m_DcmImageOrientationPatient[5] = dcmImageOrientationPatient[5];
+		CalculateUnrotatedOrigin();
 	};
 
 	/** Get the DcmImageOrientationPatient tag for the slice */
@@ -547,8 +506,6 @@ protected:
 	void CalculateUnrotatedOrigin();
 
 	double m_UnrotatedOrigin[3];
-	double m_DcmImagePositionPatient[3];
-	double m_DcmImagePositionPatientOriginal[3];
 	double m_DcmImageOrientationPatient[6];
 	mafString m_SliceABSFileName;
 	mafString m_Description;
