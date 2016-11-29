a9b41fc8ac6e2a0657e3d16ba40633fa8bd03034
Nik81 <nicola.vanella@gmail.com>
2016-11-29 16:28:50 +0100

    Updated LogicManager

--- Core/mafLogicWithManagers.h
+++ Core/mafLogicWithManagers.h
@@ -104,140 +104,140 @@ in sub classes also create the manager
 5- Init: Calls FileOpen or FileNew
 	
 */
-class MAF_EXPORT mafLogicWithManagers: public mafObserver, public mafAbsLogicManager
+class MAF_EXPORT mafLogicWithManagers : public mafObserver, public mafAbsLogicManager
 {
 public:
-               mafLogicWithManagers(mafGUIMDIFrame *mdiFrame=NULL);
-	virtual     ~mafLogicWithManagers(); 
+	mafLogicWithManagers(mafGUIMDIFrame *mdiFrame = NULL);
+	virtual     ~mafLogicWithManagers();
 
-  enum UPLOAD_FLAGS
-  {
-    UPLOAD_SELECTED_VME = 0,
-    UPLOAD_SUBTREE,
-    UPLOAD_TREE,
-    UPLOAD_COMPRESSED_VME,
-    UPLOAD_COMPRESSED_SUBTREE,
-    UPLOAD_COMPRESSED_TREE,
-  };
+	enum UPLOAD_FLAGS
+	{
+		UPLOAD_SELECTED_VME = 0,
+		UPLOAD_SUBTREE,
+		UPLOAD_TREE,
+		UPLOAD_COMPRESSED_VME,
+		UPLOAD_COMPRESSED_SUBTREE,
+		UPLOAD_COMPRESSED_TREE,
+	};
 
-  /** */
-  virtual void OnEvent(mafEventBase *maf_event);
+	/** */
+	virtual void OnEvent(mafEventBase *maf_event);
 
-  /**  Plug a new view */
+	/**  Plug a new view */
 	virtual void Plug(mafView* view, bool visibleInMenu = true);
 
 
-  /**  Plug a new operation and its undo flag: if the operation does not support
-  undo the undo flag has no effect */
+	/**  Plug a new operation and its undo flag: if the operation does not support
+	undo the undo flag has no effect */
 	virtual void Plug(mafOp *op, wxString menuPath = "", bool canUndo = true, mafGUISettings *setting = NULL);
-	
-  /** Configure the application.
-  At this point are plugged all the managers, the side-bar docking panel. 
-  Are plugged also all the setting to the dialogs interface. */
-  virtual void Configure();
+
+	/** Configure the application.
+	At this point are plugged all the managers, the side-bar docking panel.
+	Are plugged also all the setting to the dialogs interface. */
+	virtual void Configure();
 
 	/** Creates menu and tool bars*/
 	void CreateToolBarsAndPanels();
 
-  /** Program Initialization */
+	/** Program Initialization */
 	virtual void Init(int argc, char **argv);
 
-  /** Must be called before Configure */
-  void PlugVMEManager(bool b) {m_UseVMEManager =b;};
+	/** Must be called before Configure */
+	void PlugVMEManager(bool b) { m_UseVMEManager = b; };
 
-  /** Must be called before Configure */
-  void PlugViewManager(bool b){m_UseViewManager=b;};
+	/** Must be called before Configure */
+	void PlugViewManager(bool b) { m_UseViewManager = b; };
 
-  /** Must be called before Configure */
-  void PlugOpManager(bool b){m_UseOpManager=b;};
+	/** Must be called before Configure */
+	void PlugOpManager(bool b) { m_UseOpManager = b; };
 
-  /** Must be called before Configure */
-  void PlugInteractionManger(bool b){m_UseInteractionManager=b;}
+	/** Must be called before Configure */
+	void PlugInteractionManger(bool b) { m_UseInteractionManager = b; }
 
-  /** Must be called before Configure */
-  void PlugWizardManager(bool b){m_UseWizardManager=b;};
+	/** Must be called before Configure */
+	void PlugWizardManager(bool b) { m_UseWizardManager = b; };
 
-  /**  Plug a new wizard */
-  virtual void Plug(mafWizard *wizard, wxString menuPath = "");
+	/**  Plug a new wizard */
+	virtual void Plug(mafWizard *wizard, wxString menuPath = "");
 
 	/**Set the application stamp to the VMEManager and Shows the application*/
-  virtual void Show();
+	virtual void Show();
 
-  /** Fill the View and operation menu's  */
+	/** Fill the View and operation menu's  */
 	void FillMenus();
 
-  /** Return the applications' user.*/
-  mafUser *GetUser();
+	/** Return the applications' user.*/
+	mafUser *GetUser() { return m_User; };
 
-  /** Set the application stamp for the application, 
-  if set to OPEN_ALL_DATA let's the application to open all msf file. 
-  As default the application stamp is the name of the application and it is set into the Show() method. */
-  void SetApplicationStamp(mafString &app_stamp);
-  void SetApplicationStamp(std::vector<mafString> app_stamp);
+	/** Set the application stamp for the application,
+	if set to OPEN_ALL_DATA let's the application to open all msf file.
+	As default the application stamp is the name of the application and it is set into the Show() method. */
+	void SetApplicationStamp(mafString &app_stamp);
+	void SetApplicationStamp(std::vector<mafString> app_stamp);
 
-  /** Allow to set the flag for views to by External to the main frame or to by child of parent frame. */
-  void SetExternalViewFlag(bool external = false);
+	/** Allow to set the flag for views to by External to the main frame or to by child of parent frame. */
+	void SetExternalViewFlag(bool external = false);
 
-  /** Retrieve the value for the external view flag.*/
-  bool GetExternalViewFlag();
+	/** Retrieve the value for the external view flag.*/
+	bool GetExternalViewFlag();
 
-  /** Manage application exception and allow to save at least the tree. */
-  virtual void HandleException();
+	/** Manage application exception and allow to save at least the tree. */
+	virtual void HandleException();
 
 	/** Set the revision string */
-	void SetRevision(mafString revision);
+	void SetRevision(mafString revision) { m_Revision = revision; };
 
-  /** Open a Find VME dialog.*/
-  void FindVME();
+	/** Open a Find VME dialog.*/
+	void FindVME();
 
-  /* Set MAF Expert Mode*/
-  void MAFExpertModeOn(){(*GetMAFExpertMode()) = TRUE;};
-  void MAFExpertModeOff(){(*GetMAFExpertMode())= FALSE;};
-  void SetMAFExpertMode(int value){(*GetMAFExpertMode()) = value;};
+	/* Set MAF Expert Mode*/
+	void MAFExpertModeOn() { (*GetMAFExpertMode()) = TRUE; };
+	void MAFExpertModeOff() { (*GetMAFExpertMode()) = FALSE; };
+	void SetMAFExpertMode(int value) { (*GetMAFExpertMode()) = value; };
 
-  /* Set the file extension */
-  void SetFileExtension(mafString &extension) {m_Extension = extension;};
+	/* Set the file extension */
+	void SetFileExtension(mafString &extension) { m_Extension = extension; };
 
 	/** Returns the pointer to the main panel of the application.*/
-	virtual mafGUIMDIFrame *GetTopWin(){return m_Win;};
+	virtual mafGUIMDIFrame *GetTopWin() { return m_Win; };
 
 	/** Sets the flag to know if Toolbar should be built.*/
-	void PlugToolbar(bool plug) {m_PlugToolbar	= plug;};
+	void PlugToolbar(bool plug) { m_PlugToolbar = plug; };
 	/** Sets the flag to know if Side bar should be built.*/
-	void PlugSidebar(bool plug, long style = mafSideBar::DOUBLE_NOTEBOOK) {m_PlugControlPanel	= plug; m_SidebarStyle = style;};
+	void PlugSidebar(bool plug, long style = mafSideBar::DOUBLE_NOTEBOOK) { m_PlugControlPanel = plug; m_SidebarStyle = style; };
 	/** Sets the flag to know if Time bar should be built.*/
-	void PlugTimebar(bool plug) {m_PlugTimebar	= plug;};
+	void PlugTimebar(bool plug) { m_PlugTimebar = plug; };
 	/** Sets the flag to know if Log bar should be built.*/
-	void PlugLogbar(bool plug)	{m_PlugLogPanel		= plug;};
+	void PlugLogbar(bool plug) { m_PlugLogPanel = plug; };
 
-	 /** 
-  Show the splash screen for the application. To define your own splash screen image simply
-  overwrite the SPLASH_SCREEN image into the picture factory by plugging your .xpm image. \sa mafPictureFactory*/
-  virtual void ShowSplashScreen();
+	/**
+ Show the splash screen for the application. To define your own splash screen image simply
+ overwrite the SPLASH_SCREEN image into the picture factory by plugging your .xpm image. \sa mafPictureFactory*/
+	virtual void ShowSplashScreen();
 
-  /** 
-  Used to give a splash screen image directly from the bitmap without converting it in xpm.*/
-  virtual void ShowSplashScreen(wxBitmap &splashImage);
+	/**
+	Used to give a splash screen image directly from the bitmap without converting it in xpm.*/
+	virtual void ShowSplashScreen(wxBitmap &splashImage);
 
 	static void ShowWebSite(wxString url);
 
 protected:
-  //---------------------------------------------------------
-  // Description:
-  // Method to update the time bounds when 
-  // the tree representation change by adding or deleting a vme.
-  virtual void UpdateTimeBounds();
-
-  // Description:
-  // Set the time to update the time bar.
-  virtual void TimeSet(double t);
 	//---------------------------------------------------------
-  
-  /** Redefined to add View,Op,Import,Export menu */
-  virtual void CreateMenu();
-  /** create a new storage object */
-  virtual void CreateStorage(mafEvent *e);
-  
+	// Description:
+	// Method to update the time bounds when 
+	// the tree representation change by adding or deleting a vme.
+	virtual void UpdateTimeBounds();
+
+	// Description:
+	// Set the time to update the time bar.
+	virtual void TimeSet(double t);
+	//---------------------------------------------------------
+
+	/** Redefined to add View,Op,Import,Export menu */
+	virtual void CreateMenu();
+	/** create a new storage object */
+	virtual void CreateStorage(mafEvent *e);
+
 	/** This method creates and add the Toolbar, for specific app toolbar override the CreateToolBar method*/
 	void CreateAndPlugToolbar();
 
@@ -252,8 +252,8 @@ protected:
 	virtual void CreateLogPanel();
 	/** Create a null logger. This is used when no log is due. */
 	void CreateNullLog();
-	
-	
+
+
 	/** Enable/disable a Toolbar or Menu Item */
 	void EnableItem(int item, bool enable);
 
@@ -265,43 +265,43 @@ protected:
 
 	// EVENT HANDLERS
 
-  /** FILE NEW evt. handler */
+	/** FILE NEW evt. handler */
 	virtual void OnFileNew();
-  /** FILE OPEN evt. handler. 
-  By default (file_to_open = NULL) it ask the user to choose a file to open,
-  otherwise it open the given one.*/
+	/** FILE OPEN evt. handler.
+	By default (file_to_open = NULL) it ask the user to choose a file to open,
+	otherwise it open the given one.*/
 	virtual void OnFileOpen(const char *file_to_open = NULL);
-  /** FILE UPLOAD evt. handler 
-  By default (remote_file = NULL) AND the entire msf is uploaded and only the remote directory is asked to the user, 
-  otherwise given parameters are managed to upload the file correctly. 'upload_flag' can be:
-  UPLOAD_SELECTED_VME, UPLOAD_SUBTREE or UPLOAD_TREE (default) and the corresponding compressed:
-  UPLOAD_COMPRESSED_VME, UPLOAD_COMPRESSED_SUBTREE, UPLOAD_COMPRESSED_TREE.*/
-  virtual void OnFileUpload(const char *remote_file, unsigned int upload_flag = UPLOAD_TREE);
-  /** FILE HISTORY evt. handler */
+	/** FILE UPLOAD evt. handler
+	By default (remote_file = NULL) AND the entire msf is uploaded and only the remote directory is asked to the user,
+	otherwise given parameters are managed to upload the file correctly. 'upload_flag' can be:
+	UPLOAD_SELECTED_VME, UPLOAD_SUBTREE or UPLOAD_TREE (default) and the corresponding compressed:
+	UPLOAD_COMPRESSED_VME, UPLOAD_COMPRESSED_SUBTREE, UPLOAD_COMPRESSED_TREE.*/
+	virtual void OnFileUpload(const char *remote_file, unsigned int upload_flag = UPLOAD_TREE);
+	/** FILE HISTORY evt. handler */
 	virtual void OnFileHistory(int menuId);
-  /** FILE SAVE evt. handler */
-  virtual void OnFileSave();
-  /** FILE SAVEAS evt. handler */
-  virtual void OnFileSaveAs();
-  /** Called on Quit event. */
-  virtual void OnQuit();
+	/** FILE SAVE evt. handler */
+	virtual void OnFileSave();
+	/** FILE SAVEAS evt. handler */
+	virtual void OnFileSaveAs();
+	/** Called on Quit event. */
+	virtual void OnQuit();
 
-  /** Called when a user drag a non msf or zmsf file over the application.
-  Example: dragging a VTK file over the application cause the application to inport it through the importer.*/
-  virtual void ImportExternalFile(mafString &filename);
+	/** Called when a user drag a non msf or zmsf file over the application.
+	Example: dragging a VTK file over the application cause the application to inport it through the importer.*/
+	virtual void ImportExternalFile(mafString &filename);
 
-  /** Respond to a VME_SELECT evt. Instantiate the 'Select' operation. */
+	/** Respond to a VME_SELECT evt. Instantiate the 'Select' operation. */
 	virtual void VmeSelect(mafEvent &e);
 	/** Respond to a VME_SELECT evt. Update the selection on the tree and view representation. */
 	virtual void VmeSelect(mafVME *vme);
-  /** Respond to a VME_SELECTED evt. Update the selection on the tree and view representation. */
+	/** Respond to a VME_SELECTED evt. Update the selection on the tree and view representation. */
 	virtual void VmeSelected(mafVME *vme);
 
 	/** Called from VmeSelect to select a LM */
 	void SelectLandmark(mafVMELandmark *lm, bool select);
 
 	/** Respond to a VME_DCLICKED evt. Manage the 'Double click' on Selected VME. */
-  virtual void VmeDoubleClicked(mafEvent &e);
+	virtual void VmeDoubleClicked(mafEvent &e);
 	/** Respond to a VME_SHOW evt. Show/Hide the vme. */
 	virtual void VmeShow(mafVME *vme, bool visibility);
 
@@ -317,42 +317,42 @@ protected:
 	/** Respond to a VME_Modified evt.*/
 	virtual void VmeModified(mafVME *vme);
 
-  /** Respond to a VME_ADD evt. Add a new vme to the tree. */
+	/** Respond to a VME_ADD evt. Add a new vme to the tree. */
 	virtual void VmeAdd(mafVME *vme);
-  /** Respond to a VME_ADDED evt. propagate evt. to SideBar,ViewManager,ecc.. */
+	/** Respond to a VME_ADDED evt. propagate evt. to SideBar,ViewManager,ecc.. */
 	virtual void VmeAdded(mafVME *vme);
-  /** Respond to a VME_REMOVE evt. Remove a vme from the tree. */
+	/** Respond to a VME_REMOVE evt. Remove a vme from the tree. */
 	virtual void VmeRemove(mafVME *vme);
-  /** Respond to a VME_REMOVING evt. propagate evt. to SideBar,ViewManager,ecc.. */
+	/** Respond to a VME_REMOVING evt. propagate evt. to SideBar,ViewManager,ecc.. */
 	virtual void VmeRemoving(mafVME *vme);
-	
+
 	virtual void VmeVisualModeChanged(mafVME * vme);
 
 	/** Respond to a VME_CHOOSE evt. Build a dialog containing the vme tree and return the vme choosed from the user. */
-	virtual std::vector<mafVME*> VmeChoose(long vme_accept_function = 0, long style = REPRESENTATION_AS_TREE, mafString title = "Choose Node", bool multiSelect = false, mafVME *vme=NULL);
+	virtual std::vector<mafVME*> VmeChoose(long vme_accept_function = 0, long style = REPRESENTATION_AS_TREE, mafString title = "Choose Node", bool multiSelect = false, mafVME *vme = NULL);
 
-  /** Build a dialog to show all available materials. */	
-  virtual void VmeChooseMaterial(mafVME *vme, bool updateProperty);
+	/** Build a dialog to show all available materials. */
+	virtual void VmeChooseMaterial(mafVME *vme, bool updateProperty);
 
-  //Update Vme Visual Properties
-  virtual void VmeUpdateProperties(mafVME *vme, bool updatePropertyFromTag);
+	//Update Vme Visual Properties
+	virtual void VmeUpdateProperties(mafVME *vme, bool updatePropertyFromTag);
 
-  /** Called when an operation starts. Disable all menu and lock the Selection */ 
+	/** Called when an operation starts. Disable all menu and lock the Selection */
 	virtual void OpRunStarting();
-  /** Called when an operation stops. Re-enable all menu and unlock the Selection */ 
+	/** Called when an operation stops. Re-enable all menu and unlock the Selection */
 	virtual void OpRunTerminated();
 	/** Show the gui of the running operation or the settings of the selected view. */
 	virtual void OpShowGui(bool push_gui, mafGUIPanel *panel);
 	/** Hide the setting's gui when a view is destroyed. */
 	virtual void OpHideGui(bool view_closed);
-  
-  /** Show contextual menu for tree when right mouse click arrives.*/
-  virtual void TreeContextualMenu(mafEvent &e);
 
-  /** Show contextual menu for views when right mouse click arrives.*/
-  virtual void ViewContextualMenu(bool vme_menu);
+	/** Show contextual menu for tree when right mouse click arrives.*/
+	virtual void TreeContextualMenu(mafEvent &e);
 
-  /** Create a new view. */
+	/** Show contextual menu for views when right mouse click arrives.*/
+	virtual void ViewContextualMenu(bool vme_menu);
+
+	/** Create a new view. */
 	virtual void ViewCreate(int viewId);
 	/** A new View has Been Created - Logic must create the View Frame */
 	virtual void ViewCreated(mafView *v);
@@ -363,75 +363,75 @@ protected:
 
 	virtual void CameraReset();
 
-  /** Called when user change the measure unit from menù Options.*/
-  void UpdateMeasureUnit();
+	/** Called when user change the measure unit from menù Options.*/
+	void UpdateMeasureUnit();
 
-  /** Enable/Disable menu items when an operation start/end running. */
-  virtual void EnableMenuAndToolbar();
+	/** Enable/Disable menu items when an operation start/end running. */
+	virtual void EnableMenuAndToolbar();
 
-  /** Called after FileOpen or Save operation */
-  virtual void UpdateFrameTitle();
+	/** Called after FileOpen or Save operation */
+	virtual void UpdateFrameTitle();
 
-  //-------------------
-
-
-  /** to be added */
-  virtual void ConfigureWizardManager();
-
-  /** Called when an wizard starts. Disable all menu and lock the Selection */ 
-  virtual void WizardRunStarting();
-
-  /** Called when an wizard stops. Re-enable all menu and unlock the Selection */ 
-  virtual void WizardRunTerminated();
-
-  /** Redefined to add Print buttons */
-  virtual void CreateWizardToolbar();
+	//-------------------
 
 
+	/** to be added */
+	virtual void ConfigureWizardManager();
 
-  mafSideBar             *m_SideBar;
-  mafVMEManager          *m_VMEManager;
-  mafViewManager         *m_ViewManager;
-  mafOpManager           *m_OpManager;
-  mafInteractionManager  *m_InteractionManager;
+	/** Called when an wizard starts. Disable all menu and lock the Selection */
+	virtual void WizardRunStarting();
+
+	/** Called when an wizard stops. Re-enable all menu and unlock the Selection */
+	virtual void WizardRunTerminated();
+
+	/** Redefined to add Print buttons */
+	virtual void CreateWizardToolbar();
+
+
+
+	mafSideBar             *m_SideBar;
+	mafVMEManager          *m_VMEManager;
+	mafViewManager         *m_ViewManager;
+	mafOpManager           *m_OpManager;
+	mafInteractionManager  *m_InteractionManager;
 	mafHelpManager				 *m_HelpManager;
 
 	mafVMELandmark				 *m_SelectedLandmark;
-  
-  mafGUIMaterialChooser  *m_MaterialChooser;
-  mafPrintSupport     *m_PrintSupport;
 
-  mafDeviceButtonsPadMouse            *m_Mouse;
+	mafGUIMaterialChooser  *m_MaterialChooser;
+	mafPrintSupport     *m_PrintSupport;
 
-  wxMenu *m_ImportMenu; 
-  wxMenu *m_ExportMenu; 
-  wxMenu *m_RecentFileMenu;
-  wxMenu *m_OpMenu;
-  wxMenu *m_ViewMenu; 
+	mafDeviceButtonsPadMouse            *m_Mouse;
 
-  bool m_CameraLinkingObserverFlag;
-  bool m_ExternalViewFlag;
+	wxMenu *m_ImportMenu;
+	wxMenu *m_ExportMenu;
+	wxMenu *m_RecentFileMenu;
+	wxMenu *m_OpMenu;
+	wxMenu *m_ViewMenu;
 
-  bool m_UseVMEManager;
-  bool m_UseViewManager;
-  bool m_UseOpManager;
-  bool m_UseInteractionManager;
+	bool m_CameraLinkingObserverFlag;
+	bool m_ExternalViewFlag;
+
+	bool m_UseVMEManager;
+	bool m_UseViewManager;
+	bool m_UseOpManager;
+	bool m_UseInteractionManager;
 	bool m_UseHelpManager;
-  mafGUISettingsDialog *m_SettingsDialog;
-  
-  mafString m_Revision;
-  mafString m_Extension;
+	mafGUISettingsDialog *m_SettingsDialog;
 
-  mafUser *m_User; ///< Applications' user
+	mafString m_Revision;
+	mafString m_Extension;
 
-  mafWizardManager *m_WizardManager;
-  bool m_UseWizardManager;
-  bool m_WizardRunning;
+	mafUser *m_User; ///< Applications' user
+
+	mafWizardManager *m_WizardManager;
+	bool m_UseWizardManager;
+	bool m_WizardRunning;
 	bool m_RunningOperation;
-  wxGauge *m_WizardGauge;
-  wxStaticText* m_WizardLabel;
-  bool m_CancelledBeforeOpStarting;
-  wxMenu *m_WizardMenu;
+	wxGauge *m_WizardGauge;
+	wxStaticText* m_WizardLabel;
+	bool m_CancelledBeforeOpStarting;
+	wxMenu *m_WizardMenu;
 
 	long               m_ChildFrameStyle;
 	mafGUIMDIFrame       *m_Win;
