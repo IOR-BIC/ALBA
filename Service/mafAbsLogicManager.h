1e4e04e152bd0b407f0fcbfd9f66a503e3e1869e
Nik81 <nicola.vanella@gmail.com>
2017-01-16 16:43:05 +0100

    Created Service project

---
+++ Service/mafAbsLogicManager.h
@@ -0,0 +1,48 @@
+/*=========================================================================
+
+ Program: MAF2
+ Module:  mafAbsLogicManager.h
+ Authors: Nicola Vanella
+ 
+ Copyright (c) B3C
+ All rights reserved. See Copyright.txt or
+ http://www.scsitaly.com/Copyright.htm for details.
+
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#ifndef _mafAbsLogicManager_H
+#define _mafAbsLogicManager_H
+
+class mafVME;
+
+class mafAbsLogicManager
+{
+public:
+
+	// VME
+	virtual void VmeSelect(mafVME *vme) = 0;
+
+	virtual void VmeSelected(mafVME *vme) = 0;
+
+	virtual void VmeShow(mafVME *vme, bool visibility) = 0;
+
+	virtual void VmeAdd(mafVME *vme) = 0;
+
+	//virtual void VmeAdded(mafVME *vme) = 0;
+
+	virtual void VmeRemove(mafVME *vme) = 0;
+
+	//virtual void VmeRemoving(mafVME *vme) = 0;
+
+	virtual void VmeVisualModeChanged(mafVME * vme) = 0;
+
+	//Camera
+	virtual void CameraUpdate() = 0;
+
+	virtual void CameraReset() = 0;
+};
+#endif
---
+++ Testing/mafFakeLogicForTest.h
@@ -0,0 +1,73 @@
+/*=========================================================================
+
+ Program: MAF2
+ Module:  mafFakeLogicForTest.h
+ Authors: Nicola Vanella
+ 
+ Copyright (c) B3C
+ All rights reserved. See Copyright.txt or
+ http://www.scsitaly.com/Copyright.htm for details.
+
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#ifndef _mafFakeLogicForTest_H
+#define _mafFakeLogicForTest_H
+
+#include "mafAbsLogicManager.h"
+#include <vector>
+
+class mafFakeLogicForTest : public mafAbsLogicManager
+{
+
+public:
+
+	enum testFunctions
+	{
+		VME_SELECT,
+		VME_ADD,
+		VME_REMOVE,
+		VME_SHOW,
+		VME_VISUAL_MODE_CHANGED,
+	};
+
+	struct calls
+	{
+		testFunctions testFunction;
+		mafVME *vme;
+	};
+	
+	mafFakeLogicForTest();
+	~mafFakeLogicForTest();
+
+	// VME
+	virtual void VmeSelect(mafVME *vme);
+
+	virtual void VmeSelected(mafVME *vme);
+
+	virtual void VmeShow(mafVME *vme, bool visibility);
+
+	virtual void VmeAdd(mafVME *vme);
+
+	virtual void VmeRemove(mafVME *vme);
+
+	virtual void VmeVisualModeChanged(mafVME * vme);
+
+	// Camera
+	virtual void CameraUpdate();
+
+	virtual void CameraReset();
+
+	void ClearCalls();
+
+	calls GetCall(int callNum);
+	
+	void AddCall(testFunctions func, mafVME* vme);
+protected:
+	
+	std::vector<calls> m_Calls;
+};
+#endif
