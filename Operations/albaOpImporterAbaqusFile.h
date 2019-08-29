8dde83887b04df5b2c8eb9d1c610c06415aa2c06
Gianluigi Crimi <g.g.crimi@gmail.com>
2019-08-29 12:59:52 +0200

    Moved Abaqus importer and exporter from Bonemat to Alba.

---
+++ Operations/albaOpImporterAbaqusFile.h
@@ -0,0 +1,152 @@
+/*=========================================================================
+Program:   Bonemat
+Module:    albaOpImporterAbaqusFile.h
+Language:  C++
+Date:      $Date: 2010-11-23 16:50:26 $
+Version:   $Revision: 1.1.1.1.2.3 $
+Authors:   Nicola Vanella
+==========================================================================
+Copyright (c) BIC-IOR 2019 (https://github.com/IOR-BIC)
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the above copyright notice for more information.
+=========================================================================*/
+
+#ifndef __albaOpImporterAbaqusFile_H__
+#define __albaOpImporterAbaqusFile_H__
+
+//----------------------------------------------------------------------------
+// Include :
+//----------------------------------------------------------------------------
+#include "albaOp.h"
+#include <map>
+#include <set>
+#include "albaMatrix.h"
+
+//----------------------------------------------------------------------------
+// forward references :
+//----------------------------------------------------------------------------
+class albaVME;
+class albaVMEMesh;
+class albaEvent;
+class wxBusyInfo;
+class albaProgressBarHelper;
+
+struct AbaqusElset
+{
+  std::string name;
+  std::string matName;
+};
+
+struct AbaqusElement 
+{
+  int Id; 
+  int nodesNumber;
+  int type; 
+  int *nodes;
+};
+
+struct AbaqusTransform
+{
+  std::string partName;
+
+  albaMatrix matrix;
+};
+
+struct AbaqusPart
+{
+  std::string name;
+  std::vector<AbaqusElement> elementsVector;
+  std::vector<AbaqusElset> elsetsVector;
+	std::map<int,int> elementsToElset;
+};
+
+//----------------------------------------------------------------------------
+// albaOpImporterAbaqusFile :
+//----------------------------------------------------------------------------
+class ALBA_EXPORT albaOpImporterAbaqusFile : public albaOp
+{
+public:
+  albaOpImporterAbaqusFile(const wxString &label = "AbaqusImporter");
+  ~albaOpImporterAbaqusFile(); 
+
+  albaTypeMacro(albaOpImporterAbaqusFile, albaOp);
+
+  virtual void OnEvent(albaEventBase *alba_event);
+  
+  albaOp* Copy();
+
+  /** Return true for the acceptable vme type. */
+  bool Accept(albaVME *node);
+
+  /** Set the input Abaqus file */
+  void SetFileName(const char *abaqusFileNameFullPath) {m_AbaqusInputFileNameFullPath = abaqusFileNameFullPath;};
+
+  /** Get the input Abaqus file */
+  wxString GetFileName() {return m_AbaqusInputFileNameFullPath.c_str();};
+
+  /** Builds operation's interface. */
+  void OpRun();
+
+  /** Import the mesh*/
+  int Import();
+
+protected:
+
+  albaString GetWildcard();
+
+  int ParseAbaqusFile(albaString fileName);
+
+  int SetPart(std::string name="", int part=-1);
+
+  int ReadHeader(FILE *outFile);
+  int ReadNodes(FILE *outFile);
+  int ReadElements();
+  int ReadElset();
+  int ReadSolid();
+  int ReadMaterials(FILE *outFile);
+  int ReadInstance();
+  int WriteElements();
+
+  int GetLine(FILE *fp, char *buffer);
+  int ReplaceInString(char *str, char from, char to);
+  int ReadInit(albaString &fileName);
+  void ReadFinalize();
+
+  /**/
+ 	albaProgressBarHelper *m_ProgressHelper;
+
+  wxString m_AbaqusInputFileNameFullPath;
+
+  int m_ImporterType;
+  albaVMEMesh *m_ImportedVmeMesh;
+
+  albaString m_CacheDir;
+  albaString m_DataDir;
+
+  long m_OperationProgress;
+
+  FILE * m_FilePointer;
+  char *m_Buffer;
+  int m_BufferLeft;
+  int m_BufferPointer;
+  char m_Line[512];
+  long m_FileSize;
+  long m_BytesReaded;
+
+  std::map<std::string,int> m_MatIDMap;
+  int m_LastMatId;
+
+  std::vector<AbaqusPart> m_Parts;
+  std::vector<AbaqusElement>* m_Elements;
+  std::vector<AbaqusElset>* m_Elsets;
+  std::vector<AbaqusTransform> m_Transforms;
+
+	std::map<int,int>* m_ElementsToElset;
+
+  //
+  albaMatrix RotationMatrixUnnormalizedDirVector(double a, double b, double c, double d, double e, double f, double theta);
+};
+#endif
+
