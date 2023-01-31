4b5eeaec080288d6530c2fbf4e46fff8fdd2094e
gcrimi <g.g.crimi@gmail.com>
2023-01-31 09:55:52 +0100

    Fixed bug on setting plane

--- VME/albaVMESurfaceParametric.h
+++ VME/albaVMESurfaceParametric.h
@@ -174,20 +174,20 @@ public:
 	double *GetPlaneOrigin() { return m_PlaneOrigin; }
 
 	/** Sets PlaneOrigin */
-	void SetPlaneOrigin(double *origin) { m_PlaneOrigin[0] = origin[0]; m_PlaneOrigin[0] = origin[0]; m_PlaneOrigin[0] = origin[0];}
+	void SetPlaneOrigin(double *origin) { m_PlaneOrigin[0] = origin[0]; m_PlaneOrigin[1] = origin[1]; m_PlaneOrigin[2] = origin[2];}
 
 
 	/** Returns PlanePoint1 */
 	double *GetPlanePoint1() { return m_PlanePoint1; }
 
 	/** Sets PlanePoint1 */
-	void SetPlanePoint1(double *point1) { m_PlanePoint1[0] = point1[0]; m_PlanePoint1[0] = point1[0]; m_PlanePoint1[0] = point1[0]; }
+	void SetPlanePoint1(double *point1) { m_PlanePoint1[0] = point1[0]; m_PlanePoint1[1] = point1[1]; m_PlanePoint1[2] = point1[2]; }
 
 	/** Returns PlanePoint2 */
 	double *GetPlanePoint2() { return m_PlanePoint2; }
 
 	/** Sets PlanePoint2 */
-	void SetPlanePoint2(double *Point2) { m_PlanePoint2[0] = Point2[0]; m_PlanePoint2[0] = Point2[0]; m_PlanePoint2[0] = Point2[0]; }
+	void SetPlanePoint2(double *Point2) { m_PlanePoint2[0] = Point2[0]; m_PlanePoint2[1] = Point2[1]; m_PlanePoint2[2] = Point2[2]; }
 
 protected:
 
