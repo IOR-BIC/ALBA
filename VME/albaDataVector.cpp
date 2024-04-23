f7927f770ad93a5e74b2dd9e893f45a513e4149b
Gianluigi Crimi <g.g.crimi@gmail.com>
2024-04-23 13:07:19 +0200

    TRUE between "" restored

--- VME/albaDataVector.cpp
+++ VME/albaDataVector.cpp
@@ -464,7 +464,7 @@ int albaDataVector::InternalRestore(albaStorageElement *node)
     )
   {
     SetItemTypeName(item_type);
-    SetSingleFileMode(single_file == "true" || single_file == "True" || single_file == "true");
+    SetSingleFileMode(single_file == "true" || single_file == "True" || single_file == "TRUE");
     if (m_SingleFileMode)
     {
       node->GetAttribute("ArchiveFileName", m_ArchiveName);
